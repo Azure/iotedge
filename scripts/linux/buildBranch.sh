c854c4d7829f10c496b627e880b1a9798b7d9813
Mahesh Rohera <marohera@microsoft.com>
2017-04-19 00:07:22 +0000

    Merged PR 204184: First cut docker image support for ARM and x64 images along with required build
    First cut docker image support for ARM and x64 images along with required build script support

--- scripts/linux/buildBranch.sh
+++ scripts/linux/buildBranch.sh
@@ -5,44 +5,50 @@
 # This script expects that .Net Core is installed at $AGENT_WORKFOLDER\dotnet
 # by a previous step.
 
-checkEnvVar() {
-	varname=$1
-	if [ -z "${!varname}" ]; then
-		echo Error: Environment variable $varname is not set 1>&2
-		exit 1
-	fi
-}
-
 # Check if Environment variables are set.
-checkEnvVar BUILD_REPOSITORY_LOCALPATH
-checkEnvVar AGENT_WORKFOLDER
-checkEnvVar BUILD_BINARIESDIRECTORY
+TMP=${BUILD_REPOSITORY_LOCALPATH:?Env variable BUILD_REPOSITORY_LOCALPATH needs to be set and be non-empty}
+TMP=${AGENT_WORKFOLDER:?Env variable AGENT_WORKFOLDER needs to be set and be non-empty}
+TMP=${BUILD_BINARIESDIRECTORY:?Env variable BUILD_BINARIESDIRECTORY needs to be set and be non-empty}
 
-SUFFIX='Microsoft.Azure.*.sln'
+CSPROJ_SUFFIX='Microsoft.Azure.*.csproj'
+SLN_SUFFIX='Microsoft.Azure.*.sln'
 ROOTFOLDER=$BUILD_REPOSITORY_LOCALPATH
 DOTNET_ROOT_PATH=$AGENT_WORKFOLDER/dotnet
-OUTPUT_FOLDER=$BUILD_BINARIESDIRECTORY/target
+PUBLISH_FOLDER=$BUILD_BINARIESDIRECTORY/publish
+SRC_DOCKER_DIR=$ROOTFOLDER/docker
 
 if [ ! -d "${ROOTFOLDER}" ]; then
-	echo Folder $ROOTFOLDER does not exist 1>&2
-	exit 1
+    echo Folder $ROOTFOLDER does not exist 1>&2
+    exit 1
 fi
 
 if [ ! -f "${DOTNET_ROOT_PATH}/dotnet" ]; then
-	echo Path $DOTNET_ROOT_PATH/dotnet does not exist 1>&2
-	exit 1
+    echo Path $DOTNET_ROOT_PATH/dotnet does not exist 1>&2
+    exit 1
 fi
 
 if [ ! -d "${BUILD_BINARIESDIRECTORY}" ]; then
-	mkdir $BUILD_BINARIESDIRECTORY
+    mkdir $BUILD_BINARIESDIRECTORY
 fi
 
 echo Building all solutions in repo
-find $ROOTFOLDER -type f -name $SUFFIX | while read line; do
-    echo Building Solution - $line
-    $DOTNET_ROOT_PATH/dotnet clean $line
-    $DOTNET_ROOT_PATH/dotnet restore $line
-    $DOTNET_ROOT_PATH/dotnet build $line -o $OUTPUT_FOLDER
+find $ROOTFOLDER -type f -name $SLN_SUFFIX | while read soln; do
+    echo Building Solution - $soln
+    $DOTNET_ROOT_PATH/dotnet clean --output $BUILD_BINARIESDIRECTORY $soln
+    $DOTNET_ROOT_PATH/dotnet restore $soln
+    $DOTNET_ROOT_PATH/dotnet build --output $BUILD_BINARIESDIRECTORY $soln
 done
 
-exit 0
+echo Publishing all required solutions in repo
+rm -fr $PUBLISH_FOLDER
+
+find $ROOTFOLDER -type f -name $CSPROJ_SUFFIX -exec grep -l "<OutputType>Exe</OutputType>" {} + | while read proj; do
+    echo Publishing Solution - $proj
+    filename=$(basename "$proj")
+    proj_publish_dir="${filename%.*}"
+    $DOTNET_ROOT_PATH/dotnet publish -f netcoreapp2.0 -o $PUBLISH_FOLDER/$proj_publish_dir $proj
+done
+
+echo Copying $SRC_DOCKER_DIR to $PUBLISH_FOLDER/docker
+rm -fr $PUBLISH_FOLDER/docker
+cp -r $SRC_DOCKER_DIR $PUBLISH_FOLDER
--- scripts/linux/buildDockerImage.sh
+++ scripts/linux/buildDockerImage.sh
@@ -1,27 +1,53 @@
 #!/bin/bash
 
-checkEnvVar() {
-	varname=$1
-	if [ -z "${!varname}" ]; then
-		echo Error: Environment variable $varname is not set 1>&2
-		exit 1
-	fi
+###############################################################################
+# This Script builds all Edge application in their respective docker containers
+# This script expects that buildBranch.sh was invoked earlier and all the
+# necessary application files and their Dockerfile be published in
+# directory identified by environement variable BUILD_BINARIESDIRECTORY
+###############################################################################
+
+###############################################################################
+# Print usage information pertaining to this script and exit
+###############################################################################
+usage()
+{
+    echo "Missing arguments. Usage: $0 -r <registry> -u <username> -p <password> [-v <docker image version=build number]"
+    exit 1;
 }
 
-# Check if Environment variables are set.
-checkEnvVar BUILD_BINARIESDIRECTORY
+###############################################################################
+# Validate Environment Variables
+###############################################################################
+TMP=${BUILD_BINARIESDIRECTORY:?Env variable BUILD_BINARIESDIRECTORY needs to be set and be non-empty}
 
-OUTPUT_FOLDER=$BUILD_BINARIESDIRECTORY
-DOCKER_IMAGENAME=azedge
-BASEDIR=$(dirname $0)
+if [ ! -d "$BUILD_BINARIESDIRECTORY" ]; then
+    echo "Path $BUILD_BINARIESDIRECTORY does not exist"
+    exit 1
+fi
 
-usage() 
-{ 
-	echo "Missing arguments. Usage: $0 -r <registry> -u <username> -p <password> [-i <docker image name=azedge>] [-v <docker image version=build number]" 1>&2; 
-	exit 1; 
-}
+###############################################################################
+# Check if the underlying architecture is supported
+###############################################################################
+ARCH=$(uname -m)
+if [ "$ARCH" == "x86_64" ]; then
+    ARCH="x64"
+elif [ "$ARCH" == "armv7l" ]; then
+    ARCH="armv7hf"
+else
+    echo "Unsupported Architecture"
+    exit 1
+fi
 
-while getopts ":r:u:p:i:v:" o; do
+###############################################################################
+# Setup Global Variables
+###############################################################################
+PUBLISH_DIR=$BUILD_BINARIESDIRECTORY/publish
+
+###############################################################################
+# Obtain and validate the options supported by this script
+###############################################################################
+while getopts ":r:u:p:v:" o; do
     case "${o}" in
         r)
             DOCKER_REGISTRY=${OPTARG}
@@ -32,13 +58,10 @@ while getopts ":r:u:p:i:v:" o; do
         p)
             DOCKER_PASSWORD=${OPTARG}
             ;;
-        i)
-            DOCKER_IMAGENAME=${OPTARG}
-            ;;        
-		v)
+        v)
             DOCKER_IMAGEVERSION=${OPTARG}
             ;;
-		*)
+        *)
             usage
             ;;
     esac
@@ -49,33 +72,95 @@ if [ -z "${DOCKER_REGISTRY}" ] || [ -z "${DOCKER_USERNAME}" ] || [ -z "${DOCKER_
     usage
 fi
 
-if [ -z "${DOCKER_IMAGEVERSION}" ]; then 
-	if [ ! -z "${BUILD_BUILDNUMBER}" ]; then
-		DOCKER_IMAGEVERSION=$BUILD_BUILDNUMBER
-	else
-		echo Error: Docker image version not found. Either set BUILD_BUILDNUMBER environment variable, or pass in -v parameter.
-		exit 1
-	fi
+if [ -z "${DOCKER_IMAGEVERSION}" ]; then
+    if [ ! -z "${BUILD_BUILDNUMBER}" ]; then
+        DOCKER_IMAGEVERSION=$BUILD_BUILDNUMBER
+    else
+        echo "Error: Docker image version not found. Either set BUILD_BUILDNUMBER environment variable, or pass in -v parameter."
+        exit 1
+    fi
 fi
 
-if [ ! -d "$BUILD_BINARIESDIRECTORY" ]; then
-	echo Path $BUILD_BINARIESDIRECTORY does not exist 1>&2
-	exit 1
-fi
-
+###############################################################################
 # Build docker image and push it to private repo
-cp $BASEDIR/Dockerfile $OUTPUT_FOLDER
-cd $OUTPUT_FOLDER
+#
+#   @param[1] - imagename; Name of the docker edge image to publish; Required;
+#   @param[2] - arch; Arch of base image; Required;
+#   @param[3] - dockerfile; Path to the dockerfile; Optional;
+#               Leave as "" and defaults will be chosen.
+#   @param[4] - context_path; docker context path; Required;
+#   @param[5] - build_args; docker context path; Optional;
+#               Leave as "" and no build args will be supplied.
+###############################################################################
+docker_build_and_tag_and_push()
+{
+    imagename="$1"
+    arch="$2"
+    dockerfile="$3"
+    context_path="$4"
+    build_args="$5"
 
-echo Building Docker image
-docker build -t $DOCKER_IMAGENAME:$DOCKER_IMAGEVERSION .
+    if [ -z "${imagename}" ] || [ -z "${arch}" ] || [ -z "${context_path}" ]; then
+        echo "Error: Arguments are invalid [$imagename] [$arch] [$context_path]"
+        exit 1
+    fi
 
-echo Logging in to Docker registry
+    echo "Building and Pushing Docker image $imagename for $arch"
+    docker_build_cmd="docker build"
+    docker_build_cmd+=" -t $DOCKER_REGISTRY/azedge-$imagename-$arch:$DOCKER_IMAGEVERSION"
+    docker_build_cmd+=" -t $DOCKER_REGISTRY/azedge-$imagename-$arch:latest"
+    if [ ! -z "${dockerfile}" ]; then
+        docker_build_cmd+=" --file $dockerfile"
+    fi
+    docker_build_cmd+=" $context_path $build_args"
+
+    echo "Running... $docker_build_cmd"
+
+    $docker_build_cmd
+    cmd_output=$?
+
+    if [ ${cmd_output} -ne 0 ]; then
+        echo "Docker Build Failed With Exit Code $cmd_output"
+        exit 1
+    else
+        docker push $DOCKER_REGISTRY/azedge-$imagename-$arch:$DOCKER_IMAGEVERSION
+        cmd_output=$?
+        if [ ${cmd_output} -ne 0 ]; then
+            echo "Docker Build Failed With Exit Code $cmd_output"
+            exit 1
+        else
+            docker push $DOCKER_REGISTRY/azedge-$imagename-$arch:latest
+            cmd_output=$?
+            if [ ${cmd_output} -ne 0 ]; then
+                echo "Docker Push Latest Image Failed: $cmd_output"
+                exit 1
+            fi
+        fi
+    fi
+
+    return $cmd_output
+}
+
+#echo Logging in to Docker registry
 docker login $DOCKER_REGISTRY -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+if [ $? -ne 0 ]; then
+    echo "Docker Login Failed!"
+    exit 1
+fi
 
-echo Pushing Docker image to registry
-docker tag $DOCKER_IMAGENAME:$DOCKER_IMAGEVERSION $DOCKER_REGISTRY/$DOCKER_IMAGENAME:$DOCKER_IMAGEVERSION
-docker push $DOCKER_REGISTRY/$DOCKER_IMAGENAME:$DOCKER_IMAGEVERSION
+# push edge-runtime dotnet image
+EXE_DIR="dotnet-runtime"
+EXE_DOCKER_DIR=$PUBLISH_DIR/docker/$EXE_DIR/latest
+docker_build_and_tag_and_push $EXE_DIR "$ARCH" "$EXE_DOCKER_DIR/$ARCH/Dockerfile" "$EXE_DOCKER_DIR" ""
 
-exit 0
+# push edge-agent image
+EXE_DIR="Microsoft.Azure.Devices.Edge.Agent.Service"
+EXE_DOCKER_DIR=$PUBLISH_DIR/$EXE_DIR/docker
+docker_build_and_tag_and_push edge-agent "$ARCH" "$EXE_DOCKER_DIR/$ARCH/Dockerfile" "$PUBLISH_DIR/$EXE_DIR" "--build-arg EXE_DIR=."
 
+# push edge-hub image
+EXE_DIR="Microsoft.Azure.Devices.Edge.Hub.Service"
+EXE_DOCKER_DIR=$PUBLISH_DIR/$EXE_DIR/docker
+docker_build_and_tag_and_push edge-hub "$ARCH" "$EXE_DOCKER_DIR/$ARCH/Dockerfile" "$PUBLISH_DIR/$EXE_DIR" "--build-arg EXE_DIR=."
+
+echo "Done Building And Pushing Docker Images"
