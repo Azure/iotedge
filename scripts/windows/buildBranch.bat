b819baccaa960f40a332e0e98616f69f6947f622
Mike Yagley <miyagley@microsoft.com>
2017-05-26 23:39:46 +0000

    Merged PR 257392: Add default values for env vars in Windows build script
    * Allows windows build and run test scripts to be run from any directory
    * Adds sensible defaults for environment variables for running on a desktop

--- scripts/windows/buildBranch.bat
+++ scripts/windows/buildBranch.bat
@@ -1,4 +1,5 @@
 @echo off
+@setlocal EnableExtensions EnableDelayedExpansion
 
 REM This Script builds all .Net Core Solutions in the repo by recursing through
 REM the repo and finding any *.sln files
@@ -6,18 +7,15 @@ REM This script expects that .Net Core is installed at %AGENT_WORKFOLDER%\dotnet
 REM by a previous step.
 
 IF NOT DEFINED BUILD_REPOSITORY_LOCALPATH (
-	ECHO Error: BUILD_REPOSITORY_LOCALPATH needs to be set to the root of the repository.
-	EXIT /B 1
+    SET BUILD_REPOSITORY_LOCALPATH=%~dp0..\..
 )
 
 IF NOT DEFINED AGENT_WORKFOLDER (
-	ECHO Error: AGENT_WORKFOLDER needs to be set to the working folder where .Net Core is installed.
-	EXIT /B 1
+    SET AGENT_WORKFOLDER=%ProgramFiles%
 )
 
 IF NOT DEFINED BUILD_BINARIESDIRECTORY (
-	ECHO Error: BUILD_BINARIESDIRECTORY needs to be set to the folder where output binaries will be placed.
-	EXIT /B 1
+    SET BUILD_BINARIESDIRECTORY=%BUILD_REPOSITORY_LOCALPATH%\target
 )
 
 SET SUFFIX=Microsoft.Azure.*.sln
@@ -26,38 +24,44 @@ SET ROOTFOLDER=%BUILD_REPOSITORY_LOCALPATH%
 SET DOTNET_ROOT_PATH=%AGENT_WORKFOLDER%\dotnet
 SET OUTPUT_FOLDER=%BUILD_BINARIESDIRECTORY%
 
-IF NOT EXIST %ROOTFOLDER% (
-	ECHO Error: %ROOTFOLDER% not found
-	EXIT /B 1
+IF NOT EXIST "%ROOTFOLDER%" (
+    ECHO Error: %ROOTFOLDER% not found
+    EXIT /B 1
 )
 
-IF NOT EXIST %DOTNET_ROOT_PATH%\dotnet.exe (
-	ECHO Error: %DOTNET_ROOT_PATH%\dotnet.exe not found
-	EXIT /B 1
+IF NOT EXIST "%DOTNET_ROOT_PATH%\dotnet.exe" (
+    ECHO Error: %DOTNET_ROOT_PATH%\dotnet.exe not found
+    EXIT /B 1
 )
 
-IF NOT EXIST %OUTPUT_FOLDER% (
-	MKDIR %OUTPUT_FOLDER%
+IF NOT EXIST "%OUTPUT_FOLDER%" (
+    MKDIR "%OUTPUT_FOLDER%"
 )
 
 ECHO Cleaning and restoring all solutions in repo
 
 FOR /R %%f IN (%SUFFIX%) DO (
     ECHO Cleaning and Restoring packages for solution - %%f
-    %DOTNET_ROOT_PATH%\dotnet clean %%f
-    %DOTNET_ROOT_PATH%\dotnet restore %%f
+    "%DOTNET_ROOT_PATH%\dotnet" clean %%f
+    "%DOTNET_ROOT_PATH%\dotnet" restore %%f
 )
 
 ECHO Generate Antlr code files
 SET JAVACOMMAND=java
 where %JAVACOMMAND%
 if %ERRORLEVEL% NEQ 0 (
-	SET JAVACOMMAND=%UserProfile%/.nuget/packages/Antlr4.CodeGenerator/4.6.1-beta002/tools/ikvm.exe
+    SET JAVACOMMAND=%UserProfile%/.nuget/packages/Antlr4.CodeGenerator/4.6.1-beta002/tools/ikvm.exe
 )
 
-FOR /R %%f IN (%ANTLRSUFFIX%) DO (
-    ECHO Generating .cs files for - %%f
-    %JAVACOMMAND% -jar %UserProfile%/.nuget/packages/Antlr4.CodeGenerator/4.6.1-beta002/tools/antlr4-csharp-4.6.1-SNAPSHOT-complete.jar %%f -package Microsoft.Azure.Devices.Routing.Core -Dlanguage=CSharp_v4_5 -visitor -listener
+FOR /R %%A IN (%ANTLRSUFFIX%) DO (
+    SET Folder=%%~dpAgenerated
+    ECHO Generating .cs files for - %%A
+
+    IF NOT EXIST "!Folder!" (
+        MKDIR "!Folder!"
+    )
+
+    "%JAVACOMMAND%" -jar "%UserProfile%/.nuget/packages/Antlr4.CodeGenerator/4.6.1-beta002/tools/antlr4-csharp-4.6.1-SNAPSHOT-complete.jar" %%A -package Microsoft.Azure.Devices.Routing.Core -Dlanguage=CSharp_v4_5 -visitor -listener -o "!Folder!"
 )
 
 ECHO Building all solutions in repo
@@ -66,10 +70,10 @@ SET RES=0
 IF EXIST %OUTPUT_FOLDER% RD /q /s %OUTPUT_FOLDER%
 FOR /R %%f IN (%SUFFIX%) DO (
     ECHO Building Solution - %%f
-    %DOTNET_ROOT_PATH%\dotnet build %%f -o %OUTPUT_FOLDER%
-	IF %ERRORLEVEL% NEQ 0 (
-		SET RES=1
-	)
+    "%DOTNET_ROOT_PATH%\dotnet" build %%f -o "%OUTPUT_FOLDER%"
+    IF %ERRORLEVEL% NEQ 0 (
+        SET RES=1
+    )
 )
 
 EXIT /B %RES%
--- scripts/windows/runTests.bat
+++ scripts/windows/runTests.bat
@@ -1,23 +1,21 @@
 @echo off
+@setlocal EnableExtensions EnableDelayedExpansion
 
 REM This script runs all the .Net Core test projects (*test*.csproj) in the
-REM repo by recursing from the repo root. 
-REM This script expects that .Net Core is installed at 
+REM repo by recursing from the repo root.
+REM This script expects that .Net Core is installed at
 REM %AGENT_WORKFOLDER%\dotnet and output binaries are at %BUILD_BINARIESDIRECTORY%
 
 IF NOT DEFINED BUILD_REPOSITORY_LOCALPATH (
-	ECHO Error: BUILD_REPOSITORY_LOCALPATH needs to be set to the root of the repository.
-	EXIT /B 1
+    SET BUILD_REPOSITORY_LOCALPATH=%~dp0..\..
 )
 
 IF NOT DEFINED AGENT_WORKFOLDER (
-	ECHO Error: AGENT_WORKFOLDER needs to be set to the working folder where .Net Core is installed.
-	EXIT /B 1
+    SET AGENT_WORKFOLDER=%ProgramFiles%
 )
 
 IF NOT DEFINED BUILD_BINARIESDIRECTORY (
-	ECHO Error: BUILD_BINARIESDIRECTORY needs to be set to the folder where output binaries will be placed.
-	EXIT /B 1
+    SET BUILD_BINARIESDIRECTORY=%BUILD_REPOSITORY_LOCALPATH%\target
 )
 
 SET TEST_PROJ_PATTERN=Microsoft.Azure*test*.csproj
@@ -25,18 +23,18 @@ SET ROOTFOLDER=%BUILD_REPOSITORY_LOCALPATH%
 SET DOTNET_ROOT_PATH=%AGENT_WORKFOLDER%\dotnet
 SET OUTPUT_FOLDER=%BUILD_BINARIESDIRECTORY%
 
-IF NOT EXIST %ROOTFOLDER% (
-	ECHO Error: %ROOTFOLDER% not found
-	EXIT /B 1
+IF NOT EXIST "%ROOTFOLDER%" (
+    ECHO Error: %ROOTFOLDER% not found
+    EXIT /B 1
 )
 
-IF NOT EXIST %DOTNET_ROOT_PATH%\dotnet.exe (
-	ECHO Error: %DOTNET_ROOT_PATH%\dotnet.exe not found
-	EXIT /B 1
+IF NOT EXIST "%DOTNET_ROOT_PATH%\dotnet.exe" (
+    ECHO Error: %DOTNET_ROOT_PATH%\dotnet.exe not found
+    EXIT /B 1
 )
 
-IF NOT EXIST %OUTPUT_FOLDER% (
-	MKDIR %OUTPUT_FOLDER%
+IF NOT EXIST "%OUTPUT_FOLDER%" (
+    MKDIR "%OUTPUT_FOLDER%"
 )
 
 SET opencover=%ROOTFOLDER%\OpenCover.4.6.519\tools\OpenCover.Console.exe
@@ -45,10 +43,17 @@ SET targetargs=test %1 --logger trx;LogFileName=result.trx
 ECHO Running tests in all Test Projects in repo
 FOR /R %%f IN (%TEST_PROJ_PATTERN%) DO (
     ECHO Running tests for project - %%f
-  
-    %opencover% -register:user -target:%DOTNET_ROOT_PATH%/dotnet.exe -targetargs:"%targetargs% %%f" -skipautoprops -hideskipped:All  -oldstyle -output:%OUTPUT_FOLDER%\code-coverage.xml -mergeoutput:%OUTPUT_FOLDER%\code-coverage.xml
+    IF EXIST "%opencover%" (
+        "%opencover%" -register:user -target:%DOTNET_ROOT_PATH%/dotnet.exe -targetargs:"%targetargs% %%f" -skipautoprops -hideskipped:All  -oldstyle -output:%OUTPUT_FOLDER%\code-coverage.xml -mergeoutput:%OUTPUT_FOLDER%\code-coverage.xml
+    ) ELSE (
+        "%DOTNET_ROOT_PATH%\dotnet" test %%f --logger "trx;LogFileName=result.trx" -o "%OUTPUT_FOLDER%" --no-build --filter Category!=Bvt
+    )
 )
 
-%ROOTFOLDER%\OpenCoverToCoberturaConverter.0.2.6.0\tools\OpenCoverToCoberturaConverter.exe -input:%OUTPUT_FOLDER%\code-coverage.xml -output:%OUTPUT_FOLDER%\CoberturaCoverage.xml -sources:.
+IF EXIST "%ROOTFOLDER%\OpenCoverToCoberturaConverter.0.2.6.0\tools\OpenCoverToCoberturaConverter.exe" (
+    "%ROOTFOLDER%\OpenCoverToCoberturaConverter.0.2.6.0\tools\OpenCoverToCoberturaConverter.exe" -input:%OUTPUT_FOLDER%\code-coverage.xml -output:%OUTPUT_FOLDER%\CoberturaCoverage.xml -sources:.
+)
 
-%ROOTFOLDER%\ReportGenerator.2.5.6\tools\ReportGenerator.exe -reporttypes:MHtml -reports:%OUTPUT_FOLDER%\code-coverage.xml -targetdir:%OUTPUT_FOLDER%\Report
+IF EXIST "%ROOTFOLDER%\ReportGenerator.2.5.6\tools\ReportGenerator.exe" (
+    "%ROOTFOLDER%\ReportGenerator.2.5.6\tools\ReportGenerator.exe" -reporttypes:MHtml -reports:%OUTPUT_FOLDER%\code-coverage.xml -targetdir:%OUTPUT_FOLDER%\Report
+)
