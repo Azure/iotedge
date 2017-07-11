3a46ee11986a7b5823f5a6035a8102cc8ddc3cf7
Janice D'Sa <jadsa@microsoft.com>
2017-07-11 16:39:27 +0000

    Merged PR 319990: Tail logs from stopped/paused container
    When the MMA is reconciling modules, it removes (docker rm) containers that are stopped or paused and updates (docker create) them before attempting to restart them. If they were meant to be removed (as indicated by the desired properties), they would simply be removed (docker rm) and not restarted. In order to understand why a container stopped, the changes in this PR will print the last 25 logs from the container, before the container is removed. This will help us debug failed containers better.

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Docker/commands/RemoveCommand.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Docker/commands/RemoveCommand.cs
@@ -1,32 +1,68 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 namespace Microsoft.Azure.Devices.Edge.Agent.Docker.Commands
 {
-    using System.Threading;
-    using System.Threading.Tasks;
-    using global::Docker.DotNet;
-    using global::Docker.DotNet.Models;
-    using Microsoft.Azure.Devices.Edge.Agent.Core;
-    using Microsoft.Azure.Devices.Edge.Util;
+	using System.IO;
+	using System.Threading;
+	using System.Threading.Tasks;
+	using global::Docker.DotNet;
+	using global::Docker.DotNet.Models;
+	using Microsoft.Azure.Devices.Edge.Agent.Core;
+	using Microsoft.Azure.Devices.Edge.Util;
+	using Microsoft.Extensions.Logging;
 
-    public class RemoveCommand : ICommand
-    {
-        readonly IDockerClient client;
-        readonly DockerModule module;
+	public class RemoveCommand : ICommand
+	{
+		static readonly ILogger Logger = Util.Logger.Factory.CreateLogger<RemoveCommand>();
+		readonly IDockerClient client;
+		readonly DockerModule module;
+		const string LogLinesToPull = "25";
+		const int WaitForLogs = 30000; //ms
 
-        public RemoveCommand(IDockerClient client, DockerModule module)
-        {
-            this.client = Preconditions.CheckNotNull(client, nameof(client));
-            this.module = Preconditions.CheckNotNull(module, nameof(module));
-        }
+		public RemoveCommand(IDockerClient client, DockerModule module)
+		{
+			this.client = Preconditions.CheckNotNull(client, nameof(client));
+			this.module = Preconditions.CheckNotNull(module, nameof(module));
+		}
 
-        public Task ExecuteAsync(CancellationToken token)
-        {
-            var parameters = new ContainerRemoveParameters();
-            return this.client.Containers.RemoveContainerAsync(this.module.Name, parameters);
-        }
+		async Task TailLogsAsync(DockerModule module)
+		{
+			using (var cts = new CancellationTokenSource())
+			{
+				var parameters = new ContainerLogsParameters
+				{
+					ShowStdout = true,
+					Tail = RemoveCommand.LogLinesToPull
+				};
+				cts.CancelAfter(RemoveCommand.WaitForLogs);
+				using (var stream = await this.client.Containers.GetContainerLogsAsync(module.Name, parameters, cts.Token))
+				{
+					bool firstLine = true;
+					using (var reader = new StreamReader(stream))
+					{
+						while (stream.CanRead && !reader.EndOfStream)
+						{
+							if (firstLine)
+							{
+								Logger.LogInformation($"Last {RemoveCommand.LogLinesToPull} log lines from container {module.Name}:");
+								firstLine = false;
+							}
+							var line = await reader.ReadLineAsync();
+							Logger.LogInformation(line);
+						}
+					}
+				}
+			}
+		}
 
-        public Task UndoAsync(CancellationToken token) => TaskEx.Done;
+		public async Task ExecuteAsync(CancellationToken token)
+		{
+			var parameters = new ContainerRemoveParameters();
+			await this.TailLogsAsync(this.module);
+			await this.client.Containers.RemoveContainerAsync(this.module.Name, parameters);
+		}
 
-        public string Show() => $"docker rm {this.module.Name}";
-    }
+		public Task UndoAsync(CancellationToken token) => TaskEx.Done;
+
+		public string Show() => $"docker rm {this.module.Name}";
+	}
 }
