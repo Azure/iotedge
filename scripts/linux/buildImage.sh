6c0bff5e7c2b17df3774b2bad218b9078054c46c
Mike Yagley <miyagley@microsoft.com>
2017-10-26 15:59:03 +0000

    Merged PR 485678: Add script to build images one at a time
    This script is based off of the `buildDockerImage.sh` script to allow images for single projects to be built. We need to be able to disable the building of the functions binding image for ARM for the time-being. Because all of the images to be built are hard-coded in the `buildDockerImage.sh`, turning this off requires a code change. This new script will allow us to pick which images to build in the build job without making a code change.
    
    Once this is merged, we can update the build jobs, and potentially remove the `buildDockerImage.sh` script.
    
    Script help (adds -n and -P options):
    ```
    buildImage.sh [options]
    Note: Depending on the options you might have to run this as root or sudo.
    
    options
     -n, --image-name     Image name (e.g. edge-agent)
     -P, --project        Project to build image for (e.g. Microsoft.Azure.Devices.Edge.Agent.Service)
     -r, --registry       Docker registry required to build, tag and run the module
     -u, --username       Docker Registry Username
     -p, --password       Docker Username's password
     -v, --image-version  Docker Image Version. Either use this option or set env variable BUILD_BUILDNUMBER
     -t, --target-arch    Target architecture (default: uname -m)
    --bin-dir             Directory containing the output binaries. Either use this option or set env variable BUILD_BINARIESDIRECTORY
    ```

---
+++ scripts/linux/buildImage.sh
@@ -0,0 +1,249 @@
+#!/bin/bash
+
+###############################################################################
+# This Script builds a specific Edge application in their respective docker
+# containers. This script expects that buildBranch.sh was invoked earlier and
+# all the necessary application files and their Dockerfile be published in
+# directory identified by environment variable BUILD_BINARIESDIRECTORY
+###############################################################################
+
+set -e
+
+###############################################################################
+# Define Environment Variables
+###############################################################################
+ARCH=$(uname -m)
+SCRIPT_NAME=$(basename $0)
+PUBLISH_DIR=
+DOTNET_DOWNLOAD_URL=
+PROJECT=
+DOCKERFILE=
+DOCKER_IMAGENAME=
+BUILD_BINARIESDIRECTORY=${BUILD_BINARIESDIRECTORY:=""}
+
+###############################################################################
+# Function to obtain the underlying architecture and check if supported
+###############################################################################
+check_arch()
+{
+    if [ "$ARCH" == "x86_64" ]; then
+        ARCH="amd64"
+    elif [ "$ARCH" == "armv7l" ]; then
+        ARCH="arm32v7"
+    else
+        echo "Unsupported architecture"
+        exit 1
+    fi
+}
+
+###############################################################################
+# Print usage information pertaining to this script and exit
+###############################################################################
+usage()
+{
+    echo "$SCRIPT_NAME [options]"
+    echo "Note: Depending on the options you might have to run this as root or sudo."
+    echo ""
+    echo "options"
+    echo " -n, --image-name     Image name (e.g. edge-agent)"
+    echo " -P, --project        Project to build image for (e.g. Microsoft.Azure.Devices.Edge.Agent.Service)"
+    echo " -r, --registry       Docker registry required to build, tag and run the module"
+    echo " -u, --username       Docker Registry Username"
+    echo " -p, --password       Docker Username's password"
+    echo " -v, --image-version  Docker Image Version. Either use this option or set env variable BUILD_BUILDNUMBER"
+    echo " -t, --target-arch    Target architecture (default: uname -m)"
+    echo "--bin-dir             Directory containing the output binaries. Either use this option or set env variable BUILD_BINARIESDIRECTORY"
+    exit 1;
+}
+
+print_help_and_exit()
+{
+    echo "Run $SCRIPT_NAME --help for more information."
+    exit 1
+}
+
+###############################################################################
+# Obtain and validate the options supported by this script
+###############################################################################
+process_args()
+{
+    save_next_arg=0
+    for arg in $@
+    do
+        if [ $save_next_arg -eq 1 ]; then
+            DOCKER_REGISTRY="$arg"
+            save_next_arg=0
+        elif [ $save_next_arg -eq 2 ]; then
+            DOCKER_USERNAME="$arg"
+            save_next_arg=0
+        elif [ $save_next_arg -eq 3 ]; then
+            DOCKER_PASSWORD="$arg"
+            save_next_arg=0
+        elif [ $save_next_arg -eq 4 ]; then
+            DOCKER_IMAGEVERSION="$arg"
+            save_next_arg=0
+        elif [ $save_next_arg -eq 5 ]; then
+            BUILD_BINARIESDIRECTORY="$arg"
+            save_next_arg=0
+        elif [ $save_next_arg -eq 6 ]; then
+            DOTNET_DOWNLOAD_URL="$arg"
+            save_next_arg=0
+        elif [ $save_next_arg -eq 7 ]; then
+            ARCH="$arg"
+            check_arch
+            save_next_arg=0
+        elif [ $save_next_arg -eq 8 ]; then
+            PROJECT="$arg"
+            save_next_arg=0
+        elif [ $save_next_arg -eq 9 ]; then
+            DOCKER_IMAGENAME="$arg"
+            save_next_arg=0
+        else
+            case "$arg" in
+                "-h" | "--help" ) usage;;
+                "-r" | "--registry" ) save_next_arg=1;;
+                "-u" | "--username" ) save_next_arg=2;;
+                "-p" | "--password" ) save_next_arg=3;;
+                "-v" | "--image-version" ) save_next_arg=4;;
+                "--bin-dir" ) save_next_arg=5;;
+                "--dotnet-url" ) save_next_arg=6;;
+                "-t" | "--target-arch" ) save_next_arg=7;;
+                "-P" | "--project" ) save_next_arg=8;;
+                "-n" | "--image-name" ) save_next_arg=9;;
+                * ) usage;;
+            esac
+        fi
+    done
+
+    if [[ -z ${DOCKER_REGISTRY} ]]; then
+        echo "Registry parameter invalid"
+        print_help_and_exit
+    fi
+
+    if [[ -z ${DOCKER_USERNAME} ]]; then
+        echo "Docker username parameter invalid"
+        print_help_and_exit
+    fi
+
+    if [[ -z ${DOCKER_PASSWORD} ]]; then
+        echo "Docker password parameter invalid"
+        print_help_and_exit
+    fi
+
+    if [[ -z ${DOCKER_IMAGENAME} ]]; then
+        echo "Docker image name parameter invalid"
+        print_help_and_exit
+    fi
+
+    if [[ -z ${DOCKER_IMAGEVERSION} ]]; then
+        if [ ! -z "${BUILD_BUILDNUMBER}" ]; then
+            DOCKER_IMAGEVERSION=$BUILD_BUILDNUMBER
+        else
+            echo "Docker image version not found."
+            print_help_and_exit
+        fi
+    fi
+
+    if [[ -z ${BUILD_BINARIESDIRECTORY} ]] || [[ ! -d ${BUILD_BINARIESDIRECTORY} ]]; then
+        echo "Bin directory does not exist or is invalid"
+        print_help_and_exit
+    fi
+
+    PUBLISH_DIR=$BUILD_BINARIESDIRECTORY/publish
+
+    if [[ ! -d $PUBLISH_DIR ]]; then
+        echo "Publish directory does not exist or is invalid"
+        print_help_and_exit
+    fi
+
+
+    EXE_DOCKER_DIR=$PUBLISH_DIR/$PROJECT/docker
+    if [[ -z ${EXE_DOCKER_DIR} ]] || [[ ! -d ${EXE_DOCKER_DIR} ]]; then
+        echo "No docker directory for $PROJECT at $EXE_DOCKER_DIR"
+        print_help_and_exit
+    fi
+
+    DOCKERFILE="$EXE_DOCKER_DIR/linux/$ARCH/Dockerfile"
+    if [[ ! -f $DOCKERFILE ]]; then
+        echo "No Dockerfile at $DOCKERFILE"
+        print_help_and_exit
+    fi
+}
+
+###############################################################################
+# Build docker image and push it to private repo
+#
+#   @param[1] - imagename; Name of the docker edge image to publish; Required;
+#   @param[2] - arch; Arch of base image; Required;
+#   @param[3] - dockerfile; Path to the dockerfile; Optional;
+#               Leave as "" and defaults will be chosen.
+#   @param[4] - context_path; docker context path; Required;
+#   @param[5] - build_args; docker context path; Optional;
+#               Leave as "" and no build args will be supplied.
+###############################################################################
+docker_build_and_tag_and_push()
+{
+    imagename="$1"
+    arch="$2"
+    dockerfile="$3"
+    context_path="$4"
+    build_args="$5"
+
+    if [ -z "${imagename}" ] || [ -z "${arch}" ] || [ -z "${context_path}" ]; then
+        echo "Error: Arguments are invalid [$imagename] [$arch] [$context_path]"
+        exit 1
+    fi
+
+    echo "Building and pushing Docker image $imagename for $arch"
+    docker_build_cmd="docker build --no-cache"
+    docker_build_cmd+=" -t $DOCKER_REGISTRY/azureiotedge/$imagename-linux-$arch:$DOCKER_IMAGEVERSION"
+    docker_build_cmd+=" -t $DOCKER_REGISTRY/azureiotedge/$imagename-linux-$arch:latest"
+    if [ ! -z "${dockerfile}" ]; then
+        docker_build_cmd+=" --file $dockerfile"
+    fi
+    docker_build_cmd+=" $context_path $build_args"
+
+    echo "Running... $docker_build_cmd"
+
+    $docker_build_cmd
+
+    if [ $? -ne 0 ]; then
+        echo "Docker build failed with exit code $?"
+        exit 1
+    else
+        docker push $DOCKER_REGISTRY/azureiotedge/$imagename-linux-$arch:$DOCKER_IMAGEVERSION
+        if [ $? -ne 0 ]; then
+            echo "Docker push failed with exit code $?"
+            exit 1
+        else
+            docker push $DOCKER_REGISTRY/azureiotedge/$imagename-linux-$arch:latest
+            if [ $? -ne 0 ]; then
+                echo "Docker push latest image failed with exit code: $?"
+                exit 1
+            fi
+        fi
+    fi
+
+    return $?
+}
+
+###############################################################################
+# Main Script Execution
+###############################################################################
+check_arch
+process_args "$@"
+
+#echo Logging in to Docker registry
+docker login $DOCKER_REGISTRY -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+if [ $? -ne 0 ]; then
+    echo "Docker login failed!"
+    exit 1
+fi
+
+# push image
+docker_build_and_tag_and_push "$DOCKER_IMAGENAME" "$ARCH" "$DOCKERFILE" "$PUBLISH_DIR/$PROJECT" "--build-arg EXE_DIR=."
+[ $? -eq 0 ] || exit $?
+
+echo "Done building and pushing Docker image $DOCKER_IMAGENAME for $PROJECT"
+
+[ $? -eq 0 ] || exit $?
---
+++ scripts/windows/buildImage.ps1
@@ -0,0 +1,207 @@
+###############################################################################
+# This Script builds a specific Edge application in their respective docker
+# containers. This script expects that buildBranch.sh was invoked earlier and
+# all the necessary application files and their Dockerfile be published in
+# directory identified by environment variable BUILD_BINARIESDIRECTORY
+###############################################################################
+
+Param(
+    # Docker registry required to build, tag and run the module
+    [Parameter(Mandatory=$true)]
+    [String]$Registry,
+
+    # Docker Registry Username
+    [Parameter(Mandatory=$true)]
+    [String]$Username,
+
+    # Docker Username's password
+    [Parameter(Mandatory=$true)]
+    [String]$Password,
+
+    # Project Name
+    [Parameter(Mandatory=$true)]
+    [String]$Project,
+
+    # Docker Image Name
+    [Parameter(Mandatory=$true)]
+    [String]$ImageName,
+
+    # Docker Image Version
+    [ValidateNotNullOrEmpty()]
+    [String]$ImageVersion = $Env:BUILD_BUILDNUMBER,
+
+    # Target architecture
+    [ValidateNotNullOrEmpty()]
+    [String]$TargetArch = $Env:PROCESSOR_ARCHITECTURE,
+
+    # Directory containing the output binaries. Either use this option or set env variable BUILD_BINARIESDIRECTORY
+    [ValidateNotNullOrEmpty()]
+    [String]$BinDir = $Env:BUILD_BINARIESDIRECTORY,
+
+    # Use Windows vNext base images
+    [Switch]$vNext,
+
+    # Do not push images to the registry
+    [Switch]$SkipPush,
+
+    # Cleanup all images to not pollute the build machine
+    [Switch]$Cleanup
+)
+
+if (-not $ImageVersion)
+{
+    Throw "Docker image version '$ImageVersion' not found"
+}
+
+if (-not $BinDir -or -not (Test-Path $BinDir))
+{
+    Throw "Bin directory '$BinDir' does not exist or is invalid"
+}
+
+$PublishDir = Join-Path $BinDir "publish"
+
+if (-not (Test-Path $PublishDir))
+{
+    Throw "Publish directory '$PublishDir' does not exist or is invalid"
+}
+
+switch ($TargetArch)
+{
+    "AMD64" { $TargetArch = "amd64" }
+    default { throw "Unsupported arch '$TargetArch'" }
+}
+
+Function docker_login()
+{
+    #echo Logging in to Docker registry
+    $Password | docker login $Registry -u $Username --password-stdin
+    if ($LastExitCode)
+    {
+        Throw "Docker Login Failed With Exit Code $LastExitCode"
+    }
+}
+
+Function docker_build_and_tag_and_push(
+    # Name of the docker edge image to publish
+    [Parameter(Mandatory = $true)]
+    [String]$Name,
+
+    # Arch of base image
+    [Parameter(Mandatory = $true)]
+    [String]$Arch,
+
+    # Path to the dockerfile
+    [ValidateNotNullOrEmpty()]
+    [String]$Dockerfile,
+
+    # Docker context path
+    [Parameter(Mandatory = $true)]
+    [String]$ContextPath,
+
+    # Build args
+    [String]$BuildArgs,
+
+    [Switch]$Push,
+
+    [String]$Tag
+)
+{
+    $Suffix = ""
+    if ($vNext)
+    {
+        $Suffix = "-vnext"
+    }
+    $TagPrefix = "$Registry/azureiotedge/$Name-windows${Suffix}-${Arch}"
+    $FullVersionTag = "${TagPrefix}:$ImageVersion"
+    $LatestVersionTag = "${TagPrefix}:latest"
+
+    echo "Building and Pushing Docker image $Name for $Arch"
+    if ($Tag)
+    {
+        $docker_build_cmd = "docker build --no-cache -t $Tag"
+    }
+    else
+    {
+        $docker_build_cmd = "docker build --no-cache -t $FullVersionTag -t $LatestVersionTag"
+    }
+    if ($Dockerfile)
+    {
+        $docker_build_cmd += " --file $Dockerfile"
+    }
+    $docker_build_cmd += " $ContextPath $BuildArgs"
+
+    echo "Running... $docker_build_cmd"
+
+    Invoke-Expression $docker_build_cmd
+    if ($LastExitCode)
+    {
+        Throw "Docker Build Failed With Exit Code $LastExitCode"
+    }
+
+    if ($Push)
+    {
+        docker push $FullVersionTag
+        if ($LastExitCode)
+        {
+            Throw "Docker Push Failed With Exit Code $LastExitCode"
+        }
+
+        docker push $LatestVersionTag
+        if ($LastExitCode)
+        {
+            Throw "Docker Push Failed With Exit Code $LastExitCode"
+        }
+
+        docker rmi $FullVersionTag
+        docker rmi $LatestVersionTag
+    }
+}
+
+Function BuildTagPush([String]$ProjectName, [String]$ProjectPath)
+{
+    $FullProjectPath = Join-Path -Path $PublishDir -ChildPath $ProjectPath
+    $Suffix = ""
+    if ($vNext)
+    {
+        $Suffix = ".vnext"
+    }
+
+    docker_build_and_tag_and_push `
+        -Name $ProjectName `
+        -Arch $TargetArch `
+        -Dockerfile "$FullProjectPath\docker\windows\$TargetArch\Dockerfile$Suffix" `
+        -ContextPath $FullProjectPath `
+        -BuildArgs "--build-arg EXE_DIR=." `
+        -Push:(-not $SkipPush)
+}
+
+if (-not $SkipPush)
+{
+    docker_login
+}
+
+if ($vNext)
+{
+    $DockerfileDirectory = "$PublishDir\docker\dotnet-runtime\windows\$TargetArch"
+    docker_build_and_tag_and_push `
+        -Name "dotnet" `
+        -Arch $TargetArch `
+        -Dockerfile "$DockerfileDirectory\Dockerfile" `
+        -ContextPath $DockerfileDirectory `
+        -Tag "dotnet:2.0.0-runtime-nanoserver"
+}
+
+BuildTagPush $ImageName $Project
+
+echo "Done Building And Pushing Docker Images"
+
+if ($Cleanup)
+{
+    & cmd /c "docker rm $image_name 2>&1"
+    $containers = $(docker ps -a -q)
+    if ($containers)
+    {
+        docker rm -f $containers
+    }
+    docker rmi $(docker images -q)
+}
