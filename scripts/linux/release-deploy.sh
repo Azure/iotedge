32926115f926941017724f5275a2985d75270eb9
Anca Antochi <ancan@microsoft.com>
2017-11-02 19:13:31 +0000

    Merged PR 495371: Added script for deployment and removed deployment from bootstrap
    Added script for deployment and removed deployment from bootstrap

--- scripts/linux/release-deploy.sh
+++ scripts/linux/release-deploy.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
 ###############################################################################
-# This script creates a new deployment for the device provided as argument,  
-# runs boostrap setup python script and start Edge
+# This script creates a new deployment for the device provided as argument
 ###############################################################################
 
 ###############################################################################                                         
 # Print usage information pertaining to this script and exit                                                            
 ############################################################################### 
+set -e
+
 usage()
 {
     echo "$SCRIPT_NAME [options]"
     echo "Note: Depending on the options you might have to run this as root or sudo."
     echo ""
     echo "options"
+    echo " --deploy-tool                Location from where to download Deploy tool"
     echo " -v, --image-version           Docker image version. Either use this option or set env variable BUILD_BUILDNUMBER"
     echo " --iothub-hostname             IoTHub hostname"
-    echo " --device-id                   Edge device ID"
-    echo " --device-access-key           Shared device access key used to authenticate the device with IoTHub"
     echo " --iothubowner-access-key      Shared iothubowner access key used by deployment to authenticate with IoTHub"
     echo " --iothubowner-access-key-name Shared iothubowner access key name used by deployment to authenticate with IoTHub"
+    echo " --device-id                   Edge device ID"
     echo " --arch                        Target architecture (default: uname -m)"
-    echo " --edge-hostname               Edge Runtime DNS hostname (FQDN). Optional (default: OS reported hostname)"
-    echo " --docker-registries-csv       Azure Edge Modules Container" \
-         "Repositories. Optional."
-    echo "                                  CSV expressed as" \
-         "address,username,password."
-    echo " --edge-ctl                    Name of the .tar.gz file containing the iotedgectl files"
     exit 1;
 }
 
@@ -67,53 +62,45 @@ process_args()
     for arg in "$@"
     do
         if [ $save_next_arg -eq 1 ]; then
+            DEPLOY_TOOL="$arg"
+            save_next_arg=0   
+        elif [ $save_next_arg -eq 2 ]; then
             DOCKER_IMAGEVERSION="$arg"
             save_next_arg=0       
-        elif [ $save_next_arg -eq 2 ]; then
+        elif [ $save_next_arg -eq 3 ]; then
             IOTHUB_HOSTNAME="$arg"
             save_next_arg=0
-        elif [ $save_next_arg -eq 3 ]; then
-            DEVICEID="$arg"
-            save_next_arg=0
         elif [ $save_next_arg -eq 4 ]; then
-            DEVICE_SHARED_ACCESS_KEY="$arg"
-            save_next_arg=0
-        elif [ $save_next_arg -eq 5 ]; then
             IOTHUBOWNER_SHARED_ACCESS_KEY="$arg"
             save_next_arg=0
-        elif [ $save_next_arg -eq 6 ]; then
+        elif [ $save_next_arg -eq 5 ]; then
             IOTHUBOWNER_SHARED_ACCESS_KEY_NAME="$arg"
             save_next_arg=0
+        elif [ $save_next_arg -eq 6 ]; then
+            DEVICEID="$arg"
+            save_next_arg=0    
         elif [ $save_next_arg -eq 7 ]; then
             ARCH="$arg"
-            save_next_arg=0
-        elif [ $save_next_arg -eq 8 ]; then
-            EDGE_HOSTNAME="$arg"
-            save_next_arg=0
-        elif [ $save_next_arg -eq 9 ]; then
-            DOCKER_REGISTRIES_CSV="$arg"
-            process_docker_registries
-            save_next_arg=0
-        elif [ $save_next_arg -eq 10 ]; then
-            EDGE_CTL="$arg"
             save_next_arg=0
         else
             case "$arg" in
                 "-h" | "--help" ) usage;;
-                "-v" | "--image-version" ) save_next_arg=1;;
-                "--iothub-hostname" ) save_next_arg=2;;
-                "--device-id" ) save_next_arg=3;;
-                "--device-access-key" ) save_next_arg=4;;
-                "--iothubowner-access-key" ) save_next_arg=5;;
-                "--iothubowner-access-key-name" ) save_next_arg=6;;
+                "--deploy-tool" ) save_next_arg=1;;
+                "-v" | "--image-version" ) save_next_arg=2;;
+                "--iothub-hostname" ) save_next_arg=3;;
+                "--iothubowner-access-key" ) save_next_arg=4;;
+                "--iothubowner-access-key-name" ) save_next_arg=5;;
+                "--device-id" ) save_next_arg=6;;
                 "--arch" ) save_next_arg=7;;
-                "--edge-hostname" ) save_next_arg=8;;
-                "--docker-registries-csv") save_next_arg=9;;
-                "--edge-ctl") save_next_arg=10;;
                 * ) usage;;
             esac
         fi
     done
+
+    if [ -z ${DEPLOY_TOOL} ]; then
+        echo "Deploy tool Parameter Invalid"
+        print_help_and_exit
+    fi
 
     if [ -z ${DOCKER_IMAGEVERSION} ]; then
         if [ ! -z "${BUILD_BUILDNUMBER}" ]; then
@@ -129,16 +116,6 @@ process_args()
         print_help_and_exit
     fi
 
-    if [ -z ${DEVICEID} ]; then
-        echo "DeviceID Parameter Invalid"
-        print_help_and_exit
-    fi
-
-    if [ -z ${DEVICE_SHARED_ACCESS_KEY} ]; then
-        echo "Device shared access key Parameter Invalid"
-        print_help_and_exit
-    fi
-
     if [ -z ${IOTHUBOWNER_SHARED_ACCESS_KEY} ]; then
         echo "IotHub owner shared access key Parameter Invalid"
         print_help_and_exit
@@ -149,24 +126,26 @@ process_args()
         print_help_and_exit
     fi
 
-    if [ -z ${DOCKER_REGISTRIES_CSV} ]; then
-        echo "Docker registries csv Parameter Invalid"
-        print_help_and_exit
-    fi
-
-    if [ -z ${EDGE_CTL} ]; then
-        echo "Edge ctl Parameter Invalid"
+    if [ -z ${DEPLOY_TOOL} ]; then
+        echo "Deploy tool Parameter Invalid"
         print_help_and_exit
     fi
 }
 
-process_docker_registries() {
-    if [ ! -z "$DOCKER_REGISTRIES_CSV" ]; then
-        echo "Process docker registries"
-        OIFS=$IFS
-        IFS=', ' read -r -a docker_registries <<< "$DOCKER_REGISTRIES_CSV"
-        IFS=$OIFS                              
-    fi    
+function parse_config_file()
+{
+    local cfg_file="${1}"
+
+    agent_image_name="edgebuilds.azurecr.io/azureiotedge/edge-agent-linux-$ARCH:$DOCKER_IMAGEVERSION"
+    edgehub_image_name="edgebuilds.azurecr.io/azureiotedge/edge-hub-linux-$ARCH:$DOCKER_IMAGEVERSION"
+    $(jq -r --arg var1 $agent_image_name '.moduleContent."$edgeAgent"."properties.desired".systemModules.edgeAgent.settings.image = "\($var1)"' ${cfg_file} > edgeConfiguration_release_temp1.json)
+
+    $(jq -r --arg var1 $edgehub_image_name '.moduleContent."$edgeAgent"."properties.desired".systemModules.edgeHub.settings.image = "\($var1)"' edgeConfiguration_release_temp1.json > edgeConfiguration_release_temp2.json)
+
+    $(jq -r '.moduleContent."$edgeAgent"."properties.desired".modules={}' edgeConfiguration_release_temp2.json > edgeConfiguration_release_temp3.json)
+
+    cp edgeConfiguration_release_temp3.json $cfg_file 
+    rm -f edgeConfiguration_release_temp1.json edgeConfiguration_release_temp2.json edgeConfiguration_release_temp3.json
 }
 
 ###############################################################################
@@ -176,47 +155,47 @@ process_args "$@"
 check_arch
 
 agent_image_name="edgebuilds.azurecr.io/azureiotedge/edge-agent-linux-$ARCH:$DOCKER_IMAGEVERSION"
-device_connection="HostName=$IOTHUB_HOSTNAME;DeviceId=$DEVICEID;SharedAccessKey=$DEVICE_SHARED_ACCESS_KEY"
+edgehub_image_name="edgebuilds.azurecr.io/azureiotedge/edge-hub-linux-$ARCH:$DOCKER_IMAGEVERSION"
 iothub_connection="HostName=$IOTHUB_HOSTNAME;SharedAccessKeyName=$IOTHUBOWNER_SHARED_ACCESS_KEY_NAME;SharedAccessKey=$IOTHUBOWNER_SHARED_ACCESS_KEY"
+deploy_tool_path=deploy
 
-echo Bootstrap Edge
+rm -rf $deploy_tool_path
+mkdir $deploy_tool_path                                                                                                 
 
-pushd edge-bootstrap
-
-edge_ctl_file=$(basename $EDGE_CTL)
-edge_ctl_file_name="${edge_ctl_file%.tar.gz}"
-tar xvzf $EDGE_CTL
-
-pushd $edge_ctl_file_name
-sudo pip install -U .
-popd
-
-echo 'Clean up'
-rm -f $EDGE_CTL
-rm -rf $edge_ctl_file_name
-
-popd
-
-edge_hostname=
-if [ ! -z ${EDGE_HOSTNAME} ]; then
-    edge_hostname="--edge-hostname $EDGE_HOSTNAME"
+echo Downloading package $DEPLOY_TOOL
+if wget -q $DEPLOY_TOOL; then
+    echo Downloaded Deploy tool
+else
+    echo Error downloading Deploy tool
+    exit 1
 fi
 
-RES=0
+echo Unzip deploy tool
+deploy_file=$(basename $DEPLOY_TOOL)
+tar -xvf $deploy_file
+rm -f $deploy_file    
 
-sudo iotedgectl --verbose INFO setup --connection-string "$device_connection" --docker-edge-runtime-image "$agent_image_name" --docker-uri  "unix:///var/run/docker.sock" --docker-registries ${docker_registries[@]} $edge_hostname
+echo Set configuration to $DEVICEID
+pushd $deploy_tool_path
+
+echo Updating apt-get
+sudo apt-get update
+
+echo Installing jq
+sudo apt-get install -y jq
+
+echo Sanitize config file
+sed -e s%//.*%%g edgeConfiguration.json > edgeConfiguration_release.json
+
+parse_config_file "edgeConfiguration_release.json"
+
+./edge.sh configSet -d $DEVICEID -c edgeConfiguration_release.json -l $iothub_connection
 
 if [ $? -gt 0 ]; then
     RES=1
-    echo "Error running setup RES = $RES"
-    exit $RES
+    echo "Error running deployment RES = $RES"
 fi
 
-sudo iotedgectl --verbose INFO start                                                                                 
-
-if [ $? -gt 0 ]; then
-    RES=1
-    echo "Error running start RES = $RES"
-fi
+popd
 
 exit $RES
