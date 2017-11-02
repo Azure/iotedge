6f3de75e9071ad8712de0dcaa8fb5c168eb3fbe5
Sudeep Pradhan (Azure IoT) <spradhan@microsoft.com>
2017-11-02 15:47:04 +0000

    Merged PR 494807: IotHub config service E2E test
    IotHub config service E2E test

--- edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.IoTHub.Test/EdgeAgentConnectionTest.cs
+++ edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.IoTHub.Test/EdgeAgentConnectionTest.cs
@@ -17,6 +17,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.IoTHub.Test
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
     using Xunit;
+    using System.Linq;
 
     public class EdgeAgentConnectionTest
     {
@@ -115,6 +116,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.IoTHub.Test
 
             await registryManager.RemoveDeviceAsync(edgeDevice);
         }
+
         public static async Task SetAgentDesiredProperties(RegistryManager rm, string deviceId)
         {
             ConfigurationContent cc = new ConfigurationContent() { ModuleContent = new Dictionary<string, TwinContent>() };
@@ -178,6 +180,330 @@ namespace Microsoft.Azure.Devices.Edge.Agent.IoTHub.Test
             string patch = JsonConvert.SerializeObject(dp);
             twinContent.TargetContent = new TwinCollection(patch);
             await rm.ApplyConfigurationContentOnDeviceAsync(deviceId, cc);
+        }
+
+        [Bvt]
+        [Fact]
+        public async Task EdgeAgentConnectionConfigurationTest()
+        {
+            string edgeDeviceId = "testMmaEdgeDevice1" + Guid.NewGuid().ToString();
+            string configurationId = "testconfiguration-" + Guid.NewGuid().ToString();
+            string conditionPropertyName = "condition-" + Guid.NewGuid().ToString("N");
+            string conditionPropertyValue = Guid.NewGuid().ToString();
+            string iotHubConnectionString = await SecretsHelper.GetSecretFromConfigKey("iotHubConnStrKey");
+            IotHubConnectionStringBuilder iotHubConnectionStringBuilder = IotHubConnectionStringBuilder.Create(iotHubConnectionString);
+            var registryManager = RegistryManager.CreateFromConnectionString(iotHubConnectionString);
+
+            try
+            {
+                await registryManager.OpenAsync();
+
+                var edgeDevice = new Device(edgeDeviceId)
+                {
+                    Capabilities = new DeviceCapabilities { IotEdge = true },
+                    Authentication = new AuthenticationMechanism() { Type = AuthenticationType.Sas }
+                };
+                edgeDevice = await registryManager.AddDeviceAsync(edgeDevice);
+
+                var twin = await registryManager.GetTwinAsync(edgeDeviceId);
+                twin.Tags[conditionPropertyName] = conditionPropertyValue;
+                await registryManager.UpdateTwinAsync(edgeDeviceId, twin, twin.ETag);
+                await registryManager.GetTwinAsync(edgeDeviceId, "$edgeAgent");
+                await registryManager.GetTwinAsync(edgeDeviceId, "$edgeHub");
+
+                await CreateConfigurationAsync(registryManager, configurationId, $"tags.{conditionPropertyName}='{conditionPropertyValue}'", 10);
+
+                await Task.Delay(TimeSpan.FromSeconds(45));
+
+                string edgeDeviceConnectionString = $"HostName={iotHubConnectionStringBuilder.HostName};DeviceId={edgeDeviceId};SharedAccessKey={edgeDevice.Authentication.SymmetricKey.PrimaryKey}";
+                EdgeHubConnectionString edgeHubConnectionString = new EdgeHubConnectionString.EdgeHubConnectionStringBuilder(iotHubConnectionStringBuilder.HostName, edgeDeviceId)
+                    .SetSharedAccessKey(edgeDevice.Authentication.SymmetricKey.PrimaryKey)
+                    .Build();
+                IServiceClient serviceClient = new ServiceClient(edgeDeviceConnectionString, edgeDeviceId);
+                IDeviceClient deviceClient = await DeviceClient.CreateAsync(edgeHubConnectionString, serviceClient);
+
+                var moduleDeserializerTypes = new Dictionary<string, Type>
+                {
+                    { DockerType, typeof(DockerDesiredModule) }
+                };
+
+                var edgeAgentDeserializerTypes = new Dictionary<string, Type>
+                {
+                    { DockerType, typeof(EdgeAgentDockerModule) }
+                };
+
+                var edgeHubDeserializerTypes = new Dictionary<string, Type>
+                {
+                    { DockerType, typeof(EdgeHubDockerModule) }
+                };
+
+                var runtimeInfoDeserializerTypes = new Dictionary<string, Type>
+                {
+                    { DockerType, typeof(DockerRuntimeInfo) }
+                };
+
+                var deserializerTypes = new Dictionary<Type, IDictionary<string, Type>>
+                {
+                    [typeof(IModule)] = moduleDeserializerTypes,
+                    [typeof(IEdgeAgentModule)] = edgeAgentDeserializerTypes,
+                    [typeof(IEdgeHubModule)] = edgeHubDeserializerTypes,
+                    [typeof(IRuntimeInfo)] = runtimeInfoDeserializerTypes,
+                };
+
+                ISerde<DeploymentConfig> serde = new TypeSpecificSerDe<DeploymentConfig>(deserializerTypes);
+                IEdgeAgentConnection edgeAgentConnection = await EdgeAgentConnection.Create(deviceClient, serde);
+                await Task.Delay(TimeSpan.FromSeconds(10));
+
+                Option<DeploymentConfigInfo> deploymentConfigInfo = await edgeAgentConnection.GetDeploymentConfigInfoAsync();
+
+                Assert.True(deploymentConfigInfo.HasValue);
+                DeploymentConfig deploymentConfig = deploymentConfigInfo.OrDefault().DeploymentConfig;
+                Assert.NotNull(deploymentConfig);
+                Assert.NotNull(deploymentConfig.Modules);
+                Assert.NotNull(deploymentConfig.Runtime);
+                Assert.NotNull(deploymentConfig.SystemModules);
+                Assert.Equal(EdgeAgentConnection.ExpectedSchemaVersion, deploymentConfig.SchemaVersion);
+                Assert.NotNull(deploymentConfig.SystemModules.EdgeAgent);
+                Assert.Equal(configurationId, deploymentConfig.SystemModules.EdgeAgent.ConfigurationInfo.Id);
+                Assert.NotNull(deploymentConfig.SystemModules.EdgeHub);
+                Assert.Equal(configurationId, deploymentConfig.SystemModules.EdgeHub.ConfigurationInfo.Id);
+                Assert.Equal(2, deploymentConfig.Modules.Count);
+                Assert.NotNull(deploymentConfig.Modules["mongoserver"]);
+                Assert.Equal(configurationId, deploymentConfig.Modules["mongoserver"].ConfigurationInfo.Id);
+                Assert.NotNull(deploymentConfig.Modules["asa"]);
+                Assert.Equal(configurationId, deploymentConfig.Modules["asa"].ConfigurationInfo.Id);
+
+                var reportedPatch = GetEdgeAgentReportedProperties(deploymentConfigInfo.OrDefault());
+                await edgeAgentConnection.UpdateReportedPropertiesAsync(reportedPatch);
+                await Task.Delay(TimeSpan.FromSeconds(45));
+
+                var config = await registryManager.GetConfigurationAsync(configurationId);
+                Assert.NotNull(config);
+                Assert.NotNull(config.Statistics);
+                Assert.True(config.Statistics.ContainsKey("targetedCount"));
+                Assert.Equal(1, config.Statistics["targetedCount"]);
+                Assert.True(config.Statistics.ContainsKey("appliedCount"));
+                Assert.Equal(1, config.Statistics["appliedCount"]);
+                Assert.True(config.Statistics.ContainsKey("reportedSuccessfulCount"));
+                Assert.Equal(1, config.Statistics["reportedSuccessfulCount"]);
+            }
+            finally
+            {
+                try
+                {
+                    await registryManager.RemoveDeviceAsync(edgeDeviceId);
+                }
+                catch (Exception)
+                {
+                }
+
+                try
+                {
+                    await DeleteConfigurationAsync(registryManager, configurationId);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        public static async Task<Configuration> CreateConfigurationAsync(RegistryManager registryMananger, string configurationId, string targetCondition, int priority)
+        {
+            Configuration configuration = new Configuration(configurationId)
+            {
+                Labels = new Dictionary<string, string>
+                {
+                    { "App", "Stream Analytics" }
+                },
+                Content = GetDefaultConfigurationContent(),
+                Priority = priority,
+                TargetCondition = targetCondition
+            };
+
+            return await registryMananger.AddConfigurationAsync(configuration);
+        }
+
+        public static async Task DeleteConfigurationAsync(RegistryManager registryManager, string configurationId)
+        {
+            await registryManager.RemoveConfigurationAsync(configurationId);
+        }
+
+        public static TwinCollection GetEdgeAgentReportedProperties(DeploymentConfigInfo deploymentConfigInfo)
+        {
+            var deploymentConfig = deploymentConfigInfo.DeploymentConfig;
+            var reportedProperties = new
+            {
+                lastDesiredVersion = deploymentConfigInfo.Version,
+                lastDesiredStatus = new
+                {
+                    code = 200
+                },
+                runtime = new
+                {
+                    type = "docker"
+                },
+                systemModules = new
+                {
+                    edgeAgent = new
+                    {
+                        runtimeStatus = "running",
+                        description = "All good",
+                        configuration = new
+                        {
+                            id = deploymentConfig.SystemModules.EdgeAgent.ConfigurationInfo.Id
+                        }
+                    },
+                    edgeHub = new
+                    {
+                        runtimeStatus = "running",
+                        description = "All good",
+                        configuration = new
+                        {
+                            id = deploymentConfig.SystemModules.EdgeHub.ConfigurationInfo.Id
+                        }
+                    }
+                },
+                modules = new
+                {
+                    mongoserver = new
+                    {
+                        runtimeStatus = "running",
+                        description = "All good",
+                        configuration = new
+                        {
+                            id = deploymentConfig.Modules["mongoserver"].ConfigurationInfo.Id
+                        }
+                    },
+                    asa = new
+                    {
+                        runtimeStatus = "running",
+                        description = "All good",
+                        configuration = new
+                        {
+                            id = deploymentConfig.Modules["asa"].ConfigurationInfo.Id
+                        }
+                    }
+                }
+            };
+
+            string patch = JsonConvert.SerializeObject(reportedProperties);
+            return new TwinCollection(patch);
+        }
+
+        public static ConfigurationContent GetDefaultConfigurationContent()
+        {
+            return new ConfigurationContent()
+            {
+                ModuleContent = new Dictionary<string, TwinContent>
+                {
+                    { "$edgeAgent", GetEdgeAgentConfiguration() },
+                    { "$edgeHub", GetEdgeHubConfiguration() },
+                    { "mongoserver", GetTwinConfiguration("mongoserver") },
+                    { "asa", GetTwinConfiguration("asa") }
+                }
+            };
+        }
+
+        static TwinContent GetEdgeAgentConfiguration()
+        {
+            TwinContent edgeAgent = new TwinContent();
+            var desiredProperties = new
+            {
+                schemaVersion = "1.0",
+                runtime = new
+                {
+                    type = "docker",
+                    settings = new
+                    {
+                        minDockerVersion = "1.5",
+                        loggingOptions = ""
+                    }
+                },
+                systemModules = new
+                {
+                    edgeAgent = new
+                    {
+                        type = "docker",
+                        settings = new
+                        {
+                            image = "edgeAgent",
+                            createOptions = ""
+                        }
+                    },
+                    edgeHub = new
+                    {
+                        type = "docker",
+                        status = "running",
+                        restartPolicy = "always",
+                        settings = new
+                        {
+                            image = "edgeHub",
+                            createOptions = ""
+                        }
+                    }
+                },
+                modules = new
+                {
+                    mongoserver = new
+                    {
+                        version = "1.0",
+                        type = "docker",
+                        status = "running",
+                        restartPolicy = "on-failure",
+                        settings = new
+                        {
+                            image = "mongo",
+                            createOptions = ""
+                        }
+                    },
+                    asa = new
+                    {
+                        version = "1.0",
+                        type = "docker",
+                        status = "running",
+                        restartPolicy = "on-failure",
+                        settings = new
+                        {
+                            image = "asa",
+                            createOptions = ""
+                        }
+                    }
+                }
+            };
+            string patch = JsonConvert.SerializeObject(desiredProperties);
+            edgeAgent.TargetContent = new TwinCollection(patch);
+
+            return edgeAgent;
+        }
+
+        static TwinContent GetEdgeHubConfiguration()
+        {
+            TwinContent edgeHub = new TwinContent();
+            var desiredProperties = new
+            {
+                schemaVersion = "1.0",
+                routes = new Dictionary<string, string>
+                {
+                    ["route1"] = "from /* INTO $upstream",
+                },
+                storeAndForwardConfiguration = new
+                {
+                    timeToLiveSecs = 20
+                }
+            };
+            string patch = JsonConvert.SerializeObject(desiredProperties);
+
+            edgeHub.TargetContent = new TwinCollection(patch);
+
+            return edgeHub;
+        }
+
+        static TwinContent GetTwinConfiguration(string moduleName)
+        {
+            TwinContent configuration = new TwinContent();
+            configuration.TargetContent = new TwinCollection();
+            configuration.TargetContent["name"] = moduleName;
+            return configuration;
         }
 
         public static async Task UpdateAgentDesiredProperties(RegistryManager rm, string deviceId)
