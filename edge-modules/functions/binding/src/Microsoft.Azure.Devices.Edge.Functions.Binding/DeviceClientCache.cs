655877d69e9bab9967681a05888c28cdccd26349
Angelo Ribeiro <angelod@microsoft.com>
2017-11-09 02:49:01 +0000

    Merged PR 509580: Changes on the Binding to add the device Ca Cert.
    Changes on the Binding to add the device Ca Cert.

--- edge-modules/functions/binding/src/Microsoft.Azure.Devices.Edge.Functions.Binding/DeviceClientCache.cs
+++ edge-modules/functions/binding/src/Microsoft.Azure.Devices.Edge.Functions.Binding/DeviceClientCache.cs
@@ -1,10 +1,12 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 
 namespace Microsoft.Azure.Devices.Edge.Functions.Binding
 {
     using System.Collections.Concurrent;
     using Microsoft.Azure.Devices.Client;
     using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+    using System;
+    using System.Security.Cryptography.X509Certificates;
 
     class DeviceClientCache
     {
@@ -27,11 +29,15 @@ namespace Microsoft.Azure.Devices.Edge.Functions.Binding
 
         DeviceClient CreateDeviceClient(string connectionString)
         {
-            var mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only)
-            {
-                // TODO: SECURITY WARNING !!! Please remove this code after Edge Hub is not using self signed certificates !!!
-                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
-            };
+            // get CA certificate
+            string certPath = Environment.GetEnvironmentVariable("EdgeModuleCACertificateFile");
+
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);  // On Linux only root store worked
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(new X509Certificate2(X509Certificate2.CreateFromCertFile(certPath)));
+            store.Close();
+
+            var mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
             ITransportSettings[] settings = { mqttSetting };
 
             return DeviceClient.CreateFromConnectionString(connectionString, settings);
