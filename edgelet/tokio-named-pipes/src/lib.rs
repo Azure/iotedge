b7f00412552ed420e057feb5004ebc519aeba8aa
Raj Vengalil <rajave@microsoft.com>
2018-05-02 00:44:48 +0000

    Merged PR 805998: Add tokio support for named pipes
    This commit is the first in a series that will culminate with support for Windows named pipes in edgelet. This commit adds support for named pipe clients in Tokio. It wraps the named pipe implementation provided by the `mio-named-pipes` crate and adapts it to the requirements of Tokio. The key feature addition is implementation of the `AsyncRead` and `AsyncWrite` traits.

---
+++ edgelet/tokio-named-pipes/src/lib.rs
@@ -0,0 +1,149 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+#![deny(warnings)]
+#![cfg(windows)]
+
+extern crate bytes;
+extern crate futures;
+extern crate mio_named_pipes;
+extern crate tokio_core;
+extern crate tokio_io;
+extern crate winapi;
+
+use std::convert::AsRef;
+use std::fs::OpenOptions;
+use std::io::{self, Read, Write};
+use std::path::Path;
+use std::os::windows::prelude::*;
+
+use bytes::{Buf, BufMut};
+use futures::{Async, Poll};
+use mio_named_pipes::NamedPipe;
+use tokio_core::reactor::{Handle, PollEvented};
+use tokio_io::{AsyncRead, AsyncWrite};
+use winapi::um::winbase::*;
+
+#[derive(Debug)]
+pub struct PipeStream {
+    io: PollEvented<NamedPipe>,
+}
+
+impl PipeStream {
+    pub fn connect<P: AsRef<Path>>(path: P, handle: &Handle) -> io::Result<PipeStream> {
+        let mut options = OpenOptions::new();
+        options
+            .read(true)
+            .write(true)
+            .custom_flags(FILE_FLAG_OVERLAPPED);
+        let file = options.open(path)?;
+        let named_pipe = unsafe { NamedPipe::from_raw_handle(file.into_raw_handle()) };
+
+        Ok(PipeStream {
+            io: PollEvented::new(named_pipe, handle)?,
+        })
+    }
+
+    pub fn poll_read(&self) -> Async<()> {
+        self.io.poll_read()
+    }
+
+    pub fn poll_write(&self) -> Async<()> {
+        self.io.poll_write()
+    }
+
+    pub fn disconnect(&self) -> io::Result<()> {
+        self.io.get_ref().disconnect()
+    }
+
+    pub fn io_mut(&mut self) -> &mut PollEvented<NamedPipe> {
+        &mut self.io
+    }
+}
+
+impl Read for PipeStream {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.io.read(buf)
+    }
+}
+
+impl Write for PipeStream {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.io.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.io.flush()
+    }
+}
+
+impl<'a> Read for &'a PipeStream {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        (&self.io).read(buf)
+    }
+}
+
+impl<'a> Write for &'a PipeStream {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        (&self.io).write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        (&self.io).flush()
+    }
+}
+
+impl AsyncRead for PipeStream {
+    unsafe fn prepare_uninitialized_buffer(&self, _: &mut [u8]) -> bool {
+        false
+    }
+
+    fn read_buf<B: BufMut>(&mut self, buf: &mut B) -> Poll<usize, io::Error> {
+        if let Async::NotReady = <PipeStream>::poll_read(self) {
+            return Ok(Async::NotReady);
+        }
+
+        unsafe {
+            match self.io_mut().get_mut().read(buf.bytes_mut()) {
+                Err(e) => {
+                    if e.kind() == io::ErrorKind::WouldBlock {
+                        self.io.need_read();
+                        Ok(Async::NotReady)
+                    } else {
+                        Err(e)
+                    }
+                }
+                Ok(size) => {
+                    buf.advance_mut(size);
+                    Ok(size.into())
+                }
+            }
+        }
+    }
+}
+
+impl AsyncWrite for PipeStream {
+    fn shutdown(&mut self) -> Poll<(), io::Error> {
+        Ok(().into())
+    }
+
+    fn write_buf<B: Buf>(&mut self, buf: &mut B) -> Poll<usize, io::Error> {
+        if let Async::NotReady = <PipeStream>::poll_write(self) {
+            return Ok(Async::NotReady);
+        }
+
+        match self.io_mut().get_mut().write(buf.bytes()) {
+            Err(e) => {
+                if e.kind() == io::ErrorKind::WouldBlock {
+                    self.io.need_write();
+                    Ok(Async::NotReady)
+                } else {
+                    Err(e)
+                }
+            }
+            Ok(size) => {
+                buf.advance(size);
+                Ok(size.into())
+            }
+        }
+    }
+}
---
+++ edgelet/tokio-named-pipes/tests/smoke.rs
@@ -0,0 +1,193 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+#![deny(warnings)]
+#![cfg(windows)]
+
+extern crate futures;
+extern crate mio;
+extern crate mio_named_pipes;
+extern crate rand;
+extern crate tokio_core;
+extern crate tokio_io;
+
+extern crate tokio_named_pipes;
+
+use std::io::{Read, Write};
+use std::str;
+
+use futures::Future;
+use futures::sink::Sink;
+use futures::stream::Stream;
+use mio::{Events, Poll, PollOpt, Ready, Token};
+use mio_named_pipes::NamedPipe;
+use rand::Rng;
+use tokio_core::reactor::Core;
+use tokio_io::io as tio;
+use tokio_io::codec::{FramedRead, FramedWrite, LinesCodec};
+
+use tokio_named_pipes::PipeStream;
+
+macro_rules! t {
+    ($e:expr) => (match $e {
+        Ok(e) => e,
+        Err(e) => panic!("{} failed with {}", stringify!($e), e),
+    })
+}
+
+fn server() -> (NamedPipe, String) {
+    let num: u64 = rand::thread_rng().gen();
+    let name = format!(r"\\.\pipe\my-pipe-{}", num);
+    let pipe = t!(NamedPipe::new(&name));
+    (pipe, name)
+}
+
+#[test]
+#[should_panic(expected = "The system cannot find the file specified")]
+fn connect_invalid_path() {
+    let core = Core::new().unwrap();
+    let _stream = PipeStream::connect(r"\\.\pipe\boo", &core.handle()).unwrap();
+}
+
+#[test]
+fn connect_succeeds() {
+    let core = Core::new().unwrap();
+    let (_server, path) = server();
+    let _stream = PipeStream::connect(path, &core.handle()).unwrap();
+}
+
+#[test]
+fn read_data() {
+    let data = b"cow say moo";
+    let (mut server, path) = server();
+
+    let mut core = Core::new().unwrap();
+    let stream = PipeStream::connect(path, &core.handle()).unwrap();
+
+    let poll = t!(Poll::new());
+    t!(poll.register(
+        &server,
+        Token(0),
+        Ready::readable() | Ready::writable(),
+        PollOpt::level()
+    ));
+
+    // writing data
+    assert_eq!(t!(server.write(data)), data.len());
+
+    let buf = [0; 11];
+    let task = tio::read(stream, buf);
+
+    let (_, read_data, read_len) = core.run(task).unwrap();
+    assert_eq!(read_len, data.len());
+    for i in 0..read_data.len() {
+        assert_eq!(read_data[i], data[i]);
+    }
+}
+
+#[test]
+fn write_data() {
+    let (mut server, path) = server();
+    let mut core = Core::new().unwrap();
+    let stream = PipeStream::connect(path, &core.handle()).unwrap();
+
+    let write_future = tio::write_all(stream, b"cow say moo");
+    core.run(write_future).unwrap();
+
+    let poll = t!(Poll::new());
+    t!(poll.register(
+        &server,
+        Token(0),
+        Ready::readable() | Ready::writable(),
+        PollOpt::edge()
+    ));
+
+    // reading data
+    let mut events = Events::with_capacity(128);
+    loop {
+        t!(poll.poll(&mut events, None));
+        let events = events.iter().collect::<Vec<_>>();
+        if let Some(event) = events.iter().find(|e| e.token() == Token(0)) {
+            if event.readiness().is_readable() {
+                break;
+            }
+        }
+    }
+
+    let mut buf = [0; 11];
+    assert_eq!(t!(server.read(&mut buf)), 11);
+    assert_eq!(&buf[..11], b"cow say moo");
+}
+
+#[test]
+fn read_async() {
+    let data = "cow say moo\nsheep say baa\n".as_bytes();
+    let (mut server, path) = server();
+
+    let mut core = Core::new().unwrap();
+    let stream = PipeStream::connect(path, &core.handle()).unwrap();
+
+    let poll = t!(Poll::new());
+    t!(poll.register(
+        &server,
+        Token(0),
+        Ready::readable() | Ready::writable(),
+        PollOpt::edge()
+    ));
+
+    // writing data
+    assert_eq!(t!(server.write(data)), data.len());
+
+    // reading data
+    let framed_read = FramedRead::new(stream, LinesCodec::new());
+    let task = framed_read.into_future().and_then(|(line1, stream)| {
+        stream
+            .into_future()
+            .map(|(line2, _)| (line1.unwrap(), line2.unwrap()))
+    });
+
+    let result = core.run(task).unwrap();
+    assert_eq!(
+        result,
+        ("cow say moo".to_string(), "sheep say baa".to_string())
+    );
+}
+
+#[test]
+fn write_async() {
+    let (mut server, path) = server();
+    let mut core = Core::new().unwrap();
+    let stream = PipeStream::connect(path, &core.handle()).unwrap();
+
+    let framed_write = FramedWrite::new(stream, LinesCodec::new());
+    let task = framed_write
+        .send("cow say moo".to_string())
+        .and_then(|sink| sink.send("sheep say baa".to_string()));
+    core.run(task).unwrap();
+
+    let poll = t!(Poll::new());
+    t!(poll.register(
+        &server,
+        Token(0),
+        Ready::readable() | Ready::writable(),
+        PollOpt::edge()
+    ));
+
+    // reading data
+    let mut events = Events::with_capacity(128);
+    let mut output = String::new();
+    let mut buf = [0; 26];
+    loop {
+        t!(poll.poll(&mut events, None));
+        let events = events.iter().collect::<Vec<_>>();
+        if let Some(event) = events.iter().find(|e| e.token() == Token(0)) {
+            if event.readiness().is_readable() {
+                let count = t!(server.read(&mut buf));
+                output.push_str(str::from_utf8(&buf[..count]).unwrap());
+
+                if output == "cow say moo\nsheep say baa\n" {
+                    break;
+                }
+            }
+        }
+    }
+}
