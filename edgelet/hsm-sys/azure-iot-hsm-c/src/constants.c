2bbf095d1aea7c33099ec55ec099b4eec2780628
Mahesh Rohera <marohera@microsoft.com>
2018-05-15 06:50:12 +0000

    Merged PR 835132: Environment variables, constant defines and file system structure layout for HSM C for quickstart and transparent gateway scenarios
    These defines dictate how we will create all the needed HSM C files under $EDGEHOMEDIR/<HSM_CRYPTO_DIR>.
    
    Directory structure under HSM dir is as follows:
    ```
    + $EDGEHOMEDIR/<HSM_CRYPTO_DIR>
     |
     -- + <EDGE_STORE_NAME> (dir)
          |
          -- hsm_encryption.key.pem  Note: this will contain the encryption key and schema version at this time and no other data
          -- + certs  Note: all the certificates will be placed here
          --  + private private keys will be stored in this directory.
          File names are expected to named as <module_id>.key.pem and <module_id>.cert.pem
    ```

--- edgelet/hsm-sys/azure-iot-hsm-c/src/constants.c
+++ edgelet/hsm-sys/azure-iot-hsm-c/src/constants.c
@@ -1,5 +1,23 @@
 #include "hsm_constants.h"
 
-const char* const ENV_EDGE_HOME_DIR = "EDGEHOMEDIR";
-const char* const EDGELET_IDENTITY_SAS_KEY_NAME = "edgelet-identity";
+/* IOTEDGE env variables set by iotedged */
+const char* const ENV_EDGE_HOME_DIR = "IOTEDGE_HOMEDIR";
+const char* const ENV_DEVICE_CA_PATH = "IOTEDGE_DEVICE_CA_PATH";
+const char* const ENV_DEVICE_CA_CHAIN_PATH = "IOTEDGE_DEVICE_CA_CHAIN_PATH";
+const char* const ENV_DEVICE_PK_PATH = "IOTEDGE_DEVICE_PK_PATH";
+const char* const ENV_OWNER_CA_PATH = "IOTEDGE_OWNER_CA_PATH";
+
+/* HSM directory name under IOTEDGE_HOMEDIR */
+const char* const HSM_CRYPTO_DIR = "hsm";
+const char* const HSM_CRYPTO_CERTS_DIR = "certs";
+const char* const HSM_CRYPTO_PK_DIR = "private";
+
+/* HSM C misc constants */
 const char* const EDGE_STORE_NAME = "edgelet";
+const char* const EDGELET_IDENTITY_SAS_KEY_NAME = "edgelet-identity";
+const char* const OWNER_CA_ALIAS = "edge_owner_ca";
+const char* const OWNER_CA_COMMON_NAME = "Test Edge Owner CA";
+const char* const DEVICE_CA_COMMON_NAME = "Test Edge Device CA";
+const char* const EDGE_CA_ALIAS = "iotedged_ca";
+const char* const EDGE_CA_COMMON_NAME = "Test Iotedge CA";
+const uint64_t CA_VALIDITY = 90 * 24 * 3600; // 90 days
--- edgelet/hsm-sys/azure-iot-hsm-c/src/hsm_constants.h
+++ edgelet/hsm-sys/azure-iot-hsm-c/src/hsm_constants.h
@@ -1,8 +1,28 @@
 #ifndef HSM_CONSTANTS_H
 #define HSM_CONSTANTS_H
 
+#include <stdint.h>
+
+/* IOTEDGE env variables set by iotedged */
 extern const char* const ENV_EDGE_HOME_DIR;
+extern const char* const ENV_DEVICE_CA_PATH;
+extern const char* const ENV_DEVICE_CA_CHAIN_PATH;
+extern const char* const ENV_DEVICE_PK_PATH;
+extern const char* const ENV_OWNER_CA_PATH;
+
+/* HSM directory name under IOTEDGE_HOMEDIR */
+extern const char* const HSM_CRYPTO_DIR;
+extern const char* const HSM_CRYPTO_CERTS_DIR;
+extern const char* const HSM_CRYPTO_PK_DIR;
+
+/* HSM C misc constants */
 extern const char* const EDGE_STORE_NAME;
 extern const char* const EDGELET_IDENTITY_SAS_KEY_NAME;
+extern const char* const OWNER_CA_ALIAS;
+extern const char* const OWNER_CA_COMMON_NAME;
+extern const char* const DEVICE_CA_COMMON_NAME;
+extern const char* const EDGE_CA_ALIAS;
+extern const char* const EDGE_CA_COMMON_NAME;
+extern const uint64_t CA_VALIDITY;
 
 #endif  //HSM_CONSTANTS_H
