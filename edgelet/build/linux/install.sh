e2e9888c85c1e48ddfedba0b15e094478544c625
Mike Yagley <miyagley@microsoft.com>
2018-06-13 17:23:25 +0000

    Merged PR 895072: Fix arm package glibc dependency
    The edgelet arm package requires glibc >= 2.23. This is purely a problem with how the package dependencies are listed, as the edgelet can support lower level versions of glibc.
    
    This PR installs the proper cross build of glibc so that the dependency can be specified properly in the package.
    
    Related work items: #2587502

--- edgelet/build/linux/install.sh
+++ edgelet/build/linux/install.sh
@@ -12,6 +12,7 @@ set -e
 SCRIPT_NAME=$(basename "$0")
 RUSTUP="$HOME/.cargo/bin/rustup"
 TOOLCHAIN="stable"
+ARM_PACKAGE=
 
 ###############################################################################
 # Print usage information pertaining to this script and exit
@@ -23,6 +24,7 @@ usage()
     echo "options"
     echo " -h, --help          Print this help and exit."
     echo " -t, --toolchain     Toolchain (default: stable)"
+    echo " -p, --arm-package   Add additional dependencies for armhf packaging"
     exit 1;
 }
 
@@ -41,6 +43,7 @@ process_args()
             case "$arg" in
                 "-h" | "--help" ) usage;;
                 "-t" | "--toolchain" ) save_next_arg=1;;
+                "-p" | "--package-arm" ) ARM_PACKAGE=1;;
                 * ) usage;;
             esac
         fi
@@ -55,16 +58,38 @@ else
     curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain "$TOOLCHAIN"
 fi
 
+# Add trusty repo to get older version of libc6-armhf-cross
+add-apt-repository "deb http://archive.ubuntu.com/ubuntu/ trusty main universe"
+
 # Install OpenSSL, curl and uuid and valgrind
-sudo apt-get update && \
-sudo apt-get install -y \
+apt-get update && \
+apt-get install -y \
     pkg-config \
     libssl-dev \
     uuid-dev curl \
     libcurl4-openssl-dev \
     debhelper \
     dh-systemd \
-    valgrind \
-    binutils-arm-linux-gnueabihf \
-    libc6-armhf-cross \
-    gcc-arm-linux-gnueabihf
+    valgrind
+
+if [[ -n "$ARM_PACKAGE" ]]; then
+    # armhf cross tools for packaging
+    # These packages need to be pinned to a specific version to make
+    # the package dependent on the lowest version of glibc possible
+
+    apt-get install -y \
+        binutils-arm-linux-gnueabihf=2.24-2ubuntu3cross1.98 \
+        libsfasan0-armhf-cross=4.8.2-16ubuntu4cross0.11 \
+        libsfgcc-4.8-dev-armhf-cross=4.8.2-16ubuntu4cross0.11 \
+        libasan0-armhf-cross=4.8.2-16ubuntu4cross0.11 \
+        libatomic1-armhf-cross=4.8.2-16ubuntu4cross0.11 \
+        libgomp1-armhf-cross=4.8.2-16ubuntu4cross0.11 \
+        libgcc1-armhf-cross=1:4.8.2-16ubuntu4cross0.11 \
+        gcc-4.8-arm-linux-gnueabihf-base=4.8.2-16ubuntu4cross0.11 \
+        libgcc-4.8-dev-armhf-cross=4.8.2-16ubuntu4cross0.11 \
+        cpp-4.8-arm-linux-gnueabihf=4.8.2-16ubuntu4cross0.11 \
+        gcc-4.8-arm-linux-gnueabihf=4.8.2-16ubuntu4cross0.11 \
+        gcc-4.8-multilib-arm-linux-gnueabihf=4.8.2-16ubuntu4cross0.11 \
+        libc6-armhf-cross=2.19-0ubuntu2cross1.104 \
+        gcc-arm-linux-gnueabihf=4:4.8.2-1
+fi
