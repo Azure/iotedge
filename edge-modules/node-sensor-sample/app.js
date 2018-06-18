59d8f203435491803d729a85c44ffefe401c21ef
Raj Vengalil <rajave@microsoft.com>
2018-06-18 19:24:06 +0000

    Merged PR 903673: Update Node.js sensor sample to use new Node.js SDK
    Update Node.js sensor sample to use new Node.js SDK

--- edge-modules/node-sensor-sample/app.js
+++ edge-modules/node-sensor-sample/app.js
@@ -1,44 +1,72 @@
-'use strict';
+"use strict";
 
-const Protocol = require('azure-iot-device-mqtt').Mqtt;
-const { Client: DeviceClient, Message } = require('azure-iot-device');
+const Protocol = require("azure-iot-device-mqtt").Mqtt;
+const { ModuleClient, Message } = require("azure-iot-device");
 
 const config = {
-  connectionString: process.env.EdgeHubConnectionString,
   messageInterval: process.env.MessageInterval || 2000,
   maxTemp: process.env.MaxTemp || 40
 };
 
 function main() {
-  const client = DeviceClient.fromConnectionString(config.connectionString, Protocol);
-  client.open(err => {
+  ModuleClient.fromEnvironment(Protocol, (err, client) => {
     if (err) {
-      console.error(`Connection error: ${err}`);
+      console.error(`Error creating ModuleClient instance: ${err}`);
     } else {
-      // register event handler for incoming messages
-      client.on('event', msg => {
-        console.log(JSON.stringify(msg, null, 2));
-      });
+      client.open(err => {
+        if (err) {
+          console.error(`Connection error: ${err}`);
+        } else {
+          // register event handler for incoming messages
+          client.on("inputMessage", (inputName, msg) => {
+            client.complete(msg, printResultFor("completed"));
+            const data = Buffer.from(msg.data).toString();
+            console.log(`<- Data: ${JSON.stringify(JSON.parse(data))}`);
+          });
 
-      // send an event out every few seconds
-      const messageSend = () => {
-        const data = {
-          temperature: Math.floor(Math.random() * config.maxTemp)
-        };
-        client.sendEvent(
-          'nodeTemperatureOutput',
-          new Message(JSON.stringify(data)),
-        err => {
-          if (err) {
-            console.error(`Message send error: ${err}`);
-          } else {
-            setTimeout(messageSend, config.messageInterval);
-          }
-        });
-      };
-      setTimeout(messageSend, config.messageInterval);
+          client.onDeviceMethod("reset", (req, res) => {
+            console.log(
+              `Got method call [${req.requestId}, ${
+                req.methodName
+              }, ${JSON.stringify(req.payload, null, 2)}]`
+            );
+            res.send(
+              200,
+              "Device reset successful",
+              printResultFor("method response")
+            );
+          });
+
+          // send an event out every few seconds
+          const messageSend = () => {
+            const data = {
+              temperature: Math.floor(Math.random() * config.maxTemp)
+            };
+            client.sendOutputEvent(
+              "nodeTemperatureOutput",
+              new Message(JSON.stringify(data)),
+              err => {
+                if (err) {
+                  console.error(`Message send error: ${err}`);
+                } else {
+                  console.log(`-> Data: ${data.temperature}`);
+                  setTimeout(messageSend, config.messageInterval);
+                }
+              }
+            );
+          };
+          setTimeout(messageSend, config.messageInterval);
+        }
+      });
     }
   });
+}
+
+function printResultFor(op) {
+  return function printResult(err, res) {
+    if (err) console.log(op + " error: " + err.toString());
+    if (res) console.log(op + " status: " + res.constructor.name);
+  };
 }
 
 main();
