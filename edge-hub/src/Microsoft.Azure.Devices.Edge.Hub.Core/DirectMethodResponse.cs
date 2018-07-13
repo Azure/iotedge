3d5a9b62741b6324afa64816bd09c881b6f1f748
Varun Puranik <vpuranik@microsoft.com>
2018-07-13 13:10:00 -0700

    Direct method response fix

--- edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/DirectMethodResponse.cs
+++ edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/DirectMethodResponse.cs
@@ -1,7 +1,11 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 
 namespace Microsoft.Azure.Devices.Edge.Hub.Core
 {
+    using System;
+    using System.Net;
+    using Microsoft.Azure.Devices.Edge.Util;
+
     public class DirectMethodResponse
     {
         public DirectMethodResponse(string rid, byte[] data, int statusCode)
@@ -10,6 +14,15 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core
             this.Data = data;
             this.Status = statusCode;
         }
+
+        public DirectMethodResponse(Exception exception, HttpStatusCode code)
+        {
+            this.Exception = Preconditions.CheckNotNull(exception, nameof(exception));
+        }
+
+        public Exception Exception { get; }
+
+        public HttpStatusCode HttpStatusCode { get; }
 
         public byte[] Data { get; }
 
