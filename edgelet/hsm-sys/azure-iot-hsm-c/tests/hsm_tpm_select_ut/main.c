4ee11c1d9ea7392d4c598e74c108b1b4f098e876
David Robson <darobs@microsoft.com>
2018-07-17 10:54:24 -0700

    Add tests for hsm_tpm_select (#31)
    * Added selection tests plus a few more.
    
    * Remove hsm_set_env from utils.
    
    * removed unused vars.
    
    * We're not getting openssl 1.0, we're getting 1.1
    
    * Better way to get libssl1.0.0

---
+++ edgelet/hsm-sys/azure-iot-hsm-c/tests/hsm_tpm_select_ut/main.c
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "testrunnerswitcher.h"
+
+int main(void)
+{
+    size_t failedTestCount = 0;
+    RUN_TEST_SUITE(hspm_tpm_select_ut, failedTestCount);
+    return failedTestCount;
+}
