a4c840393086b8f0ab90f3228c643712b4eb08f0
Raj Vengalil <rajave@microsoft.com>
2018-07-17 00:21:42 +0000

    Merged PR 957020: Add load generator module
    This code implements a module that can be used to generate telemetry and twin update loads on Edge Hub. It supports the following configuration options that can be set via environment variables (all of them are optional):
    - `messageFrequency` - Defines the frequency at which messages should be generated specified as a string that can be parsed into a `TimeSpan`.
    - `messageSizeInBytes` - Defines how big the data (which are randomly generated) in each telemetry message should be in bytes.
    - `jitterFactor` - Specifies the jitter in frequency, i.e., how much randomness to add with respect to the frequency of telemetry and twin updates. This must be a value between `0.0` and `1.0`.
    - `transportType` - Defines what transport to use. Can specify any of the values that the `TransportType` enumeration can be deserialized into.
    - `outputName` - The output to which events should be published.
    - `twinUpdateFrequency` - Defines the frequency at which twin updates should be sent.
    
    A typical message generated by this module would look like this:
    ```json
    {
      "sequenceNumber": 42,
      "data": [random byte array of length messageSizeInBytes],
      "hash": [SHA256 hash of 'data']
    }
    ```
    
    A typical twin reported property update would look like this:
    ```json
    {
      messagesSent: [count of messages sent so far]
    }
    ```

---
+++ edge-modules/load-gen/Settings.cs
@@ -0,0 +1,78 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+namespace LoadGen
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.IO;
+    using Microsoft.Azure.Devices.Client;
+    using Microsoft.Extensions.Configuration;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using Newtonsoft.Json.Serialization;
+
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class Settings
+    {
+        public TimeSpan MessageFrequency { get; }
+
+        public double JitterFactor { get; }
+
+        public TimeSpan TwinUpdateFrequency { get; }
+
+        public ulong MessageSizeInBytes { get; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public TransportType TransportType { get; }
+
+        public string OutputName { get; }
+
+        private static readonly Lazy<Settings> defaultSettings = new Lazy<Settings>(() =>
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("config/settings.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+
+            return new Settings(
+                configuration.GetValue("messageFrequency", TimeSpan.FromMilliseconds(20)),
+                configuration.GetValue<double>("jitterFactor", 0.5),
+                configuration.GetValue("twinUpdateFrequency", TimeSpan.FromMilliseconds(500)),
+                configuration.GetValue<ulong>("messageSizeInBytes", 1024),
+                configuration.GetValue<TransportType>("transportType", TransportType.Amqp_Tcp_Only),
+                configuration.GetValue<string>("outputName", "output1")
+            );
+        });
+
+        private Settings(
+            TimeSpan messageFrequency,
+            double jitterFactor,
+            TimeSpan twinUpdateFrequency,
+            ulong messageSizeInBytes,
+            TransportType transportType,
+            string outputName)
+        {
+            this.MessageFrequency = messageFrequency;
+            this.JitterFactor = jitterFactor;
+            this.TwinUpdateFrequency = twinUpdateFrequency;
+            this.MessageSizeInBytes = messageSizeInBytes;
+            this.TransportType = transportType;
+            this.OutputName = outputName;
+        }
+
+        public static Settings Current
+        {
+            get
+            {
+                return Settings.defaultSettings.Value;
+            }
+        }
+
+        public override String ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
