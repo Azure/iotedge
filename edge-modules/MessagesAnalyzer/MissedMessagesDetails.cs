10f12ba0d00b1e4dc91cc457625dab03a02351a1
Anca Antochi <ancan@microsoft.com>
2018-07-19 16:29:09 -0700

    Analyzer module

---
+++ edge-modules/MessagesAnalyzer/MissedMessagesDetails.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+namespace MessagesAnalyzer
+{
+    using System;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    class MissedMessagesDetails
+    {
+        public long MissedMessagesCount { get; }
+
+        public DateTime StartDateTime { get; }
+
+        public DateTime EndDateTime { get; }
+
+        public MissedMessagesDetails(long missedMessagesCount, DateTime startDateTime, DateTime endDateTime)
+        {
+            this.MissedMessagesCount = missedMessagesCount;
+            this.StartDateTime = startDateTime;
+            this.EndDateTime = endDateTime;
+        }
+    }
+}
--- scripts/linux/buildBranch.sh
+++ scripts/linux/buildBranch.sh
@@ -191,6 +191,7 @@ publish_app "Microsoft.Azure.Devices.Edge.Hub.Service"
 publish_app "SimulatedTemperatureSensor"
 publish_app "TemperatureFilter"
 publish_app "load-gen"
+publish_app "MessagesAnalyzer"
 
 publish_lib "Microsoft.Azure.Devices.Edge.Functions.Binding"
 
