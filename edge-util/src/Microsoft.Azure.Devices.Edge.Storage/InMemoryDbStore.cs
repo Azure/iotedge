9da5beb29a9452c346e6733f84309ab004ff4303
Varun Puranik <vpuranik@microsoft.com>
2018-08-03 02:03:53 -0700

    Add locking logic

--- edge-util/src/Microsoft.Azure.Devices.Edge.Storage/InMemoryDbStore.cs
+++ edge-util/src/Microsoft.Azure.Devices.Edge.Storage/InMemoryDbStore.cs
@@ -13,31 +13,34 @@ namespace Microsoft.Azure.Devices.Edge.Storage
     /// </summary>
     class InMemoryDbStore : IDbStore
     {
-        // Deliberately using list (instead of a synchronied list like Concurrent dictionary) to 
-        // better simulate acutal key-value stores like RocksDb.
+        // Using a list instead of a dictionary becaues the dictionary is not ordered
         readonly List<(byte[], byte[])> keyValues;
+        readonly object listLock = new object();
 
         public InMemoryDbStore()
         {
             this.keyValues = new List<(byte[], byte[])>();
         }
 
-        public Task<bool> Contains(byte[] key)
+        public async Task<bool> Contains(byte[] key)
         {
-            int index = this.GetIndex(key);
-            return Task.FromResult(index >= 0);
+            Option<byte[]> value = await this.Get(key);
+            return value.HasValue;
         }
 
         public Task<Option<byte[]>> Get(byte[] key)
         {
-            int index = this.GetIndex(key);
-            Option<byte[]> value = index >= 0 ? Option.Some(this.keyValues[index].Item2) : Option.None<byte[]>();
-            return Task.FromResult(value);
+            lock (this.listLock)
+            {
+                int index = this.GetIndex(key);
+                Option<byte[]> value = index >= 0 ? Option.Some(this.keyValues[index].Item2) : Option.None<byte[]>();
+                return Task.FromResult(value);
+            }
         }
 
         public Task IterateBatch(int batchSize, Func<byte[], byte[], Task> callback)
         {
-            int index = this.keyValues.Count > 0 ? 0 : -1;
+            int index = 0;
             return this.IterateBatch(index, batchSize, callback);
         }
 
@@ -51,10 +54,12 @@ namespace Microsoft.Azure.Devices.Edge.Storage
         {
             if (index >= 0)
             {
-                for (int i = index; i < index + batchSize && i < this.keyValues.Count; i++)
+                List<(byte[] key, byte[] value)> snapshot = this.GetSnapshot();
+
+                for (int i = index; i < index + batchSize && i < snapshot.Count; i++)
                 {
-                    var keyClone = this.keyValues[i].Item1.Clone() as byte[];
-                    var valueClone = this.keyValues[i].Item2.Clone() as byte[];
+                    var keyClone = snapshot[i].key.Clone() as byte[];
+                    var valueClone = snapshot[i].value.Clone() as byte[];
                     await callback(keyClone, valueClone);
                 }
             }
@@ -62,53 +67,74 @@ namespace Microsoft.Azure.Devices.Edge.Storage
 
         public Task<Option<(byte[] key, byte[] value)>> GetFirstEntry()
         {
-            Option<(byte[], byte[])> firstEntry = (this.keyValues.Count > 0) ? Option.Some(this.keyValues[0]) : Option.None<(byte[], byte[])>();
-            return Task.FromResult(firstEntry);
+            lock (this.listLock)
+            {
+                Option<(byte[], byte[])> firstEntry = this.keyValues.Count > 0 ? Option.Some(this.keyValues[0]) : Option.None<(byte[], byte[])>();
+                return Task.FromResult(firstEntry);
+            }
         }
 
         public Task<Option<(byte[] key, byte[] value)>> GetLastEntry()
         {
-            Option<(byte[], byte[])> lastEntry = (this.keyValues.Count > 0) ? Option.Some(this.keyValues[this.keyValues.Count - 1]) : Option.None<(byte[], byte[])>();
-            return Task.FromResult(lastEntry);
+            lock (this.listLock)
+            {
+                Option<(byte[], byte[])> lastEntry = (this.keyValues.Count > 0) ? Option.Some(this.keyValues[this.keyValues.Count - 1]) : Option.None<(byte[], byte[])>();
+                return Task.FromResult(lastEntry);
+            }
         }
 
         public Task Put(byte[] key, byte[] value)
         {
-            int index = this.GetIndex(key);
-            if (index < 0)
+            lock (this.listLock)
             {
-                this.keyValues.Add((key, value));
+                int index = this.GetIndex(key);
+                if (index < 0)
+                {
+                    this.keyValues.Add((key, value));
+                }
+                else
+                {
+                    this.keyValues[index] = (key, value);
+                }
+                return Task.CompletedTask;
             }
-            else
-            {
-                this.keyValues[index] = (key, value);
-            }
-            return Task.CompletedTask;
         }
 
         public Task Remove(byte[] key)
         {
-            int index = this.GetIndex(key);
-            if (index >= 0)
+            lock (this.listLock)
             {
-                this.keyValues.RemoveAt(index);
+                int index = this.GetIndex(key);
+                if (index >= 0)
+                {
+                    this.keyValues.RemoveAt(index);
+                }
+                return Task.CompletedTask;
             }
-            return Task.CompletedTask;
         }
 
         internal int GetIndex(byte[] key)
         {
-            // Not locking here, to get similar behavior as the RocksDb based implementation.
-            // The application code should to ensure that locking is not required.
-            for (int i = 0; i < this.keyValues.Count; i++)
+            lock (this.listLock)
             {
-                (byte[] key, byte[] value) kv = this.keyValues[i];
-                if (key.SequenceEqual(kv.key))
+                for (int i = 0; i < this.keyValues.Count; i++)
                 {
-                    return i;
+                    (byte[] key, byte[] value) kv = this.keyValues[i];
+                    if (key.SequenceEqual(kv.key))
+                    {
+                        return i;
+                    }
                 }
             }
             return -1;
+        }
+
+        List<(byte[], byte[])> GetSnapshot()
+        {
+            lock (this.listLock)
+            {
+                return new List<(byte[], byte[])>(this.keyValues);
+            }
         }
 
         public void Dispose()
