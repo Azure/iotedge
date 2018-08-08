936e3b1cb778d7ad325976fe6cc47d2c047a80f4
Mike Yagley <myagley@gmail.com>
2018-08-08 15:27:23 +0000

    Updates to the arm dockerfile

--- edgelet/build/linux/docker/centos/build_arm_toolchain_container.sh
+++ edgelet/build/linux/docker/centos/build_arm_toolchain_container.sh
@@ -8,21 +8,21 @@ main() {
     local build_tools=gcc-linaro-${version}-x86_64_arm-linux-gnueabihf
     local target_triple=$1
 
-    if [[ ! -f Dockerfile_${target_triple} ]]
+    if [[ ! -f Dockerfile.${target_triple} ]]
     then
         echo "Expected Dockerfile in current directory."
         return 1
     fi
 
-    # extract Linaro toolchain 
+    # extract Linaro toolchain
     curl -L -o ${build_tools}.tar.xz https://releases.linaro.org/components/toolchain/binaries/latest/arm-linux-gnueabihf/${build_tools}.tar.xz 
     xzcat ${build_tools}.tar.xz | \
         tar -xvf -
-    
+
     rm ${build_tools}.tar.xz
 
     # Once toolchain is downloaded and extracted, build docker image with toolchain
-    docker build -f Dockerfile_${target_triple} --build-arg TOOLCHAIN=${build_tools} --tag ${build_tools}:centos_0.1 .
+    docker build -f Dockerfile.${target_triple} --build-arg TOOLCHAIN=${build_tools} --tag ${build_tools}:centos_7.5-1 .
 
     # cleanup
     rm -fr gcc-linaro-${version}-x86_64_arm-linux-gnueabihf
