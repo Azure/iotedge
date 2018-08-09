df54fb270bcc3646156b1622f4fd49f3f5facac2
Mike Yagley <myagley@gmail.com>
2018-08-09 21:04:30 +0000

    Fix packages

--- edgelet/build/linux/centos/arm32v7/package-iotedged.sh
+++ edgelet/build/linux/centos/arm32v7/package-iotedged.sh
@@ -5,20 +5,20 @@ set -e
 # Get directory of running script
 DIR=$(cd "$(dirname "$0")" && pwd)
 
-BUILD_REPOSITORY_LOCALPATH=${BUILD_REPOSITORY_LOCALPATH:-$DIR/../../..}
+BUILD_REPOSITORY_LOCALPATH=${BUILD_REPOSITORY_LOCALPATH:-$DIR/../../../../..}
 PROJECT_ROOT=${BUILD_REPOSITORY_LOCALPATH}/edgelet
 BUILD_DIR_REL="target/armv7-unknown-linux-gnueabihf/release"
 BUILD_DIR="$PROJECT_ROOT/$BUILD_DIR_REL"
 
-CARGO_HOME="$HOME/.cargo/"
-RUSTUP_HOME="$HOME/.rustup"
-IMAGE="gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf:centos_7.5-1"
+CARGO_HOME=${CARGO_HOME:-"$HOME/.cargo/"}
+RUSTUP_HOME=${RUSTUP_HOME:-"$HOME/.rustup"}
+IMAGE="edgebuilds.azurecr.io/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf:centos_7.5-1"
 
 REVISION=${REVISION:-1}
 DEFAULT_VERSION=$(cat $PROJECT_ROOT/version.txt)
 VERSION="${VERSION:-$DEFAULT_VERSION}"
 
-# docker pull "$IMAGE"
+docker pull "$IMAGE"
 
 run_command()
 {
@@ -38,16 +38,22 @@ run_command()
       sh -c "$1"
 }
 
+# Ensure the armv7 toolchain is installed
+rustup target add armv7-unknown-linux-gnueabihf
+rustup component add rust-src
+
 mkdir -p $BUILD_DIR
-cd $PROJECT_ROOT && make dist TARGET=$BUILD_DIR_REL VERSION=${VERSION} REVISION=${REVISION} CARGOFLAGS="--target armv7-unknown-linux-gnueabihf"
+cd $PROJECT_ROOT && make dist TARGET=$BUILD_DIR_REL VERSION=${VERSION} REVISION=${REVISION}
 
 COMMAND="
-echo '%_topdir /${BUILD_DIR_REL}/rpmbuild' > /root/.rpmmacros && \
 mkdir -p /${BUILD_DIR_REL}/rpmbuild && \
 cd /${BUILD_DIR_REL}/rpmbuild && \
 mkdir -p RPMS SOURCES SPECS SRPMS BUILD && \
-rustup target add armv7-unknown-linux-gnueabihf && \
-rustup component add rust-src && \
 cd /project/edgelet && \
-make rpm rpmbuilddir=/${BUILD_DIR_REL}/rpmbuild TARGET=/${BUILD_DIR_REL} VERSION=${VERSION} REVISION=${REVISION} CARGOFLAGS=\"--manifest-path=/project/edgelet/Cargo.toml --target armv7-unknown-linux-gnueabihf --all\""
+make rpm rpmbuilddir=/${BUILD_DIR_REL}/rpmbuild \
+         TARGET=/${BUILD_DIR_REL} \
+         VERSION=${VERSION} \
+         REVISION=${REVISION} \
+         CARGOFLAGS=\"--manifest-path=/project/edgelet/Cargo.toml --target armv7-unknown-linux-gnueabihf --all\" \
+         RPMBUILDFLAGS='-v -bb --clean --target armv7hl --define \"_topdir /${BUILD_DIR_REL}/rpmbuild\"'"
 run_command "$COMMAND"
---
+++ edgelet/build/linux/centos/arm32v7/package-libiothsm.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+# Get directory of running script
+DIR=$(cd "$(dirname "$0")" && pwd)
+
+BUILD_REPOSITORY_LOCALPATH=${BUILD_REPOSITORY_LOCALPATH:-$DIR/../../../../..}
+PROJECT_ROOT=${BUILD_REPOSITORY_LOCALPATH}/edgelet
+
+BUILD_DIR=$PROJECT_ROOT/target/hsm/build
+IMAGE="edgebuilds.azurecr.io/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf:centos_7.5-1"
+
+PACKAGE_NAME="libiothsm-std"
+REVISION=${REVISION:-1}
+DEFAULT_VERSION=$(cat $PROJECT_ROOT/version.txt)
+VERSION="${VERSION:-$DEFAULT_VERSION}"
+
+# Converts debian versioning to rpm version
+# deb 1.0.1~dev100 ~> rpm 1.0.1-0.1.dev100
+# RPM_VERSION?=$(word 1,$(subst ~, , $(VERSION)))
+# _release=$(or $(and $(word 2,$(subst ~, ,$1)),0.${REVISION}.$(word 2,$(subst ~, ,$1))),${REVISION})
+# RPM_RELEASE?=$(call _release, ${VERSION})
+#
+RPM_VERSION=`echo "$VERSION" | cut -d"~" -f1`
+RPM_TAG=`echo "$VERSION" | cut -s -d"~" -f2`
+if [[ ! -z ${RPM_TAG} ]]; then
+    RPM_RELEASE="0.${REVISION}.${RPM_TAG}"
+else
+    RPM_RELEASE="${REVISION}"
+fi
+
+docker pull "$IMAGE"
+
+run_command()
+{
+    echo "$1"
+    docker \
+      run \
+      --rm \
+      -e "USER=root" \
+      -e "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/toolchain/arm-linux-gnueabihf/bin:/cargo/bin" \
+      -v "$PROJECT_ROOT/target:/target" \
+      -v "$PROJECT_ROOT:/project" \
+      -i "$IMAGE" \
+      sh -c "$1"
+}
+
+mkdir -p $BUILD_DIR
+run_command "cd /target/hsm/build && cmake -DCMAKE_SYSROOT=/toolchain/arm-linux-gnueabihf/libc -DCMAKE_C_COMPILER=/toolchain/bin/arm-linux-gnueabihf-gcc -DCMAKE_CXX_COMPILER=/toolchain/bin/arm-linux-gnueabihf-g++ -DCMAKE_SYSTEM_NAME=Linux -DCPACK_RPM_PACKAGE_ARCHITECTURE=armv7hl -DCPACK_PACKAGE_VERSION=\"$RPM_VERSION\" -DCPACK_RPM_PACKAGE_RELEASE=\"$RPM_RELEASE\" -DBUILD_SHARED=On -Drun_unittests=Off -Duse_emulator=Off -DCMAKE_BUILD_TYPE=Release -Duse_default_uuid=On -DCPACK_GENERATOR=RPM /project/hsm-sys/azure-iot-hsm-c/"
+
+run_command "cd /target/hsm/build && make package"
