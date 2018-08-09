7c24afcca8b31b73c90cf74de1c0372ea864c80f
David Robson <darobs@microsoft.com>
2018-08-09 15:33:17 -0700

    Here are containers that build Centos 7.5 for x86_64 and ARMv7  (#103)
    * Add CentOS image builders
    
    * cargo build and cargo test work in the container created by build_arm_toolchain_container.sh
    
    - Build the container by running:
    
    build_arm_toolchain_container.sh armv7-unknown-linux-gnueabi
    
    - Run the container and install Rust and the appropriate toolchains:
    
    curl https://sh.rustup.rs -sSf | sh
    source $HOME/.cargo/env
    rustup target add armv7-unknown-linux-gnueabihf
    rustup component add rust-src
    
    - get & build edgelet:
    
    git clone --recursive https://github.com/Azure/iotedge
    cd iotedge/edgelet
    cargo build --target  armv7-unknown-linux-gnueabihf --all
    cargo test  --target  armv7-unknown-linux-gnueabihf --all
    
    * Updates to the arm dockerfile

---
+++ edgelet/build/linux/docker/centos/openssl.sh
@@ -0,0 +1,56 @@
+# Adapted from https://github.com/japaric/cross
+set -ex
+
+main() {
+    local SHLIB_VERSION_NUMBER=1.0.2 
+    local version=${SHLIB_VERSION_NUMBER}m
+    local os=$1 \
+    local triple=$2
+    local sysroot=$3
+
+    local dependencies=(
+        ca-certificates
+        curl
+        m4
+        make
+        perl
+    )
+
+    # NOTE cross toolchain must be already installed
+    local purge_list=()
+    for dep in ${dependencies[@]}; do
+        if ! yum list -q installed $dep; then
+            yum install -y $dep
+            purge_list+=( $dep )
+        fi
+    done
+
+    td=$(mktemp -d)
+
+    pushd $td
+    curl https://www.openssl.org/source/openssl-$version.tar.gz | \
+        tar --strip-components=1 -xz
+    AR=${triple}ar CC=${triple}gcc ./Configure \
+      --prefix=${sysroot}/usr \
+      --openssldir=${sysroot}/usr \
+      shared \
+      no-asm \
+      $os \
+      -fPIC \
+      ${@:4}
+    make -j$(nproc)
+    make  install_sw
+    
+    # clean up
+
+    popd
+
+    if [ ${#purge_list[@]} -ne 0 ]; then
+        yum remove -y ${purge_list[@]}
+    fi
+
+    rm -rf $td
+    rm $0
+}
+
+main "${@}"
