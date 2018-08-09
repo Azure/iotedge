3f97902fe5ac6f13d9c85b2ac8a7b91ddc992894
Mike Yagley <myagley@gmail.com>
2018-08-09 15:27:10 +0000

    Update for arm32

---
+++ edgelet/build/linux/package-arm-centos-iotedged.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+# Get directory of running script
+DIR=$(cd "$(dirname "$0")" && pwd)
+
+BUILD_REPOSITORY_LOCALPATH=${BUILD_REPOSITORY_LOCALPATH:-$DIR/../../..}
+PROJECT_ROOT=${BUILD_REPOSITORY_LOCALPATH}/edgelet
+BUILD_DIR_REL="target/armv7-unknown-linux-gnueabihf/release"
+BUILD_DIR="$PROJECT_ROOT/$BUILD_DIR_REL"
+
+CARGO_HOME="$HOME/.cargo/"
+RUSTUP_HOME="$HOME/.rustup"
+IMAGE="gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf:centos_7.5-1"
+
+REVISION=${REVISION:-1}
+DEFAULT_VERSION=$(cat $PROJECT_ROOT/version.txt)
+VERSION="${VERSION:-$DEFAULT_VERSION}"
+
+# docker pull "$IMAGE"
+
+run_command()
+{
+    echo "$1"
+    docker \
+      run \
+      -it \
+      --rm \
+      -e "USER=root" \
+      -e "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/toolchain/arm-linux-gnueabihf/bin:/toolchain/bin:/cargo/bin" \
+      -e "RUSTUP_HOME=/rustup" \
+      -v "$PROJECT_ROOT/target:/target" \
+      -v "$BUILD_REPOSITORY_LOCALPATH:/project" \
+      -v "$CARGO_HOME:/cargo" \
+      -v "$RUSTUP_HOME:/rustup" \
+      -i "$IMAGE" \
+      sh -c "$1"
+}
+
+mkdir -p $BUILD_DIR
+cd $PROJECT_ROOT && make dist TARGET=$BUILD_DIR_REL VERSION=${VERSION} REVISION=${REVISION} CARGOFLAGS="--target armv7-unknown-linux-gnueabihf"
+
+COMMAND="
+echo '%_topdir /${BUILD_DIR_REL}/rpmbuild' > /root/.rpmmacros && \
+mkdir -p /${BUILD_DIR_REL}/rpmbuild && \
+cd /${BUILD_DIR_REL}/rpmbuild && \
+mkdir -p RPMS SOURCES SPECS SRPMS BUILD && \
+rustup target add armv7-unknown-linux-gnueabihf && \
+rustup component add rust-src && \
+cd /project/edgelet && \
+make rpm rpmbuilddir=/${BUILD_DIR_REL}/rpmbuild TARGET=/${BUILD_DIR_REL} VERSION=${VERSION} REVISION=${REVISION} CARGOFLAGS=\"--manifest-path=/project/edgelet/Cargo.toml --target armv7-unknown-linux-gnueabihf --all\""
+run_command "$COMMAND"
