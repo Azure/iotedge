497cfd4a7d43a505fd7a72c0b20f505a3eef6dd4
Mike Yagley <myagley@gmail.com>
2018-08-09 23:19:28 +0000

    Add amd64 centos scripts

---
+++ edgelet/build/linux/centos/amd64/package-iotedged.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+# Get directory of running script
+DIR=$(cd "$(dirname "$0")" && pwd)
+
+BUILD_REPOSITORY_LOCALPATH=${BUILD_REPOSITORY_LOCALPATH:-$DIR/../../../../..}
+PROJECT_ROOT=${BUILD_REPOSITORY_LOCALPATH}/edgelet
+BUILD_DIR_REL="target/release"
+BUILD_DIR="$PROJECT_ROOT/$BUILD_DIR_REL"
+
+CARGO_HOME=${CARGO_HOME:-"$HOME/.cargo/"}
+RUSTUP_HOME=${RUSTUP_HOME:-"$HOME/.rustup"}
+IMAGE="edgebuilds.azurecr.io/centos-build:7.5-1"
+
+REVISION=${REVISION:-1}
+DEFAULT_VERSION=$(cat $PROJECT_ROOT/version.txt)
+VERSION="${VERSION:-$DEFAULT_VERSION}"
+
+docker pull "$IMAGE"
+
+run_command()
+{
+    echo "$1"
+    docker \
+      run \
+      --rm \
+      -e "USER=root" \
+      -e "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/cargo/bin" \
+      -e "RUSTUP_HOME=/rustup" \
+      -v "$PROJECT_ROOT/target:/target" \
+      -v "$BUILD_REPOSITORY_LOCALPATH:/project" \
+      -v "$CARGO_HOME:/cargo" \
+      -v "$RUSTUP_HOME:/rustup" \
+      -i "$IMAGE" \
+      sh -c "$1"
+}
+
+mkdir -p $BUILD_DIR
+cd $PROJECT_ROOT && make dist TARGET=$BUILD_DIR_REL VERSION=${VERSION} REVISION=${REVISION}
+
+COMMAND="
+mkdir -p /${BUILD_DIR_REL}/rpmbuild && \
+cd /${BUILD_DIR_REL}/rpmbuild && \
+mkdir -p RPMS SOURCES SPECS SRPMS BUILD && \
+cd /project/edgelet && \
+make rpm rpmbuilddir=/${BUILD_DIR_REL}/rpmbuild \
+         TARGET=/${BUILD_DIR_REL} \
+         VERSION=${VERSION} \
+         REVISION=${REVISION} \
+         RPMBUILDFLAGS='-v -bb --clean --define \"_topdir /${BUILD_DIR_REL}/rpmbuild\"'"
+run_command "$COMMAND"
--- edgelet/build/linux/centos/amd64/package-libiothsm.sh
+++ edgelet/build/linux/centos/amd64/package-libiothsm.sh
@@ -8,9 +8,9 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 BUILD_REPOSITORY_LOCALPATH=${BUILD_REPOSITORY_LOCALPATH:-$DIR/../../../../..}
 PROJECT_ROOT=${BUILD_REPOSITORY_LOCALPATH}/edgelet
 
-BUILD_DIR_REL="target/hsm/build/arm32v7"
+BUILD_DIR_REL="target/hsm/build/amd64"
 BUILD_DIR="$PROJECT_ROOT/$BUILD_DIR_REL"
-IMAGE="edgebuilds.azurecr.io/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf:centos_7.5-1"
+IMAGE="edgebuilds.azurecr.io/centos-build:7.5-1"
 
 PACKAGE_NAME="libiothsm-std"
 REVISION=${REVISION:-1}
@@ -45,6 +45,6 @@ run_command()
 }
 
 mkdir -p $BUILD_DIR
-run_command "cd /$BUILD_DIR_REL && cmake -DCMAKE_SYSROOT=/toolchain/arm-linux-gnueabihf/libc -DCMAKE_C_COMPILER=/toolchain/bin/arm-linux-gnueabihf-gcc -DCMAKE_CXX_COMPILER=/toolchain/bin/arm-linux-gnueabihf-g++ -DCMAKE_SYSTEM_NAME=Linux -DCPACK_RPM_PACKAGE_ARCHITECTURE=armv7hl -DCPACK_PACKAGE_VERSION=\"$RPM_VERSION\" -DCPACK_RPM_PACKAGE_RELEASE=\"$RPM_RELEASE\" -DBUILD_SHARED=On -Drun_unittests=Off -Duse_emulator=Off -DCMAKE_BUILD_TYPE=Release -Duse_default_uuid=On -DCPACK_GENERATOR=RPM /project/hsm-sys/azure-iot-hsm-c/"
+run_command "cd /$BUILD_DIR_REL && cmake -DCPACK_PACKAGE_VERSION=\"$RPM_VERSION\" -DCPACK_RPM_PACKAGE_RELEASE=\"$RPM_RELEASE\" -DBUILD_SHARED=On -Drun_unittests=Off -Duse_emulator=Off -DCMAKE_BUILD_TYPE=Release -Duse_default_uuid=On -DCPACK_GENERATOR=RPM /project/hsm-sys/azure-iot-hsm-c/"
 
 run_command "cd /$BUILD_DIR_REL && make package"
