93f13b885977c72ead1671d089e6633d4636650b
Varun Puranik <vpuranik@microsoft.com>
2018-08-17 18:14:34 -0700

    Change SendSettleMode for amqp Events Link to Unsettled (#167)
    * Fix SendSettleMode for amqp Events Link
    
    * Fix test
    
    * Add correct error message
    
    * Choose right Settle modes for each link
    
    * Fix test and build
    
    * Fix test

--- edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Core.Test/routing/RoutingEdgeHubTest.cs
+++ edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Core.Test/routing/RoutingEdgeHubTest.cs
@@ -105,18 +105,18 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test.Routing
 
             var routingEdgeHub = new RoutingEdgeHub(router, messageConverter, connectionManager, twinManager, "testEdgeDevice", Mock.Of<IInvokeMethodHandler>());
 
-            await Assert.ThrowsAsync<InvalidOperationException>(() => routingEdgeHub.ProcessDeviceMessage(identity.Object, badMessage));
+            await Assert.ThrowsAsync<EdgeHubMessageTooLargeException>(() => routingEdgeHub.ProcessDeviceMessage(identity.Object, badMessage));
 
             string badString = System.Text.Encoding.UTF8.GetString(new byte[300 * 1024], 0, 300 * 1024);
             var badProperties = new Dictionary<string, string> { ["toolong"] = badString };
 
             badMessage = new Message.Builder(new byte[1]).SetProperties(badProperties).Build();
 
-            await Assert.ThrowsAsync<InvalidOperationException>(() => routingEdgeHub.ProcessDeviceMessage(identity.Object, badMessage));
+            await Assert.ThrowsAsync<EdgeHubMessageTooLargeException>(() => routingEdgeHub.ProcessDeviceMessage(identity.Object, badMessage));
 
             badMessage = new Message(new byte[1], new Dictionary<string, string>(), badProperties);
 
-            await Assert.ThrowsAsync<InvalidOperationException>(() => routingEdgeHub.ProcessDeviceMessage(identity.Object, badMessage));
+            await Assert.ThrowsAsync<EdgeHubMessageTooLargeException>(() => routingEdgeHub.ProcessDeviceMessage(identity.Object, badMessage));
         }
 
         [Fact]
