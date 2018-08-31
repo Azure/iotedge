ca5af7c6811fb87a6dd1e9c507e3abd00db6989f
Damon Barry <damonb@microsoft.com>
2018-08-30 18:59:59 -0700

    Abstract Hyper::client away from MaybeProxyClient
    - create an infrastructure wrapper for Hyper::client called hyperwrap::Client
    - make hyperwrap::Client nullable for testing code that depends on it, by creating a Null enum variant that stubs the Service trait
    - write unit tests for the hyperwrap::Client factory functions
    - change MaybeProxyClient to use hyperwrap::Client

--- edgelet/edgelet-http/src/util/proxy.rs
+++ edgelet/edgelet-http/src/util/proxy.rs
@@ -1,47 +1,34 @@
 // Copyright (c) Microsoft. All rights reserved.
 
+use super::hyperwrap::Client;
 use error::Error;
-use hyper::client::{FutureResponse, HttpConnector, Service};
-use hyper::{Client as HyperClient, Error as HyperError, Request, Response, Uri};
-use hyper_proxy::{Proxy, ProxyConnector, Intercept};
-use hyper_tls::HttpsConnector;
+use hyper::client::Service;
+use hyper::Uri;
 use tokio_core::reactor::Handle;
 
-const DNS_WORKER_THREADS: usize = 4;
-
 #[derive(Clone)]
-pub enum MaybeProxyClient {
-    NoProxy(HyperClient<HttpsConnector<HttpConnector>>),
-    Proxy(HyperClient<ProxyConnector<HttpsConnector<HttpConnector>>>),
+pub struct MaybeProxyClient {
+    client: Client,
 }
 
 impl MaybeProxyClient {
     pub fn new(handle: &Handle, proxy_uri: Option<Uri>) -> Result<MaybeProxyClient, Error> {
-        let https = HttpsConnector::new(DNS_WORKER_THREADS, &handle.clone())?;
-        match proxy_uri {
-            None => {
-                Ok(MaybeProxyClient::NoProxy(HyperClient::configure().connector(https).build(&handle)))
+        Ok(MaybeProxyClient {
+            client: match proxy_uri {
+                None => Client::new(handle)?,
+                Some(uri) => Client::new_with_proxy(handle, uri)?,
             },
-            Some(uri) => {
-                let proxy = Proxy::new(Intercept::All, uri);
-                let connector = ProxyConnector::from_proxy(https, proxy)?;
-                Ok(MaybeProxyClient::Proxy(HyperClient::configure().connector(connector).build(&handle)))
-            }
-        }
+        })
     }
 }
 
 impl Service for MaybeProxyClient {
-    type Request = Request;
-    type Response = Response;
-    type Error = HyperError;
-    type Future = FutureResponse;
+    type Request = <Client as Service>::Request;
+    type Response = <Client as Service>::Response;
+    type Error = <Client as Service>::Error;
+    type Future = <Client as Service>::Future;
 
     fn call(&self, req: Self::Request) -> Self::Future {
-        match *self {
-            MaybeProxyClient::NoProxy(ref client) => client.call(req) as Self::Future,
-            MaybeProxyClient::Proxy(ref client) => client.call(req) as Self::Future,
-        }
+        self.client.call(req)
     }
 }
-
