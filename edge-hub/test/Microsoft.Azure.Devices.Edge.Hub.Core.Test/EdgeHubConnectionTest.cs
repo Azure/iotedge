0e7e8e615a272e2cdb151128b25971638bf73f10
Varun Puranik <vpuranik@microsoft.com>
2018-09-05 16:32:13 -0700

    Add support for invoking method to refresh Device Scope identities cache

--- edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Core.Test/EdgeHubConnectionTest.cs
+++ edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Core.Test/EdgeHubConnectionTest.cs
@@ -1,10 +1,21 @@
 // Copyright (c) Microsoft. All rights reserved.
-namespace Microsoft.Azure.Devices.Edge.Hub.E2E.Test
+namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test
 {
     using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Edge.Hub.Core;
+    using Microsoft.Azure.Devices.Edge.Hub.Core.Device;
+    using Microsoft.Azure.Devices.Edge.Hub.Core.Routing;
+    using Microsoft.Azure.Devices.Edge.Storage;
+    using Microsoft.Azure.Devices.Edge.Util;
     using Microsoft.Azure.Devices.Edge.Util.Test.Common;
+    using Microsoft.Azure.Devices.Routing.Core;
+    using Microsoft.Azure.Devices.Shared;
+    using Moq;
     using Xunit;
+    using Constants = Microsoft.Azure.Devices.Edge.Hub.Core.Constants;
 
     [Unit]
     public class EdgeHubConnectionTest
@@ -30,6 +41,139 @@ namespace Microsoft.Azure.Devices.Edge.Hub.E2E.Test
             {
                 EdgeHubConnection.ValidateSchemaVersion(schemaVersion);
             }
+        }
+
+        [Fact]
+        public async Task HandleMethodInvocationTest()
+        {
+            // Arrange
+            var edgeHubIdentity = Mock.Of<IModuleIdentity>();
+            var twinManager = Mock.Of<ITwinManager>();
+            var routeFactory = new EdgeRouteFactory(Mock.Of<IEndpointFactory>());
+            var twinCollectionMessageConverter = Mock.Of<Core.IMessageConverter<TwinCollection>>();
+            var twinMessageConverter = Mock.Of<Core.IMessageConverter<Twin>>();
+            var versionInfo = new VersionInfo("1.0", "1", "123");
+            var deviceScopeIdentitiesCache = new Mock<IDeviceScopeIdentitiesCache>();
+            deviceScopeIdentitiesCache.Setup(d => d.RefreshServiceIdentities(It.IsAny<IEnumerable<string>>())).Returns(Task.CompletedTask);
+            var edgeHubConnection = new EdgeHubConnection(edgeHubIdentity, twinManager, routeFactory, twinCollectionMessageConverter, twinMessageConverter, versionInfo, deviceScopeIdentitiesCache.Object);
+
+            string correlationId = Guid.NewGuid().ToString();
+            var requestPayload = new
+            {
+                deviceIds = new[]
+                {
+                    "d1",
+                    "d2"
+                }
+            };
+            byte[] requestBytes = requestPayload.ToBytes();
+            var directMethodRequest = new DirectMethodRequest(correlationId, Constants.ServiceIdentityRefreshMethodName, requestBytes, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
+
+            // Act
+            DirectMethodResponse directMethodResponse = await edgeHubConnection.HandleMethodInvocation(directMethodRequest);
+
+            // Assert
+            Assert.NotNull(directMethodResponse);
+            Assert.Equal(HttpStatusCode.OK, directMethodResponse.HttpStatusCode);
+        }
+
+        [Fact]
+        public async Task HandleMethodInvocationBadInputTest()
+        {
+            // Arrange
+            var edgeHubIdentity = Mock.Of<IModuleIdentity>();
+            var twinManager = Mock.Of<ITwinManager>();
+            var routeFactory = new EdgeRouteFactory(Mock.Of<IEndpointFactory>());
+            var twinCollectionMessageConverter = Mock.Of<Core.IMessageConverter<TwinCollection>>();
+            var twinMessageConverter = Mock.Of<Core.IMessageConverter<Twin>>();
+            var versionInfo = new VersionInfo("1.0", "1", "123");
+            var deviceScopeIdentitiesCache = new Mock<IDeviceScopeIdentitiesCache>();
+            deviceScopeIdentitiesCache.Setup(d => d.RefreshServiceIdentities(It.IsAny<IEnumerable<string>>())).Returns(Task.CompletedTask);
+            var edgeHubConnection = new EdgeHubConnection(edgeHubIdentity, twinManager, routeFactory, twinCollectionMessageConverter, twinMessageConverter, versionInfo, deviceScopeIdentitiesCache.Object);
+
+            string correlationId = Guid.NewGuid().ToString();
+            var requestPayload = new []
+            {
+                "d1",
+                "d2"
+            };
+            byte[] requestBytes = requestPayload.ToBytes();
+            var directMethodRequest = new DirectMethodRequest(correlationId, Constants.ServiceIdentityRefreshMethodName, requestBytes, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
+
+            // Act
+            DirectMethodResponse directMethodResponse = await edgeHubConnection.HandleMethodInvocation(directMethodRequest);
+
+            // Assert
+            Assert.NotNull(directMethodResponse);
+            Assert.Equal(HttpStatusCode.BadRequest, directMethodResponse.HttpStatusCode);
+        }
+
+        [Fact]
+        public async Task HandleMethodInvocationServerErrorTest()
+        {
+            // Arrange
+            var edgeHubIdentity = Mock.Of<IModuleIdentity>();
+            var twinManager = Mock.Of<ITwinManager>();
+            var routeFactory = new EdgeRouteFactory(Mock.Of<IEndpointFactory>());
+            var twinCollectionMessageConverter = Mock.Of<Core.IMessageConverter<TwinCollection>>();
+            var twinMessageConverter = Mock.Of<Core.IMessageConverter<Twin>>();
+            var versionInfo = new VersionInfo("1.0", "1", "123");
+            var deviceScopeIdentitiesCache = new Mock<IDeviceScopeIdentitiesCache>();
+            deviceScopeIdentitiesCache.Setup(d => d.RefreshServiceIdentities(It.IsAny<IEnumerable<string>>())).ThrowsAsync(new Exception("Foo"));
+            var edgeHubConnection = new EdgeHubConnection(edgeHubIdentity, twinManager, routeFactory, twinCollectionMessageConverter, twinMessageConverter, versionInfo, deviceScopeIdentitiesCache.Object);
+
+            string correlationId = Guid.NewGuid().ToString();
+            var requestPayload = new
+            {
+                deviceIds = new[]
+                {
+                    "d1",
+                    "d2"
+                }
+            };
+            byte[] requestBytes = requestPayload.ToBytes();
+            var directMethodRequest = new DirectMethodRequest(correlationId, Constants.ServiceIdentityRefreshMethodName, requestBytes, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
+
+            // Act
+            DirectMethodResponse directMethodResponse = await edgeHubConnection.HandleMethodInvocation(directMethodRequest);
+
+            // Assert
+            Assert.NotNull(directMethodResponse);
+            Assert.Equal(HttpStatusCode.InternalServerError, directMethodResponse.HttpStatusCode);
+        }
+
+        [Fact]
+        public async Task HandleMethodInvocationInvalidMethodNameTest()
+        {
+            // Arrange
+            var edgeHubIdentity = Mock.Of<IModuleIdentity>();
+            var twinManager = Mock.Of<ITwinManager>();
+            var routeFactory = new EdgeRouteFactory(Mock.Of<IEndpointFactory>());
+            var twinCollectionMessageConverter = Mock.Of<Core.IMessageConverter<TwinCollection>>();
+            var twinMessageConverter = Mock.Of<Core.IMessageConverter<Twin>>();
+            var versionInfo = new VersionInfo("1.0", "1", "123");
+            var deviceScopeIdentitiesCache = new Mock<IDeviceScopeIdentitiesCache>();
+            deviceScopeIdentitiesCache.Setup(d => d.RefreshServiceIdentities(It.IsAny<IEnumerable<string>>())).Returns(Task.CompletedTask);
+            var edgeHubConnection = new EdgeHubConnection(edgeHubIdentity, twinManager, routeFactory, twinCollectionMessageConverter, twinMessageConverter, versionInfo, deviceScopeIdentitiesCache.Object);
+
+            string correlationId = Guid.NewGuid().ToString();
+            var requestPayload = new
+            {
+                deviceIds = new[]
+                {
+                    "d1",
+                    "d2"
+                }
+            };
+            byte[] requestBytes = requestPayload.ToBytes();
+            var directMethodRequest = new DirectMethodRequest(correlationId, "ping", requestBytes, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
+
+            // Act
+            DirectMethodResponse directMethodResponse = await edgeHubConnection.HandleMethodInvocation(directMethodRequest);
+
+            // Assert
+            Assert.NotNull(directMethodResponse);
+            Assert.Equal(HttpStatusCode.NotFound, directMethodResponse.HttpStatusCode);
         }
     }
 }
