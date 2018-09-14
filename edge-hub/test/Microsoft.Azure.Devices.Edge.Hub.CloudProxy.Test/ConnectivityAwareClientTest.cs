77c08b0053a2066c4fcbcd0aaf9fdbaf4ce5b5c1
Varun Puranik <vpuranik@microsoft.com>
2018-09-13 18:34:31 -0700

    Fix Offline startup (#283)

--- edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.CloudProxy.Test/ConnectivityAwareClientTest.cs
+++ edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.CloudProxy.Test/ConnectivityAwareClientTest.cs
@@ -46,21 +46,50 @@ namespace Microsoft.Azure.Devices.Edge.Hub.CloudProxy.Test
         }
 
         [Theory]
-        [InlineData(typeof(ConnectTimeoutException), typeof(TimeoutException))]
-        [InlineData(typeof(TimeoutException), typeof(TimeoutException))]
-        [InlineData(typeof(IotHubException), typeof(IotHubException))]
-        [InlineData(typeof(InvalidOperationException), typeof(InvalidOperationException))]
-        public async Task TestExceptionTest(Type thrownException, Type expectedException)
+        [InlineData(typeof(ConnectTimeoutException), typeof(TimeoutException), true)]
+        [InlineData(typeof(TimeoutException), typeof(TimeoutException), true)]
+        [InlineData(typeof(IotHubException), typeof(IotHubException), false)]
+        [InlineData(typeof(InvalidOperationException), typeof(InvalidOperationException), false)]
+        public async Task TestExceptionTest(Type thrownException, Type expectedException, bool isTimeout)
         {
             // Arrange
             var client = new Mock<IClient>();
             client.Setup(c => c.SendEventAsync(It.IsAny<Message>())).ThrowsAsync(Activator.CreateInstance(thrownException, "msg str") as Exception);
-            var deviceConnectivityManager = new DeviceConnectivityManager();
-            var connectivityAwareClient = new ConnectivityAwareClient(client.Object, deviceConnectivityManager);
+            var deviceConnectivityManager = new Mock<IDeviceConnectivityManager>();
+            deviceConnectivityManager.Setup(d => d.CallTimedOut());
+            var connectivityAwareClient = new ConnectivityAwareClient(client.Object, deviceConnectivityManager.Object);
             var message = new Message();
 
             // Act / Assert
             await Assert.ThrowsAsync(expectedException, () => connectivityAwareClient.SendEventAsync(message));
+            if (isTimeout)
+            {
+                deviceConnectivityManager.Verify(d => d.CallTimedOut(), Times.Once);
+            }
+            else
+            {
+                deviceConnectivityManager.Verify(d => d.CallTimedOut(), Times.Never);
+            }
+        }
+
+        [Theory]
+        [InlineData(typeof(ConnectTimeoutException), typeof(TimeoutException))]
+        [InlineData(typeof(TimeoutException), typeof(TimeoutException))]
+        [InlineData(typeof(IotHubException), typeof(IotHubException))]
+        [InlineData(typeof(InvalidOperationException), typeof(InvalidOperationException))]
+        public async Task TestExceptionInSetDesiredPropertyUpdateCallbackTest(Type thrownException, Type expectedException)
+        {
+            // Arrange
+            var client = new Mock<IClient>();
+            client.Setup(c => c.SetDesiredPropertyUpdateCallbackAsync(It.IsAny<DesiredPropertyUpdateCallback>(), It.IsAny<object>())).ThrowsAsync(Activator.CreateInstance(thrownException, "msg str") as Exception);
+            var deviceConnectivityManager = new Mock<IDeviceConnectivityManager>();
+            deviceConnectivityManager.Setup(d => d.CallTimedOut());
+            var connectivityAwareClient = new ConnectivityAwareClient(client.Object, deviceConnectivityManager.Object);
+            DesiredPropertyUpdateCallback callback = (_, __) => Task.CompletedTask;
+
+            // Act / Assert
+            await Assert.ThrowsAsync(expectedException, () => connectivityAwareClient.SetDesiredPropertyUpdateCallbackAsync(callback, null));
+            deviceConnectivityManager.Verify(d => d.CallTimedOut(), Times.Never);
         }
 
         class DeviceConnectivityManager : IDeviceConnectivityManager
