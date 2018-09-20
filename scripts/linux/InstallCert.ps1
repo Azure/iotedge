bc461cc678845ff05013d4af9b624b4c888b070b
Anca Antochi <ancan@microsoft.com>
2018-09-20 13:08:54 -0700

    Fix install cert

--- scripts/linux/InstallCert.ps1
+++ scripts/linux/InstallCert.ps1
@@ -9,7 +9,7 @@ function InstallCertificate($CertificateValue)
 	$jsonObjectBytes = [System.Convert]::FromBase64String($CertificateValue)
 	$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList @(,$jsonObjectBytes)
     $storeNameEnumVal = [System.Security.Cryptography.X509Certificates.StoreName] $StoreName
-	$certStore = New-Object System.Security.Cryptography.X509Certificates.X509Store($storeNameEnumVal, [System.Security.Cryptography.X509Certificates.StoreLocation]::LocalMachine)
+	$certStore = New-Object System.Security.Cryptography.X509Certificates.X509Store($storeNameEnumVal, [System.Security.Cryptography.X509Certificates.StoreLocation]::CurrentUser)
 	$certStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
 
 	$certStore.Add($cert)
