1e979fdcabf8866f3d525b0f8dd78c74542fd0c2
Varun Puranik <vpuranik@microsoft.com>
2018-10-25 12:56:43 -0700

    Fix Try

---
+++ edge-util/test/Microsoft.Azure.Devices.Edge.Util.Test/TryTests.cs
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.Azure.Devices.Edge.Util.Test
+{
+    using System;
+    using Microsoft.Azure.Devices.Edge.Util.Test.Common;
+    using Xunit;
+
+    [Unit]
+    public class TryTests
+    {
+        [Fact]
+        public void OkWithValueTest()
+        {
+            // Arrange
+            string value = "Foo";
+            Try<string> tryVal = Try.Success(value);
+
+            // Act
+            Option<string> valueOption = tryVal.Ok();
+
+            // Assert
+            Assert.True(valueOption.HasValue);
+            Assert.Equal(value, valueOption.OrDefault());
+        }
+
+        [Fact]
+        public void OkWithExceptionTest()
+        {
+            // Arrange
+            Exception ex = new InvalidOperationException();
+            Try<string> tryVal = Try<string>.Failure(ex);
+
+            // Act
+            Option<string> valueOption = tryVal.Ok();
+
+            // Assert
+            Assert.False(valueOption.HasValue);
+            Assert.Equal(ex, tryVal.Exception);
+        }
+    }
+}
