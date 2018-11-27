2a525c9e7f0590bc50c3d0927c696a4965cd655e
Philip Lin <philipktlin@live.com>
2018-11-27 15:03:18 -0800

    update publish-branch.ps1

--- scripts/windows/build/Publish-Branch.ps1
+++ scripts/windows/build/Publish-Branch.ps1
@@ -69,12 +69,14 @@ $RELEASE_TESTS_FOLDER = Join-Path $BuildBinariesDirectory "release-tests"
 $VERSIONINFO_FILE_PATH = Join-Path $BuildRepositoryLocalPath "versionInfo.json"
 
 $SRC_SCRIPTS_DIR = Join-Path $BuildRepositoryLocalPath "scripts"
-$SRC_BIN_DIR = Join-Path $BuildRepositoryLocalPath "bin"
 $PUB_SCRIPTS_DIR = Join-Path $PUBLISH_FOLDER "scripts"
+$SRC_STRESS_DIR=Join-Path $BuildRepositoryLocalPath "stress"
+$PUB_STRESS_DIR = Join-Path $PUBLISH_FOLDER "stress"
+$SRC_BIN_DIR = Join-Path $BuildRepositoryLocalPath "bin"
 $PUB_BIN_DIR = Join-Path $PUBLISH_FOLDER "bin"
-$TEST_SCRIPTS_DIR = Join-Path $RELEASE_TESTS_FOLDER "scripts"
 $SRC_E2E_TEMPLATES_DIR = Join-Path $BuildRepositoryLocalPath "smoke/IotEdgeQuickstart/e2e_deployment_files"
 $PUB_E2E_TEMPLATES_DIR = Join-Path $PUBLISH_FOLDER "e2e_deployment_files"
+$TEST_SCRIPTS_DIR = Join-Path $RELEASE_TESTS_FOLDER "scripts"
 
 if (-not (Test-Path $DOTNET_PATH -PathType Leaf)) {
     throw "$DOTNET_PATH not found"
@@ -189,6 +191,9 @@ Copy-Item $SRC_BIN_DIR $PUB_BIN_DIR -Recurse -Force
 
 Write-Host "Copying $SRC_E2E_TEMPLATES_DIR"
 Copy-Item $SRC_E2E_TEMPLATES_DIR $PUB_E2E_TEMPLATES_DIR -Recurse -Force
+
+Write-Host "Copying $SRC_STRESS_DIR"
+Copy-Item $SRC_STRESS_DIR $PUB_STRESS_DIR -Recurse -Force
 
 <#
  # Publish tests
