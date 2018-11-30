3e3158f9b47890e4c2ceb7b24a5dfe133094c834
Anca Antochi <ancan@microsoft.com>
2018-11-30 14:08:14 -0800

    changed to Invoke-WebRequest

--- edgelet/build/windows/openssl.ps1
+++ edgelet/build/windows/openssl.ps1
@@ -25,9 +25,9 @@ function Get-OpenSSL
     }
 
     Write-Host "Downloading strawberry perl"
-    $strawberryPerlZip = "https://edgebuild.blob.core.windows.net/strawberry-perl/strawberry-perl-5.24.1.1-32bit-portable.zip"
+    $strawberryPerlUri = "https://edgebuild.blob.core.windows.net/strawberry-perl/strawberry-perl-5.24.1.1-32bit-portable.zip"
     $strawberryPerlPath = "$env:HOMEDRIVE\\vcpkg\\Downloads\\strawberry-perl-5.24.1.1-32bit-portable.zip"
-    (New-Object System.Net.WebClient).DownloadFile($strawberryPerlZip, $strawberryPerlPath)
+    Invoke-WebRequest -Uri $strawberryPerlUri -OutFile $strawberryPerlPath
 
     Write-Host "Installing OpenSSL for x64..."
     & $env:HOMEDRIVE\\vcpkg\\vcpkg.exe install openssl:x64-windows
