b1c8fb443dac4501254ba416b0df3381a8139d79
Philip Lin <phlin@microsoft.com>
2018-12-03 14:48:45 -0800

    update file header (#599)

--- edge-hub/test/Microsoft.Azure.Devices.Routing.Core.Test/ExponentialBackoffStrategy.cs
+++ edge-hub/test/Microsoft.Azure.Devices.Routing.Core.Test/ExponentialBackoffStrategy.cs
@@ -1,6 +1,4 @@
-// ---------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// ---------------------------------------------------------------
+// Copyright (c) Microsoft. All rights reserved.
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Azure.Devices.Common.ErrorHandling
--- edge-hub/test/Microsoft.Azure.Devices.Routing.Core.Test/query/errors/ErrorPositionTest.cs
+++ edge-hub/test/Microsoft.Azure.Devices.Routing.Core.Test/query/errors/ErrorPositionTest.cs
@@ -1,6 +1,4 @@
-// ---------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// ---------------------------------------------------------------
+// Copyright (c) Microsoft. All rights reserved.
 
 namespace Microsoft.Azure.Devices.Routing.Core.Test.Query.Errors
 {
