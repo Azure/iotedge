f5e50bd987a33c3054ea9a122be3f730c83b538d
Varun Puranik <vpuranik@microsoft.com>
2018-12-11 17:36:02 -0800

    EdgeHub: Pass in client cert auth allowed flag for MQTT Websockets (#629)
    * Pass in client cert auth allowed flag
    
    * Pass in the flag instead
    
    * Fix comment
    
    * Fix typo

--- edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Mqtt.Test/MqttWebSocketListenerTest.cs
+++ edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Mqtt.Test/MqttWebSocketListenerTest.cs
@@ -1,15 +1,14 @@
 // Copyright (c) Microsoft. All rights reserved.
 namespace Microsoft.Azure.Devices.Edge.Hub.Mqtt.Test
 {
+    using System.Threading.Tasks;
     using DotNetty.Buffers;
     using Microsoft.Azure.Devices.Edge.Hub.Core.Identity;
     using Microsoft.Azure.Devices.Edge.Util.Test.Common;
     using Microsoft.Azure.Devices.ProtocolGateway;
-    using Microsoft.Azure.Devices.ProtocolGateway.Identity;
     using Microsoft.Azure.Devices.ProtocolGateway.Messaging;
     using Microsoft.Azure.Devices.ProtocolGateway.Mqtt.Persistence;
     using Moq;
-    using System.Threading.Tasks;
     using Xunit;
 
     [Unit]
