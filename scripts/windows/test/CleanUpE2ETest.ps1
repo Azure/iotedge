8a33ad9ff2ec147917844f77f2b5294c07ed2be2
Philip Lin <philipktlin@live.com>
2019-01-07 20:18:59 -0800

    changes for comments

--- scripts/windows/test/CleanUpE2ETest.ps1
+++ scripts/windows/test/CleanUpE2ETest.ps1
@@ -18,25 +18,15 @@ try {
   docker -H npipe:////./pipe/iotedge_moby_engine system prune -f
 } catch {
   # Ignore this error if moby docker is not installed.
+  Write-Verbose "$_"
 }
-
-Write-Host "Stop docker"
-Stop-Service docker -Force
-Stop-Service com.docker.service -Force
-
-Write-Host "Remove nat VM switch"
-Remove-VMSwitch -Force 'nat' -ErrorAction Ignore
-
-Write-Host "Restart Host Network Service"
-Restart-Service hns
 
 . $SecurityDaemonInstallScriptPath
 Write-Host "Uninstall iotedged"
 Uninstall-SecurityDaemon -Force -DeleteConfig -DeleteMobyDataRoot
 
+Write-Host "Remove nat VM switch"
+Remove-VMSwitch -Force 'nat' -ErrorAction Ignore
+
 Write-Host "Restart Host Network Service"
 Restart-Service -name hns
-
-Write-Host "Restart docker"
-Restart-Service -name docker
-Restart-Service -name com.docker.service
