8d9b387eb97ab84aac5c902c7dbac48f4e543289
Damon Barry <damonb@microsoft.com>
2019-01-28 16:02:08 -0800

    Output host key at the end of the script

--- builds/e2e/runner.sh
+++ builds/e2e/runner.sh
@@ -5,19 +5,14 @@ set -euo pipefail
 proxy_hostname="$1"
 user="$2"
 
-# Output the host key so it can be added to the agent's known_hosts file
-echo '#DATA#'
-cat /etc/ssh/ssh_host_rsa_key.pub
-echo '#DATA#'
-
 curl -x "http://$proxy_hostname:3128" 'https://packages.microsoft.com/config/ubuntu/18.04/prod.list' > ./microsoft-prod.list
 mv ./microsoft-prod.list /etc/apt/sources.list.d/
 
 curl -x "http://$proxy_hostname:3128" 'https://packages.microsoft.com/keys/microsoft.asc' | gpg --dearmor > microsoft.gpg
 mv ./microsoft.gpg /etc/apt/trusted.gpg.d/
 
-http_proxy="http://$proxy_hostname:3128" https_proxy="http://$proxy_hostname:3128" apt-get update > /dev/null
-http_proxy="http://$proxy_hostname:3128" https_proxy="http://$proxy_hostname:3128" apt-get install -y moby-cli moby-engine > /dev/null
+http_proxy="http://$proxy_hostname:3128" https_proxy="http://$proxy_hostname:3128" apt-get update
+http_proxy="http://$proxy_hostname:3128" https_proxy="http://$proxy_hostname:3128" apt-get install -y moby-cli moby-engine
 
 > ~/proxy-env.override.conf cat <<-EOF
 [Service]
@@ -35,3 +30,8 @@ chown -R "$user:$user" "$home/proxy-env.override.conf"
 
 systemctl daemon-reload
 systemctl restart docker
+
+# Output the host key so it can be added to the agent's known_hosts file
+echo '#DATA#'
+cat /etc/ssh/ssh_host_rsa_key.pub
+echo '#DATA#'
