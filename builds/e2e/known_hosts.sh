2acb9d096d91f3823e828030291412757af96a6e
Damon Barry <damonb@microsoft.com>
2019-01-30 13:19:03 -0800

    Use linked templates so more than one custom script can run

--- builds/e2e/known_hosts.sh
+++ builds/e2e/known_hosts.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
+# usage: ./known_hosts.sh <user> <hostname> <hostkey> [<hostname> <hostkey>]...
 
 set -euo pipefail
 
 user="$1"
-host="$2"
-key="$3"
-
-suffix="$(grep -Po '^search \K.*' /etc/resolv.conf)"
-ipaddr="$(getent hosts $host | awk '{ print $1 }')"
+host_key_pair=( )
 home="$(eval echo ~$user)"
+suffix="$(grep -Po '^search \K.*' /etc/resolv.conf)"
 
 mkdir -p "$home/.ssh"
 chown "$user:$user" "$home/.ssh"
 
-> "$home/.ssh/known_hosts" cat <<-EOF
-$host, $ipaddr, $key
-$host.$suffix, $key
+for val in "${@:2}"; do
+
+host_key_pair=( "${host_key_pair[@]}" "$val" )
+if [ ${#host_key_pair[@]} -eq 2 ]; then
+
+set -- ${host_key_pair[@]}
+host_key_pair=( )
+
+ipaddr="$(getent hosts $1 | awk '{ print $1 }')"
+
+cat <<-EOF >> "$home/.ssh/known_hosts"
+$1, $ipaddr, $2
+$1.$suffix, $2
 EOF
+
+fi
+done
 
 chown "$user:$user" "$home/.ssh/known_hosts"
