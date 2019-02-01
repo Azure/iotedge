37db8f6bfd2be043f540055e419e9baa3ec9db5e
Damon Barry <damonb@microsoft.com>
2019-02-01 13:50:26 -0800

    Add ssh/proxy verification to final script

--- builds/e2e/known_hosts.sh
+++ builds/e2e/known_hosts.sh
@@ -3,7 +3,10 @@
 
 set -euo pipefail
 
+# Update ~/.ssh/known_hosts so agent can connect to runners
+
 user="$1"
+hosts=( )
 host_key_pair=( )
 home="$(eval echo ~$user)"
 suffix="$(grep -Po '^search \K.*' /etc/resolv.conf)"
@@ -12,21 +15,52 @@ mkdir -p "$home/.ssh"
 chown "$user:$user" "$home/.ssh"
 
 for val in "${@:2}"; do
+    host_key_pair=( "${host_key_pair[@]}" "$val" )
+    if [ ${#host_key_pair[@]} -eq 2 ]; then
 
-host_key_pair=( "${host_key_pair[@]}" "$val" )
-if [ ${#host_key_pair[@]} -eq 2 ]; then
+        set -- "${host_key_pair[@]}"
+        host_key_pair=( )
 
-set -- "${host_key_pair[@]}"
-host_key_pair=( )
+        hosts=( "${hosts[@]}" "$1" )
 
-ipaddr="$(getent hosts $1 | awk '{ print $1 }')"
+        ipaddr="$(getent hosts $1 | awk '{ print $1 }')"
 
-cat <<-EOF >> "$home/.ssh/known_hosts"
+        cat <<-EOF >> "$home/.ssh/known_hosts"
 $1,$ipaddr $2
 $1.$suffix $2
 EOF
 
-fi
+    fi
 done
 
 chown "$user:$user" "$home/.ssh/known_hosts"
+
+# Test that we really can:
+# (1) SSH into the runners, and
+# (2) make HTTP/S requests through the proxy
+
+agent_name=$(hostname)
+
+for host in "${hosts[@]}"; do
+    # Linux or Windows
+    ssh "$user@$host" uname
+    if [ $? -eq 0 ]; then  # Linux
+        # Verify runner can use the proxy
+        ssh "$user@$host" curl -x "http://$agent_name:3128" -L 'http://www.microsoft.com'
+        ssh "$user@$host" curl -x "http://$agent_name:3128" -L 'https://www.microsoft.com'
+
+        # Verify runner can't skirt the proxy (should time out after 5s)
+        ssh "$user@$host" timeout 5 curl -L 'http://www.microsoft.com' && exit 1 || :
+        ssh "$user@$host" timeout 5 curl -L 'https://www.microsoft.com' && exit 1 || :
+    else  # Windows
+        # Verify Windows runner VM can use the proxy (should succeed)
+        # When Invoke-WebRequest is invoked over SSH and -Proxy argument is added, we get an error back ("Access is
+        # denied" 0x5 occurred while reading the console output buffer). Avoid this by wrapping the command in try.
+        # Using 'ssh -t' also avoids the problem, but unfortunately swallows the return value.
+        ssh "$user@$host" "try { Invoke-WebRequest -UseBasicParsing -Proxy 'http://$agent_name:3128' 'http://www.microsoft.com' } catch {}"
+        ssh "$user@$host" "try { Invoke-WebRequest -UseBasicParsing -Proxy 'http://$agent_name:3128' 'https://www.microsoft.com' } catch {}"
+
+        # Verify Windows runner VM can't skirt the proxy (should time out after 5s)
+        ssh "$user@$host" "Invoke-WebRequest -UseBasicParsing -TimeoutSec 5 'http://www.microsoft.com'" && exit 1 || :
+        ssh "$user@$host" "Invoke-WebRequest -UseBasicParsing -TimeoutSec 5 'https://www.microsoft.com'" && exit 1 || :
+    fi
