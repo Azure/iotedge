a7a246d1e135f3a78dd4c922167729ce4d7553e3
David Robson <darobs@microsoft.com>
2019-02-11 17:31:21 -0800

    Create edgelet-kube - a module runtime for k8s. (#783)
    * Edgelet Kube runtime  - get runtime to make deployments.

--- edgelet/edgelet-kube/src/constants.rs
+++ edgelet/edgelet-kube/src/constants.rs
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 
+pub const EDGE_EDGE_AGENT_NAME: &str = "edgeagent";
+
 pub const EDGE_MODULE_LABEL: &str = "net.azure-devices.edge.module";
 
 pub const EDGE_ORIGINAL_MODULEID: &str = "net.azure-devices.edge.original-moduleid";
@@ -7,3 +9,13 @@ pub const EDGE_ORIGINAL_MODULEID: &str = "net.azure-devices.edge.original-module
 pub const EDGE_DEVICE_LABEL: &str = "net.azure-devices.edge.deviceid";
 
 pub const EDGE_HUBNAME_LABEL: &str = "net.azure-devices.edge.hub";
+
+pub const PROXY_CONTAINER_NAME: &str = "proxy";
+
+pub const PROXY_CONFIG_VOLUME_NAME: &str = "config-volume";
+
+pub const USE_PERSISTENT_VOLUME_CLAIMS: &str = "USE_PERSISTENT_VOLUMES";
+
+pub const PULL_SECRET_DATA_NAME: &str = ".dockerconfigjson";
+
+pub const PULL_SECRET_TYPE: &str = "kubernetes.io/dockerconfigjson";
