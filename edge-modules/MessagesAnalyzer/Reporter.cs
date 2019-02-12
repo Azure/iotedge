b1cf75813c01534c5473d83cf2ce213731312eb5
Philip Lin <philipktlin@live.com>
2019-02-12 12:51:31 -0800

    minor update

--- edge-modules/MessagesAnalyzer/Reporter.cs
+++ edge-modules/MessagesAnalyzer/Reporter.cs
@@ -57,7 +57,7 @@ namespace MessagesAnalyzer
                     if (msg.SequenceNumber - 1 != prevSequenceNumber)
                     {
                         Log.LogInformation($"Missing messages for {moduleId} from {prevSequenceNumber} to {msg.SequenceNumber} exclusive.");
-                        long currentMissing = msg.SequenceNumber - prevSequenceNumber - 2;
+                        long currentMissing = msg.SequenceNumber - prevSequenceNumber - 1;
                         missingCounter += currentMissing;
                         missedMessages.Add(new MissedMessagesDetails(currentMissing, prevEnquedDateTime, msg.EnqueuedDateTime));
                     }
@@ -72,12 +72,12 @@ namespace MessagesAnalyzer
             // check if last message is older
             if (DateTime.Compare(lastMessageDateTime.AddMilliseconds(toleranceInMilliseconds), endDateTime) < 0)
             {
-                return new ModuleReport(moduleId, StatusCode.OldMessages, totalMessagesCounter, $"No messages received for the past {toleranceInMilliseconds} milliseconds", lastMessageDateTime, missedMessages);
+                return new ModuleReport(moduleId, StatusCode.OldMessages, totalMessagesCounter, $"Missing messages: {missingCounter}. No messages received for the past {toleranceInMilliseconds} milliseconds.", lastMessageDateTime, missedMessages);
             }
 
             return missingCounter > 0
-                ? new ModuleReport(moduleId, StatusCode.SkippedMessages, totalMessagesCounter, $"Missing messages: {missingCounter}", lastMessageDateTime, missedMessages)
-                : new ModuleReport(moduleId, StatusCode.AllMessages, totalMessagesCounter, "All messages received", lastMessageDateTime);
+                ? new ModuleReport(moduleId, StatusCode.SkippedMessages, totalMessagesCounter, $"Missing messages: {missingCounter}.", lastMessageDateTime, missedMessages)
+                : new ModuleReport(moduleId, StatusCode.AllMessages, totalMessagesCounter, "All messages received.", lastMessageDateTime);
         }
     }
 }
