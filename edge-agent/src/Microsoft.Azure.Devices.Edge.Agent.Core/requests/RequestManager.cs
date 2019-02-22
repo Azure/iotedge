97dba87450e0eaf7183266c0ca6b432b23ad5113
Varun Puranik <vpuranik@microsoft.com>
2019-02-22 08:37:29 -0800

    Add logs and cleanup

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/requests/RequestManager.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/requests/RequestManager.cs
@@ -4,9 +4,11 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Requests
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Net;
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Edge.Storage;
     using Microsoft.Azure.Devices.Edge.Util;
+    using Microsoft.Extensions.Logging;
 
     public class RequestManager : IRequestManager
     {
@@ -33,6 +35,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Requests
             try
             {
                 Preconditions.CheckNonWhiteSpace(request, nameof(request));
+                Events.HandlingRequest(request, payloadJson);
                 if (!this.requestHandlers.TryGetValue(request, out IRequestHandler requestHandler))
                 {
                     string supportedCommands = string.Join(",", this.requestHandlers.Keys);
@@ -40,10 +43,13 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Requests
                     throw new ArgumentException(message);
                 }
 
-                return (200, await requestHandler.HandleRequest(payloadJson));
+                string responsePayload = await requestHandler.HandleRequest(payloadJson);
+                Events.HandledRequest(request);
+                return ((int)HttpStatusCode.OK, responsePayload);
             }
             catch (Exception ex)
             {
+                Events.ErrorHandlingRequest(request, ex);
                 return GetErrorResponse(ex);
             }
         }
@@ -53,9 +59,9 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Requests
             switch (ex)
             {
                 case ArgumentException _:
-                    return (400, GetErrorPayload(ex.Message));
+                    return ((int)HttpStatusCode.BadRequest, GetErrorPayload(ex.Message));
                 default:
-                    return (500, GetErrorPayload(ex.Message));
+                    return ((int)HttpStatusCode.InternalServerError, GetErrorPayload(ex.Message));
             }
         }
 
@@ -67,6 +73,46 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Requests
             };
             string json = errorPayload.ToJson();
             return json;
+        }
+
+        static class Events
+        {
+            const int IdStart = AgentEventIds.RequestManager;
+            static readonly ILogger Log = Logger.Factory.CreateLogger<RequestManager>();
+
+            enum EventIds
+            {
+                ScheduledModule = IdStart + 1,
+                HandlingRequest,
+                ErrorHandlingRequest
+            }
+
+            public static void ScheduledModule(IRuntimeModule module, TimeSpan elapsedTime, TimeSpan coolOffPeriod)
+            {
+                TimeSpan timeLeft = coolOffPeriod - elapsedTime;
+                Log.LogInformation(
+                    (int)EventIds.ScheduledModule,
+                    $"Module '{module.Name}' scheduled to restart after {coolOffPeriod.Humanize()} ({timeLeft.Humanize()} left).");
+            }
+
+            public static void ErrorHandlingRequest(string request, Exception exception)
+            {
+                Log.LogWarning((int)EventIds.ErrorHandlingRequest, exception, $"Error handling request {request}");
+            }
+
+            public static void HandlingRequest(string request, string payloadJson)
+            {
+                Log.LogInformation(
+                    (int)EventIds.HandlingRequest,
+                    string.IsNullOrWhiteSpace(payloadJson)
+                        ? $"Received request {request}"
+                        : $"Received request {request} with payload {payloadJson}");
+            }
+
+            public static void HandledRequest(string request)
+            {
+                Log.LogInformation((int)EventIds.HandlingRequest, $"Successfully handled request {request}");
+            }
         }
     }
 }
