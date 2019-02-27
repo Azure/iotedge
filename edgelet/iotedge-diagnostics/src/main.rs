a23974ba03b5d29fad1d4a2276bf861544911781
Arnav Singh <arsing@microsoft.com>
2019-02-27 15:04:08 -0800

    Don't panic for user errors.

--- edgelet/iotedge-diagnostics/src/main.rs
+++ edgelet/iotedge-diagnostics/src/main.rs
@@ -15,7 +15,7 @@ use hyperlocal::{UnixConnector, Uri as UnixUri};
 #[cfg(windows)]
 use hyperlocal_windows::{UnixConnector, Uri as UnixUri};
 
-fn main() {
+fn main() -> Result<(), Error> {
     let app = app_from_crate!()
         .setting(clap::AppSettings::SubcommandRequiredElseHelp)
         .setting(clap::AppSettings::VersionlessSubcommands)
@@ -56,13 +56,16 @@ fn main() {
 
     let matches = app.get_matches();
 
-    let mut runtime = tokio::runtime::Runtime::new().expect("could not create tokio runtime");
+    let mut runtime = tokio::runtime::Runtime::new()
+        .map_err(|err| format!("could not create tokio runtime: {}", err))?;
 
     match matches.subcommand() {
         ("edge-agent", Some(matches)) => {
-            let management_uri = matches.value_of("MANAGEMENT_URI").expect("unreachable");
-            let management_uri =
-                url::Url::parse(management_uri).expect("could not parse management URI");
+            let management_uri = matches
+                .value_of("MANAGEMENT_URI")
+                .expect("parameter is required");
+            let management_uri = url::Url::parse(management_uri)
+                .map_err(|err| format!("could not parse management URI: {}", err))?;
 
             let list_modules_response = match management_uri.scheme() {
                 "unix" => {
@@ -71,7 +74,7 @@ fn main() {
                     let uri = UnixUri::new(
                         management_uri
                             .to_uds_file_path()
-                            .expect("couldn't get file path from URI"),
+                            .map_err(|err| format!("couldn't get file path from URI: {}", err))?,
                         "/modules/?api-version=2018-06-28",
                     );
                     client.get(uri.into())
@@ -81,36 +84,45 @@ fn main() {
                     let client = hyper::Client::new();
                     let uri = management_uri
                         .join("/modules/?api-version=2018-06-28")
-                        .expect("could not construct list-modules request URI");
-                    client.get(uri.to_string().parse().expect(
-                        "could not convert list-modules request URI from url::Url to hyper::Uri",
-                    ))
+                        .map_err(|err| {
+                            format!("could not construct list-modules request URI: {}", err)
+                        })?;
+                    client.get(uri.to_string().parse().map_err(|err| {
+                        format!("could not convert list-modules request URI from url::Url to hyper::Uri: {}", err)
+                    })?)
                 }
 
-                scheme => panic!(
-                    "unrecognized scheme {:?} in management URI {:?}",
-                    scheme, management_uri
-                ),
+                scheme => {
+                    return Err(format!(
+                        "unrecognized scheme {:?} in management URI {:?}",
+                        scheme, management_uri
+                    )
+                    .into());
+                }
             };
 
             let f = list_modules_response
                 .then(|response| {
-                    let response = response.expect("could not execute list-modules request");
+                    let response = response.map_err(|err| {
+                        format!("could not execute list-modules request: {}", err)
+                    })?;
                     assert_eq!(
                         response.status(),
                         hyper::StatusCode::OK,
                         "list-modules request did not succeed"
                     );
-                    response.into_body().concat2()
+                    Ok::<_, Error>(response.into_body().concat2().map_err(|err| {
+                        format!("could not execute list-modules request: {}", err).into()
+                    }))
                 })
-                .then(|body| {
-                    let body = body.expect("could not execute list-modules request");
+                .flatten()
+                .and_then(|body| {
                     let _: management::models::ModuleList = serde_json::from_slice(&*body)
-                        .expect("could not parse list-modules response");
-                    Ok::<_, ()>(())
+                        .map_err(|err| format!("could not parse list-modules response: {}", err))?;
+                    Ok::<_, Error>(())
                 });
 
-            runtime.block_on(f).unwrap();
+            runtime.block_on(f)?;
         }
 
         ("local-time", _) => {
@@ -124,21 +136,39 @@ fn main() {
         }
 
         ("idle-module", Some(matches)) => {
-            let duration = matches.value_of("DURATION").expect("unreachable");
-            let duration: u64 = duration.parse().expect("could not parse duration");
+            let duration = matches.value_of("DURATION").expect("parameter is required");
+            let duration: u64 = duration
+                .parse()
+                .map_err(|err| format!("could not parse duration: {}", err))?;
 
             std::thread::sleep(std::time::Duration::from_secs(duration));
         }
 
         ("resolve-module", Some(matches)) => {
-            let hostname = matches.value_of("HOSTNAME").expect("unreachable");
+            let hostname = matches.value_of("HOSTNAME").expect("parameter is required");
 
             let _ = std::net::ToSocketAddrs::to_socket_addrs(&(hostname, 80))
-                .unwrap_or_else(|err| panic!("could not resolve {}: {}", hostname, err))
+                .map_err(|err| format!("could not resolve {}: {}", hostname, err))?
                 .next()
-                .unwrap_or_else(|| panic!("could not resolve {}: no addresses found", hostname));
+                .ok_or_else(|| format!("could not resolve {}: no addresses found", hostname))?;
         }
 
         (subcommand, _) => panic!("unexpected subcommand {}", subcommand),
+    }
+
+    Ok(())
+}
+
+struct Error(String);
+
+impl std::fmt::Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl From<String> for Error {
+    fn from(s: String) -> Self {
+        Error(s)
     }
 }
