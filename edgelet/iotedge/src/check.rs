a3cd1d456c65047884da715a522da90f61339005
Arnav Singh <arsing@microsoft.com>
2019-03-11 13:06:17 -0700

    cargo fmt

--- edgelet/iotedge/src/check.rs
+++ edgelet/iotedge/src/check.rs
@@ -222,15 +222,18 @@ impl Check {
             (
                 "Connectivity checks",
                 &[
-                    ("host can connect to and perform TLS handshake with IoT Hub AMQP port", |check| {
-                        connection_to_iot_hub_host(check, 5671)
-                    }),
-                    ("host can connect to and perform TLS handshake with IoT Hub HTTPS port", |check| {
-                        connection_to_iot_hub_host(check, 443)
-                    }),
-                    ("host can connect to and perform TLS handshake with IoT Hub MQTT port", |check| {
-                        connection_to_iot_hub_host(check, 8883)
-                    }),
+                    (
+                        "host can connect to and perform TLS handshake with IoT Hub AMQP port",
+                        |check| connection_to_iot_hub_host(check, 5671),
+                    ),
+                    (
+                        "host can connect to and perform TLS handshake with IoT Hub HTTPS port",
+                        |check| connection_to_iot_hub_host(check, 443),
+                    ),
+                    (
+                        "host can connect to and perform TLS handshake with IoT Hub MQTT port",
+                        |check| connection_to_iot_hub_host(check, 8883),
+                    ),
                     ("container can connect to IoT Hub AMQP port", |check| {
                         connection_to_iot_hub_container(check, 5671)
                     }),
