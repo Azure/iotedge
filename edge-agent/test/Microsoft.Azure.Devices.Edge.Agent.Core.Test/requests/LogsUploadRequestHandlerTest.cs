af0c91ed11b663e85c2e4fc877ac69344b03099a
Varun Puranik <vpuranik@microsoft.com>
2019-03-28 11:44:05 -0700

    Fix stylecop issues

--- edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Core.Test/requests/LogsUploadRequestHandlerTest.cs
+++ edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Core.Test/requests/LogsUploadRequestHandlerTest.cs
@@ -16,6 +16,33 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Test.Requests
     [Unit]
     public class LogsUploadRequestHandlerTest
     {
+        public static IEnumerable<object[]> GetLogsUploadRequestHandlerData()
+        {
+            string sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
+            yield return new object[] { @"{""id"": ""edgeAgent"",  ""sasUrl"": ""<sasurl>""}".Replace("<sasurl>", sasUrl), "edgeAgent", sasUrl, LogsContentEncoding.None, LogsContentType.Json, ModuleLogFilter.Empty };
+
+            sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
+            yield return new object[] { @"{""id"": ""edgeAgent"",  ""sasUrl"": ""<sasurl>"", ""encoding"": ""gzip""}".Replace("<sasurl>", sasUrl), "edgeAgent", sasUrl, LogsContentEncoding.Gzip, LogsContentType.Json, ModuleLogFilter.Empty };
+
+            sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
+            yield return new object[] { @"{""id"": ""mod1"",  ""sasUrl"": ""<sasurl>"", ""encoding"": ""gzip"", ""contentType"": ""text""}".Replace("<sasurl>", sasUrl), "mod1", sasUrl, LogsContentEncoding.Gzip, LogsContentType.Text, ModuleLogFilter.Empty };
+
+            sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
+            yield return new object[] { @"{""id"": ""edgeHub"",  ""sasUrl"": ""<sasurl>"", ""encoding"": ""none"", ""contentType"": ""json""}".Replace("<sasurl>", sasUrl), "edgeHub", sasUrl, LogsContentEncoding.None, LogsContentType.Json, ModuleLogFilter.Empty };
+
+            sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
+            var filter = new ModuleLogFilter(Option.Some(100), Option.Some(1501000), Option.Some(3), Option.Some("ERR"));
+            yield return new object[] { @"{""id"": ""edgeAgent"",  ""sasUrl"": ""<sasurl>"", ""filter"": <filter>}".Replace("<sasurl>", sasUrl).Replace("<filter>", filter.ToJson()), "edgeAgent", sasUrl, LogsContentEncoding.None, LogsContentType.Json, filter };
+
+            sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
+            filter = new ModuleLogFilter(Option.None<int>(), Option.Some(1501000), Option.None<int>(), Option.Some("ERR"));
+            yield return new object[] { @"{""id"": ""edgeAgent"",  ""sasUrl"": ""<sasurl>"", ""filter"": <filter>}".Replace("<sasurl>", sasUrl).Replace("<filter>", filter.ToJson()), "edgeAgent", sasUrl, LogsContentEncoding.None, LogsContentType.Json, filter };
+
+            sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
+            filter = new ModuleLogFilter(Option.Some(100), Option.None<int>(), Option.Some(3), Option.None<string>());
+            yield return new object[] { @"{""id"": ""edgeAgent"",  ""sasUrl"": ""<sasurl>"", ""filter"": <filter>}".Replace("<sasurl>", sasUrl).Replace("<filter>", filter.ToJson()), "edgeAgent", sasUrl, LogsContentEncoding.None, LogsContentType.Json, filter };
+        }
+
         [Theory]
         [MemberData(nameof(GetLogsUploadRequestHandlerData))]
         public async Task TestLogsUploadRequest(string payload, string id, string sasUrl, LogsContentEncoding contentEncoding, LogsContentType contentType, ModuleLogFilter filter)
@@ -65,7 +92,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Test.Requests
 
             var logsUploader = new Mock<ILogsUploader>();
             var logsProvider = new Mock<ILogsProvider>();
-            
+
             var module1LogOptions = new ModuleLogOptions(mod1, contentEncoding, contentType, filter);
             var mod1LogBytes = new byte[100];
             logsProvider.Setup(l => l.GetLogs(module1LogOptions, It.IsAny<CancellationToken>()))
@@ -96,33 +123,6 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Test.Requests
             logsProvider.VerifyAll();
             logsUploader.VerifyAll();
             runtimeInfoProvider.VerifyAll();
-        }
-
-        public static IEnumerable<object[]> GetLogsUploadRequestHandlerData()
-        {
-            string sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
-            yield return new object[] { @"{""id"": ""edgeAgent"",  ""sasUrl"": ""<sasurl>""}".Replace("<sasurl>", sasUrl), "edgeAgent", sasUrl, LogsContentEncoding.None, LogsContentType.Json, ModuleLogFilter.Empty };
-
-            sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
-            yield return new object[] { @"{""id"": ""edgeAgent"",  ""sasUrl"": ""<sasurl>"", ""encoding"": ""gzip""}".Replace("<sasurl>", sasUrl), "edgeAgent", sasUrl, LogsContentEncoding.Gzip, LogsContentType.Json, ModuleLogFilter.Empty };
-
-            sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
-            yield return new object[] { @"{""id"": ""mod1"",  ""sasUrl"": ""<sasurl>"", ""encoding"": ""gzip"", ""contentType"": ""text""}".Replace("<sasurl>", sasUrl), "mod1", sasUrl, LogsContentEncoding.Gzip, LogsContentType.Text, ModuleLogFilter.Empty };
-
-            sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
-            yield return new object[] { @"{""id"": ""edgeHub"",  ""sasUrl"": ""<sasurl>"", ""encoding"": ""none"", ""contentType"": ""json""}".Replace("<sasurl>", sasUrl), "edgeHub", sasUrl, LogsContentEncoding.None, LogsContentType.Json, ModuleLogFilter.Empty };
-
-            sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
-            var filter = new ModuleLogFilter(Option.Some(100), Option.Some(1501000), Option.Some(3), Option.Some("ERR"));
-            yield return new object[] { @"{""id"": ""edgeAgent"",  ""sasUrl"": ""<sasurl>"", ""filter"": <filter>}".Replace("<sasurl>", sasUrl).Replace("<filter>", filter.ToJson()), "edgeAgent", sasUrl, LogsContentEncoding.None, LogsContentType.Json, filter };
-
-            sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
-            filter = new ModuleLogFilter(Option.None<int>(), Option.Some(1501000), Option.None<int>(), Option.Some("ERR"));
-            yield return new object[] { @"{""id"": ""edgeAgent"",  ""sasUrl"": ""<sasurl>"", ""filter"": <filter>}".Replace("<sasurl>", sasUrl).Replace("<filter>", filter.ToJson()), "edgeAgent", sasUrl, LogsContentEncoding.None, LogsContentType.Json, filter };
-
-            sasUrl = $"https://test1.blob.core.windows.net/cont2?st={Guid.NewGuid()}";
-            filter = new ModuleLogFilter(Option.Some(100), Option.None<int>(), Option.Some(3), Option.None<string>());
-            yield return new object[] { @"{""id"": ""edgeAgent"",  ""sasUrl"": ""<sasurl>"", ""filter"": <filter>}".Replace("<sasurl>", sasUrl).Replace("<filter>", filter.ToJson()), "edgeAgent", sasUrl, LogsContentEncoding.None, LogsContentType.Json, filter };
         }
     }
 }
