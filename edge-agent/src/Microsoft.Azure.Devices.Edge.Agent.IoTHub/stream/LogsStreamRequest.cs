75d74603664d1f206585ab3473294236b142a011
Varun Puranik <vpuranik@microsoft.com>
2019-03-28 15:24:04 -0700

    Add support for filtering/processing log streams (#996)
    * Add filtering support for streaming logs
    
    * Add LogsProvider tests
    
    * Add LogsProcessor tests
    
    * Fix tests

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.IoTHub/stream/LogsStreamRequest.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.IoTHub/stream/LogsStreamRequest.cs
@@ -1,16 +1,37 @@
 // Copyright (c) Microsoft. All rights reserved.
 namespace Microsoft.Azure.Devices.Edge.Agent.IoTHub.Stream
 {
+    using System.ComponentModel;
+    using Microsoft.Azure.Devices.Edge.Agent.Core.Logs;
+    using Microsoft.Azure.Devices.Edge.Util;
+    using Newtonsoft.Json;
+
     public class LogsStreamRequest
     {
-        public LogsStreamRequest(string schemaVersion, string id)
+        public LogsStreamRequest(string schemaVersion, string id, LogsContentEncoding encoding, LogsContentType contentType, ModuleLogFilter filter)
         {
             this.SchemaVersion = schemaVersion;
-            this.Id = id;
+            this.Id = Preconditions.CheckNonWhiteSpace(id, nameof(id));
+            this.Filter = filter ?? ModuleLogFilter.Empty;
+            this.Encoding = encoding;
+            this.ContentType = contentType;
         }
 
+        [JsonProperty("schemaVersion")]
         public string SchemaVersion { get; }
 
+        [JsonProperty("id")]
         public string Id { get; }
+
+        [JsonProperty("encoding", DefaultValueHandling = DefaultValueHandling.Populate)]
+        [DefaultValue(LogsContentEncoding.None)]
+        public LogsContentEncoding Encoding { get; }
+
+        [JsonProperty("contentType", DefaultValueHandling = DefaultValueHandling.Populate)]
+        [DefaultValue(LogsContentType.Text)]
+        public LogsContentType ContentType { get; }
+
+        [JsonProperty("filter")]
+        public ModuleLogFilter Filter { get; }
     }
 }
