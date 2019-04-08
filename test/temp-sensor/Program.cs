40a60e1328925c4b011aaa4badd01446153cf781
Damon Barry <damonb@microsoft.com>
2019-04-08 15:23:54 -0700

    Create temp-sensor walking skeleton

---
+++ test/temp-sensor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace temp_sensor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // UninstallSecurityDaemon();
+            // CreateEdgeDeviceIdentity();
+            // InstallSecurityDaemon();
+            // ConfigureSecurityDaemon();
+            // EnsureSecurityDaemonIsRunning();
+            // EnsureEdgeAgentIsRunning();
+            // EnsureEdgeAgentIsConnectedToIotHub();
+            // var config = CreateEdgeConfiguration();
+            // AddTempSensor(config);
+            // DeployEdgeConfiguration(config);
+            // EnsureConfigurationIsDeployed();
+            // EnsureTempSensorIsRunning();
+            // EnsureTempSensorEventsAreSent();
+            // UpdateTempSensorTwin();
+            // EnsureTempSensorTwinUpdatesAreReported();
+            // StopSecurityDaemon();
+            // DeleteEdgeDeviceIdentity();
+        }
+    }
+}
