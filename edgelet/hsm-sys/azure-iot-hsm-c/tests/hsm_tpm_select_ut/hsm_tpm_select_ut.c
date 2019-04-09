eb6368da7d09daa70a6a656ff879a6232c7e599f
Mahesh Rohera <marohera@microsoft.com>
2019-04-09 16:30:57 -0700

    Ensure cargo test selects TPM in memory HSM

--- edgelet/hsm-sys/azure-iot-hsm-c/tests/hsm_tpm_select_ut/hsm_tpm_select_ut.c
+++ edgelet/hsm-sys/azure-iot-hsm-c/tests/hsm_tpm_select_ut/hsm_tpm_select_ut.c
@@ -38,11 +38,46 @@ MOCKABLE_FUNCTION(, const HSM_CLIENT_TPM_INTERFACE*, hsm_client_tpm_store_interf
 static TEST_MUTEX_HANDLE g_testByTest;
 static TEST_MUTEX_HANDLE g_dllByDll;
 
+DEFINE_ENUM_STRINGS(UMOCK_C_ERROR_CODE, UMOCK_C_ERROR_CODE_VALUES)
+
 #define TEST_HSM_CLIENT_TPM_INTERFACE (HSM_CLIENT_TPM_INTERFACE*)0x1000
 
 //#############################################################################
 // Test helpers
 //#############################################################################
+
+static void setup_callstack_hsm_client_tpm_init(void)
+{
+    umock_c_reset_all_calls();
+
+    #ifdef TEST_TPM_INTERFACE_IN_MEM
+        EXPECTED_CALL(hsm_client_tpm_store_init());
+    #else
+        EXPECTED_CALL(hsm_client_tpm_device_init());
+    #endif
+}
+
+static void setup_callstack_hsm_client_tpm_deinit(void)
+{
+    umock_c_reset_all_calls();
+
+    #ifdef TEST_TPM_INTERFACE_IN_MEM
+        EXPECTED_CALL(hsm_client_tpm_store_deinit());
+    #else
+        EXPECTED_CALL(hsm_client_tpm_device_deinit());
+    #endif
+}
+
+static void setup_callstack_hsm_client_tpm_interface(void)
+{
+    umock_c_reset_all_calls();
+
+    #ifdef TEST_TPM_INTERFACE_IN_MEM
+        EXPECTED_CALL(hsm_client_tpm_store_interface());
+    #else
+        EXPECTED_CALL(hsm_client_tpm_device_interface());
+    #endif
+}
 
 //#############################################################################
 // Mocked functions test hooks
@@ -50,11 +85,10 @@ static TEST_MUTEX_HANDLE g_dllByDll;
 
 static void test_hook_on_umock_c_error(UMOCK_C_ERROR_CODE error_code)
 {
-    (void)error_code;
-    // char temp_str[256];
-    // (void)snprintf(temp_str, sizeof(temp_str), "umock_c reported error :%s",
-    //                ENUM_TO_STRING(UMOCK_C_ERROR_CODE, error_code));
-    // ASSERT_FAIL(temp_str);
+    char temp_str[256];
+    (void)snprintf(temp_str, sizeof(temp_str), "umock_c reported error :%s",
+                   ENUM_TO_STRING(UMOCK_C_ERROR_CODE, error_code));
+    ASSERT_FAIL(temp_str);
 }
 
 static int test_hook_hsm_client_tpm_init(void)
@@ -96,6 +130,14 @@ BEGIN_TEST_SUITE(hsm_tpm_select_ut)
 
         REGISTER_GLOBAL_MOCK_HOOK(hsm_client_tpm_device_interface, test_hook_hsm_client_tpm_interface);
         REGISTER_GLOBAL_MOCK_FAIL_RETURN(hsm_client_tpm_device_interface, NULL);
+
+        REGISTER_GLOBAL_MOCK_HOOK(hsm_client_tpm_store_init, test_hook_hsm_client_tpm_init);
+        REGISTER_GLOBAL_MOCK_FAIL_RETURN(hsm_client_tpm_store_init, 1);
+
+        REGISTER_GLOBAL_MOCK_HOOK(hsm_client_tpm_store_deinit, test_hook_hsm_client_tpm_deinit);
+
+        REGISTER_GLOBAL_MOCK_HOOK(hsm_client_tpm_store_interface, test_hook_hsm_client_tpm_interface);
+        REGISTER_GLOBAL_MOCK_FAIL_RETURN(hsm_client_tpm_store_interface, NULL);
     }
 
     TEST_SUITE_CLEANUP(TestClassCleanup)
@@ -122,7 +164,7 @@ BEGIN_TEST_SUITE(hsm_tpm_select_ut)
     TEST_FUNCTION(hsm_client_tpm_init_success)
     {
         // arrange
-        EXPECTED_CALL(hsm_client_tpm_device_init());
+        setup_callstack_hsm_client_tpm_init();
 
         // act
         int result = hsm_client_tpm_init();
@@ -140,7 +182,7 @@ BEGIN_TEST_SUITE(hsm_tpm_select_ut)
         int test_result = umock_c_negative_tests_init();
         ASSERT_ARE_EQUAL(int, 0, test_result);
 
-        EXPECTED_CALL(hsm_client_tpm_device_init());
+        setup_callstack_hsm_client_tpm_init();
 
         umock_c_negative_tests_snapshot();
 
@@ -166,8 +208,7 @@ BEGIN_TEST_SUITE(hsm_tpm_select_ut)
         // arrange
         int result = hsm_client_tpm_init();
         ASSERT_ARE_EQUAL(int, 0, result, "Line:" TOSTRING(__LINE__));
-        umock_c_reset_all_calls();
-        EXPECTED_CALL(hsm_client_tpm_device_deinit());
+        setup_callstack_hsm_client_tpm_deinit();
 
         // act
         hsm_client_tpm_deinit();
@@ -181,8 +222,7 @@ BEGIN_TEST_SUITE(hsm_tpm_select_ut)
     TEST_FUNCTION(hsm_client_tpm_interface_success)
     {
         //arrange
-        umock_c_reset_all_calls();
-        EXPECTED_CALL(hsm_client_tpm_device_interface());
+        setup_callstack_hsm_client_tpm_interface();
 
         //act
         const HSM_CLIENT_TPM_INTERFACE* result = hsm_client_tpm_interface();
@@ -200,7 +240,7 @@ BEGIN_TEST_SUITE(hsm_tpm_select_ut)
         int test_result = umock_c_negative_tests_init();
         ASSERT_ARE_EQUAL(int, 0, test_result);
 
-        EXPECTED_CALL(hsm_client_tpm_device_interface());
+        setup_callstack_hsm_client_tpm_interface();
 
         umock_c_negative_tests_snapshot();
 
