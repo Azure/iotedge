b5609d645b676e8c42b2a9e284e716d13a457bc9
Varun Puranik <vpuranik@microsoft.com>
2019-04-08 23:34:32 -0700

    Test changes

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/requests/LogsUploadRequestHandler.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/requests/LogsUploadRequestHandler.cs
@@ -1,11 +1,15 @@
 // Copyright (c) Microsoft. All rights reserved.
 namespace Microsoft.Azure.Devices.Edge.Agent.Core.Requests
 {
+    extern alias akka;
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
+    using akka::Akka.IO;
     using Microsoft.Azure.Devices.Edge.Agent.Core.Logs;
     using Microsoft.Azure.Devices.Edge.Util;
 
@@ -42,6 +46,8 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Requests
 
         async Task UploadLogs(string sasUrl, string id, ModuleLogOptions moduleLogOptions, CancellationToken token)
         {
+            Console.WriteLine($"Uploading logs for {id}");
+            await this.GetAndPrintLogFrames(id);
             if (moduleLogOptions.ContentType == LogsContentType.Json)
             {
                 byte[] logBytes = await this.logsProvider.GetLogs(id, moduleLogOptions, token);
@@ -51,6 +57,80 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Requests
             {
                 Func<ArraySegment<byte>, Task> uploaderCallback = await this.logsUploader.GetUploaderCallback(sasUrl, id, moduleLogOptions.ContentEncoding, moduleLogOptions.ContentType);
                 await this.logsProvider.GetLogsStream(id, moduleLogOptions, uploaderCallback, token);
+            }
+        }
+
+        async Task GetAndPrintLogFrames(string id)
+        {
+            Console.WriteLine($"Getting logs for {id}");
+            Stream s = await this.runtimeInfoProvider.GetModuleLogs(id, true, Option.None<int>(), Option.None<int>(), CancellationToken.None);
+            Console.WriteLine($"Got logs for {id}.. printing log frames");
+            byte[] streamBytes = ReadStream(s);
+            PrintLogFrames(streamBytes);
+        }
+
+        static void PrintLogFrames(byte[] streamBytes)
+        {
+            int cnt = 0;
+            var bs = ByteString.FromBytes(streamBytes);
+            while (bs.Count > 0)
+            {
+                if (bs.Count < 4)
+                {
+                    Console.WriteLine("Less than 4 bytes remaining!");
+                    PrintBytes(bs.ToArray());
+                    break;
+                }
+                bs = bs.Slice(4);
+                if (bs.Count < 4)
+                {
+                    Console.WriteLine("Less than 8 bytes remaining!");
+                    PrintBytes(bs.ToArray());
+                    break;
+                }
+                int lenBytes = GetLen(bs.Slice(0, 4).ToArray());
+                if (bs.Count < 4 + lenBytes)
+                {
+                    Console.WriteLine($"Less than {4 + lenBytes} bytes remaining!");
+                    PrintBytes(bs.ToArray());
+                    break;
+                }
+                string str = bs.Slice(4, lenBytes).ToString(Encoding.UTF8);
+                Console.WriteLine($"Read frame {cnt} of length {lenBytes} - {str}");
+                bs = bs.Slice(4 + lenBytes);
+                cnt++;
+            }
+            Console.WriteLine($"Done printing log frames");
+        }
+
+        static void PrintBytes(byte[] bytes)
+        {
+            var sb = new StringBuilder("Printing byte array - {");
+            foreach (var b in bytes)
+            {
+                sb.Append(b + ", ");
+            }
+            sb.Append("}");
+            Console.WriteLine(sb.ToString());
+        }
+
+        static int GetLen(byte[] bytes)
+        {
+            Array.Reverse(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        static byte[] ReadStream(Stream s)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = s.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
             }
         }
     }
