8bd7a69db7d7acea4bfac0f8562d0af744ac0d44
mrohera <marohera@microsoft.com>
2019-04-10 12:22:05 -0700

    Disable access TPM in memory interface (#1059)
    Remove ability to configure an in memory TPM vs a HW TPM via an environment variable and added tests. This has caused failures when users have tried to use the in memory implementation which is useful only for testing.
    
    Essentially environment variable IOTEDGE_USE_TPM_DEVICE will be ignored by libiothsm and by default is built for use with a TPM device. To use the in memory implementation the library must be built using cmake flag USE_TEST_TPM_INTERFACE_IN_MEM.

--- edgelet/hsm-sys/azure-iot-hsm-c/src/hsm_client_tpm_select.c
+++ edgelet/hsm-sys/azure-iot-hsm-c/src/hsm_client_tpm_select.c
@@ -1,125 +1,39 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
-#include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
-#include "hsm_utils.h"
-#include "hsm_log.h"
+
 #include "hsm_client_tpm_device.h"
 #include "hsm_client_tpm_in_mem.h"
-
-extern const char* const ENV_TPM_SELECT;
-
-static int strcmp_i(const char* lhs, const char* rhs)
-{
-    char lc, rc;
-    int cmp = 0;
-    do
-    {
-        lc = *lhs++;
-        rc = *rhs++;
-        if ((tolower(lc) - tolower(rc)) != 0)
-        {
-            cmp = 1;
-        }
-    } while (lc != 0 && rc != 0);
-
-    return cmp;
-}
-
-// IF ENV_TPM_SELECT is set and not empty, "NO", "OFF" or "FALSE", then user wants to use the
-// TPM device for TPM functionality.
-static int use_tpm_device(bool *use_tpm)
-{
-    static const char * user_says_no[] = { "", "off", "no", "false" };
-    int array_size = sizeof(user_says_no)/sizeof(user_says_no[0]);
-    int result;
-    char * env_use_tpm;
-
-    *use_tpm = false;
-    if (hsm_get_env(ENV_TPM_SELECT, &env_use_tpm) != 0)
-    {
-        LOG_ERROR("Could not lookup env variable %s", ENV_TPM_SELECT);
-        result = __FAILURE__;
-    }
-    else
-    {
-        if (env_use_tpm != NULL)
-        {
-            *use_tpm = true;
-            for(int no = 0; no < array_size; no++)
-            {
-                if (strcmp_i(env_use_tpm, user_says_no[no]) == 0)
-                {
-                    *use_tpm = false;
-                    break;
-                }
-            }
-            free(env_use_tpm);
-        }
-        else
-        {
-            *use_tpm = false;
-        }
-        result = 0;
-    }
-
-    return result;
-}
-
-static bool g_use_tpm_device = false;
 
 int hsm_client_tpm_init(void)
 {
     int result;
-    bool use_tpm_flag = false;
-
-    if (use_tpm_device(&use_tpm_flag) != 0)
-    {
-        result = __FAILURE__;
-    }
-    else
-    {
-        if (use_tpm_flag)
-        {
-            result = hsm_client_tpm_device_init();
-            if (result == 0)
-            {
-                g_use_tpm_device = true;
-            }
-        }
-        else
-        {
-            result = hsm_client_tpm_store_init();
-        }
-    }
+    #ifdef TEST_TPM_INTERFACE_IN_MEM
+        result = hsm_client_tpm_store_init();
+    #else
+        result = hsm_client_tpm_device_init();
+    #endif
 
     return result;
 }
 
 void hsm_client_tpm_deinit(void)
 {
-    if (g_use_tpm_device)
-    {
-        hsm_client_tpm_device_deinit();
-    }
-    else
-    {
+    #ifdef TEST_TPM_INTERFACE_IN_MEM
         hsm_client_tpm_store_deinit();
-    }
+    #else
+        hsm_client_tpm_device_deinit();
+    #endif
 }
 
 const HSM_CLIENT_TPM_INTERFACE* hsm_client_tpm_interface(void)
 {
     const HSM_CLIENT_TPM_INTERFACE* result;
-    if (g_use_tpm_device)
-    {
-        result = hsm_client_tpm_device_interface();
-    }
-    else
-    {
+    #ifdef TEST_TPM_INTERFACE_IN_MEM
         result = hsm_client_tpm_store_interface();
-    }
+    #else
+        result = hsm_client_tpm_device_interface();
+    #endif
+
     return result;
 }
