1a9ce3dfb2495ec1af01c6c7407a53f6abd09900
Chandler Deng <chandde@microsoft.com>
2019-04-15 14:52:49 -0700

    add arm building scripts

---
+++ edgelet/build/windows/buildarm.ps1
@@ -0,0 +1,12 @@
+Write-Host "$env:PATH"
+Write-Host "$env:OPENSSL_DIR"
+Write-Host "$env:OPENSSL_ROOT_DIR"
+
+Write-Host "cargo.exe build --target thumbv7a-pc-windows-msvc --release"
+
+Invoke-Expression "cargo.exe build --target thumbv7a-pc-windows-msvc --release"
+
+if ($LastExitCode)
+{
+    Throw "cargo build failed with exit code $LastExitCode"
+}
