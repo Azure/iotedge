e9848b31f01cfc5c027a2ad3f806f803926ffb6b
chandde <chandde@microsoft.com>
2019-04-15 17:04:04 -0700

    Update buildarm.ps1

--- edgelet/build/windows/buildarm.ps1
+++ edgelet/build/windows/buildarm.ps1
@@ -4,6 +4,7 @@ Write-Host "$env:OPENSSL_ROOT_DIR"
 
 Write-Host "cargo.exe build --target thumbv7a-pc-windows-msvc --release"
 
+Invoke-Expression "cargo.exe --help" | Write-Host
 Invoke-Expression "cargo.exe build --target thumbv7a-pc-windows-msvc --release" | Write-Host
 
 if ($LastExitCode)
