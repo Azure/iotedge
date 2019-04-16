185c0cb662e6c38cbd7f6ef433f6498bce6fafcd
Mahesh Rohera <marohera@microsoft.com>
2019-04-15 17:21:53 -0700

    More clippy

--- edgelet/hsm-rs/src/x509.rs
+++ edgelet/hsm-rs/src/x509.rs
@@ -454,14 +454,14 @@ mod tests {
         if let Ok(x509_buffer) = result4 {
             assert_eq!(x509_buffer.as_ref(), DEFAULT_DIGEST);
         } else {
-            assert!(false);
+            panic!("Unexpected result");
         }
 
         let result5 = hsm_x509.get_certificate_info();
         if let Ok(cert_handle) = result5 {
             assert_eq!(TEST_RSA_CERT, cert_handle.pem().unwrap());
         } else {
-            assert!(false);
+            panic!("Unexpected result");
         }
     }
 
