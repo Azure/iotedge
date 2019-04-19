24ba2169915f14233f6a6ea8e3c8f6991076b73d
Chandler Deng <chandde@microsoft.com>
2019-04-19 16:57:25 -0700

    download dependencies from azure storage

--- edgelet/build/windows/util.ps1
+++ edgelet/build/windows/util.ps1
@@ -52,8 +52,15 @@ function Assert-Rust
 Param(
     [Switch] $Arm
 )
-    Write-Host "Validating Rust (stable-x86_64-pc-windows-msvc) is installed and up to date."
-    if (-not (Test-RustUp))
+    if($Arm)
+    {
+        if(-NOT (Test-Path "rust-windows-arm"))
+        {
+            # if the folder rust-windows-arm exists, we assume the private rust compiler for arm is installed
+            InstallWinArmPrivateRustCompiler
+        }
+    }
+    elseif (-not (Test-RustUp))
     {
         Write-Host "Installing rustup and stable-x86_64-pc-windows-msvc Rust."
         Invoke-RestMethod -usebasicparsing 'https://static.rust-lang.org/rustup/dist/i686-pc-windows-gnu/rustup-init.exe' -outfile 'rustup-init.exe'
@@ -83,11 +90,7 @@ Param(
 function InstallWinArmPrivateRustCompiler
 {
     $link = "https://iottools.blob.core.windows.net/iotedge-armtools/rust-windows-arm.zip"
-
     $ProgressPreference = 'SilentlyContinue'
     Invoke-WebRequest $link -out "rust-windows-arm.zip" -UseBasicParsing
-    if (Test-Path "rust-windows-arm") {
-        Remove-Item -Path "rust-windows-arm" -Recurse -Force
-    }
     Expand-Archive -Path "rust-windows-arm.zip" -DestinationPath "rust-windows-arm"
 }
