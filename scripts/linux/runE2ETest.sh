cef7aebffd046460e1a87df335f284e92fac8293
Philip Lin <philipktlin@live.com>
2019-04-18 20:37:06 -0700

    replace tab by spaces

--- scripts/linux/runE2ETest.sh
+++ scripts/linux/runE2ETest.sh
@@ -545,7 +545,7 @@ function run_quickstartcerts_test() {
         -p "$CONTAINER_REGISTRY_PASSWORD" \
         -t "$ARTIFACT_IMAGE_BUILD_NUMBER-linux-$image_architecture_label" \
         --leave-running=Core \
-		--optimize_for_performance="$optimize_for_performance" \
+        --optimize_for_performance="$optimize_for_performance" \
         --no-verify && ret=$? || ret=$?
 
     declare -a certs=( /var/lib/iotedge/hsm/certs/edge_owner_ca*.pem )
@@ -682,7 +682,7 @@ function run_tempsensor_test() {
         -p "$CONTAINER_REGISTRY_PASSWORD" \
         -n "$(hostname)" \
         -tw "$E2E_TEST_DIR/artifacts/core-linux/e2e_test_files/twin_test_tempSensor.json" \
-		--optimize_for_performance="$optimize_for_performance" \
+        --optimize_for_performance="$optimize_for_performance" \
         -t "$ARTIFACT_IMAGE_BUILD_NUMBER-linux-$image_architecture_label" && ret=$? || ret=$?
 
     local elapsed_seconds=$SECONDS
