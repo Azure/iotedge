3db2e4529079a64a0b6ff1715235dcba3450e23e
dmolokanov <dmolokanov@users.noreply.github.com>
2019-04-19 09:00:38 -0700

    Check pid against running modules

--- edgelet/edgelet-docker/src/identity.rs
+++ edgelet/edgelet-docker/src/identity.rs
@@ -3,15 +3,20 @@
 #![deny(rust_2018_idioms, warnings)]
 #![deny(clippy::all, clippy::pedantic)]
 #![allow(clippy::module_name_repetitions, clippy::use_self)]
+#![allow(unused)] // todo remove
 
 use std::marker::PhantomData;
 
-use crate::DockerModuleRuntime;
+use crate::runtime::DockerModuleRuntime;
 use edgelet_core::pid::Pid;
-use edgelet_core::AuthId;
+use edgelet_core::{AuthId, Error, ErrorKind, Module, ModuleRuntime};
 use edgelet_http::NewIdService;
+use failure::Fail;
+use futures::future::Either;
+use futures::{future, Future};
 use hyper::service::Service;
 use hyper::{Body, Error as HyperError, Request};
+use log::info;
 use tokio::prelude::Stream;
 
 #[derive(Clone)]
@@ -33,9 +38,10 @@ impl<S> DockerIdService<S> {
 
 impl<S> Service for DockerIdService<S>
 where
-    S: Service<ReqBody = Body>,
+    S: Service<ReqBody = Body> + Sync,
     <S as Service>::ResBody: Stream<Error = HyperError> + 'static,
     <<S as Service>::ResBody as Stream>::Item: AsRef<[u8]>,
+    S::Future: Send + 'static,
 {
     type ReqBody = S::ReqBody;
     type ResBody = S::ResBody;
@@ -43,24 +49,38 @@ where
     type Future = S::Future;
 
     fn call(&mut self, req: Request<Self::ReqBody>) -> Self::Future {
-        //        self.runtime.list_with_details()
-        //            .map_err(|e| Error::from(e.context(ErrorKind::ModuleRuntime)))
-
-        //        let auth_id = match self.pid {
-        //            Pid::None => AuthId::None,
-        //            Pid::Any => AuthId::Any,
-        //            Pid::Value(pid) => match self.get_module_name(pid) {
-        //                Some(name) => AuthId::Value(name),
-        //                _ => AuthId::None,
-        //            },
-        //        };
-        //
-
-        let auth_id = AuthId::None;
-
         let mut req = req;
-        req.extensions_mut().insert(auth_id);
 
+        let auth_id = match self.pid {
+            Pid::None => AuthId::None,
+            Pid::Any => AuthId::Any,
+            Pid::Value(pid) => {
+                self.runtime
+                    .list_with_details()
+                    .map_err(|e| Error::from(e.context(ErrorKind::ModuleRuntime)))
+                    .filter_map(move |(m, rs)| {
+                        // todo should check against the list of processes
+                        if rs.pid() == Pid::Value(pid) {
+                            Some(m)
+                        } else {
+                            None
+                        }
+                    })
+                    .into_future()
+                    .then(move |result| match result {
+                        Ok((Some(m), _)) => Ok(AuthId::Value(m.name().to_string())),
+                        Ok((None, _)) => {
+                            info!("Unable to find a module for caller pid: {}", pid);
+                            Ok(AuthId::None)
+                        }
+                        Err((err, _)) => Err(err),
+                    })
+                    .wait()
+                    .unwrap() // todo what should do in case of error?
+            }
+        };
+
+        req.extensions_mut().insert(auth_id);
         self.inner.call(req)
     }
 }
@@ -82,9 +102,10 @@ impl<S> DockerNewIdService<S> {
 
 impl<S> NewIdService for DockerNewIdService<S>
 where
-    S: Service<ReqBody = Body>,
-    <S as Service>::ResBody: Stream<Error = HyperError> + 'static,
-    <<S as Service>::ResBody as Stream>::Item: AsRef<[u8]>,
+    S: Service<ReqBody = Body> + Sync,
+    S::ResBody: Stream<Error = HyperError> + 'static,
+    <S::ResBody as Stream>::Item: AsRef<[u8]>,
+    S::Future: Send + 'static,
 {
     type IdService = DockerIdService<S>;
     type InnerService = S;
