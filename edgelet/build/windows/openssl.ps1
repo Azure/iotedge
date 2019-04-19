cf61a674024257d29a3624b6cc37a9a476025181
Chandler Deng <chandde@microsoft.com>
2019-04-19 16:44:15 -0700

    download dependencies from azure storage

--- edgelet/build/windows/openssl.ps1
+++ edgelet/build/windows/openssl.ps1
@@ -8,22 +8,22 @@ function Get-OpenSSL
 
     $ErrorActionPreference = 'Continue'
 
-    if (!((Test-Path -Path $env:HOMEDRIVE\vcpkg) -and ((Test-Path -Path $env:HOMEDRIVE\vcpkg\vcpkg.exe))))
+    if (!((Test-Path -Path $env:SystemDrive\vcpkg) -and ((Test-Path -Path $env:SystemDrive\vcpkg\vcpkg.exe))))
     {
         Write-Host "Installing vcpkg from github..."
-        git clone https://github.com/Microsoft/vcpkg $env:HOMEDRIVE\vcpkg
+        git clone https://github.com/Microsoft/vcpkg $env:SystemDrive\vcpkg
         if ($LastExitCode)
         {
             Throw "Failed to clone vcpkg repo with exit code $LastExitCode"
         }
         Write-Host "Bootstrapping vcpkg..."
-        & "$env:HOMEDRIVE\vcpkg\bootstrap-vcpkg.bat"
+        & "$env:SystemDrive\vcpkg\bootstrap-vcpkg.bat"
         if ($LastExitCode)
         {
             Throw "Failed to bootstrap vcpkg with exit code $LastExitCode"
         }
         Write-Host "Installing vcpkg..."
-        & $env:HOMEDRIVE\\vcpkg\\vcpkg.exe integrate install
+        & $env:SystemDrive\\vcpkg\\vcpkg.exe integrate install
         if ($LastExitCode)
         {
             Throw "Failed to install vcpkg with exit code $LastExitCode"
@@ -31,13 +31,13 @@ function Get-OpenSSL
     }
 
     Write-Host "Downloading strawberry perl"
-    if (!(Test-Path -Path $env:HOMEDRIVE\vcpkg\Downloads))
+    if (!(Test-Path -Path $env:SystemDrive\vcpkg\Downloads))
     {
-        New-Item -Type Directory "$env:HOMEDRIVE\vcpkg\Downloads" | Out-Null
+        New-Item -Type Directory "$env:SystemDrive\vcpkg\Downloads" | Out-Null
     }
     
     # $strawberryPerlUri = "https://edgebuild.blob.core.windows.net/strawberry-perl/strawberry-perl-5.24.1.1-32bit-portable.zip"
-    # $strawberryPerlPath = "$env:HOMEDRIVE\vcpkg\Downloads\strawberry-perl-5.24.1.1-32bit-portable.zip"
+    # $strawberryPerlPath = "$env:SystemDrive\vcpkg\Downloads\strawberry-perl-5.24.1.1-32bit-portable.zip"
     # Invoke-WebRequest -Uri $strawberryPerlUri -OutFile $strawberryPerlPath
 
     Get-ChildItem Env:
@@ -45,7 +45,7 @@ function Get-OpenSSL
     if($Arm)
     {
         Write-Host "Installing OpenSSL for arm..."
-        & $env:HOMEDRIVE\\vcpkg\\vcpkg.exe install openssl-windows:arm-windows
+        & $env:SystemDrive\\vcpkg\\vcpkg.exe install openssl-windows:arm-windows
         if ($LastExitCode)
         {
             Throw "Failed to install openssl vcpkg with exit code $LastExitCode"
@@ -54,7 +54,7 @@ function Get-OpenSSL
     else
     {
         Write-Host "Installing OpenSSL for x64..."
-        & $env:HOMEDRIVE\\vcpkg\\vcpkg.exe install openssl:x64-windows
+        & $env:SystemDrive\\vcpkg\\vcpkg.exe install openssl:x64-windows
         if ($LastExitCode)
         {
             Throw "Failed to install openssl vcpkg with exit code $LastExitCode"
@@ -67,16 +67,16 @@ function Get-OpenSSL
         # When executing within TF (VSTS) environment, install the env variable
         # such that all follow up build tasks have visibility of the env variable
         Write-Host "VSTS installation detected"
-        Write-Host "##vso[task.setvariable variable=OPENSSL_ROOT_DIR;]$env:HOMEDRIVE\vcpkg\installed\$(if($Arm){ "arm" }else{ "x64"})-windows"
+        Write-Host "##vso[task.setvariable variable=OPENSSL_ROOT_DIR;]$env:SystemDrive\vcpkg\installed\$(if($Arm){ "arm" }else{ "x64"})-windows"
         # Rust's openssl-sys crate needs this environment set.
-        Write-Host "##vso[task.setvariable variable=OPENSSL_DIR;]$env:HOMEDRIVE\vcpkg\installed\$(if($Arm){ "arm" }else{ "x64"})-windows"
+        Write-Host "##vso[task.setvariable variable=OPENSSL_DIR;]$env:SystemDrive\vcpkg\installed\$(if($Arm){ "arm" }else{ "x64"})-windows"
     }
     else
     {
         # for local installation, set the env variable within the USER scope
         Write-Host "Local installation detected"
-        [System.Environment]::SetEnvironmentVariable("OPENSSL_ROOT_DIR", "$env:HOMEDRIVE\vcpkg\installed\$(if($Arm){ "arm" }else{ "x64"})-windows", [System.EnvironmentVariableTarget]::User)
-        [System.Environment]::SetEnvironmentVariable("OPENSSL_DIR", "$env:HOMEDRIVE\vcpkg\installed\$(if($Arm){ "arm" }else{ "x64"})-windows", [System.EnvironmentVariableTarget]::User)
+        [System.Environment]::SetEnvironmentVariable("OPENSSL_ROOT_DIR", "$env:SystemDrive\vcpkg\installed\$(if($Arm){ "arm" }else{ "x64"})-windows", [System.EnvironmentVariableTarget]::User)
+        [System.Environment]::SetEnvironmentVariable("OPENSSL_DIR", "$env:SystemDrive\vcpkg\installed\$(if($Arm){ "arm" }else{ "x64"})-windows", [System.EnvironmentVariableTarget]::User)
     }
     
     $ErrorActionPreference = 'Stop'
