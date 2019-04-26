20c11bfdc33b5aea25acdee64bf865859799382f
Jonathan Leung <jonatl@microsoft.com>
2019-04-26 00:52:45 -0700

    Remove unecessary escape

--- scripts/windows/test/Run-E2ETest.ps1
+++ scripts/windows/test/Run-E2ETest.ps1
@@ -378,9 +378,8 @@ Function PrepareTestFromArtifacts
 
                 (Get-Content $DeploymentWorkingFilePath).replace('<Analyzer.EventHubConnectionString>',$EventHubConnectionString) | Set-Content $DeploymentWorkingFilePath
                 (Get-Content $DeploymentWorkingFilePath).replace('<LoadGen.MessageFrequency>',$LoadGenMessageFrequency) | Set-Content $DeploymentWorkingFilePath
-                $escapedSnitchAlertURL = convertto-json $SnitchAlertUrl
                 $escapedBuildId= $ArtifactImageBuildNumber -replace ".",""
-                (Get-Content $DeploymentWorkingFilePath).replace('<Snitch.AlertUrl>',$escapedSnitchAlertURL) | Set-Content $DeploymentWorkingFilePath
+                (Get-Content $DeploymentWorkingFilePath).replace('<Snitch.AlertUrl>',$SnitchAlertUrl) | Set-Content $DeploymentWorkingFilePath
                 (Get-Content $DeploymentWorkingFilePath).replace('<Snitch.BuildNumber>',$SnitchBuildNumber) | Set-Content $DeploymentWorkingFilePath
                 (Get-Content $DeploymentWorkingFilePath).replace('<Snitch.BuildId>',"$SnitchBuildNumber-$(GetImageArchitectureLabel)-linux-$escapedBuildId") | Set-Content $DeploymentWorkingFilePath
                 (Get-Content $DeploymentWorkingFilePath).replace('<Snitch.ReportingIntervalInSecs>',$SnitchReportingIntervalInSecs) | Set-Content $DeploymentWorkingFilePath
