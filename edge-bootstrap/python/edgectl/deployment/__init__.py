75b13902a2ee30e37852eb3af476fc11fbcc04f0
Mike Yagley <myagley@gmail.com>
2019-05-10 13:14:19 -0700

    Remove unmaintained samples (#1112)

--- edge-bootstrap/python/edgectl/deployment/__init__.py
+++
@@ -1,3 +0,0 @@
-from edgectl.deployment.commandbase import *
-from edgectl.deployment.commandfactory import EdgeCommandFactory
-from edgectl.deployment.deploymentdocker import EdgeDeploymentCommandDocker
