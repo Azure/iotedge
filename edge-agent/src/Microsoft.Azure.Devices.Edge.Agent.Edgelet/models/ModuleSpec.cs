b665c09ced500ccf75d79da5482f593bf37b0c36
Shantanu Singh <shsi@microsoft.com>
2019-05-17 14:40:47 -0700

    minor fix

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Edgelet/models/ModuleSpec.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Edgelet/models/ModuleSpec.cs
@@ -14,7 +14,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Edgelet.Models
 
         public ModuleSpec(string name, string type, PullPolicy pullPolicy, object settings, IEnumerable<EnvVar> environmentVariables)
         {
-            this.PullPolicy = PullPolicy;
+            this.PullPolicy = pullPolicy;
             this.Name = Preconditions.CheckNonWhiteSpace(name, nameof(name));
             this.Type = Preconditions.CheckNonWhiteSpace(type, nameof(type));
             this.Settings = Preconditions.CheckNotNull(settings, nameof(settings));
