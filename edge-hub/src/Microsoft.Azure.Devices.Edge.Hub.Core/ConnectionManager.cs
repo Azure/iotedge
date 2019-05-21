4631b19232759d0428ea4272679951063ddf294e
Varun Puranik <vpuranik@microsoft.com>
2019-05-20 18:51:44 -0700

    Add support for retrying CloudProxy

--- edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/ConnectionManager.cs
+++ edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/ConnectionManager.cs
@@ -82,15 +82,23 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core
 
         public async Task<Option<ICloudProxy>> GetCloudConnection(string id)
         {
+            Try<ICloudProxy> cloudProxyTry = await this.TryGetCloudConnection(id);
+            return cloudProxyTry
+                .Ok()
+                .Map(c => (ICloudProxy)new RetryingCloudProxy(() => this.TryGetCloudConnection(id), c));
+        }
+
+        async Task<Try<ICloudProxy>> TryGetCloudConnection(string id)
+        {
             IIdentity identity = this.identityProvider.Create(Preconditions.CheckNonWhiteSpace(id, nameof(id)));
             ConnectedDevice device = this.GetOrCreateConnectedDevice(identity);
 
             Try<ICloudConnection> cloudConnectionTry = await device.GetOrCreateCloudConnection(
-                c => this.cloudConnectionProvider.Connect(c.Identity, (i, status) => this.CloudConnectionStatusChangedHandler(i, status)));
+                c => this.cloudConnectionProvider.Connect(c.Identity, this.CloudConnectionStatusChangedHandler));
 
             Events.GetCloudConnection(device.Identity, cloudConnectionTry);
             Try<ICloudProxy> cloudProxyTry = GetCloudProxyFromCloudConnection(cloudConnectionTry, device.Identity);
-            return cloudProxyTry.Ok();
+            return cloudProxyTry;
         }
 
         public void AddSubscription(string id, DeviceSubscription deviceSubscription)
@@ -154,7 +162,7 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core
         }
 
         static Try<ICloudProxy> GetCloudProxyFromCloudConnection(Try<ICloudConnection> cloudConnection, IIdentity identity) => cloudConnection.Success
-            ? cloudConnection.Value.CloudProxy.Map(cp => Try.Success(cp))
+            ? cloudConnection.Value.CloudProxy.Map(Try.Success)
                 .GetOrElse(() => Try<ICloudProxy>.Failure(new EdgeHubConnectionException($"Unable to get cloud proxy for device {identity.Id}")))
             : Try<ICloudProxy>.Failure(cloudConnection.Exception);
 
---
+++ edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/cloud/RetryingCloudProxy.cs
@@ -0,0 +1,97 @@
+// Copyright (c) Microsoft. All rights reserved.
+namespace Microsoft.Azure.Devices.Edge.Hub.Core.Cloud
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Edge.Util;
+    using Nito.AsyncEx;
+
+    public class RetryingCloudProxy : ICloudProxy
+    {
+        readonly AsyncLock cloudProxyLock = new AsyncLock();
+        readonly Func<Task<Try<ICloudProxy>>> cloudProxyGetter;
+
+        ICloudProxy cloudProxyImplementation;
+
+        public RetryingCloudProxy(Func<Task<Try<ICloudProxy>>> cloudProxyGetter, ICloudProxy cloudProxyImplementation)
+        {
+            this.cloudProxyGetter = Preconditions.CheckNotNull(cloudProxyGetter, nameof(cloudProxyGetter));
+            this.cloudProxyImplementation = Preconditions.CheckNotNull(cloudProxyImplementation, nameof(cloudProxyImplementation));
+        }
+
+        public bool IsActive => this.cloudProxyImplementation.IsActive;
+
+        public Task<bool> CloseAsync() => this.ExecuteOperation(c => c.CloseAsync());
+
+        public Task<bool> OpenAsync() => this.ExecuteOperation(c => c.OpenAsync());
+
+        public Task SendMessageAsync(IMessage message) => this.ExecuteOperation(c => c.SendMessageAsync(message));
+
+        public Task SendMessageBatchAsync(IEnumerable<IMessage> inputMessages) => this.ExecuteOperation(c => c.SendMessageBatchAsync(inputMessages));
+
+        public Task UpdateReportedPropertiesAsync(IMessage reportedPropertiesMessage) => this.ExecuteOperation(c => c.UpdateReportedPropertiesAsync(reportedPropertiesMessage));
+
+        public Task<IMessage> GetTwinAsync() => this.ExecuteOperation(c => c.GetTwinAsync());
+
+        public Task SendFeedbackMessageAsync(string messageId, FeedbackStatus feedbackStatus) => this.ExecuteOperation(c => c.SendFeedbackMessageAsync(messageId, feedbackStatus));
+
+        public Task SetupCallMethodAsync() => this.ExecuteOperation(c => c.SetupCallMethodAsync());
+
+        public Task RemoveCallMethodAsync() => this.ExecuteOperation(c => c.RemoveCallMethodAsync());
+
+        public Task SetupDesiredPropertyUpdatesAsync() => this.ExecuteOperation(c => c.SetupDesiredPropertyUpdatesAsync());
+
+        public Task RemoveDesiredPropertyUpdatesAsync() => this.ExecuteOperation(c => c.RemoveDesiredPropertyUpdatesAsync());
+
+        public Task StartListening() => this.ExecuteOperation(c => c.StartListening());
+
+        Task ExecuteOperation(Func<ICloudProxy, Task> func) => this.ExecuteOperation(
+            async c =>
+            {
+                await func(c);
+                return 1;
+            });
+
+        async Task<T> ExecuteOperation<T>(Func<ICloudProxy, Task<T>> func)
+        {
+            ICloudProxy cloudProxy = await this.GetCloudProxy();
+            try
+            {
+                return await func(cloudProxy);
+            }
+            catch (Exception)
+            {
+                if (cloudProxy.IsActive)
+                {
+                    throw;
+                }
+
+                cloudProxy = await this.GetCloudProxy();
+                return await func(cloudProxy);
+            }
+        }
+
+        async Task<ICloudProxy> GetCloudProxy()
+        {
+            if (!this.cloudProxyImplementation.IsActive)
+            {
+                using (await this.cloudProxyLock.LockAsync())
+                {
+                    if (!this.cloudProxyImplementation.IsActive)
+                    {
+                        Try<ICloudProxy> cloudProxyTry = await this.cloudProxyGetter();
+                        if (!cloudProxyTry.Success)
+                        {
+                            throw new EdgeHubIOException("Unable to create IoTHub connection", cloudProxyTry.Exception);
+                        }
+
+                        this.cloudProxyImplementation = cloudProxyTry.Value;
+                    }
+                }
+            }
+
+            return this.cloudProxyImplementation;
+        }
+    }
+}
