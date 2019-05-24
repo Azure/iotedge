52209d62a8dee298644b81583ff465b5f9b93e03
Damon Barry <damonb@microsoft.com>
2019-05-24 10:31:08 -0700

    Move timeout logic into test setup/teardown

--- test/Microsoft.Azure.Devices.Edge.Test/EndToEnd.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/EndToEnd.cs
@@ -10,79 +10,90 @@ namespace Microsoft.Azure.Devices.Edge.Test
 
     public class EndToEnd
     {
+        CancellationTokenSource cts;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.cts = new CancellationTokenSource(Context.Current.TestTimeout);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Log.Information("Dispose token");
+        }
+
         [Test]
         public async Task TempSensor()
         {
-            string name = "temp sensor";
             string edgeAgent = Context.Current.EdgeAgent.Expect(() => new ArgumentException());
             string edgeHub = Context.Current.EdgeHub.Expect(() => new ArgumentException());
             string tempSensor = Context.Current.TempSensor.Expect(() => new ArgumentException());
 
-            using (var cts = new CancellationTokenSource(Context.Current.TestTimeout))
-            {
-                CancellationToken token = cts.Token;
+            CancellationToken token = this.cts.Token;
 
-                Log.Information("Running test '{Name}'", name);
-                await Profiler.Run(
-                    async () =>
-                    {
-                        var iotHub = new IotHub(
-                            Context.Current.ConnectionString,
-                            Context.Current.EventHubEndpoint,
-                            Context.Current.Proxy);
+            string name = "temp sensor";
+            Log.Information("Running test '{Name}'", name);
+            await Profiler.Run(
+                async () =>
+                {
+                    var iotHub = new IotHub(
+                        Context.Current.ConnectionString,
+                        Context.Current.EventHubEndpoint,
+                        Context.Current.Proxy);
 
-                        EdgeDevice device = await EdgeDevice.GetOrCreateIdentityAsync(
-                            Context.Current.DeviceId,
-                            iotHub,
-                            token);
+                    EdgeDevice device = await EdgeDevice.GetOrCreateIdentityAsync(
+                        Context.Current.DeviceId,
+                        iotHub,
+                        token);
 
-                        var config = new EdgeConfiguration(device.Id, edgeAgent, iotHub);
-                        Context.Current.Registry.ForEach(
-                            r => config.AddRegistryCredentials(r.address, r.username, r.password));
-                        config.AddEdgeHub(edgeHub);
-                        Context.Current.Proxy.ForEach(p => config.AddProxy(p));
-                        config.AddModule("tempSensor", tempSensor);
-                        await config.DeployAsync(token);
+                    var config = new EdgeConfiguration(device.Id, edgeAgent, iotHub);
+                    Context.Current.Registry.ForEach(
+                        r => config.AddRegistryCredentials(r.address, r.username, r.password));
+                    config.AddEdgeHub(edgeHub);
+                    Context.Current.Proxy.ForEach(p => config.AddProxy(p));
+                    config.AddModule("tempSensor", tempSensor);
+                    await config.DeployAsync(token);
 
-                        var hub = new EdgeModule("edgeHub", device.Id, iotHub);
-                        var sensor = new EdgeModule("tempSensor", device.Id, iotHub);
-                        await EdgeModule.WaitForStatusAsync(
-                            new[] { hub, sensor },
-                            EdgeModuleStatus.Running,
-                            token);
-                        await sensor.WaitForEventsReceivedAsync(token);
+                    var hub = new EdgeModule("edgeHub", device.Id, iotHub);
+                    var sensor = new EdgeModule("tempSensor", device.Id, iotHub);
+                    await EdgeModule.WaitForStatusAsync(
+                        new[] { hub, sensor },
+                        EdgeModuleStatus.Running,
+                        token);
+                    await sensor.WaitForEventsReceivedAsync(token);
 
-                        var sensorTwin = new ModuleTwin(sensor.Id, device.Id, iotHub);
-                        await sensorTwin.UpdateDesiredPropertiesAsync(
-                            new
+                    var sensorTwin = new ModuleTwin(sensor.Id, device.Id, iotHub);
+                    await sensorTwin.UpdateDesiredPropertiesAsync(
+                        new
+                        {
+                            properties = new
                             {
-                                properties = new
+                                desired = new
                                 {
-                                    desired = new
-                                    {
-                                        SendData = true,
-                                        SendInterval = 10
-                                    }
+                                    SendData = true,
+                                    SendInterval = 10
                                 }
-                            },
-                            token);
-                        await sensorTwin.WaitForReportedPropertyUpdatesAsync(
-                            new
+                            }
+                        },
+                        token);
+                    await sensorTwin.WaitForReportedPropertyUpdatesAsync(
+                        new
+                        {
+                            properties = new
                             {
-                                properties = new
+                                reported = new
                                 {
-                                    reported = new
-                                    {
-                                        SendData = true,
-                                        SendInterval = 10
-                                    }
+                                    SendData = true,
+                                    SendInterval = 10
                                 }
-                            },
-                            token);
-                    },
-                    "Completed test '{Name}'",
-                    name);
-            }
+                            }
+                        },
+                        token);
+                },
+                "Completed test '{Name}'",
+                name);
         }
     }
 }
