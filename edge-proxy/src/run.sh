5371c97743acf7d8e2945d5a3e28343fbc3c2e11
dmolokanov <dmolokanov@users.noreply.github.com>
2019-05-30 16:26:30 -0700

    Fixed shellcheck warnings for run.sh

--- edge-proxy/src/run.sh
+++ edge-proxy/src/run.sh
@@ -3,16 +3,16 @@ set -e
 
 # read service account token from filesystem
 file="/var/run/secrets/kubernetes.io/serviceaccount/token"
-if [[ -e ${file} ]]
+if [ -e "$file" ]
 then
-    token=`cat ${file}`
+    token=$(cat "$file")
 else
-    echo "Could not find file ${file}"
+    echo "Could not find file $file"
     exit 1
 fi
 
 config_src="/etc/traefik/traefik.toml"
-if [[ ! -e ${config_src} ]]
+if [ ! -e "$config_src" ]
 then
     echo "Could not find config ${config_src}"
     exit 1
@@ -20,12 +20,12 @@ fi
 
 # move traefik config to a new place to allow modification
 config_dst="/traefik.toml"
-cp ${config_src} ${config_dst}
+cp "$config_src" "$config_dst"
 
 # find TOKEN placeholders and replace them with a token value
-if [[ ! -z ${token} ]]
+if [ -n "$token" ]
 then
-    sed -i -e 's/__TOKEN__/'${token}'/g' ${config_dst}
+    sed -i -e "s/__TOKEN__/$token/g" "$config_dst"
 fi
 
 # call traefik entry point script with a new config
