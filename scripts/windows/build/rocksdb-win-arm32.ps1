8a7f2e241192135cfeb0f28d1e447c1cdd71a431
Chandler Deng <chandde@microsoft.com>
2019-06-18 17:04:18 -0700

    use a different vcpkg clone

--- scripts/windows/build/rocksdb-win-arm32.ps1
+++ scripts/windows/build/rocksdb-win-arm32.ps1
@@ -4,10 +4,10 @@ function Get-Rocksdb
 {
     $ErrorActionPreference = 'Continue'
 
-    if (!(Test-Path -Path $env:HOMEDRIVE\vcpkg))
+    if (!(Test-Path -Path $env:HOMEDRIVE\vcpkg1))
     {
         Write-Host "git clone vcpkg"
-        git clone https://github.com/Microsoft/vcpkg $env:HOMEDRIVE\vcpkg
+        git clone https://github.com/Microsoft/vcpkg $env:HOMEDRIVE\vcpkg1
         if ($LastExitCode)
         {
             Throw "Failed to clone vcpkg repo with exit code $LastExitCode"
@@ -18,33 +18,35 @@ function Get-Rocksdb
     # bb1bb1c94a72b891883efa6522791620ef3bbc0f maps to 5.17.2
     # https://github.com/microsoft/vcpkg/commit/bb1bb1c94a72b891883efa6522791620ef3bbc0f#diff-87525ccf58925648e3f92fec94d01d70
     
-    push-location $env:HOMEDRIVE\vcpkg
+    push-location $env:HOMEDRIVE\vcpkg1
     git pull
     git checkout bb1bb1c94a72b891883efa6522791620ef3bbc0f ports\rocksdb
     pop-location    
      
     Write-Host "always rerun bootstrap-vcpkg.bat"
-    & "$env:HOMEDRIVE\vcpkg\bootstrap-vcpkg.bat"
+    & "$env:HOMEDRIVE\vcpkg1\bootstrap-vcpkg.bat"
     if ($LastExitCode)
     {
         Throw "Failed to bootstrap vcpkg with exit code $LastExitCode"
     }
         
     Write-Host "vcpkg.exe integrate install"
-    & $env:HOMEDRIVE\\vcpkg\\vcpkg.exe integrate install
+    & $env:HOMEDRIVE\\vcpkg1\\vcpkg.exe integrate install
     if ($LastExitCode)
     {
         Throw "Failed to install vcpkg with exit code $LastExitCode"
     }
     
+    push-location $env:HOMEDRIVE\vcpkg1
     Write-Host "vcpkg.exe install rocksdb:arm-windows"
-    & $env:HOMEDRIVE\vcpkg\vcpkg.exe install rocksdb:arm-windows
+    vcpkg.exe install rocksdb:arm-windows
+    pop-location
     if ($LastExitCode)
     {
-        Throw "Failed to install openssl vcpkg with exit code $LastExitCode"
+        Throw "Failed to install rocksdb vcpkg with exit code $LastExitCode"
     }
     
     $ErrorActionPreference = 'Stop'
     
-    return "$env:HOMEDRIVE\vcpkg\installed\arm-windows\bin\rocksdb-shared.dll"
+    return "$env:HOMEDRIVE\vcpkg1\installed\arm-windows\bin\rocksdb-shared.dll"
 }
