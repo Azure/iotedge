b0431a196d30900b9ea1e757feafb126359524b3
Varun Puranik <vpuranik@microsoft.com>
2019-06-20 11:43:20 -0700

    Update edge-util/src/Microsoft.Azure.Devices.Edge.Util/metrics/MetricsConstants.cs
    Co-Authored-By: Mike Yagley <myagley@gmail.com>

--- edge-util/src/Microsoft.Azure.Devices.Edge.Util/metrics/MetricsConstants.cs
+++ edge-util/src/Microsoft.Azure.Devices.Edge.Util/metrics/MetricsConstants.cs
@@ -3,7 +3,7 @@ namespace Microsoft.Azure.Devices.Edge.Util.Metrics
 {
     public class MetricsConstants
     {
-        public const string EdgeHubMetricPrefix = "edge_hub";
+        public const string EdgeHubMetricPrefix = "edgehub";
         public const string DeviceIdLabel = "edge_device";
         public const string IotHubLabel = "iot_hub";
     }
