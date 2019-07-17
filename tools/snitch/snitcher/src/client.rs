72382775b144e06ed6391e2a4eca4de13d0319e4
Rajasekharan Vengalil <rajave@microsoft.com>
2019-07-17 16:25:47 -0700

    Fix building of Hyper URI

--- tools/snitch/snitcher/src/client.rs
+++ tools/snitch/snitcher/src/client.rs
@@ -5,9 +5,9 @@ use std::str;
 use std::sync::{Arc, Mutex};
 
 use bytes::Bytes;
+use edgelet_http::UrlConnector;
 use futures::future::{self, IntoFuture};
 use futures::{Future, Stream};
-use http::Uri;
 use hyper::header::{HeaderValue, CONTENT_LENGTH, CONTENT_TYPE, IF_MATCH};
 use hyper::service::Service;
 use hyper::{Body, Error as HyperError, Method, Request};
@@ -62,23 +62,17 @@ where
 
         let url_copy = self.host_name.clone();
         let path_copy = path.to_owned();
-        self.host_name
-            // build the full url
-            .join(&format!("{}?{}", path, query))
+
+        let scheme = self.host_name.scheme();
+        let base_path = self.host_name.path();
+        let path = format!("{}?{}", path, query);
+        UrlConnector::build_hyper_uri(scheme, base_path, &path)
             .map_err(Error::from)
             .and_then(|url| {
                 debug!("Making HTTP request with URL: {}", url);
 
-                // NOTE: 'expect' here should be OK, because this is a type
-                // conversion from url::Url to hyper::Uri and not really a URL
-                // parse operation. At this point the URL has already been parsed
-                // and is known to be good.
                 let mut builder = Request::builder();
-                let req = builder.method(method).uri(
-                    url.as_str()
-                        .parse::<Uri>()
-                        .expect("Unexpected Url to Uri conversion failure"),
-                );
+                let req = builder.method(method).uri(url);
 
                 // add an `If-Match: "*"` header if we've been asked to
                 if add_if_match {
