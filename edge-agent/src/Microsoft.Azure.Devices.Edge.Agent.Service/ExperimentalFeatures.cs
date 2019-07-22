b9f394d5c89cf4ee2313918557217abff2cdee5b
Varun Puranik <vpuranik@microsoft.com>
2019-07-22 14:00:16 -0700

    Add experimental flag for GetLogs request

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Service/ExperimentalFeatures.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Service/ExperimentalFeatures.cs
@@ -7,11 +7,12 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Service
 
     public class ExperimentalFeatures
     {
-        ExperimentalFeatures(bool enabled, bool disableCloudSubscriptions, bool enableUploadLogs)
+        ExperimentalFeatures(bool enabled, bool disableCloudSubscriptions, bool enableUploadLogs, bool enableGetLogs)
         {
             this.Enabled = enabled;
             this.DisableCloudSubscriptions = disableCloudSubscriptions;
             this.EnableUploadLogs = enableUploadLogs;
+            this.EnableGetLogs = enableGetLogs;
         }
 
         public static ExperimentalFeatures Init(IConfiguration experimentalFeaturesConfig, ILogger logger)
@@ -19,7 +20,8 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Service
             bool enabled = experimentalFeaturesConfig.GetValue("enabled", false);
             bool disableCloudSubscriptions = enabled && experimentalFeaturesConfig.GetValue("disableCloudSubscriptions", false);
             bool enableUploadLogs = enabled && experimentalFeaturesConfig.GetValue("enableUploadLogs", false);
-            var experimentalFeatures = new ExperimentalFeatures(enabled, disableCloudSubscriptions, enableUploadLogs);
+            bool enableGetLogs = enabled && experimentalFeaturesConfig.GetValue("enableGetLogs", false);
+            var experimentalFeatures = new ExperimentalFeatures(enabled, disableCloudSubscriptions, enableUploadLogs, enableGetLogs);
             logger.LogInformation($"Experimental features configuration: {experimentalFeatures.ToJson()}");
             return experimentalFeatures;
         }
@@ -29,5 +31,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Service
         public bool DisableCloudSubscriptions { get; }
 
         public bool EnableUploadLogs { get; }
+
+        public bool EnableGetLogs { get; }
     }
 }
