18b00a21e5838ac4b24c4ae4acf2eb9c18fecf0d
Damon Barry <damonb@microsoft.com>
2019-07-23 12:51:15 -0700

    Revert "Reduce duplication in YamlDocument"
    This reverts commit d491c1250ca9cb8922f107219e23d43eb8dcebb1.

--- test/Microsoft.Azure.Devices.Edge.Test.Common/YamlDocument.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/YamlDocument.cs
@@ -2,67 +2,61 @@
 
 namespace Microsoft.Azure.Devices.Edge.Test.Common
 {
-    using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
     using YamlDotNet.Serialization;
-    using Node = System.Collections.Generic.Dictionary<object, object>;
 
     class YamlDocument
     {
-        readonly Node root;
+        readonly Dictionary<object, object> root;
 
         public YamlDocument(string input)
         {
             var reader = new StringReader(input);
             var deserializer = new Deserializer();
-            this.root = (Node)deserializer.Deserialize(reader);
+            this.root = (Dictionary<object, object>)deserializer.Deserialize(reader);
         }
 
         public void ReplaceOrAdd(string dottedKey, string value)
         {
-            this.Do(
-                dottedKey,
-                (innerNode, missingKey) =>
+            Dictionary<object, object> node = this.root;
+            string[] segments = dottedKey.Split('.');
+            foreach (string key in segments.SkipLast(1))
+            {
+                if (!node.ContainsKey(key))
                 {
-                    innerNode.Add(missingKey, new Node());
-                    return true;
-                },
-                (parentNode, leafKey) =>
-                {
-                    if (!parentNode.ContainsKey(leafKey))
-                    {
-                        parentNode.Add(leafKey, value);
-                    }
+                    node.Add(key, new Dictionary<object, object>());
+                }
 
-                    parentNode[leafKey] = value;
-                });
+                node = (Dictionary<object, object>)node[key];
+            }
+
+            string leaf = segments.Last();
+            if (!node.ContainsKey(leaf))
+            {
+                node.Add(leaf, value);
+            }
+
+            node[leaf] = value;
         }
 
         public void RemoveIfExists(string dottedKey)
         {
-            this.Do(
-                dottedKey,
-                (n, k) => false,
-                (parentNode, leafKey) => parentNode.Remove(leafKey));
-        }
-
-        void Do(string dottedKey, Func<Node, string, bool> onMissing, Action<Node, string> op)
-        {
-            Node node = this.root;
+            Dictionary<object, object> node = this.root;
             string[] segments = dottedKey.Split('.');
             foreach (string key in segments.SkipLast(1))
             {
-                if (!node.ContainsKey(key) && !onMissing(node, key))
+                if (!node.ContainsKey(key))
                 {
                     return;
                 }
 
-                node = (Node)node[key];
+                node = (Dictionary<object, object>)node[key];
             }
 
-            string last = segments.Last();
-            op(node, last);
+            string leaf = segments.Last();
+            node.Remove(leaf);
         }
 
         public override string ToString()
