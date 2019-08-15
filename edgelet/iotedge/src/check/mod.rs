e1787c5c2948aeecd73b5ef96b33e0c138bcc57a
Mahesh Rohera <marohera@microsoft.com>
2019-08-15 23:32:13 +0000

    Iotedge check fixes

--- edgelet/iotedge/src/check/mod.rs
+++ edgelet/iotedge/src/check/mod.rs
@@ -734,7 +734,7 @@ fn settings_connection_string(check: &mut Check) -> Result<CheckResult, failure:
                 )?;
                 hub
             }
-            ManualAuthMethod::X509(x509) => x509.iothub_hostname().to_string(),
+            ManualAuthMethod::X509(x509) => x509.iothub_hostname().to_string()
         };
         check.iothub_hostname = Some(hub.to_owned());
     } else if check.iothub_hostname.is_none() {
@@ -1292,12 +1292,22 @@ fn settings_identity_certificates_expiry(check: &mut Check) -> Result<CheckResul
         return Ok(CheckResult::Skipped);
     };
 
-    if let Provisioning::Dps(dps) = settings.provisioning() {
-        if let AttestationMethod::X509(x509_info) = dps.attestation() {
-            let path = x509_info.identity_cert()?;
-            return CertificateValidity::parse("DPS identity certificate", &path)?
-                .to_check_result();
-        }
+    match settings.provisioning() {
+        Provisioning::Dps(dps) => {
+            if let AttestationMethod::X509(x509_info) = dps.attestation() {
+                let path = x509_info.identity_cert()?;
+                return CertificateValidity::parse("DPS identity certificate", &path)?
+                    .to_check_result();
+            }
+        },
+        Provisioning::Manual(manual) => {
+            if let ManualAuthMethod::X509(x509) = manual.authentication_method() {
+                let path = x509.identity_cert()?;
+                return CertificateValidity::parse("Manual authentication identity certificate", &path)?
+                    .to_check_result();
+            }
+        },
+        _ => (),
     }
 
     Ok(CheckResult::Ignored)
