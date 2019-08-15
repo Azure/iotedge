293bd24cfaa9f7a8d395525805c51d1d1ed048d2
Varun Puranik <vpuranik@microsoft.com>
2019-08-15 13:22:16 -0700

    Revert ProtocolGatewayMessageConverter change and update RoutingEdgeHub to not add the EnqueuedTime

--- edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Mqtt/ProtocolGatewayMessageConverter.cs
+++ edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Mqtt/ProtocolGatewayMessageConverter.cs
@@ -72,6 +72,10 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Mqtt
             // Since EdgeHub buffers messages, this does not work well, since older messages will get rejected.
             // So always set the creation time to UtcNow. This is inline with the current EdgeHub behavior.
             DateTime createdTimeUtc = DateTime.UtcNow;
+            if (message.SystemProperties.TryGetValue(SystemProperties.EnqueuedTime, out string createdTime))
+            {
+                createdTimeUtc = DateTime.Parse(createdTime, null, DateTimeStyles.RoundtripKind);
+            }
 
             if (!message.SystemProperties.TryGetValue(SystemProperties.OutboundUri, out string uriTemplateKey))
             {
