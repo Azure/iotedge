de5b4f63b27327d7688e3e33c2bea657561c2824
Shantanu Singh <shsi@microsoft.com>
2019-09-05 14:11:04 -0700

    Give names to parameters

--- scripts/linux/configureDockerIPv6.sh
+++ scripts/linux/configureDockerIPv6.sh
@@ -4,6 +4,10 @@
 [ -z "$2" ] && echo "Please specify the ipv6 subnet for the azure-iot-edge bridge network." && exit 1
 [ -z "$3" ] && echo "Please specify the network interface" && exit 1
 
+DOCKER0_BRIDGE_SUBNET=$1
+IOT_EDGE_SUBNET=$2
+NETWORK_INTERFACE=$3
+
 echo "Installing jq and grep..."
 apt-get update -y
 apt-get install -y jq grep
@@ -12,7 +16,7 @@ echo "Jq and grep installed."
 DOCKER_CONFIG_PATH=$4
 SYSCTL_CONF_PATH=$5
 
-echo "Configuring the docker daemon.json configuration file with IPv6 subnet $1..."
+echo "Configuring the docker daemon.json configuration file with IPv6 subnet $DOCKER0_BRIDGE_SUBNET..."
 
 if [[ ! -e "$DOCKER_CONFIG_PATH" ]]; then
     DEFAULT_DOCKER_CONFIG_PATH="/etc/docker/daemon.json"
@@ -20,10 +24,10 @@ if [[ ! -e "$DOCKER_CONFIG_PATH" ]]; then
     DOCKER_CONFIG_PATH=$DEFAULT_DOCKER_CONFIG_PATH
 fi
 
-cat $DOCKER_CONFIG_PATH | jq -n --arg var1 "$1" '. + { "ipv6": true, "fixed-cidr-v6": "\($var1)" }' > $DOCKER_CONFIG_PATH
+cat $DOCKER_CONFIG_PATH | jq -n --arg var1 "$DOCKER0_BRIDGE_SUBNET" '. + { "ipv6": true, "fixed-cidr-v6": "\($var1)" }' > $DOCKER_CONFIG_PATH
 echo "Configured docker for IPv6."
 
-echo "Configuring the sysctl config to allow router advertisements and enable ndp proxying on the network interface $3..."
+echo "Configuring the sysctl config to allow router advertisements and enable ndp proxying on the network interface $NETWORK_INTERFACE..."
 if [[ ! -e "$SYSCTL_CONF_PATH" ]]; then
     DEFAULT_SYSCTL_CONF_PATH="/etc/sysctl.conf"
     touch $DEFAULT_SYSCTL_CONF_PATH
@@ -37,8 +41,8 @@ echo "Backed up $SYSCTL_CONF_PATH."
 
 echo "Updating $SYSCTL_CONF_PATH..."
 
-SYSCTL_RA="net.ipv6.conf.$3.accept_ra=2"
-SYSCTL_PNDP="net.ipv6.conf.$3.proxy_ndp=1"
+SYSCTL_RA="net.ipv6.conf.$NETWORK_INTERFACE.accept_ra=2"
+SYSCTL_PNDP="net.ipv6.conf.$NETWORK_INTERFACE.proxy_ndp=1"
 
 if [[ -z $(grep "$SYSCTL_RA" "$SYSCTL_CONF_PATH") ]]; then SYSCTL_UPDATE=$SYSCTL_RA; fi
 if [[ -z $(grep "$SYSCTL_PNDP" "$SYSCTL_CONF_PATH") ]]; then 
@@ -61,6 +65,6 @@ echo "docker restarted."
 
 echo "Running ndppd configuration script."
 chmod +x ./installNdppd.sh
-sh ./installNdppd.sh $1 $2 $3
+sh ./installNdppd.sh $DOCKER0_BRIDGE_SUBNET $IOT_EDGE_SUBNET $NETWORK_INTERFACE
 echo "ndppd configuration script executed."
 
--- scripts/linux/installNdppd.sh
+++ scripts/linux/installNdppd.sh
@@ -4,22 +4,26 @@
 [ -z "$2" ] && echo "Please specify the azure-iot-edge network subnet IPv6 address to proxy" && exit 1
 [ -z "$3" ] && echo "Please specify the network interface" && exit 1
 
+DOCKER0_BRIDGE_SUBNET=$1
+IOT_EDGE_SUBNET=$2
+NETWORK_INTERFACE=$3
+
 echo "Installing ndppd..."
 apt-get update -y
 apt-get install -y ndppd
 echo "Ndppd installed."
 
-echo "Configured the ndppd configuration file with network interface $3 and IPv6 subnets $1 and $2..."
+echo "Configured the ndppd configuration file with network interface $NETWORK_INTERFACE and IPv6 subnets $DOCKER0_BRIDGE_SUBNET and $IOT_EDGE_SUBNET..."
 cat <<EOF > /etc/ndppd.conf
 route-ttl 5000
-proxy $3 {
+proxy $NETWORK_INTERFACE {
   router yes
   timeout 500
   ttl 30000
-  rule $1 {
+  rule $DOCKER0_BRIDGE_SUBNET {
     auto
   }
-  rule $2 {
+  rule $IOT_EDGE_SUBNET {
     auto
   }
 }
