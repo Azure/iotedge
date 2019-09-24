8bb3236ca232c3ff200038445ce78c2d4109dea4
Lee Fitchett <lefitche@microsoft.com>
2019-09-24 16:20:28 -0700

    callback version

--- edgelet/iotedge/src/logs.rs
+++ edgelet/iotedge/src/logs.rs
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 
+use bytes::Bytes;
 use std::io::{self, Write};
 
 use failure::Fail;
@@ -34,25 +35,33 @@ where
 
     fn execute(&mut self) -> Self::Future {
         let id = self.id.clone();
-        let result = self
-            .runtime
-            .logs(&id, &self.options)
-            .map_err(|err| Error::from(err.context(ErrorKind::ModuleRuntime)))
-            .and_then(move |logs| {
-                let chunked =
-                    Chunked::new(logs.map_err(|_| io::Error::new(io::ErrorKind::Other, "unknown")));
-                LogDecode::new(chunked)
-                    .for_each(|chunk| {
-                        match chunk {
-                            LogChunk::Stdin(b)
-                            | LogChunk::Stdout(b)
-                            | LogChunk::Stderr(b)
-                            | LogChunk::Unknown(b) => io::stdout().write(&b)?,
-                        };
-                        Ok(())
-                    })
-                    .map_err(|err| Error::from(err.context(ErrorKind::ModuleRuntime)))
-            });
-        Box::new(result)
+        pull_logs(&self.runtime, &id, &self.options, |b| {io::stdout().write(b)})
     }
+}
+
+type Temp = Box<dyn Future<Item = (), Error = Error> + Send>;
+
+pub fn pull_logs<M, F, T, E>(runtime: &M, id: &str, options: &LogOptions, cb: F) -> Temp
+where
+    M: 'static + ModuleRuntime + Clone,
+    F: 'static, Fn(&Bytes) -> Result<T, E> + Send,
+{
+    Box::new(runtime
+        .logs(id, options)
+        .map_err(|err| Error::from(err.context(ErrorKind::ModuleRuntime)))
+        .and_then(move |logs| {
+            let chunked =
+                Chunked::new(logs.map_err(|_| io::Error::new(io::ErrorKind::Other, "unknown")));
+            LogDecode::new(chunked)
+                .for_each(move |chunk| {
+                    match chunk {
+                        LogChunk::Stdin(b)
+                        | LogChunk::Stdout(b)
+                        | LogChunk::Stderr(b)
+                        | LogChunk::Unknown(b) => cb(&b)?,
+                    };
+                    Ok(())
+                })
+                .map_err(|err| Error::from(err.context(ErrorKind::ModuleRuntime)))
+        }))
 }
