527899b434b4e4b24e4e412da9fed8752732c4f5
Lee Fitchett <lefitche@microsoft.com>
2019-09-25 09:26:02 -0700

    get module names

--- edgelet/iotedge/src/bundle.rs
+++ edgelet/iotedge/src/bundle.rs
@@ -1,16 +1,17 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-use crate::logs::pull_logs;
-
-use std::fs::File;
 use std::fs;
+use std::fs::File;
 
-use futures::future::Future;
+use futures::{Future, Stream};
 
-use crate::error::{Error};
+use crate::error::{Error, ErrorKind};
+use failure::Fail;
+
+use crate::logs::pull_logs;
 use crate::Command;
 
-use edgelet_core::{LogOptions, ModuleRuntime};
+use edgelet_core::{LogOptions, Module, ModuleRuntime};
 
 pub struct Bundle<M> {
     runtime: M,
@@ -31,15 +32,31 @@ where
 
     fn execute(&mut self) -> Self::Future {
         println!("Test!");
-        let id = "edgeAgent".to_string();
-        let log_options = LogOptions::new();
 
-        fs::create_dir_all("./bundle/logs");
+        // let id = "edgeAgent".to_string();
+        // let log_options = LogOptions::new();
 
-        let mut file = File::create("./bundle/logs/foo.txt").unwrap();
+        // fs::create_dir_all("./bundle/logs");
+        // let file = File::create("./bundle/logs/foo.txt").unwrap();
+        // let log1 = pull_logs(&self.runtime, &id, &log_options, file);
 
-        let log1 = pull_logs(&self.runtime, &id, &log_options, file);
+        Box::new(self.get_modules().map(|names| {
+            for name in names {
+                println!("{}", name);
+            }
+        }))
+    }
+}
 
-        log1
+impl<M> Bundle<M>
+where
+    M: 'static + ModuleRuntime + Clone,
+{
+    fn get_modules(&self) -> impl Future<Item = Vec<String>, Error = Error> {
+        self.runtime
+            .list_with_details()
+            .map_err(|err| Error::from(err.context(ErrorKind::ModuleRuntime)))
+            .map(|(module, _state)| module.name().to_string())
+            .collect()
     }
 }
