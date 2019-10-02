7a7313c5fc5ca1e2a76e5018ba8427b8f6d63f43
Philip Lin <philipktlin@live.com>
2019-10-02 13:02:16 -0700

    Fix ReportedPropertiesValidator for TwinManager V2.

--- edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Core.Test/twin/ReportedPropertiesValidatorTest.cs
+++ edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Core.Test/twin/ReportedPropertiesValidatorTest.cs
@@ -8,6 +8,7 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test.Twin
     using Microsoft.Azure.Devices.Edge.Util.Test.Common;
     using Microsoft.Azure.Devices.Shared;
     using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
     using Xunit;
 
     public class ReportedPropertiesValidatorTest
@@ -34,7 +35,8 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test.Twin
                         }
                     }
                 })),
-                null
+                null,
+                string.Empty
             };
 
             yield return new object[]
@@ -45,10 +47,11 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test.Twin
                     level = new
                     {
                         ok = "ok",
-                        s = longString
+                        propertyWithBigValue = longString
                     }
                 })),
-                typeof(InvalidOperationException)
+                typeof(InvalidOperationException),
+                "Value associated with property name propertyWithBigValue has length 5000 that exceeds maximum length of 4096"
             };
 
             yield return new object[]
@@ -57,10 +60,11 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test.Twin
                 {
                     level = new
                     {
-                        number = -4503599627370497
+                        invalidNumber = -4503599627370497
                     }
                 })),
-                typeof(InvalidOperationException)
+                typeof(InvalidOperationException),
+                "Property invalidNumber has an out of bound value. Valid values are between -4503599627370496 and 4503599627370495"
             };
 
             yield return new object[]
@@ -84,7 +88,8 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test.Twin
                         }
                     }
                 })),
-                typeof(InvalidOperationException)
+                typeof(InvalidOperationException),
+                "Nested depth of twin property exceeds 5"
             };
 
             yield return new object[]
@@ -93,23 +98,46 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test.Twin
                 {
                     array = new[] { 0, 1, 2 }
                 })),
-                typeof(InvalidOperationException)
+                typeof(InvalidOperationException),
+                "Property array has a value of unsupported type. Valid types are integer, float, string, bool, null and nested object"
             };
 
             yield return new object[]
             {
                 new TwinCollection(JsonConvert.SerializeObject(new
                 {
-                    tooBig = new byte[10 * 1024]
+                    tooBig = longString
                 })),
-                typeof(InvalidOperationException)
+                typeof(InvalidOperationException),
+                "Value associated with property name tooBig has length 5000 that exceeds maximum length of 4096"
+            };
+
+            yield return new object[]
+            {
+                new TwinCollection(JsonConvert.SerializeObject(new
+                {
+                    ok = "ok",
+                    level1 = new
+                    {
+                        ok = null as string
+                    }
+                })),
+                null,
+                string.Empty
+            };
+
+            yield return new object[]
+            {
+                new TwinCollection("{ \"ok\":\"good\", \"level1\": { \"field1\": null } }"),
+                null,
+                string.Empty
             };
         }
 
         [Unit]
         [Theory]
         [MemberData(nameof(GetTwinCollections))]
-        public void ValidateReportedPropertiesTest(TwinCollection twinCollection, Type expectedExceptionType)
+        public void ValidateReportedPropertiesTest(TwinCollection twinCollection, Type expectedExceptionType, string expectedExceptionMessage)
         {
             // Arrange
             var reportedPropertiesValidator = new ReportedPropertiesValidator();
@@ -121,7 +149,8 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test.Twin
             }
             else
             {
-                Assert.Throws(expectedExceptionType, () => reportedPropertiesValidator.Validate(twinCollection));
+                Exception ex = Assert.Throws(expectedExceptionType, () => reportedPropertiesValidator.Validate(twinCollection));
+                Assert.Equal(expectedExceptionMessage, ex.Message);
             }
         }
     }
