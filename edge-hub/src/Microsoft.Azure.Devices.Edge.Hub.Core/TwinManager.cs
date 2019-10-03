4acce3576be89cfc8c929229d6c5c7c6a6b41cc5
Philip Lin <philipktlin@live.com>
2019-10-03 08:34:17 -0700

    Fix Twin property 4K length validation (#1786) (#1799)
    * Update Twin property value validation to apply 4K length limit on JValue type only.
    * Add test to check twin property having null value.
    * Fix ReportedPropertiesValidator for TwinManager V2.

--- edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/TwinManager.cs
+++ edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/TwinManager.cs
@@ -103,6 +103,36 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core
 
         internal static void ValidateTwinProperties(JToken properties) => ValidateTwinProperties(properties, 1);
 
+        // TODO: Move to a Twin helper class (along with Twin manager update).
+        internal static string EncodeTwinKey(string key)
+        {
+            Preconditions.CheckNonWhiteSpace(key, nameof(key));
+            var sb = new StringBuilder();
+            foreach (char ch in key)
+            {
+                switch (ch)
+                {
+                    case '.':
+                        sb.Append("%2E");
+                        break;
+
+                    case '$':
+                        sb.Append("%24");
+                        break;
+
+                    case ' ':
+                        sb.Append("%20");
+                        break;
+
+                    default:
+                        sb.Append(ch);
+                        break;
+                }
+            }
+
+            return sb.ToString();
+        }
+
         internal void ConnectionEstablishedCallback(object sender, IIdentity identity)
         {
             Events.ConnectionEstablished(identity.Id);
@@ -271,15 +301,20 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core
 
                 ValidateValueType(kvp.Name, kvp.Value);
 
-                string s = kvp.Value.ToString();
-                ValidatePropertyValueLength(kvp.Name, s);
-
-                if ((kvp.Value is JValue) && (kvp.Value.Type is JTokenType.Integer))
+                if (kvp.Value is JValue)
                 {
-                    ValidateIntegerValue(kvp.Name, (long)kvp.Value);
+                    if (kvp.Value.Type is JTokenType.Integer)
+                    {
+                        ValidateIntegerValue(kvp.Name, (long)kvp.Value);
+                    }
+                    else
+                    {
+                        string s = kvp.Value.ToString();
+                        ValidatePropertyValueLength(kvp.Name, s);
+                    }
                 }
 
-                if ((kvp.Value != null) && (kvp.Value is JObject))
+                if (kvp.Value != null && kvp.Value is JObject)
                 {
                     if (currentDepth > TwinPropertyMaxDepth)
                     {
@@ -617,36 +652,6 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core
             }
 
             await cloudProxy.ForEachAsync(cp => cp.UpdateReportedPropertiesAsync(reported));
-        }
-
-        // TODO: Move to a Twin helper class (along with Twin manager update).
-        internal static string EncodeTwinKey(string key)
-        {
-            Preconditions.CheckNonWhiteSpace(key, nameof(key));
-            var sb = new StringBuilder();
-            foreach (char ch in key)
-            {
-                switch (ch)
-                {
-                    case '.':
-                        sb.Append("%2E");
-                        break;
-
-                    case '$':
-                        sb.Append("%24");
-                        break;
-
-                    case ' ':
-                        sb.Append("%20");
-                        break;
-
-                    default:
-                        sb.Append(ch);
-                        break;
-                }
-            }
-
-            return sb.ToString();
         }
 
         static class Events
