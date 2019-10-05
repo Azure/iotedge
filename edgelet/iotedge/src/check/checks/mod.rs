eeee3055da8f54b9a4b73a5220934eed86f4e58d
Lee Fitchett <lefitche@microsoft.com>
2019-10-04 18:11:49 -0700

    add hostname check

--- edgelet/iotedge/src/check/checks/mod.rs
+++ edgelet/iotedge/src/check/checks/mod.rs
@@ -1,9 +1,11 @@
 pub mod container_engine_installed;
+pub mod hostname;
 pub mod well_formed_config;
 pub mod well_formed_connection_string;
 pub mod windows_host_version;
 
 pub use self::container_engine_installed::ContainerEngineInstalled;
+pub use self::hostname::Hostname;
 pub use self::well_formed_config::WellFormedConfig;
 pub use self::well_formed_connection_string::WellFormedConnectionString;
 pub use self::windows_host_version::WindowsHostVersion;
