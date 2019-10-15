e551d8f346fa0e30cbe7a52d0bb2d1cd98e94497
Lee Fitchett <lefitche@microsoft.com>
2019-10-15 16:39:36 -0700

    Include test runtime data in check json output (#1833)
    This breaks each test into its own file and gives it a corresponding struct. These structs are filled with copies of the data as the test runs and included in the json output.
    
    It also adds system info like remaining disk space to additional info.
    
    Here is a sample output json: https://gist.github.com/lfitchett/bb08656b8fcebef19315cff6645e1d01

---
+++ edgelet/iotedge/src/check/checks/well_formed_connection_string.rs
@@ -0,0 +1,57 @@
+use failure::{self, Context, ResultExt};
+
+use edgelet_core::{self, ManualAuthMethod, Provisioning, RuntimeSettings};
+
+use crate::check::{checker::Checker, Check, CheckResult};
+
+#[derive(Default, serde_derive::Serialize)]
+pub(crate) struct WellFormedConnectionString {
+    iothub_hostname: Option<String>,
+    device_id: Option<String>,
+}
+
+impl Checker for WellFormedConnectionString {
+    fn id(&self) -> &'static str {
+        "connection-string"
+    }
+    fn description(&self) -> &'static str {
+        "config.yaml has well-formed connection string"
+    }
+    fn execute(&mut self, check: &mut Check) -> CheckResult {
+        self.inner_execute(check)
+            .unwrap_or_else(CheckResult::Failed)
+    }
+    fn get_json(&self) -> serde_json::Value {
+        serde_json::to_value(self).unwrap()
+    }
+}
+
+impl WellFormedConnectionString {
+    fn inner_execute(&mut self, check: &mut Check) -> Result<CheckResult, failure::Error> {
+        let settings = if let Some(settings) = &check.settings {
+            settings
+        } else {
+            return Ok(CheckResult::Skipped);
+        };
+
+        if let Provisioning::Manual(manual) = settings.provisioning() {
+            let hub = match manual.authentication_method() {
+                ManualAuthMethod::DeviceConnectionString(cs) => {
+                    let (_, device, hub) = cs.parse_device_connection_string().context(
+                                "Invalid connection string format detected.\n\
+                                Please check the value of the provisioning.device_connection_string parameter.",
+                )?;
+                    self.device_id = Some(device);
+                    hub
+                }
+                ManualAuthMethod::X509(x509) => x509.iothub_hostname().to_owned(),
+            };
+            check.iothub_hostname = Some(hub.clone());
+            self.iothub_hostname = check.iothub_hostname.clone();
+        } else if check.iothub_hostname.is_none() {
+            return Err(Context::new("Device is not using manual provisioning, so Azure IoT Hub hostname needs to be specified with --iothub-hostname").into());
+        };
+
+        Ok(CheckResult::Ok)
+    }
+}
