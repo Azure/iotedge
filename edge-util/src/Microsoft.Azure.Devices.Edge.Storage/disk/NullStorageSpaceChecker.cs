c49184669c3ab17925f2d3f211c888361357cd70
dylanbronson <dybronso@microsoft.com>
2019-10-18 14:14:55 -0700

    Fixing namespace placement typo

--- edge-util/src/Microsoft.Azure.Devices.Edge.Storage/disk/NullStorageSpaceChecker.cs
+++ edge-util/src/Microsoft.Azure.Devices.Edge.Storage/disk/NullStorageSpaceChecker.cs
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft. All rights reserved.
-using System;
-using System.Threading.Tasks;
-using Microsoft.Azure.Devices.Edge.Util;
 
 namespace Microsoft.Azure.Devices.Edge.Storage.Disk
 {
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Edge.Util;
     public class NullStorageSpaceChecker : IStorageSpaceChecker
     {
         public void SetMaxStorageSize(long maxStorageBytes, Option<int> checkFrequency)
