2f458eec2745f1ddf3227e78570346aa190b856b
yophilav <54859653+yophilav@users.noreply.github.com>
2019-10-22 12:40:46 -0700

    Introducing E2E for X509 DPS for an Edge Device
    Introducing E2E for X509 DPS for an Edge Device on a new end-to-end test framework. 
    New Test function: DpsX509 in ` /home/bear/Desktop/iotedge/test/Microsoft.Azure.Devices.Edge.Test/Provisioning.cs `
    Original Test: `run_dps_provisioning_test "X509"` in ` runE2ETest.sh `

--- test/Microsoft.Azure.Devices.Edge.Test.Common/DaemonConfiguration.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/DaemonConfiguration.cs
@@ -8,6 +8,7 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
 
     public class DaemonConfiguration
     {
+        const string GlobalEndPoint = "https://global.azure-devices-provisioning.net";
         readonly string configYamlFile;
         readonly YamlDocument config;
 
@@ -28,6 +29,14 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
             this.config.ReplaceOrAdd("agent.env.UpstreamProtocol", "AmqpWs");
         }
 
+        void SetBasicDpsParam(string idScope)
+        {
+            this.config.RemoveIfExists("provisioning");
+            this.config.ReplaceOrAdd("provisioning.source", "dps");
+            this.config.ReplaceOrAdd("provisioning.global_endpoint", GlobalEndPoint);
+            this.config.ReplaceOrAdd("provisioning.scope_id", idScope);
+        }
+
         public void SetDeviceConnectionString(string value)
         {
             this.config.RemoveIfExists("provisioning");
@@ -37,13 +46,22 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
 
         public void SetDpsSymmetricKey(string idScope, string registrationId, string deviceKey)
         {
-            this.config.RemoveIfExists("provisioning");
-            this.config.ReplaceOrAdd("provisioning.source", "dps");
-            this.config.ReplaceOrAdd("provisioning.global_endpoint", "https://global.azure-devices-provisioning.net");
-            this.config.ReplaceOrAdd("provisioning.scope_id", idScope);
+            this.SetBasicDpsParam(idScope);
             this.config.ReplaceOrAdd("provisioning.attestation.method", "symmetric_key");
             this.config.ReplaceOrAdd("provisioning.attestation.registration_id", registrationId);
             this.config.ReplaceOrAdd("provisioning.attestation.symmetric_key", deviceKey);
+        }
+
+        public void SetDpsX509(string idScope, string registrationId, IdCertificates cert)
+        {
+            Uri certUri = new Uri(cert.CertificatePath, UriKind.Absolute);
+            Uri pKeyUri = new Uri(cert.KeyPath, UriKind.Absolute);
+
+            this.SetBasicDpsParam(idScope);
+            this.config.ReplaceOrAdd("provisioning.attestation.method", "x509");
+            this.config.ReplaceOrAdd("provisioning.attestation.identity_cert", certUri.ToString());
+            this.config.ReplaceOrAdd("provisioning.attestation.identity_pk", pKeyUri.ToString());
+            this.config.ReplaceOrAdd("provisioning.attestation.registration_id", registrationId);
         }
 
         public void SetDeviceHostname(string value)
@@ -51,7 +69,7 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
             this.config.ReplaceOrAdd("hostname", value);
         }
 
-        public void SetCertificates(EdgeCertificates certs)
+        public void SetCertificates(CaCertificates certs)
         {
             this.config.ReplaceOrAdd("certificates.device_ca_cert", certs.CertificatePath);
             this.config.ReplaceOrAdd("certificates.device_ca_pk", certs.KeyPath);
