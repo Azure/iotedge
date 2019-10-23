b31070ff3e5368e81b03a575270492c1fad55f8c
Lee Fitchett <lefitche@microsoft.com>
2019-10-23 13:41:11 -0700

    make test project

---
+++ edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.MetricsCollector.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Microsoft.Azure.Devices.Edge.Agent.MetricsCollector.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
