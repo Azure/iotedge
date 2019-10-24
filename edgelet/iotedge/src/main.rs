070ecba75a2872ad35e4dbbd990198f8dd0907ca
Lee Fitchett <lefitche@microsoft.com>
2019-10-24 13:51:23 -0700

    Add location to support bundle output (#1903)

--- edgelet/iotedge/src/main.rs
+++ edgelet/iotedge/src/main.rs
@@ -271,10 +271,10 @@ fn run() -> Result<(), Error> {
                         .help("Sets the hostname of the Azure IoT Hub that this device would connect to. If using manual provisioning, this does not need to be specified.")
                         .takes_value(true),
                 ).arg(
-                    Arg::with_name("verbose")
-                        .help("Use verbose output")
-                        .long("verbose")
-                        .short("v")
+                    Arg::with_name("quiet")
+                        .help("Suppress output")
+                        .long("quiet")
+                        .short("q")
                         .takes_value(false),
                 ),
         )
@@ -381,7 +381,7 @@ fn run() -> Result<(), Error> {
                 .with_tail(LogTail::All)
                 .with_since(since);
             let include_ms_only = args.is_present("include-edge-runtime-only");
-            let verbose = args.is_present("verbose");
+            let verbose = !args.is_present("quiet");
             let iothub_hostname = args.value_of("iothub-hostname").map(ToOwned::to_owned);
             tokio_runtime.block_on(
                 SupportBundle::new(
