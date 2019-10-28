f84c81e8a64a5097e984fe44138f45c859843751
Lee Fitchett <lefitche@microsoft.com>
2019-10-28 11:27:48 -0700

    add unsubscribe test

--- edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Core.Test/metrics/ExceptionCounterTest.cs
+++ edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Core.Test/metrics/ExceptionCounterTest.cs
@@ -30,14 +30,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Test.Metrics
                 { typeof(JsonSerializationException), "json_serialization" },
                 { typeof(ArgumentException), "argument" },
             };
-
-            HashSet<Type> ignoredExeptions = new HashSet<Type>
-            {
-                typeof(TaskCanceledException),
-                typeof(OperationCanceledException),
-            };
-
-            using (new ExceptionCounter(recognizedExceptions, ignoredExeptions))
+            using (new ExceptionCounter(recognizedExceptions, new HashSet<Type>()))
             {
                 /* test */
                 Assert.Equal(expected, result);
@@ -78,7 +71,6 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Test.Metrics
                 { typeof(JsonSerializationException), "json_serialization" },
                 { typeof(ArgumentException), "argument" },
             };
-
             HashSet<Type> ignoredExeptions = new HashSet<Type>
             {
                 typeof(TaskCanceledException),
@@ -113,13 +105,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Test.Metrics
                 { typeof(JsonSerializationException), "json_serialization" },
                 { typeof(ArgumentException), "argument" },
             };
-
-            HashSet<Type> ignoredExeptions = new HashSet<Type>
-            {
-                typeof(TaskCanceledException),
-                typeof(OperationCanceledException),
-            };
-            using (new ExceptionCounter(recognizedExceptions, ignoredExeptions))
+            using (new ExceptionCounter(recognizedExceptions, new HashSet<Type>()))
             {
                 /* test */
                 await Task.Run(() => { this.ThrowAndCatch(new ArgumentException()); });
@@ -140,35 +126,61 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Test.Metrics
         }
 
         [Fact]
-        public void IgnoresExceptions()
+        public void DisposeUnsubscribes()
         {
             /* setup */
             Dictionary<string, long> result = this.MockCounter();
             Dictionary<string, long> expected = new Dictionary<string, long>();
-
-            Dictionary<Type, string> recognizedExceptions = new Dictionary<Type, string>
+            using (new ExceptionCounter(new Dictionary<Type, string>(), new HashSet<Type>()))
             {
-                { typeof(JsonSerializationException), "json_serialization" },
-                { typeof(ArgumentException), "argument" },
-            };
+                this.ThrowAndCatch(new Exception());
+                expected["other"] = 1;
+                Assert.Equal(expected, result);
+            }
 
-            HashSet<Type> ignoredExeptions = new HashSet<Type>
+            this.ThrowAndCatch(new Exception());
+            Assert.Equal(expected, result);
+        }
+
+        private Dictionary<string, long> MockCounter()
+        {
+            Dictionary<string, long> result = new Dictionary<string, long>();
+            void Increment(long val, string[] tags)
             {
-                typeof(TaskCanceledException),
-                typeof(OperationCanceledException),
-            };
-            using (new ExceptionCounter(recognizedExceptions, ignoredExeptions))
+                if (result.ContainsKey(tags[0]))
+                {
+                    result[tags[0]]++;
+                }
+                else
+                {
+                    result[tags[0]] = 1;
+                }
+            }
+
+            var metricsProvider = new Mock<IMetricsProvider>();
+
+            var counter = new Mock<IMetricsCounter>();
+            counter.Setup(x => x.Increment(It.IsAny<long>(), It.IsAny<string[]>())).Callback((Action<long, string[]>)Increment);
+            metricsProvider.Setup(x => x.CreateCounter(
+                    "exceptions_total",
+                    It.IsAny<string>(),
+                    new List<string> { "exception_name" }))
+                .Returns(counter.Object);
+
+            Util.Metrics.Metrics.Init(metricsProvider.Object, new NullMetricsListener(), NullLogger.Instance);
+
+            return result;
+        }
+
+        private void ThrowAndCatch(Exception e)
+        {
+            try
             {
-                /* test */
-                Assert.Equal(expected, result);
-
-                this.ThrowAndCatch(new ArgumentException());
-                expected["argument"] = 1;
-                Assert.Equal(expected, result);
-
-                this.ThrowAndCatch(new TaskCanceledException());
-                Assert.Equal(expected, result);
-
-                this.ThrowAndCatch(new TaskCanceledException());
-                this.ThrowAndCatch(new OperationCanceledException());
-                Assert.Equal(e
+                throw e;
+            }
+            catch
+            {
+            }
+        }
+    }
+}
