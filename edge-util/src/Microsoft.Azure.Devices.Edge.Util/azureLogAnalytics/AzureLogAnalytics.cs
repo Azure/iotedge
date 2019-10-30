0b2c637bbd36d47fec1322880a086244baff56a8
Bear <yophilav@microsoft.com>
2019-10-29 17:05:34 -0700

    Stypecop 1

--- edge-util/src/Microsoft.Azure.Devices.Edge.Util/azureLogAnalytics/AzureLogAnalytics.cs
+++ edge-util/src/Microsoft.Azure.Devices.Edge.Util/azureLogAnalytics/AzureLogAnalytics.cs
@@ -6,10 +6,11 @@ namespace Microsoft.Azure.Devices.Edge.Util.AzureLogAnalytics
     using System.Net;
     using System.Security.Cryptography;
     using System.Text;
-    
-    // Sample code from:
-    // https://github.com/veyalla/MetricsCollector/blob/master/modules/MetricsCollector/AzureLogAnalytics.cs
-    // https://dejanstojanovic.net/aspnet/2018/february/send-data-to-azure-log-analytics-from-c-code/
+
+    /* Sample code from:
+    /* https://github.com/veyalla/MetricsCollector/blob/master/modules/MetricsCollector/AzureLogAnalytics.cs
+    /* https://dejanstojanovic.net/aspnet/2018/february/send-data-to-azure-log-analytics-from-c-code/
+    */
 
     public class AzureLogAnalytics
     {
@@ -28,14 +29,14 @@ namespace Microsoft.Azure.Devices.Edge.Util.AzureLogAnalytics
 
         public void Post(byte[] content)
         {
-            string requestUriString = $"https://{WorkspaceId}.ods.opinsights.azure.com/api/logs?api-version={ApiVersion}";
+            string requestUriString = $"https://{this.WorkspaceId}.ods.opinsights.azure.com/api/logs?api-version={this.ApiVersion}";
             DateTime dateTime = DateTime.UtcNow;
             string dateString = dateTime.ToString("r");
-            string signature = GetSignature("POST", content.Length, "application/json", dateString, "/api/logs");
+            string signature = this.GetSignature("POST", content.Length, "application/json", dateString, "/api/logs");
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(requestUriString);
             request.ContentType = "application/json";
             request.Method = "POST";
-            request.Headers["Log-Type"] = LogType;
+            request.Headers["Log-Type"] = this.LogType;
             request.Headers["x-ms-date"] = dateString;
             request.Headers["Authorization"] = signature;
             using (Stream requestStreamAsync = request.GetRequestStream())
@@ -43,15 +44,11 @@ namespace Microsoft.Azure.Devices.Edge.Util.AzureLogAnalytics
                 requestStreamAsync.Write(content, 0, content.Length);
             }
 
-            //BEARWASHERE
-            Console.WriteLine("Content: "+ System.Text.Encoding.UTF8.GetString(content));
-
             using (HttpWebResponse responseAsync = (HttpWebResponse)request.GetResponse())
             {
-
                 Console.WriteLine(responseAsync.StatusDescription);
 
-                // Get the stream containing content returned by the server.  
+                // Get the stream containing content returned by the server.
                 // The using block ensures the stream is automatically closed.
                 using (Stream dataStream = responseAsync.GetResponseStream())
                 {
@@ -66,9 +63,9 @@ namespace Microsoft.Azure.Devices.Edge.Util.AzureLogAnalytics
         {
             string message = $"{method}\n{contentLength}\n{contentType}\nx-ms-date:{date}\n{resource}";
             byte[] bytes = Encoding.UTF8.GetBytes(message);
-            using (HMACSHA256 encryptor = new HMACSHA256(Convert.FromBase64String(SharedKey)))
+            using (HMACSHA256 encryptor = new HMACSHA256(Convert.FromBase64String(this.SharedKey)))
             {
-                return $"SharedKey {WorkspaceId}:{Convert.ToBase64String(encryptor.ComputeHash(bytes))}";
+                return $"SharedKey {this.WorkspaceId}:{Convert.ToBase64String(encryptor.ComputeHash(bytes))}";
             }
         }
     }
