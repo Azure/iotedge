30126eb6ce9e987cc88ccb5ffe1fc6757efaf063
Bear <yophilav@microsoft.com>
2019-10-30 14:35:51 -0700

    StypeCop 2

--- edge-modules/MessagesAnalyzer/Controllers/ReportController.cs
+++ edge-modules/MessagesAnalyzer/Controllers/ReportController.cs
@@ -1,36 +1,34 @@
 // Copyright (c) Microsoft. All rights reserved.
 namespace MessagesAnalyzer.Controllers
 {
+    using System.Text;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Azure.Devices.Edge.Util.AzureLogAnalytics;
-    using System.Text;
-    using System;
 
     [Route("api/[controller]")]
     [ApiController]
     public class ReportController : Controller
     {
-
         AzureLogAnalytics logAnalytics = null;
 
         // GET api/report
         [HttpGet]
         public ActionResult<string> Get()
         {
-            
-            if(Settings.Current.LogaAnalyticEnabled && this.logAnalytics == null)
+            if (Settings.Current.LogaAnalyticEnabled && this.logAnalytics == null)
             {
                 this.logAnalytics = new AzureLogAnalytics(
-                    Settings.Current.LogAnalyticWorkspaceId, 
-                    Settings.Current.LogAnalyticSharedKey, 
+                    Settings.Current.LogAnalyticWorkspaceId,
+                    Settings.Current.LogAnalyticSharedKey,
                     Settings.Current.LogAnalyticLogType);
             }
 
             string resultJson = Reporter.GetReceivedMessagesReport(Settings.Current.ToleranceInMilliseconds).ToString();
-            if(Settings.Current.LogaAnalyticEnabled)
+            if (Settings.Current.LogaAnalyticEnabled)
             {
                 this.logAnalytics.Post(Encoding.UTF8.GetBytes(resultJson));
             }
+
             return resultJson;
         }
     }
