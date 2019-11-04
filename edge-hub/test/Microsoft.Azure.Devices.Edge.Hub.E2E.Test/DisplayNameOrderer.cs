fc38ccf6c6f3af2a973ecf2c2eb94a6ae4532d2d
Shantanu Singh <shsi@microsoft.com>
2019-11-04 12:19:25 -0800

    Stylecop

--- edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.E2E.Test/DisplayNameOrderer.cs
+++ edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.E2E.Test/DisplayNameOrderer.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Xunit;
-using Xunit.Abstractions;
-
+// Copyright (c) Microsoft. All rights reserved.
 namespace Microsoft.Azure.Devices.Edge.Hub.E2E.Test
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xunit;
+    using Xunit.Abstractions;
+
     public class DisplayNameOrderer : ITestCollectionOrderer
     {
         public IEnumerable<ITestCollection> OrderTestCollections(IEnumerable<ITestCollection> testCollections)
