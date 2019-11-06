0dd1f7e1a93ad5e9280f04242d02964545fb246f
Daniel Prilik <danielprilik@gmail.com>
2019-11-06 13:36:49 -0800

    create oci-common crate

---
+++ containrs/oci-common/README.md
@@ -0,0 +1,3 @@
+# oci-common
+
+A grab-bag of various types and utilities that are common across multiple OCI spec crates.
---
+++ containrs/oci-common/src/lib.rs
@@ -0,0 +1,6 @@
+//! Types and constants used across various [OCI Specs](https://github.com/opencontainers/)
+
+pub mod types;
+
+#[macro_use]
+mod fixed_newtype;
