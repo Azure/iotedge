fc0c5bedbcc24bf090066e4fa70a556ea8d95bc7
Bear <yophilav@microsoft.com>
2019-11-08 15:11:15 -0800

    Update the Settings.cs

--- edge-modules/MessagesAnalyzer/Settings.cs
+++ edge-modules/MessagesAnalyzer/Settings.cs
@@ -4,13 +4,14 @@ namespace MessagesAnalyzer
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using Microsoft.Azure.Devices.Edge.Util;
     using Microsoft.Extensions.Configuration;
 
     class Settings
     {
         const string ExcludeModulesIdsPropertyName = "ExcludeModules:Ids";
         const string EventHubConnectionStringPropertyValue = "eventHubConnectionString";
-        const string DeviceIdPropertyName = "DeviceId";
+        const string DeviceIdPropertyName = "IOTEDGE_DEVICEID";
         const string ConsumerGroupIdPropertyName = "ConsumerGroupId";
         const string WebhostPortPropertyName = "WebhostPort";
         const string ToleranceInMillisecondsPropertyName = "ToleranceInMilliseconds";
@@ -21,7 +22,6 @@ namespace MessagesAnalyzer
         const string LogAnalyticWorkspaceIdName = "LogAnalyticWorkspaceId";
         const string LogAnalyticSharedKeyName = "LogAnalyticSharedKey";
         const string LogAnalyticLogTypeName = "LogAnalyticLogType";
-        static readonly string DefaultDeviceId = Environment.GetEnvironmentVariable("IOTEDGE_DEVICEID");
 
         static readonly Lazy<Settings> Setting = new Lazy<Settings>(
             () =>
@@ -37,7 +37,7 @@ namespace MessagesAnalyzer
                 return new Settings(
                     configuration.GetValue<string>(EventHubConnectionStringPropertyValue),
                     configuration.GetValue(ConsumerGroupIdPropertyName, DefaultConsumerGroupId),
-                    configuration.GetValue(DeviceIdPropertyName, DefaultDeviceId),
+                    configuration.GetValue<string>(DeviceIdPropertyName),
                     excludedModules,
                     configuration.GetValue(WebhostPortPropertyName, DefaultWebhostPort),
                     configuration.GetValue(ToleranceInMillisecondsPropertyName, DefaultToleranceInMilliseconds),
@@ -47,15 +47,15 @@ namespace MessagesAnalyzer
                     configuration.GetValue<string>(LogAnalyticLogTypeName));
             });
 
-        Settings(string eventHubCs, string consumerGroupId, string deviceId, IList<string> excludedModuleIds, string webhostPort, double tolerance, string logAnalyticEnabledText, string logAnalyticsWorkspaceIdName, string logAnalyticsSharedKeyName, string logAnalyticsLogTypeName)
+        Settings(string eventHubConnectionString, string consumerGroupId, string deviceId, IList<string> excludedModuleIds, string webhostPort, double tolerance, string logAnalyticEnabledText, string logAnalyticsWorkspaceIdName, string logAnalyticsSharedKeyName, string logAnalyticsLogTypeName)
         {
             bool logAnalyticEnabled;
-            this.EventHubConnectionString = eventHubCs;
-            this.ConsumerGroupId = consumerGroupId;
+            this.EventHubConnectionString = Preconditions.CheckNonWhiteSpace(eventHubConnectionString, nameof(eventHubConnectionString));
+            this.ConsumerGroupId = Preconditions.CheckNonWhiteSpace(consumerGroupId, nameof(consumerGroupId));
             this.ExcludedModuleIds = excludedModuleIds;
-            this.DeviceId = deviceId;
-            this.WebhostPort = webhostPort;
-            this.ToleranceInMilliseconds = tolerance;
+            this.DeviceId = Preconditions.CheckNonWhiteSpace(deviceId, nameof(deviceId));
+            this.WebhostPort = Preconditions.CheckNonWhiteSpace(webhostPort, nameof(webhostPort));
+            this.ToleranceInMilliseconds = Preconditions.CheckRange(tolerance, 0);
             bool.TryParse(logAnalyticEnabledText, out logAnalyticEnabled);
             this.LogAnalyticEnabled = logAnalyticEnabled;
             this.LogAnalyticWorkspaceId = logAnalyticsWorkspaceIdName;
