ab1fd227f03f8ca852dc2c3e6dd77b87677b2f76
Bear <yophilav@microsoft.com>
2019-11-17 14:33:34 -0800

    Update the string

--- edge-modules/Analyzer/TestAgentInformation.cs
+++ edge-modules/Analyzer/TestAgentInformation.cs
@@ -3,30 +3,33 @@ namespace Analyzer
 {
     using System;
     using System.Collections.Generic;
-    using System.Runtime.InteropServices;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Serialization;
 
     [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
     class TestAgentInformation
     {
-        public string Architecture { get; }
         public string ConsumerGroupId { get; }
-        public string Name { get; }
-        public string Os { get; }
-        public string OsVersion { get; }
-        public string User { get; }
         public string DeviceId { get; }
+        public string Os { get; }
+        public string OsArchitecture { get; }
+        public string OsDescription { get; }
+        public string OsVersion { get; }
+        public string ProcessArchitecture { get; }
+        public string User { get; }
 
         public TestAgentInformation()
         {
-            this.Architecture = Architecture.ToString();
+            string[] OsDescriptionTokens = System.Runtime.InteropServices.RuntimeInformation.OSDescription.Split(' ');
+
             this.ConsumerGroupId = Settings.Current.ConsumerGroupId;
-            this.Name = Settings.Current.DeviceId;
-            this.Os = OSPlatform.ToString();
-            this.OsVersion = RuntimeEnvironment.GetSystemVersion();
-            this.User = agentUser;
             this.DeviceId = Settings.Current.DeviceId;
+            this.Os = OsDescriptionTokens[0];
+            this.OsArchitecture = System.Runtime.InteropServices.RuntimeInformation.OSArchitecture.ToString();
+            this.OsDescription = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            this.OsVersion = OsDescriptionTokens[1];
+            this.ProcessArchitecture = System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture.ToString();
+            this.User = Environment.UserName.ToString();
         }
 
         public override string ToString()
