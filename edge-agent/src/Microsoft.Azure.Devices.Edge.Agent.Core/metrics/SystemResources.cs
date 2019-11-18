177ba87ac6e8d9157e5eb8706390bdf9ee4c1787
Lee Fitchett <lefitche@microsoft.com>
2019-11-18 11:03:04 -0800

    get module cpu

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/metrics/SystemResources.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/metrics/SystemResources.cs
@@ -2,7 +2,10 @@
 
 namespace Microsoft.Azure.Devices.Edge.Agent.Core.Metrics
 {
+    using System;
+    using System.Collections.Generic;
     using Microsoft.Azure.Devices.Edge.Util;
+    using Newtonsoft.Json;
 
     public class SystemResources
     {
@@ -13,7 +16,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Metrics
             this.UsedRam = Preconditions.CheckNotNull(usedRam, nameof(usedRam));
             this.TotalRam = Preconditions.CheckNotNull(totalRam, nameof(totalRam));
             this.Disks = Preconditions.CheckNotNull(disks, nameof(disks));
-            this.DockerStats = Preconditions.CheckNotNull(dockerStats, nameof(dockerStats));
+            this.ModuleStats = JsonConvert.DeserializeObject<ModuleStats[]>(Preconditions.CheckNotNull(dockerStats, nameof(dockerStats)));
         }
 
         public long HostUptime { get; }
@@ -26,7 +29,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Metrics
 
         public Disk[] Disks { get; }
 
-        public string DockerStats { get; }
+        public ModuleStats[] ModuleStats { get; }
     }
 
     public class Disk
@@ -50,4 +53,64 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Metrics
 
         public string FileType { get; }
     }
+
+#pragma warning disable SA1300 // Element should begin with upper-case letter
+
+    public class ModuleStats
+    {
+        //TODO: change to moduleName
+        public string module { get; set; }
+        public DockerStats stats { get; set; }
+    }
+
+    public class DockerStats
+    {
+        public DockerCpuStats cpu_stats { get; set; }
+        public DockerCpuStats precpu_stats { get; set; }
+        public MemoryStats memory_stats { get; set; }
+        public Dictionary<string, NetworkInfo> networks { get; set; }
+        public int num_procs { get; set; }
+        public PidsStats pids_stats { get; set; }
+        public DateTime preread { get; set; }
+        public DateTime read { get; set; }
+    }
+
+    public class DockerCpuStats
+    {
+        public CpuUsage cpu_usage { get; set; }
+        public int online_cpus { get; set; }
+        public ulong system_cpu_usage { get; set; }
+    }
+
+    public class CpuUsage
+    {
+        public ulong total_usage { get; set; }
+        public ulong usage_in_kernelmode { get; set; }
+        public ulong usage_in_usermode { get; set; }
+    }
+
+    public class MemoryStats
+    {
+        public ulong limit { get; set; }
+        public ulong max_usage { get; set; }
+        public ulong usage { get; set; }
+    }
+
+    public class NetworkInfo
+    {
+        public int rx_bytes { get; set; }
+        public int rx_dropped { get; set; }
+        public int rx_errors { get; set; }
+        public int rx_packets { get; set; }
+        public int tx_bytes { get; set; }
+        public int tx_dropped { get; set; }
+        public int tx_errors { get; set; }
+        public int tx_packets { get; set; }
+    }
+
+    public class PidsStats
+    {
+        public int current { get; set; }
+    }
+#pragma warning restore SA1300 // Element should begin with upper-case letter
 }
--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/metrics/SystemResourcesMetrics.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/metrics/SystemResourcesMetrics.cs
@@ -4,12 +4,15 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Metrics
 {
     using System;
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using System.Runtime.InteropServices;
     using System.Text;
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Edge.Util;
     using Microsoft.Azure.Devices.Edge.Util.Edged;
     using Microsoft.Azure.Devices.Edge.Util.Metrics;
     using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json;
 
     public class SystemResourcesMetrics : IDisposable
     {
@@ -23,6 +26,8 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Metrics
         IMetricsGauge totalSpace;
         IMetricsHistogram usedMemory;
         IMetricsGauge totalMemory;
+        IMetricsHistogram cpuPercentage;
+        IMetricsGauge createdPids;
 
         public SystemResourcesMetrics(IMetricsProvider metricsProvider, Func<Task<SystemResources>> getSystemResources, string apiVersion)
         {
@@ -53,12 +58,22 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Metrics
             this.usedMemory = Preconditions.CheckNotNull(metricsProvider.CreateHistogram(
                 "used_memory_bytes",
                 "Amount of RAM used by all processes",
-                new List<string> { }));
+                new List<string> { "module" }));
 
             this.totalMemory = Preconditions.CheckNotNull(metricsProvider.CreateGauge(
                 "total_memory_bytes",
                 "RAM available",
-                new List<string> { }));
+                new List<string> { "module" }));
+
+            this.cpuPercentage = Preconditions.CheckNotNull(metricsProvider.CreateHistogram(
+                "used_cpu_percent",
+                "Percent of cpu used by all processes",
+                new List<string> { "module" }));
+
+            this.createdPids = Preconditions.CheckNotNull(metricsProvider.CreateGauge(
+                "created_pids",
+                "The number of processes or threads the container has created",
+                new List<string> { "module" }));
         }
 
         public void Start(ILogger logger)
@@ -81,17 +96,60 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Metrics
         async Task Update()
         {
             SystemResources systemResources = await this.getSystemResources();
-            Console.WriteLine($"\n\n\n{systemResources.DockerStats}\n\n\n");
+            Console.WriteLine($"\n\n\n{JsonConvert.SerializeObject(systemResources.ModuleStats)}\n\n\n");
 
             this.hostUptime.Set(systemResources.HostUptime, new string[] { });
             this.iotedgedUptime.Set(systemResources.IotEdgedUptime, new string[] { });
-            this.usedMemory.Update(systemResources.UsedRam, new string[] { });
-            this.totalMemory.Set(systemResources.TotalRam, new string[] { });
+            this.usedMemory.Update(systemResources.UsedRam, new string[] { "host" });
+            this.totalMemory.Set(systemResources.TotalRam, new string[] { "host" });
 
             foreach (Disk disk in systemResources.Disks)
             {
                 this.usedSpace.Update(disk.AvailableSpace, new string[] { disk.Name, disk.FileSystem, disk.FileType });
                 this.totalSpace.Set(disk.TotalSpace, new string[] { disk.Name, disk.FileSystem, disk.FileType });
+            }
+
+            this.SetModuleStats(systemResources);
+        }
+
+        void SetModuleStats(SystemResources systemResources)
+        {
+            foreach (ModuleStats module in systemResources.ModuleStats)
+            {
+                var tags = new string[] { module.module };
+                // TODO see about double histograms
+                this.cpuPercentage.Update((long)(this.GetCpuUsage(module) * 10000), tags);
+                this.totalMemory.Set(module.stats.memory_stats.limit, tags);
+                this.usedMemory.Update((long)module.stats.memory_stats.usage, tags);
+                this.createdPids.Set(module.stats.pids_stats.current, tags);
+
+                Console.WriteLine($"{module.module} cpu: {this.GetCpuUsage(module) * 100}.2f %");
+                Console.WriteLine($"{module.module} memory: {module.stats.memory_stats.usage / (double)module.stats.memory_stats.limit}.2f %");
+            }
+        }
+
+        double GetCpuUsage(ModuleStats module)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                double moduleDiff = module.stats.cpu_stats.cpu_usage.total_usage - module.stats.precpu_stats.cpu_usage.total_usage;
+                double systemDiff = module.stats.cpu_stats.system_cpu_usage - module.stats.precpu_stats.system_cpu_usage;
+
+                return moduleDiff / systemDiff;
+            }
+            else
+            {
+                double totalIntervals = (module.stats.read - module.stats.preread).TotalMilliseconds * 10; // Get number of 100ns intervals during read
+                ulong intervalsUsed = module.stats.cpu_stats.cpu_usage.total_usage - module.stats.precpu_stats.cpu_usage.total_usage;
+
+                if (totalIntervals > 0)
+                {
+                    return intervalsUsed / totalIntervals;
+                }
+                else
+                {
+                    return 0;
+                }
             }
         }
     }
