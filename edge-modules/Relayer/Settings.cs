0c1cfc2424335eeddc149c6251476bf6d047b2f5
dylanbronson <dybronso@microsoft.com>
2019-11-22 09:01:54 -0800

    Adding Relayer initial logic for connectivity tests

---
+++ edge-modules/Relayer/Settings.cs
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft. All rights reserved.
+namespace Relayer
+{
+    using System;
+    using System.IO;
+    using Microsoft.Azure.Devices.Client;
+    using Microsoft.Extensions.Configuration;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using Newtonsoft.Json.Serialization;
+
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class Settings
+    {
+        static readonly Lazy<Settings> DefaultSettings = new Lazy<Settings>(
+            () =>
+            {
+                IConfiguration configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("config/settings.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+
+                return new Settings(
+                    configuration.GetValue("transportType", TransportType.Amqp_Tcp_Only),
+                    configuration.GetValue("outputName", "output1"));
+            });
+
+        Settings(
+            TransportType transportType,
+            string outputName)
+        {
+            this.TransportType = transportType;
+            this.OutputName = outputName;
+        }
+
+        public static Settings Current => DefaultSettings.Value;
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public TransportType TransportType { get; }
+
+        public string OutputName { get; }
+
+        public override string ToString() => JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
+}
