24a473077be7f6b83008aba4cf252edb83586c27
Varun Puranik <vpuranik@microsoft.com>
2019-11-27 17:11:21 -0800

    Add integration test

--- edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.E2E.Test/SslProtocolsTest.cs
+++ edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.E2E.Test/SslProtocolsTest.cs
@@ -2,6 +2,7 @@
 namespace Microsoft.Azure.Devices.Edge.Hub.E2E.Test
 {
     using System;
+    using System.IO;
     using System.Net;
     using System.Net.Security;
     using System.Net.Sockets;
@@ -13,36 +14,78 @@ namespace Microsoft.Azure.Devices.Edge.Hub.E2E.Test
 
     [Integration]
     [Collection("Microsoft.Azure.Devices.Edge.Hub.E2E.SslProtocolsTest")]
-    public class SslProtocolsTest
+    public class SslProtocolsTest : IDisposable
     {
-        [Fact]
-        public void DefaultSslProtocolsTest()
+        readonly IDisposable protocolHead;
+
+        public SslProtocolsTest()
         {
-            using (IDisposable protocolHead = EdgeHubFixtureCollection.GetFixture(SslProtocols.Tls12))
-            {
-                //ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
-                //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11;
-                //Uri uri = new Uri("https://127.0.0.1:433");
-                //WebRequest webRequest = WebRequest.Create(uri);
-                //WebResponse webResponse = webRequest.GetResponse();
-
-                string server = "127.0.0.1";
-                TcpClient client = new TcpClient(server, 443);
-
-                using (SslStream sslStream = new SslStream(client.GetStream(), false,
-                                                           new RemoteCertificateValidationCallback(ValidateServerCertificate), null))
-                {
-                    sslStream.AuthenticateAsClient(server, null, SslProtocols.Tls11, false);
-                    // This is where you read and send data
-                }
-                client.Close();
-            }
+            this.protocolHead = EdgeHubFixtureCollection.GetFixture(SslProtocols.Tls12);
         }
 
-        public static bool ValidateServerCertificate(object sender, X509Certificate certificate,
-            X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        public void Dispose()
         {
-            return true;
+            this.protocolHead?.Dispose();
+        }
+
+        [Fact]
+        public void SslProtocolConnectionTest()
+        {
+            int httpsPort = 443;
+            int amqpsPort = 5671;
+            int mqttPort = 8883;
+            SslProtocols validProtocol = SslProtocols.Tls12;
+            SslProtocols invalidProtocol1 = SslProtocols.Tls;
+            SslProtocols invalidProtocol2 = SslProtocols.Tls11;
+            Type expectedException = typeof(IOException);
+
+            TestConnection(httpsPort, validProtocol, null);
+            TestConnection(amqpsPort, validProtocol, null);
+            TestConnection(mqttPort, validProtocol, null);
+
+            TestConnection(httpsPort, invalidProtocol1, expectedException);
+            TestConnection(amqpsPort, invalidProtocol1, expectedException);
+            TestConnection(mqttPort, invalidProtocol1, expectedException);
+
+            TestConnection(httpsPort, invalidProtocol2, expectedException);
+            TestConnection(amqpsPort, invalidProtocol2, expectedException);
+            TestConnection(mqttPort, invalidProtocol2, expectedException);
+
+        }
+
+        static void TestConnection(int port, SslProtocols protocol, Type expectedException)
+        {
+            string server = "127.0.0.1";
+            TcpClient client = new TcpClient(server, port);
+            Exception exception = null;
+
+            using (SslStream sslStream = new SslStream(
+                client.GetStream(),
+                false,
+                (sender, certificate, chain, errors) => true,
+                null))
+            {
+                try
+                {
+
+                    sslStream.AuthenticateAsClient(server, null, protocol, false);
+                }
+                catch (Exception e)
+                {
+                    exception = e;
+                }
+            }
+            client.Close();
+
+            if (expectedException == null)
+            {
+                Assert.Null(exception);
+            }
+            else
+            {
+                Assert.NotNull(exception);
+                Assert.Equal(expectedException, exception.GetType());
+            }
         }
     }
 }
