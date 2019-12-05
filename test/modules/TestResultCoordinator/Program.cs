3d9ef781e045e2f1d686fed548b2926475264bc5
dylanbronson <dybronso@microsoft.com>
2019-12-05 15:26:51 -0800

    Adding TestResultCoordinator file structure

---
+++ test/modules/TestResultCoordinator/Program.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft. All rights reserved.
+namespace TestResultCoordinator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Add Coordinator logic
+        }
+    }
+}
