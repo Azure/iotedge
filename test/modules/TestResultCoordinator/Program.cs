0b223cb6efbfbfecb3033467e2b502f9f71fb983
dylanbronson <dybronso@microsoft.com>
2019-12-05 17:13:49 -0800

    Adding ModuleLib and Stylecop

--- test/modules/TestResultCoordinator/Program.cs
+++ test/modules/TestResultCoordinator/Program.cs
@@ -1,11 +1,20 @@
 // Copyright (c) Microsoft. All rights reserved.
 namespace TestResultCoordinator
 {
+    using System;
+    using System.Threading;
+    using Microsoft.Azure.Devices.Edge.ModuleUtil;
+    using Microsoft.Azure.Devices.Edge.Util;
+    using Microsoft.Extensions.Logging;
+
     class Program
     {
+        static readonly ILogger Logger = ModuleUtil.CreateLogger("TestResultCoordinator");
+
         static void Main(string[] args)
         {
             // TODO: Add Coordinator logic
+            (CancellationTokenSource cts, ManualResetEventSlim completed, Option<object> handler) = ShutdownHandler.Init(TimeSpan.FromSeconds(5), Logger);
         }
     }
 }
