d8b8e044f6288a1017c1c879507ccfda071378ea
dylanbronson <dybronso@microsoft.com>
2019-12-06 09:50:36 -0800

    Add shutdown logic and remove unused reference

--- test/modules/TestResultCoordinator/Program.cs
+++ test/modules/TestResultCoordinator/Program.cs
@@ -3,6 +3,7 @@ namespace TestResultCoordinator
 {
     using System;
     using System.Threading;
+    using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Edge.ModuleUtil;
     using Microsoft.Azure.Devices.Edge.Util;
     using Microsoft.Extensions.Logging;
@@ -11,10 +12,15 @@ namespace TestResultCoordinator
     {
         static readonly ILogger Logger = ModuleUtil.CreateLogger("TestResultCoordinator");
 
-        static void Main(string[] args)
+        static async Task Main()
         {
             // TODO: Add Coordinator logic
             (CancellationTokenSource cts, ManualResetEventSlim completed, Option<object> handler) = ShutdownHandler.Init(TimeSpan.FromSeconds(5), Logger);
+
+            await cts.Token.WhenCanceled();
+            completed.Set();
+            handler.ForEach(h => GC.KeepAlive(h));
+            Console.WriteLine("TestResultCoordinator Main() exited.");
         }
     }
 }
