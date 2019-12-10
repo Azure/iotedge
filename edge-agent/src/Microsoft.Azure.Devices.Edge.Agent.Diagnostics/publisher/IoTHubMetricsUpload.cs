f63aa2b4a2a384836b4f6c9828384c732a53565c
Lee Fitchett <lefitche@microsoft.com>
2019-12-09 16:06:22 -0800

    checkpoint 1

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Diagnostics/publisher/IoTHubMetricsUpload.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Diagnostics/publisher/IoTHubMetricsUpload.cs
@@ -10,15 +10,22 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Diagnostics.Publisher
     using Microsoft.Azure.Devices.Client;
     using Microsoft.Azure.Devices.Edge.Agent.Core.Requests;
     using Microsoft.Azure.Devices.Edge.Agent.IoTHub;
+    using Microsoft.Azure.Devices.Edge.Storage;
     using Microsoft.Azure.Devices.Edge.Util;
+    using Microsoft.Extensions.Logging.Abstractions;
 
     public sealed class IoTHubMetricsUpload : IMetricsPublisher
     {
+        const int MaxRetries = 20;
         readonly IEdgeAgentConnection edgeAgentConnection;
+        readonly IKeyValueStore<Guid, (byte[] data, int retry)> dataStore;
 
-        public IoTHubMetricsUpload(IEdgeAgentConnection edgeAgentConnection)
+        Task retryTask = null;
+
+        public IoTHubMetricsUpload(IEdgeAgentConnection edgeAgentConnection, IStoreProvider storeProvider)
         {
             this.edgeAgentConnection = Preconditions.CheckNotNull(edgeAgentConnection, nameof(edgeAgentConnection));
+            this.dataStore = Preconditions.CheckNotNull(storeProvider.GetEntityStore<Guid, (byte[], int)>("Diagnostic Messages"), "dataStore");
         }
 
         public async Task PublishAsync(IEnumerable<Metric> metrics, CancellationToken cancellationToken)
@@ -29,9 +36,28 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Diagnostics.Publisher
             // TODO: add check for too big of a message
             if (data.Length > 0)
             {
-                Message message = this.BuildMessage(data);
+                await this.SendMessage(data);
+            }
+        }
+
+        async Task<bool> SendMessage(byte[] data, int retryNum = 0)
+        {
+            Message message = this.BuildMessage(data);
+            try
+            {
                 await this.edgeAgentConnection.SendEventAsync(message);
             }
+            catch (Exception ex)
+            {
+                if (ex.HasTimeoutException() && retryNum <= MaxRetries)
+                {
+                    await this.dataStore.Put(Guid.NewGuid(), (data, retryNum + 1));
+                    this.StartRetrying();
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         Message BuildMessage(byte[] data)
@@ -40,6 +66,55 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Diagnostics.Publisher
             message.ContentType = "application/x-azureiot-edgeruntimediagnostics";
 
             return message;
+        }
+
+        void StartRetrying()
+        {
+            if (this.retryTask == null)
+            {
+                this.retryTask = Task.Run(async () =>
+                {
+                    foreach (TimeSpan sleepTime in this.Backoff())
+                    {
+                        await Task.Delay(sleepTime);
+                        if (await this.Retry())
+                        {
+                            break;
+                        }
+                    }
+
+                    this.retryTask = null;
+                });
+            }
+        }
+
+        async Task<bool> Retry()
+        {
+            bool allCompleatedSuccesfully = true;
+            await this.dataStore.IterateBatch(1000, async (guid, d) =>
+            {
+                if (!await this.SendMessage(d.data, d.retry))
+                {
+                    allCompleatedSuccesfully = false;
+                }
+
+                await this.dataStore.Remove(guid);
+            });
+
+            return allCompleatedSuccesfully;
+        }
+
+        IEnumerable<TimeSpan> Backoff()
+        {
+            yield return TimeSpan.FromMinutes(5);
+            yield return TimeSpan.FromMinutes(5);
+            yield return TimeSpan.FromMinutes(10);
+            yield return TimeSpan.FromMinutes(15);
+
+            while (true)
+            {
+                yield return TimeSpan.FromMinutes(30);
+            }
         }
     }
 }
