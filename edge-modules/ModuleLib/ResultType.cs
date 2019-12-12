2b2baa2add3d18fe68ebf7102b13f8b5b054831b
dylanbronson <dybronso@microsoft.com>
2019-12-11 17:43:54 -0800

    Adding ResultType enum

---
+++ edge-modules/ModuleLib/ResultType.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft. All rights reserved.
+namespace Microsoft.Azure.Devices.Edge.ModuleUtil
+{
+    public enum ResultType
+    {
+        Messages,
+        DirectMethod,
+        Twin
+    }
+}
