a6bf0bfec698b4d4947b0fd8d3c70aecf33c3777
Bear <yophilav@microsoft.com>
2019-12-16 16:47:20 -0800

    abstract class version

--- edge-modules/DirectMethodSender/ServiceClientWrapper.cs
+++ edge-modules/DirectMethodSender/ServiceClientWrapper.cs
@@ -1,44 +1,23 @@
 // Copyright (c) Microsoft. All rights reserved.
 namespace DirectMethodSender
 {
-    using System;
-    using System.Net;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices;
     using Microsoft.Extensions.Logging;
     using TransportType = Microsoft.Azure.Devices.TransportType;
 
-    public class ServiceClientWrapper : IDirectMethodClient
+    public class ServiceClientWrapper : DirectMethodClientBase
     {
-
-        public readonly string ConnectionString;
-        public readonly TransportType TransportType;
-        public readonly ILogger Logger;
-        ServiceClient serviceClient = null;
+        readonly ServiceClient serviceClient;
         int directMethodCount = 1;
 
-        private ServiceClientWrapper()
-        {
-        }
-
         private ServiceClientWrapper(
-            string connectionString,
-            TransportType transportType,
+            ServiceClient serviceClient,
             ILogger logger)
+            : base(logger)
         {
-            this.ConnectionString = connectionString;
-            this.TransportType =transportType;
-            this.Logger = logger;
-
-            this.serviceClient = ServiceClient.CreateFromConnectionString(
-                this.ConnectionString,
-                this.TransportType);
-        }
-
-        public async Task CloseClientAsync()
-        {
-            await this.serviceClient.CloseAsync();
+            this.serviceClient = serviceClient;
         }
 
         public static ServiceClientWrapper Create(
@@ -46,56 +25,19 @@ namespace DirectMethodSender
             TransportType transportType,
             ILogger logger)
         {
+            ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(connectionString, transportType);
             return new ServiceClientWrapper(
-                connectionString,
-                transportType,
+                serviceClient,
                 logger);
         }
 
-        public async Task<HttpStatusCode> InvokeDirectMethodAsync(CancellationTokenSource cts)
+        public override Task CloseAsync() => this.serviceClient.CloseAsync();
+
+        internal override async Task<int> InvokeDeviceMethodAsync(string deviceId, string targetModuleId, CancellationToken none)
         {
-            ILogger logger = this.Logger;
-            logger.LogInformation("Invoke DirectMethod from cloud: started.");
-
-            string deviceId = Settings.Current.DeviceId;
-            string targetModuleId = Settings.Current.TargetModuleId;
-
-            logger.LogInformation($"Calling Direct Method from cloud on device {deviceId} targeting module [{targetModuleId}] with count {this.directMethodCount}.");
-
-            try
-            {
-                CloudToDeviceMethod cloudToDeviceMethod = new CloudToDeviceMethod("HelloWorldMethod").SetPayloadJson("{ \"Message\": \"Hello\" }");
-                CloudToDeviceMethodResult result = await this.serviceClient.InvokeDeviceMethodAsync(deviceId, targetModuleId, cloudToDeviceMethod, CancellationToken.None);
-
-                string statusMessage = $"Calling Direct Method from cloud with count {this.directMethodCount} returned with status code {result.Status}";
-                if (result.Status == (int)HttpStatusCode.OK)
-                {
-                    logger.LogDebug(statusMessage);
-                }
-                else
-                {
-                    logger.LogError(statusMessage);
-                }
-
-                this.directMethodCount++;
-                logger.LogInformation("Invoke DirectMethod from cloud: finished.");
-                return (HttpStatusCode)result.Status;
-            }
-            catch (Exception e)
-            {
-                logger.LogError($"Exception caught with count {this.directMethodCount}: {e}");
-                return HttpStatusCode.InternalServerError;
-            }
-        }
-
-        public async Task OpenClientAsync()
-        {
-            await this.serviceClient.OpenAsync();
-        }
-
-        public Task SendEventAsync(string outputName, string message)
-        {
-            return Task.CompletedTask;
+            CloudToDeviceMethod cloudToDeviceMethod = new CloudToDeviceMethod("HelloWorldMethod").SetPayloadJson("{ \"Message\": \"Hello\" }");
+            CloudToDeviceMethodResult result = await this.serviceClient.InvokeDeviceMethodAsync(deviceId, targetModuleId, cloudToDeviceMethod, CancellationToken.None);
+            return result.Status;
         }
     }
 }
