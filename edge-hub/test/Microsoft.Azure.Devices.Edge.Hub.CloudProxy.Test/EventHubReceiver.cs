6583831993c379144711c74cfb5f36a2c942e12e
als5ev <als5ev@virginia.edu>
2019-12-17 01:40:45 -0500

    stop reading old messages

--- edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.CloudProxy.Test/EventHubReceiver.cs
+++ edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.CloudProxy.Test/EventHubReceiver.cs
@@ -4,52 +4,35 @@ namespace Microsoft.Azure.Devices.Edge.Hub.CloudProxy.Test
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Common;
     using Microsoft.Azure.EventHubs;
 
     public class EventHubReceiver
     {
-        readonly EventHubClient eventHubClient;
+        const string EventHubConsumerGroup = "ci-tests";
+        readonly string eventHubConnectionString;
 
         public EventHubReceiver(string eventHubConnectionString)
         {
-            this.eventHubClient = EventHubClient.CreateFromConnectionString(eventHubConnectionString);
+            this.eventHubConnectionString = eventHubConnectionString;
         }
 
-        public async Task<IList<EventData>> GetMessagesFromAllPartitions(DateTime startTime, int maxPerPartition = 10, int waitTimeSecs = 5)
+        public async Task<List<EventData>> GetMessagesForDevice(string deviceId, DateTime startTime, int maxPerPartition = 100, int waitTimeSecs = 5)
         {
+            EventHubClient eventHubClient = EventHubClient.CreateFromConnectionString(this.eventHubConnectionString);
+            PartitionReceiver partitionReceiver = eventHubClient.CreateReceiver(
+                EventHubConsumerGroup,
+                EventHubPartitionKeyResolver.ResolveToPartition(deviceId, (await eventHubClient.GetRuntimeInformationAsync()).PartitionCount),
+                EventPosition.FromEnqueuedTime(startTime));
+
             var messages = new List<EventData>();
-            EventHubRuntimeInformation rtInfo = await this.eventHubClient.GetRuntimeInformationAsync();
-            foreach (string partition in rtInfo.PartitionIds)
-            {
-                PartitionReceiver partitionReceiver = this.eventHubClient.CreateReceiver(
-                    PartitionReceiver.DefaultConsumerGroupName,
-                    partition,
-                    EventPosition.FromEnqueuedTime(startTime));
+            IEnumerable<EventData> events = await partitionReceiver.ReceiveAsync(maxPerPartition, TimeSpan.FromSeconds(waitTimeSecs));
+            messages.AddRange(events);
 
-                // Retry a few times to make sure we get all expected messages.
-                for (int i = 0; i < 3; i++)
-                {
-                    IEnumerable<EventData> events = await partitionReceiver.ReceiveAsync(maxPerPartition, TimeSpan.FromSeconds(waitTimeSecs));
-                    if (events != null)
-                    {
-                        messages.AddRange(events);
-                    }
-
-                    if (i < 3)
-                    {
-                        await Task.Delay(TimeSpan.FromSeconds(5));
-                    }
-                }
-
-                await partitionReceiver.CloseAsync();
-            }
+            await partitionReceiver.CloseAsync();
+            await eventHubClient.CloseAsync();
 
             return messages;
-        }
-
-        public async Task Close()
-        {
-            await this.eventHubClient.CloseAsync();
         }
     }
 }
