d6152b0343b839f0980c512ff86cd14e312e7183
Andrew <andsmi@microsoft.com>
2019-12-18 16:25:47 -0800

    fix build error from changed interface definition

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Diagnostics/publisher/LogAnalyticsUpload.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Diagnostics/publisher/LogAnalyticsUpload.cs
@@ -24,17 +24,19 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Diagnostics.Publisher
             this.logType = Preconditions.CheckNonWhiteSpace(logType, nameof(logType));
         }
 
-        public async Task PublishAsync(IEnumerable<Metric> metrics, CancellationToken cancellationToken)
+        public async Task<bool> PublishAsync(IEnumerable<Metric> metrics, CancellationToken cancellationToken)
         {
             try
             {
                 Preconditions.CheckNotNull(metrics, nameof(metrics));
                 await AzureLogAnalytics.Instance.PostAsync(this.workspaceId, this.workspaceKey, JsonConvert.SerializeObject(metrics), this.logType);
                 Log.LogInformation($"Successfully sent metrics to LogAnalytics");
+                return true;
             }
             catch (Exception e)
             {
                 Log.LogError(e, "Error uploading metrics to LogAnalytics");
+                return false;
             }
         }
     }
