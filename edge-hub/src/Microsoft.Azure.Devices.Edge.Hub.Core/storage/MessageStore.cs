dfc634a4672b37a1e8e0050ef64d6ac9f1e0245b
Varun Puranik <vpuranik@microsoft.com>
2019-12-20 17:04:10 -0800

    Add support for Queue length metric in EdgeHub (#2139)
    * Add support for Queue length metric in EdgeHub
    
    * Fix tests
    
    * Fix build
    
    * Update edge-hub/src/Microsoft.Azure.Devices.Routing.Core/Message.cs
    
    Co-Authored-By: Lee Fitchett <lefitche@microsoft.com>
    
    * Add comments and rename inner object to "inner"
    
    * Fix indentation
    
    * Remove offset check. Some existing tests validate offset to be -ve
    
    Co-authored-by: Lee Fitchett <lefitche@microsoft.com>

--- edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/storage/MessageStore.cs
+++ edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/storage/MessageStore.cs
@@ -13,6 +13,8 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Storage
     using Microsoft.Azure.Devices.Edge.Util;
     using Microsoft.Azure.Devices.Routing.Core;
     using Microsoft.Azure.Devices.Routing.Core.Checkpointers;
+    using Microsoft.Azure.Devices.Routing.Core.MessageSources;
+    using Microsoft.Azure.Devices.Routing.Core.Query.Types;
     using Microsoft.Extensions.Logging;
     using Newtonsoft.Json;
     using static System.FormattableString;
@@ -72,7 +74,7 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Storage
             }
         }
 
-        public async Task<long> Add(string endpointId, IMessage message)
+        public async Task<IMessage> Add(string endpointId, IMessage message)
         {
             Preconditions.CheckNotNull(message, nameof(message));
             if (!this.endpointSequentialStores.TryGetValue(Preconditions.CheckNonWhiteSpace(endpointId, nameof(endpointId)), out ISequentialStore<MessageRef> sequentialStore))
@@ -110,7 +112,7 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Storage
                 {
                     long offset = await sequentialStore.Append(new MessageRef(edgeMessageId));
                     Events.MessageAdded(offset, edgeMessageId, endpointId, this.messageCount);
-                    return offset;
+                    return new MessageWithOffset(message, offset);
                 }
             }
             catch (Exception)
@@ -164,7 +166,7 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Storage
 
             public MessageWrapper(IMessage message, DateTime timeStamp, int refCount)
             {
-                Preconditions.CheckArgument(timeStamp != default(DateTime));
+                Preconditions.CheckArgument(timeStamp != default);
                 this.Message = Preconditions.CheckNotNull(message, nameof(message));
                 this.TimeStamp = timeStamp;
                 this.RefCount = Preconditions.CheckRange(refCount, 0, nameof(refCount));
@@ -469,7 +471,7 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Storage
                             else
                             {
                                 messageWrapper
-                                    .Map(m => this.AddMessageOffset(m.Message, item.offset))
+                                    .Map(m => new MessageWithOffset(m.Message, item.offset))
                                     .ForEach(m => messageList.Add(m));
                             }
                         }
@@ -485,18 +487,6 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Storage
                 }
 
                 return messageList;
-            }
-
-            IMessage AddMessageOffset(IMessage message, long offset)
-            {
-                return new Message(
-                    message.MessageSource,
-                    message.Body,
-                    message.Properties.ToDictionary(),
-                    message.SystemProperties.ToDictionary(),
-                    offset,
-                    message.EnqueuedTime,
-                    message.DequeuedTime);
             }
         }
 
@@ -521,6 +511,38 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Storage
             public string EdgeMessageId { get; }
 
             public DateTime TimeStamp { get; }
+        }
+
+        // Wrapper to allow adding offset to an existing IMessage object
+        class MessageWithOffset : IMessage
+        {
+            readonly IMessage inner;
+
+            public MessageWithOffset(IMessage message, long offset)
+            {
+                this.inner = Preconditions.CheckNotNull(message, nameof(message));
+                this.Offset = Preconditions.CheckRange(offset, 0, nameof(offset));
+            }
+
+            public void Dispose() => this.inner.Dispose();
+
+            public IMessageSource MessageSource => this.inner.MessageSource;
+
+            public byte[] Body => this.inner.Body;
+
+            public IReadOnlyDictionary<string, string> Properties => this.inner.Properties;
+
+            public IReadOnlyDictionary<string, string> SystemProperties => this.inner.SystemProperties;
+
+            public long Offset { get; }
+
+            public DateTime EnqueuedTime => this.inner.EnqueuedTime;
+
+            public DateTime DequeuedTime => this.inner.DequeuedTime;
+
+            public QueryValue GetQueryValue(string queryString) => this.inner.GetQueryValue(queryString);
+
+            public long Size() => this.inner.Size();
         }
 
         static class MetricsV0
