6e8b52ad56313c3b83c398ca72dcd22298412a5a
dylanbronson <dybronso@microsoft.com>
2019-12-23 08:53:35 -0800

    Adding NetworkControllerResult

---
+++ edge-modules/ModuleLib/networkcontrollerresult/NetworkControllerType.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft. All rights reserved.
+namespace Microsoft.Azure.Devices.Edge.ModuleUtil.NetworkControllerResult
+{
+    public enum NetworkControllerType
+    {
+        Offline,
+        Sattelite,
+        Cellular
+    }
+}
---
+++ edge-modules/ModuleLib/networkcontrollerresult/NetworkStatus.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft. All rights reserved.
+namespace Microsoft.Azure.Devices.Edge.ModuleUtil.NetworkControllerResult
+{
+    public enum NetworkStatus
+    {
+        Enabled,
+        Disabled,
+        Unknown
+    }
+}
