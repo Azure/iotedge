ea45cbab8d90da80334eac34bd2e906fd43737b9
Philip Lin <philipktlin@live.com>
2020-01-03 16:52:21 -0800

    Add deployment test module in connectivity test.

--- test/modules/DeploymentTester/Settings.cs
+++ test/modules/DeploymentTester/Settings.cs
@@ -25,12 +25,12 @@ namespace DeploymentTester
                     configuration.GetValue<string>("IOTEDGE_DEVICEID"),
                     configuration.GetValue<string>("IOTEDGE_MODULEID"),
                     configuration.GetValue("DEPLOYMENT_TESTER_MODE", DeploymentTesterMode.Receiver),
-                    configuration.GetValue<string>("IOT_HUB_CONNECTION_STRING"),
+                    Option.Maybe(configuration.GetValue<string>("IOT_HUB_CONNECTION_STRING")),
                     configuration.GetValue("testResultCoordinatorUrl", new Uri("http://testresultcoordinator:5001")),
                     configuration.GetValue("testStartDelay", TimeSpan.FromMinutes(2)),
                     configuration.GetValue("testDuration", TimeSpan.FromHours(1)),
                     configuration.GetValue<string>("trackingId"),
-                    configuration.GetValue<string>("targetModuleId"),
+                    Option.Maybe(configuration.GetValue<string>("targetModuleId")),
                     configuration.GetValue("DEPLOYMENT_UPDATE_PERIOD", TimeSpan.FromMinutes(3)));
             });
 
@@ -38,20 +38,27 @@ namespace DeploymentTester
             string deviceId,
             string moduleId,
             DeploymentTesterMode testMode,
-            string iotHubConnectionString,
+            Option<string> iotHubConnectionString,
             Uri testResultCoordinatorUrl,
             TimeSpan testStartDelay,
             TimeSpan testDuration,
             string trackingId,
-            string targetModuleId,
+            Option<string> targetModuleId,
             TimeSpan deploymentUpdatePeriod)
         {
             this.DeviceId = Preconditions.CheckNonWhiteSpace(deviceId, nameof(deviceId));
             this.ModuleId = Preconditions.CheckNonWhiteSpace(moduleId, nameof(moduleId));
-            this.IoTHubConnectionString = Preconditions.CheckNonWhiteSpace(iotHubConnectionString, nameof(iotHubConnectionString));
             this.TestResultCoordinatorUrl = Preconditions.CheckNotNull(testResultCoordinatorUrl, nameof(testResultCoordinatorUrl));
             this.TrackingId = Preconditions.CheckNonWhiteSpace(trackingId, nameof(trackingId));
-            this.TargetModuleId = Preconditions.CheckNonWhiteSpace(targetModuleId, nameof(targetModuleId));
+
+            if (testMode == DeploymentTesterMode.Sender)
+            {
+                Preconditions.CheckArgument(iotHubConnectionString.HasValue, nameof(iotHubConnectionString));
+                this.IoTHubConnectionString = iotHubConnectionString;
+
+                Preconditions.CheckArgument(targetModuleId.HasValue, nameof(targetModuleId));
+                this.TargetModuleId = targetModuleId;
+            }
 
             this.TestMode = testMode;
             this.TestStartDelay = testStartDelay;
@@ -67,7 +74,7 @@ namespace DeploymentTester
 
         public DeploymentTesterMode TestMode { get; }
 
-        public string IoTHubConnectionString { get; }
+        public Option<string> IoTHubConnectionString { get; }
 
         public Uri TestResultCoordinatorUrl { get; }
 
@@ -77,7 +84,7 @@ namespace DeploymentTester
 
         public string TrackingId { get; }
 
-        public string TargetModuleId { get; set; }
+        public Option<string> TargetModuleId { get; set; }
 
         public TimeSpan DeploymentUpdatePeriod { get; set; }
 
@@ -94,7 +101,7 @@ namespace DeploymentTester
                 { nameof(this.TestStartDelay), this.TestStartDelay.ToString() },
                 { nameof(this.TestDuration), this.TestDuration.ToString() },
                 { nameof(this.TrackingId), this.TrackingId },
-                { nameof(this.TargetModuleId), this.TargetModuleId },
+                { nameof(this.TargetModuleId), this.TargetModuleId.ToString() },
             };
 
             return $"Settings:{Environment.NewLine}{string.Join(Environment.NewLine, fields.Select(f => $"{f.Key}={f.Value}"))}";
