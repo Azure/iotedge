269cc77111ccccd7ff3569d28277b2b8e7728716
Andrew <andsmi@microsoft.com>
2020-01-07 14:03:53 -0800

    fix build by applying unmade changes from #2028

--- edge-modules/TestAnalyzer/TestStatusStorage.cs
+++ edge-modules/TestAnalyzer/TestStatusStorage.cs
@@ -29,7 +29,7 @@ namespace TestAnalyzer
             {
                 var partitionsList = new List<string> { "messages", "directMethods", "twins" };
                 IDbStoreProvider dbStoreprovider = DbStoreProvider.Create(
-                    new RocksDbOptionsProvider(systemEnvironment, optimizeForPerformance),
+                    new RocksDbOptionsProvider(systemEnvironment, optimizeForPerformance, Option.None<ulong>()),
                     this.GetStoragePath(storagePath),
                     partitionsList);
 
