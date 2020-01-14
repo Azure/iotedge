b617c16edcec5c63e14c2c4de257fcd736fcc0f6
Andrew Smith <andsmi@microsoft.com>
2020-01-14 11:18:02 -0800

    log usercontext

--- edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.CloudProxy/CloudProxy.cs
+++ edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.CloudProxy/CloudProxy.cs
@@ -367,7 +367,8 @@ namespace Microsoft.Azure.Devices.Edge.Hub.CloudProxy
                 Preconditions.CheckNotNull(methodrequest, nameof(methodrequest));
 
                 Events.Log.LogInformation("Cloud DM received");
-                Events.Log.LogInformation(methodrequest.DataAsJson, Formatting.Indented);
+                Events.Log.LogInformation(JsonConvert.SerializeObject(methodrequest.DataAsJson, Formatting.Indented));
+                Events.Log.LogInformation(JsonConvert.SerializeObject(usercontext, Formatting.Indented));
 
                 Events.MethodCallReceived(this.cloudProxy.clientId);
                 var direceMethodRequest = new DirectMethodRequest(this.cloudProxy.clientId, methodrequest.Name, methodrequest.Data, DeviceMethodMaxResponseTimeout);
