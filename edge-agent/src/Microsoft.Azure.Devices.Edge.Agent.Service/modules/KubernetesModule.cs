db31edab213e4ed147241fc68c03e932bb63a52a
gaya <33068020+ggjjj@users.noreply.github.com>
2020-01-14 22:07:26 -0800

    Debug mode changes to test code flow

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Service/modules/KubernetesModule.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Service/modules/KubernetesModule.cs
@@ -12,7 +12,6 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Service.Modules
     using k8s;
     using Microsoft.Azure.Devices.Edge.Agent.Core;
     using Microsoft.Azure.Devices.Edge.Agent.Core.DeviceManager;
-    using Microsoft.Azure.Devices.Edge.Agent.Docker;
     using Microsoft.Azure.Devices.Edge.Agent.Edgelet;
     using Microsoft.Azure.Devices.Edge.Agent.IoTHub;
     using Microsoft.Azure.Devices.Edge.Agent.IoTHub.SdkClient;
