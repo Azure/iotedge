dcffc650bb8ebd76a8f96fd53f83a50d3454415e
Bear <yophilav@microsoft.com>
2020-01-22 17:27:22 -0800

    Starting work on the msg

--- test/modules/EdgeHubRestartTester/src/Program.cs
+++ test/modules/EdgeHubRestartTester/src/Program.cs
@@ -21,55 +21,30 @@ namespace EdgeHubRestartTester
         {
             Logger.LogInformation($"Starting module restarter with the following settings:\r\n{Settings.Current}");
 
-            if (Settings.Current.DesiredModulesToRestart.Count == 0)
-            {
-                Logger.LogInformation("No modules names found in input. Stopping.");
-                return 0;
-            }
-
-            (CancellationTokenSource cts, ManualResetEventSlim completed, Option<object> handler) = ShutdownHandler.Init(TimeSpan.FromSeconds(5), Logger);
-
-            await RestartModules(cts);
-
-            completed.Set();
-            handler.ForEach(h => GC.KeepAlive(h));
-            Logger.LogInformation("EdgeHubRestartTester Main() finished.");
-            return 0;
-        }
-
-        /// <summary>
-        /// Restarts random modules periodically (with default restart occurrence once every 10 minutes)
-        /// </summary>
-        static async Task RestartModules(CancellationTokenSource cts)
-        {
+            ModuleClient moduleClient = null;
             try
             {
-                ServiceClient iotHubServiceClient = ServiceClient.CreateFromConnectionString(Settings.Current.ServiceClientConnectionString);
+                TestResultReportingClient reportClient = new TestResultReportingClient { BaseUrl = Settings.Current.ReportingEndpointUrl.AbsoluteUri };
 
-                CloudToDeviceMethod c2dMethod = new CloudToDeviceMethod("RestartModule");
-                Random random = new Random();
 
-                string payloadSchema = "{{ \"SchemaVersion\": \"1.0\", \"Id\": \"{0}\" }}";
-                List<string> moduleNames = Settings.Current.DesiredModulesToRestart;
+                moduleClient = await ModuleUtil.CreateModuleClientAsync(
+                    Settings.Current.MessageTransportType,
+                    ModuleUtil.DefaultTimeoutErrorDetectionStrategy,
+                    ModuleUtil.DefaultTransientRetryStrategy,
+                    Logger);
+
+
+                (CancellationTokenSource cts, ManualResetEventSlim completed, Option<object> handler) = ShutdownHandler.Init(TimeSpan.FromSeconds(5), Logger);
+
                 while (!cts.Token.IsCancellationRequested)
                 {
-                    string payload = string.Format(payloadSchema, moduleNames[random.Next(0, moduleNames.Count)]);
-                    Logger.LogInformation("RestartModule Method Payload: {0}", payload);
-                    c2dMethod.SetPayloadJson(payload);
+                    (DateTime restartTime, HttpStatusCode restartStatus) = await RestartModules(iotHubServiceClient, cts);
 
-                    try
-                    {
-                        CloudToDeviceMethodResult response = await iotHubServiceClient.InvokeDeviceMethodAsync(Settings.Current.DeviceId, "$edgeAgent", c2dMethod);
-                        if (response.Status != (int)HttpStatusCode.OK)
-                        {
-                            Logger.LogError($"Calling Direct Method failed with status code {response.Status}.");
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Logger.LogError($"Exception caught for payload {payload}: {e}");
-                    }
+                    // BEARWASHERE -- Send DM until it passes, task it
+                    // BEARWASHERE -- Send Msg until it passes, task it
+                    // BEARWASHERE -- Send the "pass" response
 
+                    // BEARWASHERE -- FIgureout how to much delay I should have.
                     await Task.Delay(Settings.Current.RestartIntervalInMins * 60 * 1000, cts.Token);
                 }
             }
@@ -78,8 +53,43 @@ namespace EdgeHubRestartTester
                 Logger.LogError($"Exception caught: {e}");
                 throw;
             }
+            finally
+            {
+                moduleClient?.Dispose();
+            }
 
-            Logger.LogInformation("RestartModules finished.");
+            completed.Set();
+            handler.ForEach(h => GC.KeepAlive(h));
+            Logger.LogInformation("EdgeHubRestartTester Main() finished.");
+            return 0;
+        }
+
+        static void SendMessage(ModuleClient moduleClient)
+        {
+            // BEARWASHERE -- TODO
+        }
+
+        static async Task<Tuple<DateTime, HttpStatusCode>> RestartModules(ServiceClient iotHubServiceClient, CancellationTokenSource cts)
+        {
+            CloudToDeviceMethod c2dMethod = new CloudToDeviceMethod("RestartModule");
+            string payloadSchema = "{{ \"SchemaVersion\": \"1.0\", \"Id\": \"{0}\" }}";
+            string payload = string.Format(payloadSchema, "$edgeHub");
+            Logger.LogInformation("RestartModule Method Payload: {0}", payload);
+            c2dMethod.SetPayloadJson(payload);
+
+            try
+            {
+                CloudToDeviceMethodResult response = await iotHubServiceClient.InvokeDeviceMethodAsync(Settings.Current.DeviceId, "$edgeAgent", c2dMethod);
+                if ((HttpStatusCode)response.Status != HttpStatusCode.OK)
+                {
+                    Logger.LogError($"Calling Direct Method failed with status code {response.Status}.");
+                }
+                return new Tuple<DateTime, HttpStatusCode>(DateTime.UtcNow, (HttpStatusCode)response.Status);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"Exception caught for payload {payload}: {e}");
+            }
         }
     }
 }
--- test/modules/EdgeHubRestartTester/src/Settings.cs
+++ test/modules/EdgeHubRestartTester/src/Settings.cs
@@ -15,22 +15,13 @@ namespace EdgeHubRestartTester
         Settings(
             string serviceClientConnectionString,
             string deviceId,
-            string desiredModulesToRestartCSV,
+            string reportingEndpointUrl,
             int restartIntervalInMins)
         {
             this.ServiceClientConnectionString = Preconditions.CheckNonWhiteSpace(serviceClientConnectionString, nameof(serviceClientConnectionString));
             this.DeviceId = Preconditions.CheckNonWhiteSpace(deviceId, nameof(deviceId));
+            this.ReportingEndpointUrl = Uri(Preconditions.CheckNonWhiteSpace(reportingEndpointUrl, nameof(reportingEndpointUrl)));
             this.RestartIntervalInMins = Preconditions.CheckRange(restartIntervalInMins, 0);
-
-            // mitigate unintended repeated commas
-            this.DesiredModulesToRestart = new List<string>();
-            foreach (string name in desiredModulesToRestartCSV.Split(","))
-            {
-                if (!string.IsNullOrWhiteSpace(name))
-                {
-                    this.DesiredModulesToRestart.Add(name);
-                }
-            }
         }
 
         static Settings Create()
@@ -42,19 +33,21 @@ namespace EdgeHubRestartTester
                 .Build();
 
             return new Settings(
-                configuration.GetValue<string>("ServiceClientConnectionString", string.Empty),
+                configuration.GetValue<string>("IOT_HUB_CONNECTION_STRING", string.Empty),
                 configuration.GetValue<string>("IOTEDGE_DEVICEID", string.Empty),
-                configuration.GetValue<string>("DesiredModulesToRestartCSV", string.Empty),
-                configuration.GetValue<int>("RestartIntervalInMins", 10));
+                configuration.GetValue<string>("ReportingEndpointUrl"),
+                configuration.GetValue<int>("RestartIntervalInMins", 5));
         }
 
         public string ServiceClientConnectionString { get; }
 
         public string DeviceId { get; }
 
-        public List<string> DesiredModulesToRestart { get; }
+        public Uri ReportingEndpointUrl { get; }
 
         public int RestartIntervalInMins { get; }
+
+        public string MessageTransportType { get; }
 
         public override string ToString()
         {
@@ -62,7 +55,7 @@ namespace EdgeHubRestartTester
             var fields = new Dictionary<string, string>
             {
                 { nameof(this.DeviceId), this.DeviceId },
-                { nameof(this.DesiredModulesToRestart), string.Join(",", this.DesiredModulesToRestart) },
+                { nameof(this.ReportingEndpointUrl), this.ReportingEndpointUrl.ToString() },
                 { nameof(this.RestartIntervalInMins), this.RestartIntervalInMins.ToString() },
             };
 
