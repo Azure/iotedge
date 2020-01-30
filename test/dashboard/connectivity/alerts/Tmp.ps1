a4429179d5cbd0fe507c98513665cf5183de3262
Andrew <andsmi@microsoft.com>
2020-01-30 14:58:12 -0800

    reformat tmp script call

--- test/dashboard/connectivity/alerts/Tmp.ps1
+++ test/dashboard/connectivity/alerts/Tmp.ps1
@@ -1,3 +1,10 @@
 $tags = @{module_name = "edgeHub"; quartile = ".9" }
 
-.\Create-Alert.ps1 -MetricName "edgeAgent_used_cpu_percent" -QueryType "value" -QueryComparison ">" -QueryTarget 0 -Tags $tags -TestId "asjkdn" -AlertingInterval 15
+.\Create-Alert.ps1 `
+-MetricName "edgeAgent_used_cpu_percent" `
+-QueryType "value" `
+-QueryComparison ">" `
+-QueryTarget 0 `
+-Tags $tags `
+-TestId "asjkdn" `
+-AlertingInterval 15
