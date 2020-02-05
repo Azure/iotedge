bd3abbde56ccce53993f953c169e108aeb2a70c2
Lee Fitchett <lefitche@microsoft.com>
2020-02-05 13:24:03 -0800

    Move scripts to folder

--- test/connectivity/scripts/CheckKpiAlerts.ps1
+++ test/connectivity/scripts/CheckKpiAlerts.ps1
@@ -19,4 +19,5 @@ $query = "Alert | where AlertName in ('$alertNames')"
 $query
 $triggeredAlerts = az monitor log-analytics query -w fdf47b96-87f3-4b86-90b9-d83e2deae8a0 --analytics-query $query --verbose
 
+$triggeredAlerts
 return $triggeredAlerts.length -lt 100
--- test/dashboard/CheckAlerts.sh
+++
@@ -1,4 +0,0 @@
-sudo curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-az extension add --name log-analytics
-az login --service-principal -u http://azure-cli-2016-08-05-14-31-15 -p VerySecret --tenant
-az monitor log-analytics query -w fdf47b96-87f3-4b86-90b9-d83e2deae8a0 --analytics-query "Alert  | limit 5"
