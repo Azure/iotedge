b2465301b46e3bd822ac6fe37d47d06415c6d3af
Lee Fitchett <lefitche@microsoft.com>
2020-02-14 10:36:47 -0800

    check inner exception

--- edge-util/src/Microsoft.Azure.Devices.Edge.Util/ConfigurationExtensions.cs
+++ edge-util/src/Microsoft.Azure.Devices.Edge.Util/ConfigurationExtensions.cs
@@ -19,11 +19,16 @@ namespace Microsoft.Azure.Devices.Edge.Util
                 return configuration.GetValue(key, defaultValue);
             }
             catch (Exception ex)
-            when (ex is OverflowException || ex is FormatException)
+            when (IsParseException(ex))
             {
                 Log.LogWarning(ex, $"Could not parse timespan for {key}");
                 return defaultValue;
             }
+        }
+
+        static bool IsParseException(Exception ex)
+        {
+            return ex != null && ex is OverflowException || ex is FormatException || IsParseException(ex.InnerException);
         }
     }
 }
