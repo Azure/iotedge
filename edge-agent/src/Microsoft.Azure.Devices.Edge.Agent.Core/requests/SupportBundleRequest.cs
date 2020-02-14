0aa8462016d9261b50e164461bd35df1254cbb22
Lee Fitchett <lefitche@microsoft.com>
2020-02-14 10:52:37 -0800

    make handler classes

---
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/requests/SupportBundleRequest.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.Azure.Devices.Edge.Agent.Core.Requests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class SupportBundleRequest
+    {
+    }
+}
---
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/requests/SupportBundleRequestHandler.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.Azure.Devices.Edge.Agent.Core.Requests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Edge.Util;
+
+    public class SupportBundleRequestHandler : RequestHandlerBase<SupportBundleRequest, TaskStatusResponse>
+    {
+        public override string RequestName => "UploadSupportBundle";
+
+        protected override async Task<Option<TaskStatusResponse>> HandleRequestInternal(Option<SupportBundleRequest> payload, CancellationToken cancellationToken)
+        {
+            await Task.Yield();
+
+            (string correlationId, BackgroundTaskStatus status) = BackgroundTask.Run(() => Task.CompletedTask, "upload logs", cancellationToken);
+            return Option.Some(TaskStatusResponse.Create(correlationId, status));
+        }
+    }
+}
