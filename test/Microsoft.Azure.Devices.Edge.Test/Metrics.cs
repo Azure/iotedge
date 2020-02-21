c6b81efc3f1894f2b1179a2008dbe98fccbbaef0
Damon Barry <damonb@microsoft.com>
2020-02-20 17:03:53 -0800

    Remove edge hub routes config

--- test/Microsoft.Azure.Devices.Edge.Test/Metrics.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/Metrics.cs
@@ -33,17 +33,7 @@ namespace Microsoft.Azure.Devices.Edge.Test
                     builder.AddModule(ModuleName, metricsValidatorImage);
 
                     builder.GetModule("$edgeHub")
-                        .WithEnvironment(("experimentalfeatures__enabled", "true"), ("experimentalfeatures__enableMetrics", "true"))
-                        .WithDesiredProperties(new Dictionary<string, object>
-                        {
-                                {
-                                    "routes", new
-                                    {
-                                        All = "FROM /messages/* INTO $upstream",
-                                        QueueLengthTest = "FROM /messages/modules/MetricsValidator/outputs/ToSelf INTO BrokeredEndpoint(\"/modules/MetricsValidator/inputs/FromSelf\")"
-                                    }
-                                }
-                        });
+                        .WithEnvironment(("experimentalfeatures__enabled", "true"), ("experimentalfeatures__enableMetrics", "true"));
                 },
                 token);
 
