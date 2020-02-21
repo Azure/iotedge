0f1ca43f3caba582d6c7e44f3b52b9b6b362a827
Lee Fitchett <lefitche@microsoft.com>
2020-02-21 11:33:44 -0800

    use --output - for stdout

--- edgelet/iotedge/src/main.rs
+++ edgelet/iotedge/src/main.rs
@@ -241,10 +241,10 @@ fn run() -> Result<(), Error> {
             SubCommand::with_name("support-bundle")
                 .about("Bundles troubleshooting information")
                 .arg(
-                    Arg::with_name("path")
-                        .help("Path of output file. Only used if --output file")
-                        .long("path")
-                        .short("p")
+                    Arg::with_name("output")
+                        .help("Location to output file. Use - for stdout")
+                        .long("output")
+                        .short("o")
                         .takes_value(true)
                         .value_name("FILENAME")
                         .default_value("support_bundle.zip"),
@@ -275,14 +275,6 @@ fn run() -> Result<(), Error> {
                         .long("quiet")
                         .short("q")
                         .takes_value(false),
-                ).arg(
-                    Arg::with_name("output")
-                        .help("Output location")
-                        .long("output")
-                        .short("o")
-                        .possible_values(&["file", "console"])
-                        .takes_value(true)
-                        .default_value("file"),
                 ),
         )
         .subcommand(SubCommand::with_name("version").about("Show the version information"))
@@ -378,7 +370,7 @@ fn run() -> Result<(), Error> {
             tokio_runtime.block_on(Logs::new(id, options, runtime()?).execute())
         }
         ("support-bundle", Some(args)) => {
-            let location = args.value_of_os("path").expect("arg has a default value");
+            let location = args.value_of_os("output").expect("arg has a default value");
             let since = args
                 .value_of("since")
                 .map(|s| parse_since(s))
@@ -392,14 +384,11 @@ fn run() -> Result<(), Error> {
             let include_ms_only = args.is_present("include-edge-runtime-only");
             let verbose = !args.is_present("quiet");
             let iothub_hostname = args.value_of("iothub-hostname").map(ToOwned::to_owned);
-            let output_location = args
-                .value_of("output")
-                .map(|arg| match arg {
-                    "file" => OutputLocation::File(location.to_owned()),
-                    "console" => OutputLocation::Console,
-                    _ => unreachable!(),
-                })
-                .expect("arg has a default value");
+            let output_location = if location == "-" {
+                OutputLocation::Console
+            } else {
+                OutputLocation::File(location.to_owned())
+            };
 
             tokio_runtime.block_on(
                 SupportBundle::new(
