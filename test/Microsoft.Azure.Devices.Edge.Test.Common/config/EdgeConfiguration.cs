2556fdd835f3a54e62fcde0a67f232f229ca350a
Damon Barry <damonb@microsoft.com>
2020-02-21 10:58:02 -0800

    Move logging to another location

--- test/Microsoft.Azure.Devices.Edge.Test.Common/config/EdgeConfiguration.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/config/EdgeConfiguration.cs
@@ -33,8 +33,10 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common.Config
         {
             return Profiler.Run(
                 () => iotHub.DeployDeviceConfigurationAsync(this.deviceId, this.config, token),
-                "Deployed edge configuration to device with modules:\n    {Modules}",
-                string.Join("\n    ", this.moduleImages));
+                "Deployed edge configuration to device with modules:\n>   {Modules}\n>   {Names}\n----\n{Config}\n----\n",
+                string.Join("\n    ", this.moduleImages),
+                string.Join("\n    ", this.ModuleNames),
+                JsonConvert.SerializeObject(this.config));
         }
 
         public override string ToString() => JsonConvert.SerializeObject(this.config, Formatting.Indented);
