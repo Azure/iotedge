b4d9a063e5a98fe44000c531ccc220b054838ad1
Philip Lin <philipktlin@live.com>
2020-02-21 17:13:59 -0800

    add test info into test report for LH and stress test

--- test/modules/TestAnalyzer/AggregateCloudOperationReport.cs
+++ test/modules/TestAnalyzer/AggregateCloudOperationReport.cs
@@ -3,13 +3,14 @@ namespace TestAnalyzer
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Serialization;
 
     [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
     class AggregateCloudOperationReport
     {
-        public AggregateCloudOperationReport(string moduleId, IDictionary<string, Tuple<int, DateTime>> statusCodes)
+        public AggregateCloudOperationReport(string moduleId, IDictionary<string, Tuple<int, DateTime>> statusCodes, IDictionary<string, string> testInfo)
         {
             this.ModuleId = moduleId;
             this.StatusCodes = new List<CloudOperationReport>();
@@ -27,6 +28,10 @@ namespace TestAnalyzer
         public string ModuleId { get; }
 
         public IList<CloudOperationReport> StatusCodes { get; }
+
+        public IDictionary<string, string> TestInfo { get; }
+
+        public bool IsPassed => this.StatusCodes.All(x => x.StatusCode.Equals("200"));
 
         public override string ToString()
         {
