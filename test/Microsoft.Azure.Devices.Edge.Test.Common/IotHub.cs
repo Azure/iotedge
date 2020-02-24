dd1c614736c2842731b84fde65f0a9528f44fbb1
Damon Barry <damonb@microsoft.com>
2020-02-24 15:38:51 -0800

    Revert "Dump the deployment config"
    This reverts commit 075396f7a996fd75a6a1f794921c12e62843f2d6.

--- test/Microsoft.Azure.Devices.Edge.Test.Common/IotHub.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/IotHub.cs
@@ -106,11 +106,8 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
         public Task DeployDeviceConfigurationAsync(
             string deviceId,
             ConfigurationContent config,
-            CancellationToken token)
-        {
-            Log.Information(">>> DEPLOYING CONFIG:\n{Config}\n=====\n", JsonConvert.SerializeObject(config, Formatting.Indented));
-            return this.RegistryManager.ApplyConfigurationContentOnDeviceAsync(deviceId, config, token);
-        }
+            CancellationToken token) => this.RegistryManager.ApplyConfigurationContentOnDeviceAsync(deviceId, config, token);
+
         public Task<Twin> GetTwinAsync(
             string deviceId,
             string moduleId,
