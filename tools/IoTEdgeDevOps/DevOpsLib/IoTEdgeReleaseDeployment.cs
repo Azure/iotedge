5864d7fcbfbeae5c201141bdb790e494a8867b3b
Andrew <andsmi@microsoft.com>
2020-03-02 16:14:57 -0800

    eliminate use of null

--- tools/IoTEdgeDevOps/DevOpsLib/IoTEdgeReleaseDeployment.cs
+++ tools/IoTEdgeDevOps/DevOpsLib/IoTEdgeReleaseDeployment.cs
@@ -18,6 +18,7 @@ namespace DevOpsLib
         {
             ValidationUtil.ThrowIfNonPositive(id, nameof(id));
             ValidationUtil.ThrowIfNonPositive(attempt, nameof(attempt));
+            ValidationUtil.ThrowIfNull(tasks, nameof(tasks));
 
             this.id = id;
             this.attempt = attempt;
@@ -37,7 +38,7 @@ namespace DevOpsLib
         public HashSet<IoTEdgePipelineTask> Tasks => this.tasks;
 
         public static IoTEdgeReleaseDeployment Create(VstsReleaseDeployment vstsReleaseDeployment)
-            => new IoTEdgeReleaseDeployment(vstsReleaseDeployment.Id, vstsReleaseDeployment.Attempt, vstsReleaseDeployment.Status, vstsReleaseDeployment.LastModifiedOn, vstsReleaseDeployment.Tasks?.Select(IoTEdgePipelineTask.Create).ToHashSet());
+            => new IoTEdgeReleaseDeployment(vstsReleaseDeployment.Id, vstsReleaseDeployment.Attempt, vstsReleaseDeployment.Status, vstsReleaseDeployment.LastModifiedOn, vstsReleaseDeployment.Tasks?.Select(IoTEdgePipelineTask.Create).ToHashSet() ?? new HashSet<IoTEdgePipelineTask>());
 
         public bool Equals(IoTEdgeReleaseDeployment other)
         {
