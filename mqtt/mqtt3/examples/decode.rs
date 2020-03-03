96d8866a6bfaf4f5c9bed3b9e19f1c9b01db9519
dmolokanov <dmolokanov@users.noreply.github.com>
2020-03-02 16:51:12 -0800

    Format code

--- mqtt/mqtt3/examples/decode.rs
+++ mqtt/mqtt3/examples/decode.rs
@@ -7,41 +7,46 @@
 /// Example:
 ///
 ///     cargo run --example decode -- /path/to/some/raw/mqtt/packet.bin
-
 use std::io::Read;
 
 use bytes::Buf;
-use tokio_util::codec::{ Decoder, Encoder };
+use tokio_util::codec::{Decoder, Encoder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let filename = std::env::args_os().nth(1).ok_or("expected one argument set to the name of the file to decode")?;
-	let file = std::fs::OpenOptions::new().read(true).open(filename)?;
-	let mut file = std::io::BufReader::new(file);
+    let filename = std::env::args_os()
+        .nth(1)
+        .ok_or("expected one argument set to the name of the file to decode")?;
+    let file = std::fs::OpenOptions::new().read(true).open(filename)?;
+    let mut file = std::io::BufReader::new(file);
 
-	let mut codec: mqtt3::proto::PacketCodec = Default::default();
+    let mut codec: mqtt3::proto::PacketCodec = Default::default();
 
-	let mut data = vec![];
-	file.read_to_end(&mut data)?;
-	let mut bytes: bytes::BytesMut = (&*data).into();
+    let mut data = vec![];
+    file.read_to_end(&mut data)?;
+    let mut bytes: bytes::BytesMut = (&*data).into();
 
-	let packet = codec.decode(&mut bytes)?.ok_or("incomplete packet")?;
-	println!("{:#?}", packet);
+    let packet = codec.decode(&mut bytes)?.ok_or("incomplete packet")?;
+    println!("{:#?}", packet);
 
-	let input_remaining = bytes.len();
-	codec.encode(packet.clone(), &mut bytes)?;
-	bytes.advance(input_remaining);
+    let input_remaining = bytes.len();
+    codec.encode(packet.clone(), &mut bytes)?;
+    bytes.advance(input_remaining);
 
-	let packet2 = codec.decode(&mut bytes)?.ok_or("could not decode re-encoded packet")?;
-	assert_eq!(packet, packet2);
+    let packet2 = codec
+        .decode(&mut bytes)?
+        .ok_or("could not decode re-encoded packet")?;
+    assert_eq!(packet, packet2);
 
-	if !bytes.is_empty() {
-		return Err("leftover bytes".into());
-	}
+    if !bytes.is_empty() {
+        return Err("leftover bytes".into());
+    }
 
-	codec.encode(packet.clone(), &mut bytes)?;
+    codec.encode(packet.clone(), &mut bytes)?;
 
-	let packet2 = codec.decode(&mut bytes)?.ok_or("could not decode re-encoded packet")?;
-	assert_eq!(packet, packet2);
+    let packet2 = codec
+        .decode(&mut bytes)?
+        .ok_or("could not decode re-encoded packet")?;
+    assert_eq!(packet, packet2);
 
-	Ok(())
+    Ok(())
 }
--- mqtt/mqtt3/examples/will.rs
+++ mqtt/mqtt3/examples/will.rs
@@ -16,116 +16,132 @@ mod common;
 
 #[derive(Debug, structopt::StructOpt)]
 struct Options {
-	#[structopt(help = "Address of the MQTT server.", long = "server")]
-	server: std::net::SocketAddr,
+    #[structopt(help = "Address of the MQTT server.", long = "server")]
+    server: std::net::SocketAddr,
 
-	#[structopt(help = "Client ID used to identify this application to the server. If not given, a server-generated ID will be used.", long = "client-id")]
-	client_id: Option<String>,
+    #[structopt(
+        help = "Client ID used to identify this application to the server. If not given, a server-generated ID will be used.",
+        long = "client-id"
+    )]
+    client_id: Option<String>,
 
-	#[structopt(help = "Username used to authenticate with the server, if any.", long = "username")]
-	username: Option<String>,
+    #[structopt(
+        help = "Username used to authenticate with the server, if any.",
+        long = "username"
+    )]
+    username: Option<String>,
 
-	#[structopt(help = "Password used to authenticate with the server, if any.", long = "password")]
-	password: Option<String>,
+    #[structopt(
+        help = "Password used to authenticate with the server, if any.",
+        long = "password"
+    )]
+    password: Option<String>,
 
-	#[structopt(
+    #[structopt(
 		help = "Maximum back-off time between reconnections to the server, in seconds.",
 		long = "max-reconnect-back-off",
 		default_value = "30",
 		parse(try_from_str = common::duration_from_secs_str),
 	)]
-	max_reconnect_back_off: std::time::Duration,
+    max_reconnect_back_off: std::time::Duration,
 
-	#[structopt(
+    #[structopt(
 		help = "Keep-alive time advertised to the server, in seconds.",
 		long = "keep-alive",
 		default_value = "5",
 		parse(try_from_str = common::duration_from_secs_str),
 	)]
-	keep_alive: std::time::Duration,
+    keep_alive: std::time::Duration,
 
-	#[structopt(help = "The topic of the will.", long = "topic")]
-	topic: String,
+    #[structopt(help = "The topic of the will.", long = "topic")]
+    topic: String,
 
-	#[structopt(help = "The QoS of the will.", long = "qos", parse(try_from_str = common::qos_from_str))]
-	qos: mqtt3::proto::QoS,
+    #[structopt(help = "The QoS of the will.", long = "qos", parse(try_from_str = common::qos_from_str))]
+    qos: mqtt3::proto::QoS,
 
-	#[structopt(help = "The payload of the will.", long = "payload")]
-	payload: String,
+    #[structopt(help = "The payload of the will.", long = "payload")]
+    payload: String,
 }
 
 fn main() {
-	env_logger::Builder::from_env(env_logger::Env::new().filter_or("MQTT3_LOG", "mqtt3=debug,mqtt3::logging=trace,will=info")).init();
+    env_logger::Builder::from_env(
+        env_logger::Env::new().filter_or("MQTT3_LOG", "mqtt3=debug,mqtt3::logging=trace,will=info"),
+    )
+    .init();
 
-	let Options {
-		server,
-		client_id,
-		username,
-		password,
-		max_reconnect_back_off,
-		keep_alive,
-		topic,
-		qos,
-		payload,
-	} = structopt::StructOpt::from_args();
+    let Options {
+        server,
+        client_id,
+        username,
+        password,
+        max_reconnect_back_off,
+        keep_alive,
+        topic,
+        qos,
+        payload,
+    } = structopt::StructOpt::from_args();
 
-	let mut runtime = tokio::runtime::Runtime::new().expect("couldn't initialize tokio runtime");
+    let mut runtime = tokio::runtime::Runtime::new().expect("couldn't initialize tokio runtime");
 
-	let will = mqtt3::proto::Publication {
-		topic_name: topic.clone(),
-		qos,
-		retain: false,
-		payload: payload.into(),
-	};
+    let will = mqtt3::proto::Publication {
+        topic_name: topic.clone(),
+        qos,
+        retain: false,
+        payload: payload.into(),
+    };
 
-	let mut client =
-		mqtt3::Client::new(
-			client_id,
-			username,
-			Some(will),
-			move || {
-				let password = password.clone();
-				Box::pin(async move {
-					let io = tokio::net::TcpStream::connect(&server).await;
-					io.map(|io| (io, password))
-				})
-			},
-			max_reconnect_back_off,
-			keep_alive,
-		);
+    let mut client = mqtt3::Client::new(
+        client_id,
+        username,
+        Some(will),
+        move || {
+            let password = password.clone();
+            Box::pin(async move {
+                let io = tokio::net::TcpStream::connect(&server).await;
+                io.map(|io| (io, password))
+            })
+        },
+        max_reconnect_back_off,
+        keep_alive,
+    );
 
-	let mut update_subscription_handle = client.update_subscription_handle().expect("couldn't get subscription update handle");
-	runtime.spawn(async move {
-		let result = update_subscription_handle.subscribe(mqtt3::proto::SubscribeTo { topic_filter: topic, qos }).await;
-		if let Err(err) = result {
-			panic!("couldn't update subscription: {}", err);
-		}
-	});
+    let mut update_subscription_handle = client
+        .update_subscription_handle()
+        .expect("couldn't get subscription update handle");
+    runtime.spawn(async move {
+        let result = update_subscription_handle
+            .subscribe(mqtt3::proto::SubscribeTo {
+                topic_filter: topic,
+                qos,
+            })
+            .await;
+        if let Err(err) = result {
+            panic!("couldn't update subscription: {}", err);
+        }
+    });
 
-	let () = runtime.block_on(async move {
-		use futures_util::StreamExt;
+    let () = runtime.block_on(async move {
+        use futures_util::StreamExt;
 
-		while let Some(event) = client.next().await {
-			let event = event.unwrap();
+        while let Some(event) = client.next().await {
+            let event = event.unwrap();
 
-			if let mqtt3::Event::Publication(publication) = event {
-				match std::str::from_utf8(&publication.payload) {
-					Ok(s) =>
-						log::info!(
-							"Received publication: {:?} {:?} {:?}",
-							publication.topic_name,
-							s,
-							publication.qos,
-						),
-					Err(_) =>
-						log::info!(
-							"Received publication: {:?} {:?} {:?}",
-							publication.topic_name,
-							publication.payload,
-							publication.qos,
-						),
-				}
-			}
-		}
-	});
+            if let mqtt3::Event::Publication(publication) = event {
+                match std::str::from_utf8(&publication.payload) {
+                    Ok(s) => log::info!(
+                        "Received publication: {:?} {:?} {:?}",
+                        publication.topic_name,
+                        s,
+                        publication.qos,
+                    ),
+                    Err(_) => log::info!(
+                        "Received publication: {:?} {:?} {:?}",
+                        publication.topic_name,
+                        publication.payload,
+                        publication.qos,
+                    ),
+                }
+            }
+        }
+    });
 }
