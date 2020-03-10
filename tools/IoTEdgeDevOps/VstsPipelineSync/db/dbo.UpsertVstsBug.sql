ed7ee56fd7d99f3b62d43e40e8d91c51961a5757
Andrew Smith <als5ev@virginia.edu>
2020-03-10 11:58:15 -0700

    fix spacing issues

--- tools/IoTEdgeDevOps/VstsPipelineSync/db/dbo.UpsertVstsBug.sql
+++ tools/IoTEdgeDevOps/VstsPipelineSync/db/dbo.UpsertVstsBug.sql
@@ -1,17 +1,17 @@
 CREATE PROCEDURE [dbo].[UpsertVstsBug]
-	@QueryName varchar(20),
-	@BugCount int
+    @QueryName varchar(20),
+    @BugCount int
 AS
     DECLARE @now datetime2;
     SET @now = SYSDATETIME();
 
-	IF EXISTS (SELECT 1 FROM dbo.VstsBug WHERE QueryName = @QueryName)
+    IF EXISTS (SELECT 1 FROM dbo.VstsBug WHERE QueryName = @QueryName)
     BEGIN
         UPDATE dbo.VstsBug
-		SET QueryName = @QueryName,
-		    BugCount = @BugCount,
-			UpdatedAt = @now
-		WHERE QueryName = @QueryName
+        SET QueryName = @QueryName,
+            BugCount = @BugCount,
+            UpdatedAt = @now
+        WHERE QueryName = @QueryName
     END
     ELSE
     BEGIN
