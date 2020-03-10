1600899c30fa030aa70dc55f44c115d897bcebd2
Andrew Smith <als5ev@virginia.edu>
2020-03-10 10:20:51 -0700

    hardcode dict of bug id

--- tools/IoTEdgeDevOps/DevOpsLib/BugQueryId.cs
+++ tools/IoTEdgeDevOps/DevOpsLib/BugQueryId.cs
@@ -5,11 +5,9 @@ namespace DevOpsLib
 {
     public static class BugQueryId
     {
-        public const string SampleQuery = "d7f4e036-0ccb-46a6-a33d-87a932296543";
-
         public readonly static Dictionary<string, string> QueryNamestoIds = new Dictionary<string, string>() 
         { 
-            {nameof(SampleQuery), SampleQuery  } 
+            {"SampleQuery", "d7f4e036-0ccb-46a6-a33d-87a932296543"} 
         };
     }
 }
