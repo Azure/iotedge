39edd73cfd6a0585c467e1fa4dbb717101610f96
Andrew Smith <als5ev@virginia.edu>
2020-03-23 09:25:31 -0700

    windows use dotnet core 3.1

--- test/Microsoft.Azure.Devices.Edge.Test/Metrics.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/Metrics.cs
@@ -3,7 +3,6 @@ namespace Microsoft.Azure.Devices.Edge.Test
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Net;
     using System.Threading;
     using System.Threading.Tasks;
