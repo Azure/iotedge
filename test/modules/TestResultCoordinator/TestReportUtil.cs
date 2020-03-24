94741dbabd687e7c7c4cb95db536788b6bc37fbc
Bear <yophilav@microsoft.com>
2020-03-24 10:42:07 -0700

    Remove debugging log

--- test/modules/TestResultCoordinator/TestReportUtil.cs
+++ test/modules/TestResultCoordinator/TestReportUtil.cs
@@ -152,7 +152,7 @@ namespace TestResultCoordinator
             CloudToDeviceMethod uploadLogRequest =
                 new CloudToDeviceMethod("UploadLogs")
                     .SetPayloadJson($"{{ \"schemaVersion\": \"1.0\", \"sasUrl\": \"{blobContainerWriteUri.AbsoluteUri}\", \"items\": [{{ \"id\": \".*\", \"filter\": {{}} }}], \"encoding\": \"gzip\", \"contentType\": \"json\" }}");
-            logger.LogError($"BEARWASHERE - {uploadLogRequest.ToString()}: {uploadLogRequest.GetPayloadAsJson()}");
+
             CloudToDeviceMethodResult uploadLogResponse = await serviceClient.InvokeDeviceMethodAsync(Settings.Current.DeviceId, "$edgeAgent", uploadLogRequest);
 
             (string status, string correlationId) = GetUploadLogResponseResult(uploadLogResponse.GetPayloadAsJson());
