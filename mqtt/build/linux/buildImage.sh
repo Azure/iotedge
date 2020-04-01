d33ea016deae58c862e7c055dbc0f7a59c234103
Vadim Kovalyov <vakavali@microsoft.com>
2020-03-31 19:25:20 -0700

    Fixed minor issues in scripts

--- mqtt/build/linux/buildImage.sh
+++ mqtt/build/linux/buildImage.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# This script copies the iodedged executable files that go into the azureiotedge-iotedged image,
-# for each supported arch (x86_64, arm32v7, arm64v8).
-# It then publishes executable files along with their corresponding dockerfiles to the publish directory,
-# so that buildImage.sh can build the container image.
-
 set -e
-###############################################################################
-# Define Environment Variables
-###############################################################################
 
 ARCH=$1
 DOCKER_REGISTRY=$2
@@ -19,26 +11,25 @@ DOCKER_IMAGEVERSION=${BUILD_BUILDNUMBER:=$5}
 if [[ "$ARCH" == "amd64" ]]; then
     ARCH="amd64"
     DOCKER_IMAGENAME="azureiotedge/mqttd-linux-amd64"
-    TARGET="x86_64-unknown-linux-musl"
     DOCKERFILE="../../docker/linux/amd64/Dockerfile"
 elif [[ "$ARCH" == "armv7" ]]; then
     ARCH="arm32v7"
     DOCKER_IMAGENAME="azureiotedge/mqttd-linux-arm32v7"
-    TARGET="armv7-unknown-linux-musleabihf"
     DOCKERFILE="../../docker/linux/arm32v7/Dockerfile"
 else
     echo "Unsupported architecture"
     exit 1
 fi
 
-echo "Arch:                 $ARCH"
+echo "ARCH:                 $ARCH"
 echo "DOCKER_IMAGENAME:     $DOCKER_IMAGENAME"
 echo "TARGET:               $TARGET"
 echo "DOCKERFILE:           $DOCKERFILE"
+echo "DOCKER_IMAGEVERSION:  $DOCKER_IMAGEVERSION"
 
-echo "Building and pushing Docker image $imagename for $arch"
+echo "Building and pushing Docker image $DOCKER_IMAGENAME for $ARCH"
 docker_build_cmd="docker build --no-cache"
-docker_build_cmd+=" -t $DOCKER_REGISTRY/$DOCKER_IMAGENAME"
+docker_build_cmd+=" -t $DOCKER_REGISTRY/$DOCKER_IMAGENAME:$DOCKER_IMAGEVERSION"
 docker_build_cmd+=" --file $DOCKERFILE"
 docker_build_cmd+=" ../.."
 
