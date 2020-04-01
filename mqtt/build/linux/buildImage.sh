fed1129236620450a79f7c46f8e7afb6cc40b53e
Vadim Kovalyov <vakavali@microsoft.com>
2020-04-01 15:01:02 -0700

    Fixed script relateive paths

--- mqtt/build/linux/buildImage.sh
+++ mqtt/build/linux/buildImage.sh
@@ -9,11 +9,11 @@ DOCKER_IMAGEVERSION=${BUILD_BUILDNUMBER:=$3}
 if [[ "$ARCH" == "amd64" ]]; then
     ARCH="amd64"
     DOCKER_IMAGENAME="azureiotedge/mqttd-linux-amd64"
-    DOCKERFILE="../../docker/linux/amd64/Dockerfile"
+    DOCKERFILE="mqtt/docker/linux/amd64/Dockerfile"
 elif [[ "$ARCH" == "armv7" ]]; then
     ARCH="arm32v7"
     DOCKER_IMAGENAME="azureiotedge/mqttd-linux-arm32v7"
-    DOCKERFILE="../../docker/linux/arm32v7/Dockerfile"
+    DOCKERFILE="mqtt/docker/linux/arm32v7/Dockerfile"
 else
     echo "Unsupported architecture"
     exit 1
@@ -29,7 +29,7 @@ echo "Building and pushing Docker image $DOCKER_IMAGENAME for $ARCH"
 docker_build_cmd="docker build --no-cache"
 docker_build_cmd+=" -t $DOCKER_REGISTRY/$DOCKER_IMAGENAME:$DOCKER_IMAGEVERSION"
 docker_build_cmd+=" --file $DOCKERFILE"
-docker_build_cmd+=" ../.."
+docker_build_cmd+=" mqtt/."
 
 echo "Running... $docker_build_cmd"
 
