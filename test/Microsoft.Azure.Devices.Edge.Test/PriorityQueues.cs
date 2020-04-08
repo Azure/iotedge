c38b1617c6457249e4610b7ab83740374f3f9965
dylanbronson <dybronso@microsoft.com>
2020-04-08 13:58:57 -0700

    Bringing down delay

--- test/Microsoft.Azure.Devices.Edge.Test/PriorityQueues.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/PriorityQueues.cs
@@ -210,7 +210,7 @@ namespace Microsoft.Azure.Devices.Edge.Test
 
             // Wait long enough for TTL to expire for some of the messages
             Log.Information("Waiting for 45 seconds for TTL's to expire");
-            await Task.Delay(45000);
+            await Task.Delay(20000);
 
             Action<EdgeConfigBuilder> addRelayerConfig = new Action<EdgeConfigBuilder>(
                 builder =>
