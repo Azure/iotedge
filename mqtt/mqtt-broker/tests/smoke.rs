f7effb1b3c6eac8d5a7f17c42318c0c77f9de90a
Vadim Kovalyov <vakavali@microsoft.com>
2020-04-22 18:27:01 -0700

    Some test fixes

--- mqtt/mqtt-broker/tests/smoke.rs
+++ mqtt/mqtt-broker/tests/smoke.rs
@@ -1,7 +1,9 @@
+use futures_util::{FutureExt, StreamExt};
 use mqtt3::proto::QoS::{AtLeastOnce, AtMostOnce, ExactlyOnce};
 use mqtt3::proto::SubscribeTo;
 use mqtt3::{Event, ReceivedPublication};
 use mqtt_broker::{AuthId, BrokerBuilder, Server};
+use std::future::Future;
 
 mod common;
 
@@ -31,7 +33,7 @@ async fn basic_connect_clean_session() {
 
 #[tokio::test]
 async fn basic_pub_sub() {
-    const SERVER: &str = "localhost:0";
+    const SERVER: &str = "localhost:1884";
     const TOPIC: &str = "topic/A";
 
     let broker = BrokerBuilder::default()
@@ -48,8 +50,6 @@ async fn basic_pub_sub() {
         .client_id("mqtt-smoke-tests")
         .build();
 
-    client.events_receiver.recv().await; //drain connect event
-
     client
         .subscribe(SubscribeTo {
             topic_filter: TOPIC.into(),
@@ -57,11 +57,6 @@ async fn basic_pub_sub() {
         })
         .await
         .expect("couldn't subscribe to a topic");
-
-    matches::assert_matches!(
-        client.events_receiver.recv().await,
-        Some(Event::SubscriptionUpdates(_))
-    );
 
     client
         .publish(mqtt3::proto::Publication {
@@ -73,11 +68,6 @@ async fn basic_pub_sub() {
         .await
         .expect("couldn't publish");
 
-    matches::assert_matches!(
-        client.events_receiver.recv().await,
-        Some(Event::Publication(ReceivedPublication{payload, ..})) if payload == bytes::Bytes::from("qos 0")
-    );
-
     client
         .publish(mqtt3::proto::Publication {
             topic_name: TOPIC.into(),
@@ -87,11 +77,6 @@ async fn basic_pub_sub() {
         })
         .await
         .expect("couldn't publish");
-
-    matches::assert_matches!(
-        client.events_receiver.recv().await,
-        Some(Event::Publication(ReceivedPublication{payload, ..})) if payload == bytes::Bytes::from("qos 1")
-    );
 
     client
         .publish(mqtt3::proto::Publication {
@@ -103,11 +88,111 @@ async fn basic_pub_sub() {
         .await
         .expect("couldn't publish");
 
+    client.events_receiver.recv().await; //skip connect event
+
+    matches::assert_matches!(
+        client.events_receiver.recv().await,
+        Some(Event::SubscriptionUpdates(_))
+    );
+    matches::assert_matches!(
+        client.events_receiver.recv().await,
+        Some(Event::Publication(ReceivedPublication{payload, ..})) if payload == bytes::Bytes::from("qos 0")
+    );
+    matches::assert_matches!(
+        client.events_receiver.recv().await,
+        Some(Event::Publication(ReceivedPublication{payload, ..})) if payload == bytes::Bytes::from("qos 1")
+    );
     matches::assert_matches!(
         client.events_receiver.recv().await,
         Some(Event::Publication(ReceivedPublication{payload, ..})) if payload == bytes::Bytes::from("qos 2")
     );
 
     client.shutdown().await.expect("couldn't shutdown");
+    broker_shutdown.send(()).expect("couldn't shutdown broker");
     client.join().await.expect("can't wait for the client");
+    broker_task
+        .await
+        .unwrap()
+        .expect("can't wait for the broker");
+}
+
+#[tokio::test]
+async fn basic_pub_sub2() {
+    const SERVER: &str = "localhost:1885";
+    const TOPIC: &str = "topic/A";
+
+    let broker = BrokerBuilder::default()
+        .authenticator(|_| Ok(Some(AuthId::Anonymous)))
+        .authorizer(|_| Ok(true))
+        .build();
+
+    let (broker_shutdown, rx) = tokio::sync::oneshot::channel::<()>();
+    let transports = vec![mqtt_broker::TransportBuilder::Tcp(SERVER)];
+    let broker_task = tokio::spawn(Server::from_broker(broker).serve(transports, rx.map(drop)));
+
+    let mut client = mqtt3::Client::new(
+        Some("client-mqtt-smoke".to_string()),
+        None,
+        None,
+        move || {
+            Box::pin(async move {
+                let io = tokio::net::TcpStream::connect(&SERVER).await;
+                io.map(|io| (io, None))
+            })
+        },
+        std::time::Duration::from_secs(1),
+        std::time::Duration::from_secs(60),
+    );
+
+    client.next().await;
+
+    let t1 = client.subscribe(SubscribeTo {
+        topic_filter: TOPIC.into(),
+        qos: AtMostOnce,
+    });
+
+    matches::assert_matches!(client.next().await, Some(Ok(Event::SubscriptionUpdates(_))));
+    t1.expect("can't subscribe");
+
+    let _ = client.publish(mqtt3::proto::Publication {
+        topic_name: TOPIC.into(),
+        qos: AtMostOnce,
+        retain: false,
+        payload: "qos 0".into(),
+    });
+
+    matches::assert_matches!(
+        client.next().await,
+        Some(Ok(Event::Publication(ReceivedPublication{payload, ..}))) if payload == bytes::Bytes::from("qos 0")
+    );
+
+    let _ = client.publish(mqtt3::proto::Publication {
+        topic_name: TOPIC.into(),
+        qos: AtLeastOnce,
+        retain: false,
+        payload: "qos 1".into(),
+    });
+
+    matches::assert_matches!(
+        client.next().await,
+        Some(Ok(Event::Publication(ReceivedPublication{payload, ..}))) if payload == bytes::Bytes::from("qos 1")
+    );
+
+    let _ = client.publish(mqtt3::proto::Publication {
+        topic_name: TOPIC.into(),
+        qos: ExactlyOnce,
+        retain: false,
+        payload: "qos 2".into(),
+    });
+
+    matches::assert_matches!(
+        client.next().await,
+        Some(Ok(Event::Publication(ReceivedPublication{payload, ..}))) if payload == bytes::Bytes::from("qos 2")
+    );
+
+    broker_shutdown.send(()).expect("couldn't shutdown broker");
+    broker_task
+        .await
+        .unwrap()
+        .expect("can't wait for the broker");
 }
