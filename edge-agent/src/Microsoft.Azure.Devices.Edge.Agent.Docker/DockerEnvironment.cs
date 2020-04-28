acbe56f447a6573ce51c20edb41e886450b54baf
Damon Barry <damonb@microsoft.com>
2020-04-27 17:50:22 -0700

    Update test

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Docker/DockerEnvironment.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Docker/DockerEnvironment.cs
@@ -98,7 +98,8 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Docker
                         break;
 
                     case Core.Constants.EdgeAgentModuleName:
-                        var env = dockerRuntimeInfo.Config.CreateOptions.Labels.TryGetValue(Core.Constants.Labels.Env, out string envStr)
+                        var labels = dockerRuntimeInfo.Config.CreateOptions?.Labels ?? new Dictionary<string, string>();
+                        var env = labels.TryGetValue(Core.Constants.Labels.Env, out string envStr)
                             ? JsonConvert.DeserializeObject<Dictionary<string, EnvVal>>(envStr)
                             : new Dictionary<string, EnvVal>();
 
