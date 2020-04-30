99aef13b66b1b02a7279739be271994cd3f528bc
Vadim Kovalyov <vakavali@microsoft.com>
2020-04-30 12:32:53 -0700

    Fix broken arm32 build. (#2905)
    Our ARM32 mqtt image build fails b/c of the outdated base image toolchain, with error `use of unstable library feature 'result_map_or_else'`.
    
    This is not a proper fix, but rather a temp workaround to make our mqtt images build green. We need to update our build image ASAP.

--- mqtt/mqtt-broker/src/snapshot.rs
+++ mqtt/mqtt-broker/src/snapshot.rs
@@ -88,7 +88,7 @@ where
         // We don't really care about the error here ---
         //   we just want to join the thread handle to get the result
         //   or deal with the panic
-        rx.await.map_or_else(drop, drop);
+        let _ = rx.await;
 
         // propagate any panics onto the event loop thread
         match handle.join() {
