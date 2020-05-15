8dd6f618794a5016d444e33dc8738349fa50f0ac
Andrew Smith <als5ev@virginia.edu>
2020-05-15 16:21:25 -0700

    merge upstream master

--- scripts/linux/runE2ETest.sh
+++ scripts/linux/runE2ETest.sh
@@ -27,11 +27,7 @@ function clean_up() {
         docker system prune -af --volumes || true
 
         echo 'Restart docker'
-<<<<<<< HEAD
-        systemctl restart docker
-=======
         systemctl restart docker # needed due to https://github.com/moby/moby/issues/23302
->>>>>>> 80dfe7097cb4e63a4fc70514d3a125a2b6126ec5
     fi
 }
 
