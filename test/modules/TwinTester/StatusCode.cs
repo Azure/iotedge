c7c5d5a3cc93ec4b6246ed7c8c74ad508f08fe68
Philip Lin <philipktlin@live.com>
2020-05-20 15:56:52 -0700

    address comment

--- test/modules/TwinTester/StatusCode.cs
+++ test/modules/TwinTester/StatusCode.cs
@@ -8,12 +8,12 @@ namespace TwinTester
         DesiredPropertyReceived = 202,
         ReportedPropertyReceived = 203,
         ReportedPropertyUpdated = 204,
-        ReportedPropertyReceivedAfterThreshold = 205,
         DesiredPropertyUpdateNoCallbackReceived = 501,
         DesiredPropertyUpdateNotInEdgeTwin = 502,
         DesiredPropertyUpdateTotalFailure = 503,
         ReportedPropertyUpdateCallFailure = 504,
         ReportedPropertyUpdateNotInCloudTwin = 505,
         ReportedPropertyUpdateNotInCloudTwinAfterCleanUpThreshold = 506,
+        ReportedPropertyReceivedAfterThreshold = 507,
     }
 }
