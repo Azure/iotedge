a6e23f963f61a83acc6c3ec744102a7a9599029f
Philip Lin <philipktlin@live.com>
2020-05-20 11:03:32 -0700

    Add cleanup threshold for reported properties validation

--- test/modules/TwinTester/StatusCode.cs
+++ test/modules/TwinTester/StatusCode.cs
@@ -8,10 +8,12 @@ namespace TwinTester
         DesiredPropertyReceived = 202,
         ReportedPropertyReceived = 203,
         ReportedPropertyUpdated = 204,
+        ReportedPropertyReceivedAfterThreshold = 205,
         DesiredPropertyUpdateNoCallbackReceived = 501,
         DesiredPropertyUpdateNotInEdgeTwin = 502,
         DesiredPropertyUpdateTotalFailure = 503,
         ReportedPropertyUpdateCallFailure = 504,
-        ReportedPropertyUpdateNotInCloudTwin = 505
+        ReportedPropertyUpdateNotInCloudTwin = 505,
+        ReportedPropertyUpdateNotInCloudTwinAfterCleanUpThreshold = 506,
     }
 }
