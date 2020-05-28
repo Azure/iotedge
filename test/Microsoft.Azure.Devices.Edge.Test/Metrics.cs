af9324c423def63af4a5ad5d5febeb1609fa8608
Lee Fitchett <lefitche@microsoft.com>
2020-05-28 12:51:44 -0700

    try single deploy

--- test/Microsoft.Azure.Devices.Edge.Test/Metrics.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/Metrics.cs
@@ -45,14 +45,14 @@ namespace Microsoft.Azure.Devices.Edge.Test
         {
             // First deploy everything needed for this test, including a temporary image that will be removed later to bump the "stopped" metric
             string metricsValidatorImage = Context.Current.MetricsValidatorImage.Expect(() => new InvalidOperationException("Missing Metrics Validator image"));
-            await this.runtime.DeployConfigurationAsync(
-                builder =>
-                    {
-                        builder.AddTemporaryModule();
-                        builder.AddMetricsValidatorConfig(metricsValidatorImage);
-                    }, token);
+            // await this.runtime.DeployConfigurationAsync(
+            //    builder =>
+            //        {
+            //            builder.AddTemporaryModule();
+            //            builder.AddMetricsValidatorConfig(metricsValidatorImage);
+            //        }, token);
 
-            // Next remove the temporary image from the deployment
+            //// Next remove the temporary image from the deployment
             await this.runtime.DeployConfigurationAsync(
                 builder => { builder.AddMetricsValidatorConfig(metricsValidatorImage); },
                 token,
