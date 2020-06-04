c622afb4104af74581378a3e72f718fee47c0296
Philip Lin <philipktlin@live.com>
2020-06-04 09:01:50 -0700

    address comment

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/IModuleIdentity.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/IModuleIdentity.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 namespace Microsoft.Azure.Devices.Edge.Agent.Core
 {
-    using Microsoft.Azure.Devices.Edge.Util;
-
     public interface IModuleIdentity
     {
         string IotHubHostname { get; }
