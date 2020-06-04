61330ff8eed83cffc9eefbe291aa91afc3dcff0d
Bear <yophilav@microsoft.com>
2020-06-03 17:36:07 -0700

    Update the DirectMethodSender to wait for IoTHub

--- test/modules/DirectMethodSender/DirectMethodCloudSender.cs
+++ test/modules/DirectMethodSender/DirectMethodCloudSender.cs
@@ -45,7 +45,12 @@ namespace DirectMethodSender
             ulong directMethodCount,
             CancellationToken none)
         {
+            // serviceClientTimeouts needs to be explicitly set to let IoTHub figure out where does the device is connected to the IoTHub server.
+            // Otherwise, the IoTHub would immediately throw the DeviceNotFound exception if the target device re-connect to a different server than its original connection.
+            TimeSpan serviceClientTimeouts = TimeSpan.FromSeconds(5);
             CloudToDeviceMethod cloudToDeviceMethod = new CloudToDeviceMethod(methodName).SetPayloadJson($"{{ \"Message\": \"Hello\", \"DirectMethodCount\": \"{directMethodCount.ToString()}\" }}");
+            cloudToDeviceMethod.ConnectionTimeout = serviceClientTimeouts;
+            cloudToDeviceMethod.ResponseTimeout = serviceClientTimeouts;
             CloudToDeviceMethodResult result = await this.serviceClient.InvokeDeviceMethodAsync(deviceId, targetModuleId, cloudToDeviceMethod, CancellationToken.None);
             return result.Status;
         }
