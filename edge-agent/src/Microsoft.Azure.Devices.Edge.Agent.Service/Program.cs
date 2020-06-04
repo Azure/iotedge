a9a38703ed1838eef37b59f3f56727e70166ab98
Philip Lin <philipktlin@live.com>
2020-06-04 09:37:21 -0700

    revert mode

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Service/Program.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Service/Program.cs
@@ -97,7 +97,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Service
 
             try
             {
-                mode = configuration.GetValue(Constants.ModeKey, "iotedged");
+                mode = configuration.GetValue(Constants.ModeKey, "docker");
                 configSourceConfig = configuration.GetValue<string>("ConfigSource");
                 backupConfigFilePath = configuration.GetValue<string>("BackupConfigFilePath");
                 maxRestartCount = configuration.GetValue<int>("MaxRestartCount");
