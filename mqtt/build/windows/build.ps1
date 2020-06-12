afa8b31d01386f7846d7234ee0d994bd3b4040b3
Andrew Smith <als5ev@virginia.edu>
2020-06-12 13:40:18 -0700

    use release build for broker build images pipeline

--- mqtt/build/windows/build.ps1
+++ mqtt/build/windows/build.ps1
@@ -26,7 +26,7 @@ if ($Arm) {
 cd (Get-MqttFolder)
 
 Write-Host "$cargo build --all $(if ($Arm) { '--target thumbv7a-pc-windows-msvc' }) $(if ($Release) { '--release' })"
-Invoke-Expression "$cargo build --all $(if ($Arm) { '--target thumbv7a-pc-windows-msvc' }) $(if ($Release) { '--release' })"
+Invoke-Expression "$cargo build --all $(if ($Arm) { '--target thumbv7a-pc-windows-msvc' }) $(if ($Release) { '--release --no-default-features' })"
 
 if ($LastExitCode -ne 0) {
     Throw "cargo build failed with exit code $LastExitCode"
