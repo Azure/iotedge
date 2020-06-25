611816ce024aa910bfeb362d2d83f4d71609d4d9
Andrew Smith <andsmi@microsoft.com>
2020-06-25 00:49:27 -0700

    cargo fmt

--- edge-hub/watchdog/linux/src/main.rs
+++ edge-hub/watchdog/linux/src/main.rs
@@ -1,15 +1,15 @@
 extern crate nix;
 
-use std::{thread};
-use std::io::Error;
-use std::process::{Command, Stdio, Child};
-use std::sync::{Arc};
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::time::Duration;
-use nix::unistd::Pid;
 use nix::sys::signal::{self, Signal};
+use nix::unistd::Pid;
 use signal_hook::{iterator::Signals, SIGTERM};
-use tracing::{info, error};
+use std::io::Error;
+use std::process::{Child, Command, Stdio};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+use tracing::{error, info};
 use tracing_subscriber;
 
 fn main() -> Result<(), Error> {
@@ -19,22 +19,21 @@ fn main() -> Result<(), Error> {
         Ok(has_received_sigterm) => has_received_sigterm,
         Err(e) => {
             error!("Failed to register sigterm listener. Shutting down.");
-            return Err(e)
+            return Err(e);
         }
     };
 
     let mut edgehub = Command::new("dotnet")
-            .arg("/app/Microsoft.Azure.Devices.Edge.Hub.Service.dll")
-            .stdout(Stdio::inherit())
-            .spawn()
-            .expect("Failed to execute Edge Hub process");
-    
-    let mut broker = Command::new("/usr/local/bin/mqttd")
-            .stdout(Stdio::inherit())
-            .spawn()
-            .expect("failed to execute MQTT broker process");
-
+        .arg("/app/Microsoft.Azure.Devices.Edge.Hub.Service.dll")
+        .stdout(Stdio::inherit())
+        .spawn()
+        .expect("Failed to execute Edge Hub process");
     info!("Launched Edge Hub process with pid {:?}", edgehub.id());
+
+    let mut broker = Command::new("/usr/local/bin/mqttd")
+        .stdout(Stdio::inherit())
+        .spawn()
+        .expect("failed to execute MQTT broker process");
     info!("Launched MQTT Broker process with pid {:?}", broker.id());
 
     wait_for_sigterm_or_processes_to_shutdown(&mut edgehub, &mut broker, has_received_sigterm);
@@ -68,19 +67,22 @@ fn register_sigterm_listener() -> Result<Arc<std::sync::atomic::AtomicBool>, Err
     Ok(has_received_sigterm)
 }
 
-fn wait_for_sigterm_or_processes_to_shutdown(mut edgehub: &mut Child, mut broker: &mut Child, has_received_sigterm: Arc<std::sync::atomic::AtomicBool>) {
+fn wait_for_sigterm_or_processes_to_shutdown(
+    mut edgehub: &mut Child,
+    mut broker: &mut Child,
+    has_received_sigterm: Arc<std::sync::atomic::AtomicBool>,
+) {
     let mut is_edgehub_running = is_child_process_running(&mut edgehub);
     let mut is_broker_running = is_child_process_running(&mut broker);
     while has_received_sigterm.load(Ordering::Relaxed) && is_edgehub_running && is_broker_running {
         is_edgehub_running = is_child_process_running(&mut edgehub);
         is_broker_running = is_child_process_running(&mut broker);
 
-        thread::sleep(Duration::from_secs(4));
+        thread::sleep(Duration::from_secs(1));
     }
 }
 
-fn is_child_process_running(child_process: &mut Child) -> bool
-{
+fn is_child_process_running(child_process: &mut Child) -> bool {
     return !child_process.try_wait().unwrap().is_some();
 }
 
@@ -98,7 +100,7 @@ fn shutdown(mut edgehub: &mut Child, mut broker: &mut Child) {
         sigterm_child_process(&mut broker);
     }
 
-    thread::sleep(Duration::from_secs(10));
+    thread::sleep(Duration::from_secs(60));
 
     is_edgehub_running = is_child_process_running(&mut edgehub);
     is_broker_running = is_child_process_running(&mut broker);
@@ -112,23 +114,20 @@ fn shutdown(mut edgehub: &mut Child, mut broker: &mut Child) {
     }
 }
 
-fn sigterm_child_process(child: &mut Child)
-{
+fn sigterm_child_process(child: &mut Child) {
     match signal::kill(Pid::from_raw(child.id() as i32), Signal::SIGTERM) {
-        Ok(_) => {},
+        Ok(_) => {}
         Err(e) => {
             error!("Failed to send SIGTERM signal to child process. {:?}", e);
         }
     };
 }
 
-fn kill_child_process(child: &mut Child)
-{
+fn kill_child_process(child: &mut Child) {
     match child.kill() {
-        Ok(_) => {},
+        Ok(_) => {}
         Err(e) => {
             error!("Failed to kill child process. {:?}", e);
         }
     };
 }
-
