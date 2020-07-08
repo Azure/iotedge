4aa526e61581faa2d6fe254beeb9654a0f4f4ecd
Andrew Smith <als5ev@virginia.edu>
2020-07-08 00:28:41 -0700

    remove unneeded cmake args

--- scripts/linux/cross-platform-rust-build.sh
+++ scripts/linux/cross-platform-rust-build.sh
@@ -60,7 +60,7 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 BUILD_REPOSITORY_LOCALPATH="$(realpath "${BUILD_REPOSITORY_LOCALPATH:-$DIR/../..}")"
 
 CMAKE_ARGS='-DCMAKE_BUILD_TYPE=Release'
-CMAKE_ARGS="$CMAKE_ARGS -DBUILD_SHARED=On -Drun_unittests=Off -Duse_default_uuid=On -Duse_emulator=Off -Duse_http=Off"
+CMAKE_ARGS="$CMAKE_ARGS -Drun_unittests=Off -Duse_http=Off"
 
 DOCKER_VOLUME_MOUNTS=''
 
