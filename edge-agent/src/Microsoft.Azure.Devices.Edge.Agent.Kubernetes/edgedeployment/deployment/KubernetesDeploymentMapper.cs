4dacbb89b9d9484de1321427e6621c43c42240eb
David Robson <darobs@microsoft.com>
2020-07-13 18:11:39 -0700

    NetworkMode fix

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Kubernetes/edgedeployment/deployment/KubernetesDeploymentMapper.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Kubernetes/edgedeployment/deployment/KubernetesDeploymentMapper.cs
@@ -132,7 +132,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Kubernetes.EdgeDeployment.Deploymen
 
         static bool IsHostNetwork(CreatePodParameters config)
         {
-            string hostNet = config.HostConfig.Map(h => h.NetworkMode).OrDefault();
+            string hostNet = config.HostConfig.FlatMap(h => Option.Maybe(h.NetworkMode)).OrDefault();
             return string.Compare(hostNet, "host", true) == 0;
         }
 
