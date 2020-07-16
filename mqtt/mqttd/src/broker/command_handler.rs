755a6bcfe30598472c9b43d7731946f8014a80fb
Andrew Smith <andsmi@microsoft.com>
2020-07-15 21:25:19 -0700

    tmp commit

--- mqtt/mqttd/src/broker/command_handler.rs
+++ mqtt/mqttd/src/broker/command_handler.rs
@@ -1,13 +1,16 @@
-use tracing::error;
-use tracing::info;
-use tracing::warn;
-// use mqtt_broker_core::ClientId;
 use futures_util::StreamExt;
 use mqtt3::proto;
 use mqtt_broker::BrokerHandle;
 use mqtt_broker::Message;
 use mqtt_broker::SystemEvent;
+use regex::Regex;
 use std::time::Duration;
+use tracing::error;
+use tracing::info;
+
+const CLIENT_ID: &str = "deviceid/$edgeHub/$broker/$control";
+const TOPIC_FILTER: &str = "$edgehub/{}/disconnect";
+const CLIENT_EXTRACTION_REGEX: &str = r"(?<=\$edgehub\/)(.*)(?=\/disconnect)";
 
 // TODO: should it be pub
 pub struct CommandHandler {
@@ -21,28 +24,27 @@ impl CommandHandler {
 
     pub async fn run(mut self) {
         // TODO: get device id from env
-        let client_id = "deviceid/$edgeHub/$broker/$control";
-        let username = "";
 
         let mut client = mqtt3::Client::new(
-            Some(client_id.to_string()),
-            Some(username.to_string()),
+            Some(CLIENT_ID.to_string()),
+            None,
             None,
             move || {
-                let password = "";
                 Box::pin(async move {
                     let io = tokio::net::TcpStream::connect("127.0.0.1:1883").await; // TODO: read from config or broker
-                    io.map(|io| (io, Some(password.to_string())))
+                    io.map(|io| (io, None))
                 })
             },
             Duration::from_secs(1),
             Duration::from_secs(60),
         );
 
-        let topic_filter = "$edgehub/{}/disconnect".to_string();
         let qos = proto::QoS::AtLeastOnce;
         // TODO: log error with client and topic
-        if let Err(_e) = client.subscribe(proto::SubscribeTo { topic_filter, qos }) {
+        if let Err(_e) = client.subscribe(proto::SubscribeTo {
+            topic_filter: TOPIC_FILTER.to_string(),
+            qos,
+        }) {
             // TODO: better message
             error!("could not subscribe to command topic")
         } else {
@@ -60,21 +62,53 @@ impl CommandHandler {
             if let mqtt3::Event::Publication(publication) = event {
                 let client_id = Self::parse_client_id(publication.topic_name);
 
-                if let Err(e) = self
-                    .broker_handle
-                    .send(Message::System(SystemEvent::ForceClientDisconnect(
-                        client_id.into(),
-                    )))
-                    .await
-                {
-                    warn!(message = "failed to signal broker to disconnect client", error=%e);
+                match client_id {
+                    Some(client_id) => {
+                        if let Err(e) = self
+                            .broker_handle
+                            .send(Message::System(SystemEvent::ForceClientDisconnect(
+                                client_id.into(),
+                            )))
+                            .await
+                        {
+                            error!(message = "failed to signal broker to disconnect client", error=%e);
+                        }
+                    }
+                    None => {
+                        error!("no client id in disconnect request");
+                    }
                 }
             }
         }
     }
 
-    // TODO: implement parse
-    fn parse_client_id(topic_name: String) -> String {
-        topic_name
+    fn parse_client_id(topic_name: String) -> Option<String> {
+        // TODO: eliminate unwrap
+        let re = Regex::new(CLIENT_EXTRACTION_REGEX);
+
+        // TODO: clean up
+        // TODO: "disconnect client topic"?
+        match re {
+            Ok(re) => match re.captures(topic_name.as_ref()) {
+                Some(capture) => match capture.get(0) {
+                    Some(captured_match) => Some(captured_match.as_str().to_string()),
+                    None => {
+                        error!("no client id found for client disconnect topic");
+                        None
+                    }
+                },
+                None => {
+                    error!("could not parse client id from client disconnect topic");
+                    None
+                }
+            },
+            Err(e) => {
+                error!(
+                    "could not create regex to parse client id from client disconnect topic. {}",
+                    e
+                );
+                None
+            }
+        }
     }
 }
