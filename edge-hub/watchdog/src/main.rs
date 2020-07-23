c0542d062298b59471940bbc2f832b27a5b35c93
Andrew Smith <als5ev@virginia.edu>
2020-07-23 16:31:16 -0700

    EdgeHub Build Fixes (#3235)
    This PR contains the following changes:
    - Since the broker now dynamically retrieves the cert and stores it in the location specified in the config, we need to add this path to the config. We also need to change the config to fit the new format from Denis's recent workload PR.
    - The build pipeline injects a build argument (EXE_DIR=.) when building the edgehub image, which overwrites the intended path and bloats the image size. We should not use this build argument in the Dockerfile.
    - There was a misconfiguration on arm for the path of the executables. I forgot to remove the "./edge-hub" inside the arm dockerfiles while debugging something unrelated

--- edge-hub/watchdog/src/main.rs
+++ edge-hub/watchdog/src/main.rs
@@ -36,14 +36,14 @@ fn main() -> Result<()> {
     let edgehub_handle = run(
         "Edge Hub",
         "dotnet",
-        vec!["/app/Microsoft.Azure.Devices.Edge.Hub.Service/Microsoft.Azure.Devices.Edge.Hub.Service.dll".to_string()],
+        vec!["/app/Microsoft.Azure.Devices.Edge.Hub.Service.dll".to_string()],
         Arc::clone(&should_shutdown),
     )?;
 
     let broker_handle = match run(
         "MQTT Broker",
         "/usr/local/bin/mqttd",
-        vec!["-c".to_string(), "/app/mqttd/production.json".to_string()],
+        vec!["-c".to_string(), "/app/mqttd/broker.json".to_string()],
         Arc::clone(&should_shutdown),
     ) {
         Ok(handle) => Some(handle),
