0d45e518a69280a11ecb51864d016b4de36fef34
R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN <67037013+R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN@users.noreply.github.com>
2020-07-26 17:52:55 -0700

    Add additional settings to configuration

--- src/config.rs
+++ src/config.rs
@@ -5,7 +5,46 @@ use std::path::Path;
 use serde::Deserialize;
 use zeroize::Zeroize;
 
-#[derive(Clone, Deserialize, Zeroize)]
+fn default_storage_location() -> String {
+    "/etc/secretstore".to_string()
+}
+
+fn default_socket_directory() -> String {
+    "/var/tmp".to_string()
+}
+
+fn default_encryption_source() -> EncryptionSource {
+    EncryptionSource::Automatic
+}
+
+fn default_device_cert() -> String {
+    "".to_string()
+}
+
+fn default_device_cert_pk() -> String {
+    "".to_string()
+}
+
+fn default_trusted_certs() -> Vec<String> {
+    Vec::new()
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum EncryptionSource {
+    Automatic,
+    FixedKey(String),
+    RemoteKey(String)
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum StorePermissions {
+    Read,
+    ReadWrite
+}
+
+#[derive(Clone, Debug, Deserialize, Zeroize)]
 #[zeroize(drop)]
 pub struct AADCredentials {
     pub tenant_id: String,
@@ -13,18 +52,39 @@ pub struct AADCredentials {
     pub client_secret: String
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Principal {
     pub name: String,
-    pub authtype: String,
-    pub gid: u32
-    // pub secrets: String
+    pub uid: u32,
+    pub permissions: StorePermissions
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
+pub struct LocalSettings {
+    #[serde(default = "default_storage_location")]
+    pub storage_location: String,
+    #[serde(default = "default_socket_directory")]
+    pub socket_directory: String,
+    #[serde(default = "default_encryption_source")]
+    pub encryption_source: EncryptionSource
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Certificates {
+    #[serde(default = "default_device_cert")]
+    pub device_cert: String,
+    #[serde(default = "default_device_cert_pk")]
+    pub device_cert_pk: String,
+    #[serde(default = "default_trusted_certs")]
+    pub trusted_certs: Vec<String>
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Configuration {
     pub credentials: AADCredentials,
-    // pub principal: Vec<Principal>
+    pub principal: Vec<Principal>,
+    pub local: LocalSettings,
+    pub certificates: Certificates
 }
 
 pub fn load(path: &Path) -> Configuration {
