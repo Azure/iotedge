f8671443f0e6537eab549927482fd997e639b4ce
yophilav <54859653+yophilav@users.noreply.github.com>
2020-07-27 17:24:04 -0700

    Update dockerfiles to only pull image from docker hub (#3279)
    Update dockerfiles to only pull image from docker hub

--- scripts/linux/createArmBase.sh
+++ scripts/linux/createArmBase.sh
@@ -4,8 +4,6 @@
 # This script creates an ARM docker image as a base for the edgeHub module.
 # then pushes it to the appropriate registries.
 # It assumes that the caller is logged into registries:
-# edgebuilds.azurecr.io
-# edgerelease.azurecr.io
 # hub.docker.com
 ###############################################################################
 
@@ -142,7 +140,7 @@ process_args()
         print_help_and_exit
     fi
 
-    registries=("edgebuilds.azurecr.io/" "edgerelease.azurecr.io/" "")
+    registries=("")
     DOCKER_IMAGE_TAGS=("${registries[@]/%/$DOCKER_NAMESPACE/$DOCKER_IMAGENAME:$DOCKER_IMAGEVERSION-linux-$ARCH}")
 }
 
