ac7d58561d64551ce9002ab017f65fc38461b869
Lee Fitchett <lefitche@microsoft.com>
2020-08-12 13:12:08 -0700

    Proxy Support for `iotedge check` (#3396)
    iotedge check will now use a proxy server if specified in config.yaml
    
    This also rewrites the diagnostics module in c# since it would be difficult to get openssl in the container for rust to use.

--- edgelet/iotedge/src/check/checks/container_engine_dns.rs
+++ edgelet/iotedge/src/check/checks/container_engine_dns.rs
@@ -17,7 +17,7 @@ impl Checker for ContainerEngineDns {
     fn description(&self) -> &'static str {
         "DNS server"
     }
-    fn execute(&mut self, check: &mut Check) -> CheckResult {
+    fn execute(&mut self, check: &mut Check, _: &mut tokio::runtime::Runtime) -> CheckResult {
         self.inner_execute(check)
             .unwrap_or_else(CheckResult::Failed)
     }
