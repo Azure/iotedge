9752db1a1efacfb05da0519ac0b7bbb7c570986d
Lee Fitchett <lefitche@microsoft.com>
2020-08-19 14:25:54 -0700

    change default summary config

--- edge-util/src/Microsoft.Azure.Devices.Edge.Util/metrics/prometheus.net/MetricsHistogram.cs
+++ edge-util/src/Microsoft.Azure.Devices.Edge.Util/metrics/prometheus.net/MetricsHistogram.cs
@@ -19,12 +19,11 @@ namespace Microsoft.Azure.Devices.Edge.Util.Metrics.Prometheus.Net
                 {
                     Objectives = new[]
                     {
-                        new QuantileEpsilonPair(0.5, 0.05),
-                        new QuantileEpsilonPair(0.9, 0.05),
-                        new QuantileEpsilonPair(0.95, 0.01),
-                        new QuantileEpsilonPair(0.99, 0.01),
-                        new QuantileEpsilonPair(0.999, 0.01),
-                        new QuantileEpsilonPair(0.9999, 0.01),
+                        new QuantileEpsilonPair(0.1, 0.05),
+                        new QuantileEpsilonPair(0.25, 0.05),
+                        new QuantileEpsilonPair(0.5, 0.01),
+                        new QuantileEpsilonPair(0.75, 0.01),
+                        new QuantileEpsilonPair(0.9, 0.01),
                     },
                     LabelNames = labelNames.ToArray(),
                     MaxAge = maxAge,
