bf5302c0cc1a2f8b561e47f1660fcd0a34d4494b
dylanbronson <dybronso@microsoft.com>
2020-08-20 17:47:25 -0700

    Adding unit tests

--- edge-hub/core/test/Microsoft.Azure.Devices.Edge.Hub.MqttBrokerAdapter.Test/AuthorizedScopesHandlerTest.cs
+++ edge-hub/core/test/Microsoft.Azure.Devices.Edge.Hub.MqttBrokerAdapter.Test/AuthorizedScopesHandlerTest.cs
@@ -4,26 +4,57 @@ namespace Microsoft.Azure.Devices.Edge.Hub.MqttBrokerAdapter.Test
 {
     using Microsoft.Azure.Devices.Edge.Hub.Core;
     using Moq;
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using System.Text;
     using System.Threading.Tasks;
     using Xunit;
 
     public class AuthorizedScopesHandlerTest
     {
         [Fact]
-        public async Task PublishIdentitiesTest()
+        public void PublishIdentitiesTest()
         {
+            // Arrange
             var capture = new SendCapture();
             var connector = GetConnector(capture);
-            IDeviceScopeIdentitiesCache deviceScopeIdentitiesCache = new Mock(IDeviceScopeIdentitiesCache);
+            var deviceScopeIdentitiesCache = new Mock<IDeviceScopeIdentitiesCache>();
+            var sut = new AuthorizedScopesHandler(deviceScopeIdentitiesCache.Object);
+            sut.SetConnector(connector.Object);
+            IList<string> identities = new List<string>() { "d1", "d2", "d3" };
 
-            var sut = new AuthorizedScopesHandler(connectionRegistry, identityProvider);
+            // Act
+            deviceScopeIdentitiesCache.Raise(d => d.ServiceIdentitiesUpdated += null, null, identities);
 
+            // Assert
+            Assert.Equal("$edgehub/authorization", capture.Topic);
+            Assert.Equal(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(identities)), capture.Content);
         }
 
-        protected static IMqttBrokerConnector GetConnector(SendCapture sendCapture = null)
+        [Fact]
+        public void GetAndPublishIdentitiesTest()
         {
-            var connector = Mock.Of<IMqttBrokerConnector>();
-            Mock.Get(connector)
+            // Arrange
+            var capture = new SendCapture();
+            var connector = GetConnector(capture);
+            IList<string> identities = new List<string>() { "d1", "d2", "d3" };
+            var deviceScopeIdentitiesCache = new Mock<IDeviceScopeIdentitiesCache>();
+            deviceScopeIdentitiesCache.Setup(d => d.GetAllIds()).ReturnsAsync(identities);
+            var sut = new AuthorizedScopesHandler(deviceScopeIdentitiesCache.Object);
+            sut.SetConnector(connector.Object);
+
+            // Act
+            connector.Raise(c => c.OnConnected += null, null, null);
+
+            // Assert
+            Assert.Equal("$edgehub/authorization", capture.Topic);
+            Assert.Equal(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(identities)), capture.Content);
+        }
+
+        protected static Mock<IMqttBrokerConnector> GetConnector(SendCapture sendCapture = null)
+        {
+            var connector = new Mock<IMqttBrokerConnector>();
+            connector
                 .Setup(c => c.SendAsync(It.IsAny<string>(), It.IsAny<byte[]>()))
                 .Returns((string topic, byte[] content) =>
                 {
