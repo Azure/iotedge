5f166ccb7ff98868bd08e41bb304a32fbf6f27b3
Anca Antochi <ancan@microsoft.com>
2020-08-21 13:07:23 -0700

    Generic settings

--- mqtt/mqtt-bridge/src/controller.rs
+++ mqtt/mqtt-bridge/src/controller.rs
@@ -1,13 +1,15 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
 use tracing::info;
 
-use crate::bridge::NestedBridge;
+use crate::bridge::Bridge;
 use crate::settings::Settings;
 
 /// Controller that handles the settings and monitors changes, spawns new Bridges and monitors shutdown signal.
 #[derive(Default)]
 pub struct BridgeController {
-    nested_bridge: Option<NestedBridge>,
+    bridges: HashMap<String, Bridge>
 }
 
 impl BridgeController {
@@ -17,11 +19,11 @@ impl BridgeController {
 
     pub async fn start(&mut self) -> Result<()> {
         let settings = Settings::new()?;
-        if let Some(_hostname) = settings.nested_bridge().gateway_hostname() {
-            let nested_bridge = NestedBridge::new(settings.clone());
-            nested_bridge.start().await;
+        if let Some(upstream) = settings.upstream() {
+            let nested_bridge = Bridge::new(upstream.clone());
+                nested_bridge.start().await;
 
-            self.nested_bridge = Some(nested_bridge);
+                self.bridges.insert(upstream.name().to_string(), nested_bridge);
         } else {
             info!("No nested bridge found.")
         };
