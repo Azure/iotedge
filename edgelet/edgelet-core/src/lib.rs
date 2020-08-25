4740fe4ca96cd7590d23485f4ea3a10f37823ab2
R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN <67037013+R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN@users.noreply.github.com>
2020-08-25 09:28:05 -0700

    Build hyper URI from Unix socket

--- edgelet/edgelet-core/src/lib.rs
+++ edgelet/edgelet-core/src/lib.rs
@@ -123,5 +123,3 @@ pub const UNIX_SCHEME: &str = "unix";
 
 /// This is the name of the network created by the iotedged
 pub const DEFAULT_NETWORKID: &str = "azure-iot-edge";
-
-pub const DEFAULT_SECRET_STORE_SOCKET: &str = "unix:///var/secretstore/cmd.sock";
