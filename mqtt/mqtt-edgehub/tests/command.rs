a30902fa4e9c088df33f0f0328566bcb35975c5e
Andrew Smith <andsmi@microsoft.com>
2020-08-26 14:22:48 +0000

    fix tests

--- mqtt/mqtt-edgehub/tests/command.rs
+++ mqtt/mqtt-edgehub/tests/command.rs
@@ -53,7 +53,7 @@ async fn disconnect_client() {
 
     let topic = "$edgehub/disconnect";
     edgehub_client
-        .publish_qos1(topic, r#"test-client"#, false)
+        .publish_qos1(topic, r#""test-client""#, false)
         .await;
 
     assert_eq!(test_client.next().await, None);
