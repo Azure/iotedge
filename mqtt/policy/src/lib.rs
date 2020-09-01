7b22843f643bce377954c86a7cb8f5f1cedf6276
Vadim Kovalyov <vadim.kovalyov@gmail.com>
2020-09-01 16:17:53 -0700

    cargo fmt

--- mqtt/policy/src/lib.rs
+++ mqtt/policy/src/lib.rs
@@ -17,9 +17,9 @@ mod matcher;
 mod substituter;
 mod validator;
 
-pub use crate::errors::{Error, Result};
-pub use crate::matcher::{DefaultResourceMatcher, ResourceMatcher};
 pub use crate::core::PolicyBuilder;
 pub use crate::core::{Decision, Policy, Request};
+pub use crate::errors::{Error, Result};
+pub use crate::matcher::{DefaultResourceMatcher, ResourceMatcher};
 pub use crate::substituter::{DefaultSubstituter, Substituter};
 pub use crate::validator::{DefaultValidator, PolicyValidator};
