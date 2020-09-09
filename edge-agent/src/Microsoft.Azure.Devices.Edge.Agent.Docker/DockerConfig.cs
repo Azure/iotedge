afe90f3461e2a66f692ef6e2b8a116ed7d90a24b
Hugues Bouvier <33438817+huguesBouvier@users.noreply.github.com>
2020-09-09 15:07:06 -0700

    Environment variable in image name

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Docker/DockerConfig.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Docker/DockerConfig.cs
@@ -15,8 +15,10 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Docker
     {
         // This is not the actual docker image regex, but a less strict version.
         const string ImageRegexPattern = @"^(?<repo>([^/]*/)*)(?<image>[^/:]+)(?<tag>:[^/:]+)?$";
-
+        // Check if it's prefix is "$upstream" and replace with environment variable.
+        const string ImageUpstreamRegexPattern = @"^\$upstream(?<path>:[1-9].*)";
         static readonly Regex ImageRegex = new Regex(ImageRegexPattern);
+        static readonly Regex ImageUpstreamRegex = new Regex(ImageUpstreamRegexPattern);
         readonly CreateContainerParameters createOptions;
 
         public DockerConfig(string image)
@@ -87,8 +89,39 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Docker
 
         internal static string ValidateAndGetImage(string image)
         {
+            return ValidateAndGetImage(image, new EnvironmentWrapper());
+        }
+
+        internal static string ValidateAndGetImage(string image, IEnvironmentWrapper env)
+        {
             image = Preconditions.CheckNonWhiteSpace(image, nameof(image)).Trim();
+
+            if (image[0] == '$')
+            {
+                Match matchHost = ImageUpstreamRegex.Match(image);
+                if (matchHost.Success
+                    && (matchHost.Groups["path"]?.Length > 0)
+                    && (env != null))
+                {
+                    string hostAddress = env.GetVariable(Core.Constants.GatewayHostnameVariableName);
+
+                    if (hostAddress != null)
+                    {
+                        image = hostAddress + matchHost.Groups["path"].Value;
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException($"Could not find environment variable: {Core.Constants.GatewayHostnameVariableName}");
+                    }
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Image {image} is not in the right format. If your intention is to use an environment variable, check the port is specified.");
+                }
+            }
+
             Match match = ImageRegex.Match(image);
+
             if (match.Success)
             {
                 if (match.Groups["tag"]?.Length > 0)
@@ -202,6 +235,19 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Docker
             }
 
             public override bool CanConvert(Type objectType) => objectType == typeof(DockerConfig);
+        }
+    }
+
+    internal interface IEnvironmentWrapper
+    {
+        string GetVariable(string variableName);
+    }
+
+    internal class EnvironmentWrapper : IEnvironmentWrapper
+    {
+        public string GetVariable(string variableName)
+        {
+            return Environment.GetEnvironmentVariable(variableName);
         }
     }
 }
