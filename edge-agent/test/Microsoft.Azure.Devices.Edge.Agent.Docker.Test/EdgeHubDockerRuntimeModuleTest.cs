6836fb4f1c0389a832afe2da92e573330c49e4fa
yophilav <54859653+yophilav@users.noreply.github.com>
2020-09-14 10:45:37 -0700

    Fix edgeHub restart by supporting version (#3405)
    Fix edgeHub restart by supporting version

--- edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Docker.Test/EdgeHubDockerRuntimeModuleTest.cs
+++ edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Docker.Test/EdgeHubDockerRuntimeModuleTest.cs
@@ -50,6 +50,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Docker.Test
   ""lastRestartTimeUtc"": ""0001-01-01T00:00:00"",
   ""runtimeStatus"": ""running"",
   ""type"": ""docker"",
+  ""version"": """",
   ""settings"": {
     ""image"": ""edg0eHubImage:latest"",
     ""createOptions"": ""{}""
@@ -214,6 +215,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Docker.Test
   ""lastRestartTimeUtc"": ""0001-01-01T00:00:00"",
   ""runtimeStatus"": ""running"",
   ""type"": ""docker"",
+  ""version"": """",
   ""settings"": {
     ""image"": ""edg0eHubImage:latest"",
     ""createOptions"": ""{}""
