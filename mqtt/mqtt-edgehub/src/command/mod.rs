b9852bad08a5bbdba869b630f5b6579af02e7cbc
Denis Molokanov <dmolokanov@users.noreply.github.com>
2020-09-14 10:05:34 -0700

    Added some comments

--- mqtt/mqtt-edgehub/src/command/mod.rs
+++ mqtt/mqtt-edgehub/src/command/mod.rs
@@ -10,14 +10,23 @@ use std::error::Error as StdError;
 
 use mqtt3::ReceivedPublication;
 
+/// A command trait to be implemented by any MQTT command handler.
 pub trait Command {
+    /// An error type that occurs when handling command.
     type Error;
 
+    /// A topic to receive command.
     fn topic(&self) -> &str;
 
+    /// Unwraps command from MQTT publication and process it.
     fn handle(&mut self, publication: &ReceivedPublication) -> Result<(), Self::Error>;
 }
 
+/// Wrapper to take any command and wrap error type as a trait object.
+///
+/// We need this to collect all commands in a `HashMap<dyn Command<Error = Box<dyn StdError + Send>>`.
+/// Otherwise there is no good way to bring different commands to the same
+/// trait object type with associated trait object error type.
 pub struct DynCommand<C> {
     inner: C,
 }
