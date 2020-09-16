8b0ac883ceb9f62f49081c762c751a9ecb23640b
Andrew.Smith <andsmi@microsoft.com>
2020-09-16 14:53:24 +0000

    address comments

--- mqtt/mqtt-bridge/src/persist/waking_state/waking_store.rs
+++ mqtt/mqtt-bridge/src/persist/waking_state/waking_store.rs
@@ -1,7 +1,7 @@
 use std::collections::hash_map::Entry;
 use std::{boxed::Box, collections::HashMap, task::Waker};
 
-use bincode::{deserialize, serialize, ErrorKind};
+use bincode::{self, ErrorKind};
 use mqtt3::proto::Publication;
 use rocksdb::{IteratorMode, DB};
 use tracing::error;
@@ -21,21 +21,18 @@ pub struct WakingStore {
 
 impl WakingStore {
     pub fn new(db: DB) -> Self {
-        let in_flight = HashMap::new();
-        let waker = None;
-
         Self {
             db,
-            in_flight,
-            waker,
+            in_flight: HashMap::new(),
+            waker: None,
         }
     }
 }
 
 impl StreamWakeableState for WakingStore {
     fn insert(&mut self, key: Key, value: Publication) -> Result<(), PersistError> {
-        let key_bytes = serialize(&key).map_err(PersistError::Serialization)?;
-        let publication_bytes = serialize(&value).map_err(PersistError::Serialization)?;
+        let key_bytes = bincode::serialize(&key).map_err(PersistError::Serialization)?;
+        let publication_bytes = bincode::serialize(&value).map_err(PersistError::Serialization)?;
         self.db
             .put(key_bytes, publication_bytes)
             .map_err(PersistError::Insertion)?;
@@ -53,7 +50,7 @@ impl StreamWakeableState for WakingStore {
         let mut output = vec![];
 
         for (iterations, extracted) in iter.enumerate() {
-            let key: Result<Key, Box<ErrorKind>> = deserialize(&*extracted.0);
+            let key: Result<Key, Box<ErrorKind>> = bincode::deserialize(&*extracted.0);
             let key = match key {
                 Ok(key) => key,
                 Err(e) => {
@@ -62,7 +59,8 @@ impl StreamWakeableState for WakingStore {
                 }
             };
 
-            let publication: Result<Publication, Box<ErrorKind>> = deserialize(&*extracted.1);
+            let publication: Result<Publication, Box<ErrorKind>> =
+                bincode::deserialize(&*extracted.1);
             let publication = match publication {
                 Ok(publication) => publication,
                 Err(e) => {
@@ -86,7 +84,7 @@ impl StreamWakeableState for WakingStore {
     }
 
     fn remove_in_flight(&mut self, key: &Key) -> Option<Publication> {
-        let key_bytes = serialize(&key).unwrap();
+        let key_bytes = bincode::serialize(&key).unwrap();
         self.db.delete(key_bytes).unwrap();
         self.in_flight.remove(key)
     }
