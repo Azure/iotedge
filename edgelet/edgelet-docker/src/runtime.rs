e2ed141b569be5e4bd42c339b98ca7109ae4b940
Damon Barry <damonbarry@users.noreply.github.com>
2020-09-18 08:37:05 -0700

    Report provisioning type in metrics (#3563)
    Cherry-picked from master 76ca4cb16fe019f58fb5c817f84d2b056ae3d042.

--- edgelet/edgelet-docker/src/runtime.rs
+++ edgelet/edgelet-docker/src/runtime.rs
@@ -20,7 +20,8 @@ use docker::models::{ContainerCreateBody, InlineResponse200, Ipam, NetworkConfig
 use edgelet_core::{
     AuthId, Authenticator, GetTrustBundle, Ipam as CoreIpam, LogOptions, MakeModuleRuntime,
     MobyNetwork, Module, ModuleId, ModuleRegistry, ModuleRuntime, ModuleRuntimeState, ModuleSpec,
-    RegistryOperation, RuntimeOperation, SystemInfo as CoreSystemInfo, SystemResources, UrlExt,
+    ProvisioningInfo, RegistryOperation, RuntimeOperation, RuntimeSettings,
+    SystemInfo as CoreSystemInfo, SystemResources, UrlExt,
 };
 use edgelet_http::{Pid, UrlConnector};
 use edgelet_utils::{ensure_not_empty_with_context, log_failure};
@@ -59,6 +60,7 @@ lazy_static! {
 pub struct DockerModuleRuntime {
     client: DockerClient<UrlConnector>,
     system_resources: Arc<Mutex<System>>,
+    provisioning_info: ProvisioningInfo,
 }
 
 impl DockerModuleRuntime {
@@ -206,7 +208,7 @@ impl MakeModuleRuntime for DockerModuleRuntime {
         //      https://github.com/rust-lang/rust-clippy/issues/3730
         #[allow(clippy::result_map_unwrap_or_else)]
         let created = init_client(settings.moby_runtime().uri())
-            .map(|client| {
+            .map(move |client| {
                 let network_id = settings.moby_runtime().network().name().to_string();
                 let (enable_i_pv6, ipam) = get_ipv6_settings(settings.moby_runtime().network());
                 info!("Using runtime network id {}", network_id);
@@ -242,13 +244,14 @@ impl MakeModuleRuntime for DockerModuleRuntime {
                         log_failure(Level::Warn, &e);
                         e
                     })
-                    .map(|client| {
+                    .map(move |client| {
                         let mut system_resources = System::new_all();
                         system_resources.refresh_all();
                         info!("Successfully initialized module runtime");
                         DockerModuleRuntime {
                             client,
                             system_resources: Arc::new(Mutex::new(system_resources)),
+                            provisioning_info: ProvisioningInfo::new(settings.provisioning()),
                         }
                     });
 
@@ -574,11 +577,13 @@ impl ModuleRuntime for DockerModuleRuntime {
     fn system_info(&self) -> Self::SystemInfoFuture {
         info!("Querying system info...");
 
+        let provisioning = self.provisioning_info.clone();
+
         Box::new(
             self.client
                 .system_api()
                 .system_info()
-                .then(|result| match result {
+                .then(move |result| match result {
                     Ok(system_info) => {
                         let system_info = CoreSystemInfo {
                             os_type: system_info
@@ -590,6 +595,7 @@ impl ModuleRuntime for DockerModuleRuntime {
                                 .unwrap_or(&String::from("Unknown"))
                                 .to_string(),
                             version: edgelet_core::version_with_source_version(),
+                            provisioning,
                             cpus: system_info.NCPU().unwrap_or_default(),
                             kernel_version: system_info
                                 .kernel_version()
