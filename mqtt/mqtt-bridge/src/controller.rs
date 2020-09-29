386283c6c635e3a8051779fb26efeb83707c7cbe
Andrew Smith <als5ev@virginia.edu>
2020-09-29 19:59:54 +0000

    fix bridge controller

--- mqtt/mqtt-bridge/src/controller.rs
+++ mqtt/mqtt-bridge/src/controller.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
-use tracing::info;
+use futures_util::future::join_all;
+use tracing::{error, info};
 
 use crate::bridge::{Bridge, BridgeError};
 use crate::settings::Settings;
@@ -25,15 +26,25 @@ impl BridgeController {
         let settings = Settings::new().map_err(BridgeError::LoadingSettings)?;
 
         if let Some(upstream) = settings.upstream() {
-            let mut bridge =
-                Bridge::new(system_address, device_id.into(), upstream.clone()).await?;
-
-            bridge.start().await?;
-
+            let bridge = Bridge::new(system_address, device_id.into(), upstream.clone()).await?;
             self.bridges.insert(upstream.name().to_string(), bridge);
         } else {
             info!("No upstream settings detected. Not starting bridge.")
         };
+
+        let mut bridge_futs = vec![];
+        for (_, bridge) in self.bridges.iter_mut() {
+            bridge_futs.push(bridge.start())
+        }
+
+        let bridges_status = join_all(bridge_futs).await;
+        for status in bridges_status {
+            if let Err(e) = status {
+                // TODO PRE: give context to error
+                error!(message = "error while running bridge", err = %e);
+            }
+        }
+
         Ok(())
     }
 }
