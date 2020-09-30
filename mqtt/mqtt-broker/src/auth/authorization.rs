8518abe7df4b7c2c5be67446a902a90a612de7ac
dylanbronson <55515325+dylanbronson@users.noreply.github.com>
2020-09-30 10:25:43 -0700

    [mqtt] Add ClientInfo to SessionState so that Offline sessions can have a last known ClientInfo (#3606)
    Adding ClientInfo to SessionState, so that Offline sessions will have the "last known" client_info.
    Now broker can reevaluate offline sessions when an auth update comes in.

--- mqtt/mqtt-broker/src/auth/authorization.rs
+++ mqtt/mqtt-broker/src/auth/authorization.rs
@@ -2,7 +2,7 @@ use std::{any::Any, convert::Infallible, error::Error as StdError};
 
 use mqtt3::proto;
 
-use crate::{ClientId, ClientInfo};
+use crate::ClientInfo;
 
 /// A trait to check a MQTT client permissions to perform some actions.
 pub trait Authorizer {
@@ -74,26 +74,16 @@ impl Authorizer for AllowAll {
 /// Describes a client activity to authorized.
 #[derive(Debug)]
 pub struct Activity {
-    client_id: ClientId,
     client_info: ClientInfo,
     operation: Operation,
 }
 
 impl Activity {
-    pub fn new(
-        client_id: impl Into<ClientId>,
-        client_info: ClientInfo,
-        operation: Operation,
-    ) -> Self {
+    pub fn new(client_info: ClientInfo, operation: Operation) -> Self {
         Self {
-            client_id: client_id.into(),
             client_info,
             operation,
         }
-    }
-
-    pub fn client_id(&self) -> &ClientId {
-        &self.client_id
     }
 
     pub fn client_info(&self) -> &ClientInfo {
@@ -104,8 +94,8 @@ impl Activity {
         &self.operation
     }
 
-    pub fn into_parts(self) -> (ClientId, ClientInfo, Operation) {
-        (self.client_id, self.client_info, self.operation)
+    pub fn into_parts(self) -> (ClientInfo, Operation) {
+        (self.client_info, self.operation)
     }
 }
 
@@ -267,8 +257,7 @@ mod tests {
     fn default_auth_always_deny_any_action() {
         let auth = DenyAll;
         let activity = Activity::new(
-            "client-auth-id",
-            ClientInfo::new(peer_addr(), "client-id"),
+            ClientInfo::new("client-auth-id", peer_addr(), "client-id"),
             Operation::new_connect(connect()),
         );
 
@@ -281,8 +270,7 @@ mod tests {
     fn authorizer_wrapper_around_function() {
         let auth = AllowAll;
         let activity = Activity::new(
-            "client-auth-id",
-            ClientInfo::new(peer_addr(), "client-id"),
+            ClientInfo::new("client-auth-id", peer_addr(), "client-id"),
             Operation::new_connect(connect()),
         );
 
