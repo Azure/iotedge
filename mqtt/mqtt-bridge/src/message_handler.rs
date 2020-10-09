c37bbde68beeafca6710c5657ac3b3f67c991d84
Andrew Smith <als5ev@virginia.edu>
2020-10-09 14:46:43 +0000

    fix message handler tests

--- mqtt/mqtt-bridge/src/message_handler.rs
+++ mqtt/mqtt-bridge/src/message_handler.rs
@@ -108,192 +108,184 @@ impl EventHandler for MessageHandler<WakingMemoryStore> {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use bytes::Bytes;
-//     use futures_util::stream::StreamExt;
-//     use futures_util::stream::TryStreamExt;
-//     use std::str::FromStr;
+#[cfg(test)]
+mod tests {
+    use bytes::Bytes;
+    use futures_util::stream::StreamExt;
+    use futures_util::stream::TryStreamExt;
+    use std::str::FromStr;
 
-//     use mqtt3::{
-//         proto::{Publication, QoS},
-//         Event, ReceivedPublication,
-//     };
-//     use mqtt_broker::TopicFilter;
+    use mqtt3::{
+        proto::{Publication, QoS},
+        Event, ReceivedPublication,
+    };
+    use mqtt_broker::TopicFilter;
 
-//     use crate::bridge::Bridge;
-//     use crate::bridge::MessageHandler;
-//     use crate::client::EventHandler;
-//     use crate::persist::PublicationStore;
-//     use crate::settings::Settings;
+    use super::MessageHandler;
+    use super::TopicMapper;
+    use crate::client::EventHandler;
+    use crate::persist::PublicationStore;
+    use crate::settings::Settings;
 
-// // TODO PRE: move below tests to message handler
-// #[tokio::test]
-// async fn message_handler_saves_message_with_local_and_forward_topic() {
-//     let batch_size: usize = 5;
-//     let settings = Settings::from_file("tests/config.json").unwrap();
-//     let connection_settings = settings.upstream().unwrap();
+    // TODO PRE: move below tests to message handler
+    #[tokio::test]
+    async fn message_handler_saves_message_with_local_and_forward_topic() {
+        let batch_size: usize = 5;
+        let settings = Settings::from_file("tests/config.json").unwrap();
+        let connection_settings = settings.upstream().unwrap();
 
-//     let topics: Vec<TopicMapper> = connection_settings
-//         .forwards()
-//         .iter()
-//         .map(move |sub| TopicMapper {
-//             topic_settings: sub.clone(),
-//             topic_filter: TopicFilter::from_str(sub.pattern()).unwrap(),
-//         })
-//         .collect();
+        let topics: Vec<TopicMapper> = connection_settings
+            .forwards()
+            .iter()
+            .map(move |sub| TopicMapper {
+                topic_settings: sub.clone(),
+                topic_filter: TopicFilter::from_str(sub.pattern()).unwrap(),
+            })
+            .collect();
 
-//     let persistor = PublicationStore::new_memory(batch_size);
-//     let mut handler = MessageHandler::new(persistor, topics);
+        let persistor = PublicationStore::new_memory(batch_size);
+        let mut handler = MessageHandler::new(persistor, topics);
 
-//     let pub1 = ReceivedPublication {
-//         topic_name: "local/floor/1".to_string(),
-//         qos: QoS::AtLeastOnce,
-//         retain: true,
-//         payload: Bytes::new(),
-//         dup: false,
-//     };
+        let pub1 = ReceivedPublication {
+            topic_name: "local/floor/1".to_string(),
+            qos: QoS::AtLeastOnce,
+            retain: true,
+            payload: Bytes::new(),
+            dup: false,
+        };
 
-//     let expected = Publication {
-//         topic_name: "remote/floor/1".to_string(),
-//         qos: QoS::AtLeastOnce,
-//         retain: true,
-//         payload: Bytes::new(),
-//     };
+        let expected = Publication {
+            topic_name: "remote/floor/1".to_string(),
+            qos: QoS::AtLeastOnce,
+            retain: true,
+            payload: Bytes::new(),
+        };
 
-//     handler
-//         .handle_event(Event::Publication(pub1))
-//         .await
-//         .unwrap();
+        handler.handle_event(Event::Publication(pub1)).unwrap();
 
-//     let loader = handler.inner.loader();
+        let loader = handler.inner.loader();
+        let mut loader_borrow = loader.borrow_mut();
 
-//     let extracted1 = loader.lock().try_next().await.unwrap().unwrap();
-//     assert_eq!(extracted1.1, expected);
-// }
+        let extracted1 = loader_borrow.try_next().await.unwrap().unwrap();
+        assert_eq!(extracted1.1, expected);
+    }
 
-// #[tokio::test]
-// async fn message_handler_saves_message_with_forward_topic() {
-//     let batch_size: usize = 5;
-//     let settings = Settings::from_file("tests/config.json").unwrap();
-//     let connection_settings = settings.upstream().unwrap();
+    #[tokio::test]
+    async fn message_handler_saves_message_with_forward_topic() {
+        let batch_size: usize = 5;
+        let settings = Settings::from_file("tests/config.json").unwrap();
+        let connection_settings = settings.upstream().unwrap();
 
-//     let topics: Vec<TopicMapper> = connection_settings
-//         .forwards()
-//         .iter()
-//         .map(move |sub| TopicMapper {
-//             topic_settings: sub.clone(),
-//             topic_filter: TopicFilter::from_str(sub.pattern()).unwrap(),
-//         })
-//         .collect();
+        let topics: Vec<TopicMapper> = connection_settings
+            .forwards()
+            .iter()
+            .map(move |sub| TopicMapper {
+                topic_settings: sub.clone(),
+                topic_filter: TopicFilter::from_str(sub.pattern()).unwrap(),
+            })
+            .collect();
 
-//     let persistor = PublicationStore::new_memory(batch_size);
-//     let mut handler = MessageHandler::new(persistor, topics);
+        let persistor = PublicationStore::new_memory(batch_size);
+        let mut handler = MessageHandler::new(persistor, topics);
 
-//     let pub1 = ReceivedPublication {
-//         topic_name: "temp/1".to_string(),
-//         qos: QoS::AtLeastOnce,
-//         retain: true,
-//         payload: Bytes::new(),
-//         dup: false,
-//     };
+        let pub1 = ReceivedPublication {
+            topic_name: "temp/1".to_string(),
+            qos: QoS::AtLeastOnce,
+            retain: true,
+            payload: Bytes::new(),
+            dup: false,
+        };
 
-//     let expected = Publication {
-//         topic_name: "floor/kitchen/temp/1".to_string(),
-//         qos: QoS::AtLeastOnce,
-//         retain: true,
-//         payload: Bytes::new(),
-//     };
+        let expected = Publication {
+            topic_name: "floor/kitchen/temp/1".to_string(),
+            qos: QoS::AtLeastOnce,
+            retain: true,
+            payload: Bytes::new(),
+        };
 
-//     handler
-//         .handle_event(Event::Publication(pub1))
-//         .await
-//         .unwrap();
+        handler.handle_event(Event::Publication(pub1)).unwrap();
 
-//     let loader = handler.inner.loader();
+        let loader = handler.inner.loader();
+        let mut loader_borrow = loader.borrow_mut();
 
-//     let extracted1 = loader.lock().try_next().await.unwrap().unwrap();
-//     assert_eq!(extracted1.1, expected);
-// }
+        let extracted1 = loader_borrow.try_next().await.unwrap().unwrap();
+        assert_eq!(extracted1.1, expected);
+    }
 
-// #[tokio::test]
-// async fn message_handler_saves_message_with_no_forward_mapping() {
-//     let batch_size: usize = 5;
-//     let settings = Settings::from_file("tests/config.json").unwrap();
-//     let connection_settings = settings.upstream().unwrap();
+    #[tokio::test]
+    async fn message_handler_saves_message_with_no_forward_mapping() {
+        let batch_size: usize = 5;
+        let settings = Settings::from_file("tests/config.json").unwrap();
+        let connection_settings = settings.upstream().unwrap();
 
-//     let topics: Vec<TopicMapper> = connection_settings
-//         .forwards()
-//         .iter()
-//         .map(move |sub| TopicMapper {
-//             topic_settings: sub.clone(),
-//             topic_filter: TopicFilter::from_str(sub.pattern()).unwrap(),
-//         })
-//         .collect();
+        let topics: Vec<TopicMapper> = connection_settings
+            .forwards()
+            .iter()
+            .map(move |sub| TopicMapper {
+                topic_settings: sub.clone(),
+                topic_filter: TopicFilter::from_str(sub.pattern()).unwrap(),
+            })
+            .collect();
 
-//     let persistor = PublicationStore::new_memory(batch_size);
-//     let mut handler = MessageHandler::new(persistor, topics);
+        let persistor = PublicationStore::new_memory(batch_size);
+        let mut handler = MessageHandler::new(persistor, topics);
 
-//     let pub1 = ReceivedPublication {
-//         topic_name: "pattern/p1".to_string(),
-//         qos: QoS::AtLeastOnce,
-//         retain: true,
-//         payload: Bytes::new(),
-//         dup: false,
-//     };
+        let pub1 = ReceivedPublication {
+            topic_name: "pattern/p1".to_string(),
+            qos: QoS::AtLeastOnce,
+            retain: true,
+            payload: Bytes::new(),
+            dup: false,
+        };
 
-//     let expected = Publication {
-//         topic_name: "pattern/p1".to_string(),
-//         qos: QoS::AtLeastOnce,
-//         retain: true,
-//         payload: Bytes::new(),
-//     };
+        let expected = Publication {
+            topic_name: "pattern/p1".to_string(),
+            qos: QoS::AtLeastOnce,
+            retain: true,
+            payload: Bytes::new(),
+        };
 
-//     handler
-//         .handle_event(Event::Publication(pub1))
-//         .await
-//         .unwrap();
+        handler.handle_event(Event::Publication(pub1)).unwrap();
 
-//     let loader = handler.inner.loader();
+        let loader = handler.inner.loader();
+        let mut loader_borrow = loader.borrow_mut();
 
-//     let extracted1 = loader.lock().try_next().await.unwrap().unwrap();
-//     assert_eq!(extracted1.1, expected);
-// }
+        let extracted1 = loader_borrow.try_next().await.unwrap().unwrap();
+        assert_eq!(extracted1.1, expected);
+    }
 
-// #[tokio::test]
-// async fn message_handler_no_topic_match() {
-//     let batch_size: usize = 5;
-//     let settings = Settings::from_file("tests/config.json").unwrap();
-//     let connection_settings = settings.upstream().unwrap();
+    #[tokio::test]
+    async fn message_handler_no_topic_match() {
+        let batch_size: usize = 5;
+        let settings = Settings::from_file("tests/config.json").unwrap();
+        let connection_settings = settings.upstream().unwrap();
 
-//     let topics: Vec<TopicMapper> = connection_settings
-//         .forwards()
-//         .iter()
-//         .map(move |sub| TopicMapper {
-//             topic_settings: sub.clone(),
-//             topic_filter: TopicFilter::from_str(sub.pattern()).unwrap(),
-//         })
-//         .collect();
+        let topics: Vec<TopicMapper> = connection_settings
+            .forwards()
+            .iter()
+            .map(move |sub| TopicMapper {
+                topic_settings: sub.clone(),
+                topic_filter: TopicFilter::from_str(sub.pattern()).unwrap(),
+            })
+            .collect();
 
-//     let persistor = PublicationStore::new_memory(batch_size);
-//     let mut handler = MessageHandler::new(persistor, topics);
+        let persistor = PublicationStore::new_memory(batch_size);
+        let mut handler = MessageHandler::new(persistor, topics);
 
-//     let pub1 = ReceivedPublication {
-//         topic_name: "local/temp/1".to_string(),
-//         qos: QoS::AtLeastOnce,
-//         retain: true,
-//         payload: Bytes::new(),
-//         dup: false,
-//     };
+        let pub1 = ReceivedPublication {
+            topic_name: "local/temp/1".to_string(),
+            qos: QoS::AtLeastOnce,
+            retain: true,
+            payload: Bytes::new(),
+            dup: false,
+        };
 
-//     handler
-//         .handle_event(Event::Publication(pub1))
-//         .await
-//         .unwrap();
+        handler.handle_event(Event::Publication(pub1)).unwrap();
 
-//     let loader = handler.inner.loader();
+        let loader = handler.inner.loader();
+        let mut loader_borrow = loader.borrow_mut();
 
-//     let mut interval = tokio::time::interval(std::time::Duration::from_secs(1));
-//     futures_util::future::select(interval.next(), loader.lock().next()).await;
-// }
-// }
+        let mut interval = tokio::time::interval(std::time::Duration::from_secs(1));
+        futures_util::future::select(interval.next(), loader_borrow.next()).await;
+    }
+}
