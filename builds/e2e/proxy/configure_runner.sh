c6b72afa97ea5805d2c27448a4f3119af30c9f24
Damon Barry <damonb@microsoft.com>
2020-10-19 16:58:22 -0700

    Remove unused variables

--- builds/e2e/proxy/configure_runner.sh
+++ builds/e2e/proxy/configure_runner.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-agent_url='https://vstsagentpackage.azureedge.net/agent/2.175.2/vsts-agent-linux-x64-2.175.2.tar.gz'
-agent_file="${agent_url##*/}"
-
 proxy="http://${1}:3128"
 
 export http_proxy=$proxy
