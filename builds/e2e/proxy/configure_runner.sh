a888510c61e14d06e8bc21415c53959cc82231d6
Damon Barry <damonb@microsoft.com>
2020-10-19 11:32:56 -0700

    Make string interpolation more explicit

--- builds/e2e/proxy/configure_runner.sh
+++ builds/e2e/proxy/configure_runner.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 agent_url='https://vstsagentpackage.azureedge.net/agent/2.175.2/vsts-agent-linux-x64-2.175.2.tar.gz'
 agent_file="${agent_url##*/}"
 
-proxy="http://$1:3128"
+proxy="http://${1}:3128"
 
 export http_proxy=$proxy
 export https_proxy=$proxy
