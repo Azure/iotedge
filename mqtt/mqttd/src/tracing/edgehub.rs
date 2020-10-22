0cf81e705289c33be1f6d88600ffee36db9b4457
Anca Antochi <ancan@microsoft.com>
2020-10-22 15:34:54 -0700

    Address review comment

--- mqtt/mqttd/src/tracing/edgehub.rs
+++ mqtt/mqttd/src/tracing/edgehub.rs
@@ -1,4 +1,4 @@
-use std::{io, str::FromStr};
+use std::{env, io};
 
 use tracing::{log, Level};
 use tracing_log::LogTracer;
@@ -9,21 +9,19 @@ const BROKER_LOG_LEVEL_ENV: &str = "BROKER_LOG";
 const EDGE_HUB_LOG_LEVEL_ENV: &str = "RuntimeLogLevel";
 
 pub fn init() {
-    let log_level = EnvFilter::try_from_env(BROKER_LOG_LEVEL_ENV)
-        .or_else(|_| EnvFilter::try_from_env(EDGE_HUB_LOG_LEVEL_ENV))
-        .or_else(|_| EnvFilter::try_from_default_env())
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-
-    let filter = log::LevelFilter::from_str(log_level.to_string().as_str())
-        .unwrap_or_else(|_| log::LevelFilter::Info);
+    let log_level = env::var(BROKER_LOG_LEVEL_ENV)
+        .or_else(|_| env::var(EDGE_HUB_LOG_LEVEL_ENV))
+        .or_else(|_| env::var(EnvFilter::DEFAULT_ENV))
+        .map_or_else(|_| "info".into(), |level| level.to_lowercase());
 
     let subscriber = fmt::Subscriber::builder()
         .with_ansi(atty::is(atty::Stream::Stderr))
         .with_max_level(Level::TRACE)
         .with_writer(io::stderr)
-        .with_env_filter(log_level)
+        .with_env_filter(EnvFilter::new(log_level.clone()))
         .finish();
     let _ = tracing::subscriber::set_global_default(subscriber);
 
+    let filter = log_level.parse().unwrap_or(log::LevelFilter::Info);
     let _ = LogTracer::builder().with_max_level(filter).init();
 }
