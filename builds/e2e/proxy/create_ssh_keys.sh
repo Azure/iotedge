26a441321741f899fa84bcca419f1acc12a8cc27
Damon Barry <damonb@microsoft.com>
2020-10-21 17:39:53 -0700

    Fix another place where cert lines feeds were lost

--- builds/e2e/proxy/create_ssh_keys.sh
+++ builds/e2e/proxy/create_ssh_keys.sh
@@ -10,13 +10,12 @@ do
     ssh-keygen -t rsa -b 4096 -N '' -f "id_rsa$i" || exit 1
 done
 
+comma=','
 echo '{"keyinfo": [' > $AZ_SCRIPTS_OUTPUT_PATH
-for i in `seq $(($num_keypairs-1))`
+for i in `seq $num_keypairs`
 do
-    json="{\"privateKey\":\"$(cat id_rsa$i)\",\"publicKey\":\"$(cat id_rsa$i.pub)\"},"
-    echo $json >> $AZ_SCRIPTS_OUTPUT_PATH
+    if ((i == $num_keypairs)); then comma=''; fi
+    json="{\"privateKey\":\"$(cat id_rsa$i)\",\"publicKey\":\"$(cat id_rsa$i.pub)\"}$comma"
+    echo "$json" >> $AZ_SCRIPTS_OUTPUT_PATH
 done
-# the last JSON object cannot have a trailing comma
-json="{\"privateKey\":\"$(cat id_rsa$num_keypairs)\",\"publicKey\":\"$(cat id_rsa$num_keypairs.pub)\"}"
-echo "$json" >> $AZ_SCRIPTS_OUTPUT_PATH
 echo ']}' >> $AZ_SCRIPTS_OUTPUT_PATH
