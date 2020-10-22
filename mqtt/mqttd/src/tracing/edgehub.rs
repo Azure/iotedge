dcf19cc22f2c653638502c7dee7a0e878c8061f1
Denis Molokanov <dmolokanov@users.noreply.github.com>
2020-10-22 14:45:52 -0700

    implement trace formatter identical to edgehub

--- mqtt/mqttd/src/tracing/edgehub.rs
+++ mqtt/mqttd/src/tracing/edgehub.rs
@@ -1,7 +1,7 @@
-use std::io;
-
 use tracing::Level;
 use tracing_subscriber::{fmt, EnvFilter};
+
+use super::Format;
 
 const BROKER_LOG_LEVEL_ENV: &str = "BROKER_LOG";
 
@@ -14,10 +14,9 @@ pub fn init() {
         .unwrap_or_else(|_| EnvFilter::new("info"));
 
     let subscriber = fmt::Subscriber::builder()
-        .with_ansi(atty::is(atty::Stream::Stderr))
         .with_max_level(Level::TRACE)
-        .with_writer(io::stderr)
         .with_env_filter(log_level)
+        .on_event(Format::default())
         .finish();
     let _ = tracing::subscriber::set_global_default(subscriber);
 }
---
+++ mqtt/mqttd/src/tracing/format.rs
@@ -0,0 +1,117 @@
+use std::marker::PhantomData;
+
+use tracing::{Event, Level};
+use tracing_log::NormalizeEvent;
+use tracing_subscriber::fmt::{
+    time::ChronoUtc, time::FormatTime, Context, FormatEvent, NewVisitor,
+};
+
+/// Marker for `Format` that indicates that the syslog format should be used.
+pub(crate) struct Syslog;
+
+/// Custom event formatter.
+pub(crate) struct Format<F = Syslog, T = ChronoUtc> {
+    format: PhantomData<F>,
+    timer: T,
+}
+
+impl Default for Format<Syslog, ChronoUtc> {
+    fn default() -> Self {
+        Format {
+            format: PhantomData,
+            timer: ChronoUtc::with_format("%F %T.%3f %:z".into()),
+        }
+    }
+}
+
+impl<N, T> FormatEvent<N> for Format<Syslog, T>
+where
+    N: for<'a> NewVisitor<'a>,
+    T: FormatTime,
+{
+    fn format_event(
+        &self,
+        ctx: &Context<'_, N>,
+        writer: &mut dyn std::fmt::Write,
+        event: &Event<'_>,
+    ) -> std::fmt::Result {
+        let normalized_meta = event.normalized_metadata();
+        let meta = normalized_meta.as_ref().unwrap_or_else(|| event.metadata());
+
+        let (fmt_level, fmt_ctx) = (FmtLevel::new(meta.level()), FullCtx::new(&ctx));
+
+        write!(writer, "<{}> ", fmt_level.syslog_level())?;
+        self.timer.format_time(writer)?;
+        write!(writer, "[{}] - [{}{}] ", fmt_level, fmt_ctx, meta.target(),)?;
+
+        {
+            let mut recorder = ctx.new_visitor(writer, true);
+            event.record(&mut recorder);
+        }
+
+        writeln!(writer)
+    }
+}
+
+/// Wrapper around `Level` to format it accordingly to `syslog` rules.
+struct FmtLevel<'a> {
+    level: &'a Level,
+}
+
+impl<'a> FmtLevel<'a> {
+    fn new(level: &'a Level) -> Self {
+        Self { level }
+    }
+
+    fn syslog_level(&self) -> i8 {
+        match *self.level {
+            Level::ERROR => 3,
+            Level::WARN => 4,
+            Level::INFO => 6,
+            Level::DEBUG | Level::TRACE => 7,
+        }
+    }
+}
+
+impl<'a> std::fmt::Display for FmtLevel<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self.level {
+            Level::TRACE => f.pad("TRCE"),
+            Level::DEBUG => f.pad("DBUG"),
+            Level::INFO => f.pad("INFO"),
+            Level::WARN => f.pad("WARN"),
+            Level::ERROR => f.pad("ERR!"),
+        }
+    }
+}
+
+/// Wrapper around log entry context to format entry.
+struct FullCtx<'a, N> {
+    ctx: &'a Context<'a, N>,
+}
+
+impl<'a, N: 'a> FullCtx<'a, N> {
+    fn new(ctx: &'a Context<'a, N>) -> Self {
+        Self { ctx }
+    }
+}
+
+impl<'a, N> std::fmt::Display for FullCtx<'a, N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut seen = false;
+        self.ctx.visit_spans(|_, span| {
+            write!(f, "{}", span.name())?;
+            seen = true;
+
+            let fields = span.fields();
+            if !fields.is_empty() {
+                write!(f, "{{{}}}", fields)?;
+            }
+            ":".fmt(f)
+        })?;
+        if seen {
+            f.pad(" ")?;
+        }
+        Ok(())
+    }
+}
