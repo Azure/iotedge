11016a5778c1e8e518a52195fe177688eefffea4
Chad Zawistowski <chzawist@microsoft.com>
2020-10-25 23:25:04 -0700

    Add download step for Mariner toolkit

--- edgelet/build/linux/docker/cross-compile/mariner/build_amd64_container.sh
+++ edgelet/build/linux/docker/cross-compile/mariner/build_amd64_container.sh
@@ -11,6 +11,8 @@ main() {
         return 1
     fi
 
+    # Download Mariner toolkit
+    curl "https://marineriotedge.file.core.windows.net/mariner-build-env/toolkit-1.0.20201018.tar.gz?sv=2019-12-12&ss=bfqt&srt=sco&sp=rl&se=2020-11-05T14:58:28Z&st=2020-10-26T05:58:28Z&spr=https&sig=%2BqFE6WNhF01fUMa5kOiNBwIlG7hvJo9LZaRgqLfe6Q8%3D" --output toolkit-1.0.20201018.tar.gz
     build_container $dockerfile $CONTAINER_REGISTRY/mariner-build:1.0.20201018-1
 }
 
