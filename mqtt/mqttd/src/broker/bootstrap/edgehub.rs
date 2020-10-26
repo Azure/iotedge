83b5f054101a916abae948bca5f544896de3c4bb
Vadim Kovalyov <vadim.kovalyov@gmail.com>
2020-10-26 11:23:46 -0700

    Enable policy engine by default.

--- mqtt/mqttd/src/broker/bootstrap/edgehub.rs
+++ mqtt/mqttd/src/broker/bootstrap/edgehub.rs
@@ -21,6 +21,7 @@ use mqtt_broker::{
     ServerCertificate,
 };
 use mqtt_edgehub::{
+    auth::PolicyAuthorizer,
     auth::{EdgeHubAuthenticator, EdgeHubAuthorizer, LocalAuthenticator, LocalAuthorizer},
     command::{
         AuthorizedIdentitiesCommand, BridgeUpdateCommand, CommandHandler, CommandHandlerError,
@@ -52,8 +53,12 @@ pub async fn broker(
     state: Option<BrokerSnapshot>,
     broker_ready: &BrokerReady,
 ) -> Result<Broker<impl Authorizer>> {
-    // TODO: Use AllowAll as bottom level authorizer until Policies are sent over from EdgeHub
-    let authorizer = LocalAuthorizer::new(EdgeHubAuthorizer::new(AllowAll, broker_ready.handle()));
+    let device_id = env::var(DEVICE_ID_ENV).context(DEVICE_ID_ENV)?;
+
+    let authorizer = LocalAuthorizer::new(EdgeHubAuthorizer::new(
+        PolicyAuthorizer::new(device_id, broker_ready.handle()),
+        broker_ready.handle(),
+    ));
 
     let broker = BrokerBuilder::default()
         .with_authorizer(authorizer)
