5605e2d2b843c1225bd2d929cb4fa00cb78880ee
vipeller <51135538+vipeller@users.noreply.github.com>
2020-10-29 17:53:01 -0700

    using constant instead of hardcoded string

--- edge-hub/core/src/Microsoft.Azure.Devices.Edge.Hub.MqttBrokerAdapter/DeviceProxy.cs
+++ edge-hub/core/src/Microsoft.Azure.Devices.Edge.Hub.MqttBrokerAdapter/DeviceProxy.cs
@@ -44,7 +44,7 @@ namespace Microsoft.Azure.Devices.Edge.Hub.MqttBrokerAdapter
             // Although it is a direct client, it uses the indirect topics
             if (identity is ModuleIdentity moduleIdentity)
             {
-                if (string.Equals(moduleIdentity.ModuleId, "$edgeHub"))
+                if (string.Equals(moduleIdentity.ModuleId, Constants.EdgeHubModuleId))
                 {
                     this.isDirectClient = false;
                 }
