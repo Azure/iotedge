bf734e565a7e62bb65ef7b5477c24284d979251f
hugues bouvier <33438817+huguesBouvier@users.noreply.github.com>
2020-11-06 16:45:28 -0800

    fix for iotedge check (#3924) (#3927)
    * fix for iotedge check
    
    * removing test function
    
    * removing unused lib
    
    * fixed comments

--- edge-modules/iotedge-diagnostics-dotnet/src/Program.cs
+++ edge-modules/iotedge-diagnostics-dotnet/src/Program.cs
@@ -2,24 +2,12 @@
 namespace Diagnostics
 {
     using System;
-    using System.IO;
     using System.Linq;
     using System.Net;
     using System.Net.Http;
-    using System.Net.Sockets;
-    using System.Text;
-    using System.Threading;
     using System.Threading.Tasks;
-    using Microsoft.Azure.Devices.Client;
-    using Microsoft.Azure.Devices.Client.Transport.Mqtt;
     using Microsoft.Azure.Devices.Edge.Util;
-    using Microsoft.Azure.Devices.Edge.Util.Concurrency;
-    using Microsoft.Azure.Devices.Edge.Util.TransientFaultHandling;
-    using Microsoft.Azure.Devices.Shared;
     using Microsoft.Extensions.Configuration;
-    using Newtonsoft.Json;
-    using ProxyLib.Proxy;
-    using ProxyLib.Proxy.Exceptions;
 
     class Program
     {
@@ -83,52 +71,32 @@ namespace Diagnostics
 
         static async Task Upstream(string hostname, string port, string proxy)
         {
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
+            {
+                    return true; // Is valid
+            };
+
             if (proxy != null)
             {
-                Uri proxyUri = new Uri(proxy);
-                IProxyClient proxyClient = MakeProxy(proxyUri);
-
-                // Setup timeouts
-                proxyClient.ReceiveTimeout = (int)TimeSpan.FromSeconds(60).TotalMilliseconds;
-                proxyClient.SendTimeout = (int)TimeSpan.FromSeconds(60).TotalMilliseconds;
-
-                // Get TcpClient to futher work
-                var client = proxyClient.CreateConnection(hostname, int.Parse(port));
-                client.GetStream();
+                Environment.SetEnvironmentVariable("https_proxy", proxy);
             }
-            else
+
+            var httpClient = new HttpClient(httpClientHandler);
+            var logsUrl = string.Format("https://{0}/devices/0000/modules", hostname);
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, logsUrl);
+            HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequest, HttpCompletionOption.ResponseHeadersRead);
+
+            var keys = httpResponseMessage.Headers.GetValues("iothub-errorcode");
+            if (!keys.Contains("InvalidProtocolVersion"))
             {
-                TcpClient client = new TcpClient();
-                await client.ConnectAsync(hostname, int.Parse(port));
-                client.GetStream();
+                throw new Exception($"Wrong value for iothub-errorcode header");
             }
         }
 
         static void ParentHostname(string parent_hostname)
         {
             _ = Dns.GetHostEntry(parent_hostname);
-        }
-
-        static IProxyClient MakeProxy(Uri proxyUri)
-        {
-            // Uses https://github.com/grinay/ProxyLib
-            ProxyClientFactory factory = new ProxyClientFactory();
-            if (proxyUri.UserInfo == string.Empty)
-            {
-                return factory.CreateProxyClient(ProxyType.Http, proxyUri.Host, proxyUri.Port);
-            }
-            else
-            {
-                if (proxyUri.UserInfo.Contains(':'))
-                {
-                    var userPass = proxyUri.UserInfo.Split(':');
-                    return factory.CreateProxyClient(ProxyType.Http, proxyUri.Host, proxyUri.Port, userPass[0], userPass[1]);
-                }
-                else
-                {
-                    throw new Exception($"Invalid user info: {proxyUri.UserInfo}");
-                }
-            }
         }
     }
 }
