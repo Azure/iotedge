07d9343e778bf0eb53375f1719c204f6c8a55aa6
hugues bouvier <33438817+huguesBouvier@users.noreply.github.com>
2020-11-09 16:11:22 -0800

    Fixing bug in iotedge check (#3937)
    adding comment

--- edge-modules/iotedge-diagnostics-dotnet/src/Program.cs
+++ edge-modules/iotedge-diagnostics-dotnet/src/Program.cs
@@ -5,9 +5,11 @@ namespace Diagnostics
     using System.Linq;
     using System.Net;
     using System.Net.Http;
+    using System.Net.Sockets;
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Edge.Util;
     using Microsoft.Extensions.Configuration;
+    using ProxyLib.Proxy;
 
     class Program
     {
@@ -71,32 +73,82 @@ namespace Diagnostics
 
         static async Task Upstream(string hostname, string port, string proxy)
         {
-            var httpClientHandler = new HttpClientHandler();
-            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
+            if (port == "443")
             {
+                var httpClientHandler = new HttpClientHandler();
+                httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
+                {
                     return true; // Is valid
-            };
+                };
 
-            if (proxy != null)
-            {
-                Environment.SetEnvironmentVariable("https_proxy", proxy);
+                if (proxy != null)
+                {
+                    Environment.SetEnvironmentVariable("https_proxy", proxy);
+                }
+
+                var httpClient = new HttpClient(httpClientHandler);
+                var logsUrl = string.Format("https://{0}/devices/0000/modules", hostname);
+                var httpRequest = new HttpRequestMessage(HttpMethod.Get, logsUrl);
+                HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequest, HttpCompletionOption.ResponseHeadersRead);
+
+                var keys = httpResponseMessage.Headers.GetValues("iothub-errorcode");
+                if (!keys.Contains("InvalidProtocolVersion"))
+                {
+                    throw new Exception($"Wrong value for iothub-errorcode header");
+                }
             }
-
-            var httpClient = new HttpClient(httpClientHandler);
-            var logsUrl = string.Format("https://{0}/devices/0000/modules", hostname);
-            var httpRequest = new HttpRequestMessage(HttpMethod.Get, logsUrl);
-            HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequest, HttpCompletionOption.ResponseHeadersRead);
-
-            var keys = httpResponseMessage.Headers.GetValues("iothub-errorcode");
-            if (!keys.Contains("InvalidProtocolVersion"))
+            else
             {
-                throw new Exception($"Wrong value for iothub-errorcode header");
+                // The current rust code never put proxy parameter when port is != than 443.
+                // So the code below is never exercised. It was put there to avoid silently ignoring the proxy
+                // if the rust code is changed.
+                if (proxy != null)
+                {
+                    Uri proxyUri = new Uri(proxy);
+                    IProxyClient proxyClient = MakeProxy(proxyUri);
+
+                    // Setup timeouts
+                    proxyClient.ReceiveTimeout = (int)TimeSpan.FromSeconds(60).TotalMilliseconds;
+                    proxyClient.SendTimeout = (int)TimeSpan.FromSeconds(60).TotalMilliseconds;
+
+                    // Get TcpClient to futher work
+                    var client = proxyClient.CreateConnection(hostname, int.Parse(port));
+                    client.GetStream();
+                }
+                else
+                {
+                    TcpClient client = new TcpClient();
+                    await client.ConnectAsync(hostname, int.Parse(port));
+                    client.GetStream();
+                }
             }
         }
 
         static void ParentHostname(string parent_hostname)
         {
             _ = Dns.GetHostEntry(parent_hostname);
+        }
+
+        static IProxyClient MakeProxy(Uri proxyUri)
+        {
+            // Uses https://github.com/grinay/ProxyLib
+            ProxyClientFactory factory = new ProxyClientFactory();
+            if (proxyUri.UserInfo == string.Empty)
+            {
+                return factory.CreateProxyClient(ProxyType.Http, proxyUri.Host, proxyUri.Port);
+            }
+            else
+            {
+                if (proxyUri.UserInfo.Contains(':'))
+                {
+                    var userPass = proxyUri.UserInfo.Split(':');
+                    return factory.CreateProxyClient(ProxyType.Http, proxyUri.Host, proxyUri.Port, userPass[0], userPass[1]);
+                }
+                else
+                {
+                    throw new Exception($"Invalid user info: {proxyUri.UserInfo}");
+                }
+            }
         }
     }
 }
