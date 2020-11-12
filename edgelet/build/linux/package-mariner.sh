3751ba84bd59bee99469a935a6f5d62b6669832a
Chad Zawistowski <chzawist@microsoft.com>
2020-11-12 14:51:17 -0800

    Extend SAS token by several hours

--- edgelet/build/linux/package-mariner.sh
+++ edgelet/build/linux/package-mariner.sh
@@ -29,11 +29,11 @@ mkdir -p "${PROJECT_ROOT}/SPECS/libiothsm-std/SOURCES/"
 cp "${PROJECT_ROOT}/../azure-iotedge-${VERSION}.tar.gz" "${PROJECT_ROOT}/SPECS/libiothsm-std/SOURCES/"
 
 # Mariner package builds may not touch the internet, so provide Cargo dependencies
-curl "https://marineriotedge.file.core.windows.net/mariner-build-env/azure-iotedge-1.0.10-cargo.tar.gz?sv=2019-12-12&ss=bf&srt=o&sp=rl&se=2020-11-12T01:27:33Z&st=2020-11-11T17:27:33Z&spr=https&sig=MvkiNjw9v%2Ff0gkWyc9npVosAGGDcMF0er8TkHg0dBiA%3D" --output azure-iotedge-1.0.10-cargo.tar.gz
+curl "https://marineriotedge.file.core.windows.net/mariner-build-env/azure-iotedge-1.0.10-cargo.tar.gz?sv=2019-12-12&ss=bfqt&srt=o&sp=rlpx&se=2020-11-13T06:50:00Z&st=2020-11-12T22:50:00Z&spr=https&sig=islDvt5euCCf%2FyhKyAsY18TWepRgPq3pLkV9OQ%2FQbfs%3D" --output azure-iotedge-1.0.10-cargo.tar.gz
 mv azure-iotedge-1.0.10-cargo.tar.gz "${PROJECT_ROOT}/SPECS/azure-iotedge/SOURCES/"
 
 # Download Mariner toolkit
-curl "https://marineriotedge.file.core.windows.net/mariner-build-env/toolkit-1.0.20201029-x86_64.tar.gz?sv=2019-12-12&ss=bf&srt=o&sp=rl&se=2020-11-12T01:27:33Z&st=2020-11-11T17:27:33Z&spr=https&sig=MvkiNjw9v%2Ff0gkWyc9npVosAGGDcMF0er8TkHg0dBiA%3D" --output toolkit-1.0.20201018.tar.gz
+curl "https://marineriotedge.file.core.windows.net/mariner-build-env/toolkit-1.0.20201029-x86_64.tar.gz?sv=2019-12-12&ss=bfqt&srt=o&sp=rlpx&se=2020-11-13T06:50:00Z&st=2020-11-12T22:50:00Z&spr=https&sig=islDvt5euCCf%2FyhKyAsY18TWepRgPq3pLkV9OQ%2FQbfs%3D" --output toolkit-1.0.20201018.tar.gz
 mv toolkit-*.tar.gz ./toolkit.tar.gz
 tar xzf toolkit.tar.gz
 cd toolkit
