70da3364f1afc63cb07603f8f628c638f3a2c3c6
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2020-11-25 10:41:37 -0800

    Rename config file variable name

--- test/Microsoft.Azure.Devices.Edge.Test.Common/DaemonConfiguration.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/DaemonConfiguration.cs
@@ -10,14 +10,14 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
     public class DaemonConfiguration
     {
         const string GlobalEndPoint = "https://global.azure-devices-provisioning.net";
-        readonly string configYamlFile;
-        readonly YamlDocument config;
+        readonly string edgedConfigFile;
+        readonly YamlDocument edgedConfig;
 
-        public DaemonConfiguration(string configYamlFile, Option<string> agentImage, Option<Registry> agentRegistry)
+        public DaemonConfiguration(string edgedConfigFile, Option<string> agentImage, Option<Registry> agentRegistry)
         {
-            this.configYamlFile = configYamlFile;
-            string contents = File.ReadAllText(this.configYamlFile);
-            this.config = new YamlDocument(contents);
+            this.edgedConfigFile = edgedConfigFile;
+            string contents = File.ReadAllText(this.edgedConfigFile);
+            this.edgedConfig = new YamlDocument(contents);
             this.UpdateAgentImage(
                 agentImage.GetOrElse("mcr.microsoft.com/azureiotedge-agent:1.0"),
                 agentRegistry);
@@ -25,39 +25,39 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
 
         public void UpdateAgentImage(string agentImage, Option<Registry> agentRegistry)
         {
-            this.config.ReplaceOrAdd("agent.config.image", agentImage);
+            this.edgedConfig.ReplaceOrAdd("agent.config.image", agentImage);
             agentRegistry.ForEach(
                 r =>
                 {
-                    this.config.ReplaceOrAdd("agent.config.auth.serveraddress", r.Address);
-                    this.config.ReplaceOrAdd("agent.config.auth.username", r.Username);
-                    this.config.ReplaceOrAdd("agent.config.auth.password", r.Password);
+                    this.edgedConfig.ReplaceOrAdd("agent.config.auth.serveraddress", r.Address);
+                    this.edgedConfig.ReplaceOrAdd("agent.config.auth.username", r.Username);
+                    this.edgedConfig.ReplaceOrAdd("agent.config.auth.password", r.Password);
                 });
         }
 
         public void AddHttpsProxy(Uri proxy)
         {
-            this.config.ReplaceOrAdd("agent.env.https_proxy", proxy.ToString());
+            this.edgedConfig.ReplaceOrAdd("agent.env.https_proxy", proxy.ToString());
             // The config.yaml file is configured during test suite
             // initialization, before we know which protocol a given test
             // will use. Always use AmqpWs, and when each test deploys a
             // configuration, it can use whatever it wants.
-            this.config.ReplaceOrAdd("agent.env.UpstreamProtocol", "AmqpWs");
+            this.edgedConfig.ReplaceOrAdd("agent.env.UpstreamProtocol", "AmqpWs");
         }
 
         void SetBasicDpsParam(string idScope)
         {
-            this.config.RemoveIfExists("provisioning");
-            this.config.ReplaceOrAdd("provisioning.source", "dps");
-            this.config.ReplaceOrAdd("provisioning.global_endpoint", GlobalEndPoint);
-            this.config.ReplaceOrAdd("provisioning.scope_id", idScope);
+            this.edgedConfig.RemoveIfExists("provisioning");
+            this.edgedConfig.ReplaceOrAdd("provisioning.source", "dps");
+            this.edgedConfig.ReplaceOrAdd("provisioning.global_endpoint", GlobalEndPoint);
+            this.edgedConfig.ReplaceOrAdd("provisioning.scope_id", idScope);
         }
 
         public void SetDeviceConnectionString(string connectionString)
         {
-            this.config.RemoveIfExists("provisioning");
-            this.config.ReplaceOrAdd("provisioning.source", "manual");
-            this.config.ReplaceOrAdd("provisioning.device_connection_string", connectionString);
+            this.edgedConfig.RemoveIfExists("provisioning");
+            this.edgedConfig.ReplaceOrAdd("provisioning.source", "manual");
+            this.edgedConfig.ReplaceOrAdd("provisioning.device_connection_string", connectionString);
         }
 
         public void SetDeviceManualX509(string hubhostname, string deviceId, string identityCertPath, string identity_pk_path)
@@ -65,21 +65,21 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
             Uri certUri = new Uri(identityCertPath, UriKind.Absolute);
             Uri pKeyUri = new Uri(identity_pk_path, UriKind.Absolute);
 
-            this.config.RemoveIfExists("provisioning");
-            this.config.ReplaceOrAdd("provisioning.source", "manual");
-            this.config.ReplaceOrAdd("provisioning.authentication.method", "x509");
-            this.config.ReplaceOrAdd("provisioning.authentication.iothub_hostname", hubhostname);
-            this.config.ReplaceOrAdd("provisioning.authentication.device_id", deviceId);
-            this.config.ReplaceOrAdd("provisioning.authentication.identity_cert", certUri.ToString());
-            this.config.ReplaceOrAdd("provisioning.authentication.identity_pk", pKeyUri.ToString());
+            this.edgedConfig.RemoveIfExists("provisioning");
+            this.edgedConfig.ReplaceOrAdd("provisioning.source", "manual");
+            this.edgedConfig.ReplaceOrAdd("provisioning.authentication.method", "x509");
+            this.edgedConfig.ReplaceOrAdd("provisioning.authentication.iothub_hostname", hubhostname);
+            this.edgedConfig.ReplaceOrAdd("provisioning.authentication.device_id", deviceId);
+            this.edgedConfig.ReplaceOrAdd("provisioning.authentication.identity_cert", certUri.ToString());
+            this.edgedConfig.ReplaceOrAdd("provisioning.authentication.identity_pk", pKeyUri.ToString());
         }
 
         public void SetDpsSymmetricKey(string idScope, string registrationId, string deviceKey)
         {
             this.SetBasicDpsParam(idScope);
-            this.config.ReplaceOrAdd("provisioning.attestation.method", "symmetric_key");
-            this.config.ReplaceOrAdd("provisioning.attestation.registration_id", registrationId);
-            this.config.ReplaceOrAdd("provisioning.attestation.symmetric_key", deviceKey);
+            this.edgedConfig.ReplaceOrAdd("provisioning.attestation.method", "symmetric_key");
+            this.edgedConfig.ReplaceOrAdd("provisioning.attestation.registration_id", registrationId);
+            this.edgedConfig.ReplaceOrAdd("provisioning.attestation.symmetric_key", deviceKey);
         }
 
         public void SetDpsX509(string idScope, string registrationId, IdCertificates cert)
@@ -88,44 +88,44 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
             Uri pKeyUri = new Uri(cert.KeyPath, UriKind.Absolute);
 
             this.SetBasicDpsParam(idScope);
-            this.config.ReplaceOrAdd("provisioning.attestation.method", "x509");
-            this.config.ReplaceOrAdd("provisioning.attestation.identity_cert", certUri.ToString());
-            this.config.ReplaceOrAdd("provisioning.attestation.identity_pk", pKeyUri.ToString());
-            this.config.ReplaceOrAdd("provisioning.attestation.registration_id", registrationId);
+            this.edgedConfig.ReplaceOrAdd("provisioning.attestation.method", "x509");
+            this.edgedConfig.ReplaceOrAdd("provisioning.attestation.identity_cert", certUri.ToString());
+            this.edgedConfig.ReplaceOrAdd("provisioning.attestation.identity_pk", pKeyUri.ToString());
+            this.edgedConfig.ReplaceOrAdd("provisioning.attestation.registration_id", registrationId);
         }
 
         public void SetDeviceHostname(string value)
         {
-            this.config.ReplaceOrAdd("hostname", value);
+            this.edgedConfig.ReplaceOrAdd("hostname", value);
         }
 
         public void SetParentHostname(string value)
         {
-            this.config.ReplaceOrAdd("parent_hostname", value);
+            this.edgedConfig.ReplaceOrAdd("parent_hostname", value);
         }
 
         public void SetCertificates(CaCertificates certs)
         {
-            this.config.ReplaceOrAdd("certificates.device_ca_cert", certs.CertificatePath);
-            this.config.ReplaceOrAdd("certificates.device_ca_pk", certs.KeyPath);
-            this.config.ReplaceOrAdd("certificates.trusted_ca_certs", certs.TrustedCertificatesPath);
+            this.edgedConfig.ReplaceOrAdd("certificates.device_ca_cert", certs.CertificatePath);
+            this.edgedConfig.ReplaceOrAdd("certificates.device_ca_pk", certs.KeyPath);
+            this.edgedConfig.ReplaceOrAdd("certificates.trusted_ca_certs", certs.TrustedCertificatesPath);
         }
 
         public void RemoveCertificates()
         {
-            this.config.RemoveIfExists("certificates");
+            this.edgedConfig.RemoveIfExists("certificates");
         }
 
         public void Update()
         {
-            var attr = File.GetAttributes(this.configYamlFile);
-            File.SetAttributes(this.configYamlFile, attr & ~FileAttributes.ReadOnly);
+            var attr = File.GetAttributes(this.edgedConfigFile);
+            File.SetAttributes(this.edgedConfigFile, attr & ~FileAttributes.ReadOnly);
 
-            File.WriteAllText(this.configYamlFile, this.config.ToString());
+            File.WriteAllText(this.edgedConfigFile, this.edgedConfig.ToString());
 
             if (attr != 0)
             {
-                File.SetAttributes(this.configYamlFile, attr);
+                File.SetAttributes(this.edgedConfigFile, attr);
             }
         }
     }
