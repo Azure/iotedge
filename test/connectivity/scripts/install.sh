86ba911074227373940083640d3ee013dc58529a
Hugues Bouvier <33438817+huguesBouvier@users.noreply.github.com>
2020-12-03 15:05:28 -0800

    test

--- test/connectivity/scripts/install.sh
+++ test/connectivity/scripts/install.sh
@@ -323,6 +323,8 @@ fi
 process_args "$@"
 
 working_folder="$E2E_TEST_DIR/working"
+iotedged_artifact_folder="$(get_iotedged_artifact_folder $E2E_TEST_DIR)"
+
 #@TODO remove hardcoding
 #connectivity_deployment_artifact_file="$E2E_TEST_DIR/artifacts/core-linux/e2e_deployment_files/$DEPLOYMENT_FILE_NAME"
 connectivity_deployment_artifact_file="e2e_deployment_files/$DEPLOYMENT_FILE_NAME"
