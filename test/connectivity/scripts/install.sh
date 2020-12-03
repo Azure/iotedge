5701d3e425eb4c604b030bd1c4f9a45c12ac8cd2
Hugues Bouvier <33438817+huguesBouvier@users.noreply.github.com>
2020-12-02 17:52:23 -0800

    test

--- test/connectivity/scripts/install.sh
+++ test/connectivity/scripts/install.sh
@@ -310,7 +310,7 @@ else
 fi
 
 
-az iot edge set-modules --device-id ${iotEdgeDevicesName} --hub-name ${iotHubName} --content ${DEPLOYMENT_FILE_NAME} --output none
+az iot edge set-modules --device-id ${iotEdgeDevicesName} --hub-name ${iotHubName} --content ${deployment_working_file} --output none
 
 #clean up
 #az iot hub device-identity delete -n ${iotHubName} -d ${iotEdgeDevicesName}
