19fe4ee9958871a93050bfbf1f4196b4882b29a4
Hugues Bouvier <33438817+huguesBouvier@users.noreply.github.com>
2020-12-03 17:24:28 -0800

    test

--- test/connectivity/scripts/install.sh
+++ test/connectivity/scripts/install.sh
@@ -369,7 +369,7 @@ process_args "$@"
 
 get_image_architecture_label
 
-if [ ! -z $CUSTOM_EDGE_AGENT_IMAGE ]; then
+if [ -z $CUSTOM_EDGE_AGENT_IMAGE ]; then
     CUSTOM_EDGE_AGENT_IMAGE="$ARTIFACT_IMAGE_BUILD_NUMBER-linux-$image_architecture_label"
 fi
 working_folder="$E2E_TEST_DIR/working"
