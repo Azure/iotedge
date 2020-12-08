91bb06fec3997aa83af9a048f0d527ae303f08e8
Hugues Bouvier <33438817+huguesBouvier@users.noreply.github.com>
2020-12-07 19:04:36 -0800

    test

--- scripts/linux/nested-edge-deploy-VM.sh
+++ scripts/linux/nested-edge-deploy-VM.sh
@@ -107,13 +107,7 @@ function prepare_test_from_artifacts() {
     sed -i -e "s@<CR.Username>@$CONTAINER_REGISTRY_USERNAME@g" "$deployment_working_file"
     sed -i -e "s@<CR.Password>@$CONTAINER_REGISTRY_PASSWORD@g" "$deployment_working_file"
     sed -i -e "s@<IoTHubConnectionString>@$IOT_HUB_CONNECTION_STRING@g" "$deployment_working_file"
-    sed -i -e "s@<TestDuration>@$TEST_DURATION@g" "$deployment_working_file"
-    sed -i -e "s@<TestStartDelay>@$TEST_START_DELAY@g" "$deployment_working_file"
-    sed -i -e "s@<TrackingId>@$tracking_id@g" "$deployment_working_file"
-    sed -i -e "s@<LogAnalyticsWorkspaceId>@$LOG_ANALYTICS_WORKSPACEID@g" "$deployment_working_file"
-    sed -i -e "s@<LogAnalyticsSharedKey>@$LOG_ANALYTICS_SHAREDKEY@g" "$deployment_working_file"
-    sed -i -e "s@<UpstreamProtocol>@$UPSTREAM_PROTOCOL@g" "$deployment_working_file"
-    
+  
     if [[ ! -z "$CUSTOM_EDGE_AGENT_IMAGE" ]]; then
         sed -i -e "s@\"image\":.*azureiotedge-agent:.*\"@\"image\": \"$CUSTOM_EDGE_AGENT_IMAGE\"@g" "$deployment_working_file"
     fi
@@ -121,31 +115,6 @@ function prepare_test_from_artifacts() {
     if [[ ! -z "$CUSTOM_EDGE_HUB_IMAGE" ]]; then
         sed -i -e "s@\"image\":.*azureiotedge-hub:.*\"@\"image\": \"$CUSTOM_EDGE_HUB_IMAGE\"@g" "$deployment_working_file"
     fi
-    
-    sed -i -e "s@<LoadGen.MessageFrequency>@$LOADGEN_MESSAGE_FREQUENCY@g" "$deployment_working_file"
-
-    sed -i -e "s@<EdgeHubRestartTest.RestartPeriod>@$RESTART_TEST_RESTART_PERIOD@g" "$deployment_working_file"
-    sed -i -e "s@<EdgeHubRestartTest.SdkOperationTimeout>@$RESTART_TEST_SDK_OPERATION_TIMEOUT@g" "$deployment_working_file"
-
-    sed -i -e "s@<TestResultCoordinator.ConsumerGroupId>@$EVENT_HUB_CONSUMER_GROUP_ID@g" "$deployment_working_file"
-    sed -i -e "s@<TestResultCoordinator.EventHubConnectionString>@$EVENTHUB_CONNECTION_STRING@g" "$deployment_working_file"
-    sed -i -e "s@<TestResultCoordinator.VerificationDelay>@$VERIFICATION_DELAY@g" "$deployment_working_file"
-    sed -i -e "s@<TestResultCoordinator.OptimizeForPerformance>@$optimize_for_performance@g" "$deployment_working_file"
-    sed -i -e "s@<TestResultCoordinator.LogAnalyticsLogType>@$LOG_ANALYTICS_LOGTYPE@g" "$deployment_working_file"
-    sed -i -e "s@<TestResultCoordinator.logUploadEnabled>@$log_upload_enabled@g" "$deployment_working_file"
-    sed -i -e "s@<TestResultCoordinator.StorageAccountConnectionString>@$STORAGE_ACCOUNT_CONNECTION_STRING@g" "$deployment_working_file"
-    sed -i -e "s@<TestInfo>@$TEST_INFO@g" "$deployment_working_file"
-
-    sed -i -e "s@<NetworkController.OfflineFrequency0>@${NETWORK_CONTROLLER_FREQUENCIES[0]}@g" "$deployment_working_file"
-    sed -i -e "s@<NetworkController.OnlineFrequency0>@${NETWORK_CONTROLLER_FREQUENCIES[1]}@g" "$deployment_working_file"
-    sed -i -e "s@<NetworkController.RunsCount0>@${NETWORK_CONTROLLER_FREQUENCIES[2]}@g" "$deployment_working_file"
-    sed -i -e "s@<NetworkController.RunProfile>@$NETWORK_CONTROLLER_RUNPROFILE@g" "$deployment_working_file"
-    
-    sed -i -e "s@<DeploymentTester1.DeploymentUpdatePeriod>@$DEPLOYMENT_TEST_UPDATE_PERIOD@g" "$deployment_working_file"
-
-    sed -i -e "s@<MetricsCollector.MetricsEndpointsCSV>@$METRICS_ENDPOINTS_CSV@g" "$deployment_working_file"
-    sed -i -e "s@<MetricsCollector.ScrapeFrequencyInSecs>@$METRICS_SCRAPE_FREQUENCY_IN_SECS@g" "$deployment_working_file"
-    sed -i -e "s@<MetricsCollector.UploadTarget>@$METRICS_UPLOAD_TARGET@g" "$deployment_working_file"
 }
 
 
@@ -176,99 +145,30 @@ function process_args() {
             IOT_HUB_CONNECTION_STRING="$arg"
             saveNextArg=0
         elif [ $saveNextArg -eq 8 ]; then
-            EVENTHUB_CONNECTION_STRING="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 9 ]; then
-            EVENT_HUB_CONSUMER_GROUP_ID="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 10 ]; then
-            TEST_DURATION="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 11 ]; then
-            TEST_START_DELAY="$arg"
-            saveNextArg=0 
-        elif [ $saveNextArg -eq 12 ]; then
-            LOADGEN_MESSAGE_FREQUENCY="$arg"
-            saveNextArg=0   
-        elif [ $saveNextArg -eq 13 ]; then
-            NETWORK_CONTROLLER_FREQUENCIES=($arg)
-            saveNextArg=0
-        elif [ $saveNextArg -eq 14 ]; then
-            NETWORK_CONTROLLER_RUNPROFILE="$arg"
-            saveNextArg=0    
-        elif [ $saveNextArg -eq 15 ]; then
-            LOG_ANALYTICS_WORKSPACEID="$arg"
-            saveNextArg=0 
-        elif [ $saveNextArg -eq 16 ]; then
-            LOG_ANALYTICS_SHAREDKEY="$arg"
-            saveNextArg=0 
-        elif [ $saveNextArg -eq 17 ]; then
-            LOG_ANALYTICS_LOGTYPE="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 18 ]; then
-            VERIFICATION_DELAY="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 19 ]; then
-            UPSTREAM_PROTOCOL="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 20 ]; then
-            DEPLOYMENT_TEST_UPDATE_PERIOD="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 21 ]; then
-            TIME_FOR_REPORT_GENERATION="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 22 ]; then
-            METRICS_ENDPOINTS_CSV="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 23 ]; then
-            METRICS_SCRAPE_FREQUENCY_IN_SECS="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 24 ]; then
-            METRICS_UPLOAD_TARGET="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 25 ]; then
             STORAGE_ACCOUNT_CONNECTION_STRING="$arg"
             saveNextArg=0
-        elif [ $saveNextArg -eq 26 ]; then
-            DEVOPS_ACCESS_TOKEN="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 27 ]; then
-            DEVOPS_BUILDID="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 28 ]; then
+        elif [ $saveNextArg -eq 9 ]; then
             DEPLOYMENT_FILE_NAME="$arg"
             saveNextArg=0
-        elif [ $saveNextArg -eq 29 ]; then
-            RESTART_TEST_RESTART_PERIOD="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 30 ]; then
-            RESTART_TEST_SDK_OPERATION_TIMEOUT="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 31 ]; then
+        elif [ $saveNextArg -eq 10 ]; then
             EDGE_RUNTIME_BUILD_NUMBER="$arg"
             saveNextArg=0
-        elif [ $saveNextArg -eq 32 ]; then
+        elif [ $saveNextArg -eq 11 ]; then
             CUSTOM_EDGE_AGENT_IMAGE="$arg"
             saveNextArg=0
-        elif [ $saveNextArg -eq 33 ]; then
+        elif [ $saveNextArg -eq 12 ]; then
             CUSTOM_EDGE_HUB_IMAGE="$arg"
             saveNextArg=0
-        elif [ $saveNextArg -eq 34 ]; then
-            TEST_RUNTIME_LOG_LEVEL="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 35 ]; then
-            TEST_INFO="$arg"
-            saveNextArg=0
-        elif [ $saveNextArg -eq 36 ]; then
+        elif [ $saveNextArg -eq 13 ]; then
             SUBSCRIPTION="$arg"
             saveNextArg=0
-        elif [ $saveNextArg -eq 37 ]; then
+        elif [ $saveNextArg -eq 14 ]; then
             LEVEL="$arg"
             saveNextArg=0         
-        elif [ $saveNextArg -eq 38 ]; then
+        elif [ $saveNextArg -eq 15 ]; then
             PARENT_NAME="$arg"
             saveNextArg=0
-        elif [ $saveNextArg -eq 39 ]; then
+        elif [ $saveNextArg -eq 16 ]; then
             PARENT_CONNECTION_STRING="$arg"
             saveNextArg=0                          
         else              
@@ -281,38 +181,15 @@ function process_args() {
                 '-containerRegistryUsername' ) saveNextArg=5;;
                 '-containerRegistryPassword' ) saveNextArg=6;;
                 '-iotHubConnectionString' ) saveNextArg=7;;
-                '-eventHubConnectionString' ) saveNextArg=8;;
-                '-eventHubConsumerGroupId' ) saveNextArg=9;;
-                '-testDuration' ) saveNextArg=10;;
-                '-testStartDelay' ) saveNextArg=11;;
-                '-loadGenMessageFrequency' ) saveNextArg=12;;
-                '-networkControllerFrequency' ) saveNextArg=13;;
-                '-networkControllerRunProfile' ) saveNextArg=14;;
-                '-logAnalyticsWorkspaceId' ) saveNextArg=15;;
-                '-logAnalyticsSharedKey' ) saveNextArg=16;;
-                '-logAnalyticsLogType' ) saveNextArg=17;;
-                '-verificationDelay' ) saveNextArg=18;;
-                '-upstreamProtocol' ) saveNextArg=19;;
-                '-deploymentTestUpdatePeriod' ) saveNextArg=20;;
-                '-timeForReportingGeneration' ) saveNextArg=21;;
-                '-metricsEndpointsCSV' ) saveNextArg=22;;
-                '-metricsScrapeFrequencyInSecs' ) saveNextArg=23;;
-                '-metricsUploadTarget' ) saveNextArg=24;;
-                '-storageAccountConnectionString' ) saveNextArg=25;;
-                '-devOpsAccessToken' ) saveNextArg=26;;
-                '-devOpsBuildId' ) saveNextArg=27;;
-                '-deploymentFileName' ) saveNextArg=28;;
-                '-EdgeHubRestartTestRestartPeriod' ) saveNextArg=29;;
-                '-EdgeHubRestartTestSdkOperationTimeout' ) saveNextArg=30;;
-                '-edgeRuntimeBuildNumber' ) saveNextArg=31;;
-                '-customEdgeAgentImage' ) saveNextArg=32;;
-                '-customEdgeHubImage' ) saveNextArg=33;;
-                '-testRuntimeLogLevel' ) saveNextArg=34;;
-                '-testInfo' ) saveNextArg=35;;
-                '-subscription' ) saveNextArg=36;;
-                '-level' ) saveNextArg=37;;             
-                '-parentName' ) saveNextArg=38;; 
-                '-parentConnectionString' ) saveNextArg=39;;              
+                '-storageAccountConnectionString' ) saveNextArg=8;;
+                '-deploymentFileName' ) saveNextArg=9;;
+                '-edgeRuntimeBuildNumber' ) saveNextArg=10;;
+                '-customEdgeAgentImage' ) saveNextArg=11;;
+                '-customEdgeHubImage' ) saveNextArg=12;;
+                '-subscription' ) saveNextArg=13;;
+                '-level' ) saveNextArg=14;;             
+                '-parentName' ) saveNextArg=15;; 
+                '-parentConnectionString' ) saveNextArg=16;;              
                 '-waitForTestComplete' ) WAIT_FOR_TEST_COMPLETE=1;;
                 '-cleanAll' ) CLEAN_ALL=1;;
                 
@@ -327,18 +204,11 @@ function process_args() {
     # Required parameters  
     [[ -z "$SUBSCRIPTION" ]] && { print_error 'SUBSCRIPTION is required.'; exit 1; }
     [[ -z "$LEVEL" ]] && { print_error 'Level is required.'; exit 1; }
-    [[ -z "$RELEASE_LABEL" ]] && { print_error 'Release label is required.'; exit 1; }
     [[ -z "$ARTIFACT_IMAGE_BUILD_NUMBER" ]] && { print_error 'Artifact image build number is required'; exit 1; }
     [[ -z "$CONTAINER_REGISTRY_USERNAME" ]] && { print_error 'Container registry username is required'; exit 1; }
     [[ -z "$CONTAINER_REGISTRY_PASSWORD" ]] && { print_error 'Container registry password is required'; exit 1; }
     [[ -z "$DEPLOYMENT_FILE_NAME" ]] && { print_error 'Deployment file name is required'; exit 1; }
-    [[ -z "$EVENTHUB_CONNECTION_STRING" ]] && { print_error 'Event hub connection string is required'; exit 1; }
     [[ -z "$IOT_HUB_CONNECTION_STRING" ]] && { print_error 'IoT hub connection string is required'; exit 1; }
-    [[ -z "$LOG_ANALYTICS_SHAREDKEY" ]] && { print_error 'Log analytics shared key is required'; exit 1; }
-    [[ -z "$LOG_ANALYTICS_WORKSPACEID" ]] && { print_error 'Log analytics workspace id is required'; exit 1; }
-    [[ -z "$METRICS_ENDPOINTS_CSV" ]] && { print_error 'Metrics endpoints csv is required'; exit 1; }
-    [[ -z "$METRICS_SCRAPE_FREQUENCY_IN_SECS" ]] && { print_error 'Metrics scrape frequency is required'; exit 1; }
-    [[ -z "$METRICS_UPLOAD_TARGET" ]] && { print_error 'Metrics upload target is required'; exit 1; }
     [[ -z "$STORAGE_ACCOUNT_CONNECTION_STRING" ]] && { print_error 'Storage account connection string is required'; exit 1; }
 
     echo 'Required parameters are provided'
@@ -366,12 +236,6 @@ set -e
 
 # Import test-related functions
 . $(dirname "$0")/testHelper.sh
-
-is_build_canceled=$(is_cancel_build_requested $DEVOPS_ACCESS_TOKEN $DEVOPS_BUILDID)         
-if [ "$is_build_canceled" -eq 1 ]; then
-    print_highlighted_message "build is canceled."
-    exit 3
-fi
 
 process_args "$@"
 
