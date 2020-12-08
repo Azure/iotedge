a178111a07675d5bb951d176e54ae75466cb8048
dylanbronson <dybronso@microsoft.com>
2020-12-08 15:19:10 -0800

    Removing preview iothub and eventhub endpoint

--- test/Microsoft.Azure.Devices.Edge.Test/helpers/ManualProvisioningFixture.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/helpers/ManualProvisioningFixture.cs
@@ -19,14 +19,12 @@ namespace Microsoft.Azure.Devices.Edge.Test.Helpers
         protected readonly IotHub iotHub;
         protected IEdgeDaemon daemon;
 
-        public ManualProvisioningFixture(string connectionString, string eventHubEndpoint)
-        {
-            this.iotHub = new IotHub(connectionString, eventHubEndpoint, Context.Current.Proxy);
-        }
-
         public ManualProvisioningFixture()
-            : this(Context.Current.ConnectionString, Context.Current.EventHubEndpoint)
         {
+            this.iotHub = new IotHub(
+                Context.Current.ConnectionString,
+                Context.Current.EventHubEndpoint,
+                Context.Current.Proxy);
         }
 
         [OneTimeSetUp]
