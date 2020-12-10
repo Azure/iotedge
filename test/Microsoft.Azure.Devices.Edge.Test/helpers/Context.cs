739f96c91540ac28687fc4ed8c42d2617f947609
dylanbronson <55515325+dylanbronson@users.noreply.github.com>
2020-12-09 16:11:06 -0800

    Removing bootstrap workaround for e2e tests (#4066) (#4076)
    Cherry-pick from main branch to 1.2
    
    Previously, we implemented a workaround for end-to-end (e2e) tests because config.yaml + iotedged was default pulling version 1.0.9 because we use the 1.0 tag, and 1.0.9 was tagged as 1.0 version. This was an issue because there was validation logic in the e2e tests that checks that EA has been deployed correctly. But this validation logic didn't work if the EA versions were incompatible. EA 1.0.10 had a change that 1.0.9 didn't have that made the two versions incompatible. So we added a custom bootstrap image to the e2e tests that would start the config.yaml with a 1.0.10 version of EA that was compatible with the newer version of EA. 
    
    Once we tagged 1.0.10 with 1.0, all of these changes can no go away, since we don't need to have a bootstrap image anymore.
    This PR removes the bootstrap workaround.

--- test/Microsoft.Azure.Devices.Edge.Test/helpers/Context.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/helpers/Context.cs
@@ -103,7 +103,6 @@ namespace Microsoft.Azure.Devices.Edge.Test.Helpers
             this.LoadGenImage = Option.Maybe(Get("loadGenImage"));
             this.RelayerImage = Option.Maybe(Get("relayerImage"));
             this.NetworkControllerImage = Option.Maybe(Get("networkControllerImage"));
-            this.EdgeAgentBootstrapImage = Option.Maybe(Get("edgeAgentBootstrapImage"));
             this.TestTimeout = TimeSpan.FromMinutes(context.GetValue("testTimeoutMinutes", 5));
             this.Verbose = context.GetValue<bool>("verbose");
             this.ParentHostname = Option.Maybe(Get("parentHostname"));
@@ -169,8 +168,6 @@ namespace Microsoft.Azure.Devices.Edge.Test.Helpers
         public Option<string> RelayerImage { get; }
 
         public Option<string> NetworkControllerImage { get; }
-
-        public Option<string> EdgeAgentBootstrapImage { get; }
 
         public TimeSpan TestTimeout { get; }
 
