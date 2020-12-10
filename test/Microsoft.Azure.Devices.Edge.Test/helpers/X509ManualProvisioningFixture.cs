e18b339e354c6034ce35fc2391d305f223925c2b
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2020-12-10 13:19:52 -0800

    Fix X509ManualProvision

--- test/Microsoft.Azure.Devices.Edge.Test/helpers/X509ManualProvisioningFixture.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/helpers/X509ManualProvisioningFixture.cs
@@ -2,6 +2,7 @@
 namespace Microsoft.Azure.Devices.Edge.Test.Helpers
 {
     using System;
+    using System.IO;
     using System.Security.Cryptography.X509Certificates;
     using System.Threading;
     using System.Threading.Tasks;
@@ -25,7 +26,7 @@ namespace Microsoft.Azure.Devices.Edge.Test.Helpers
                         DateTime startTime = DateTime.Now;
                         string deviceId = DeviceId.Current.Generate();
 
-                        (X509Thumbprint thumbprint, IdCertificates certs) = await this.CreateIdentityCertAsync(
+                        (X509Thumbprint thumbprint, string certPath, string keyPath) = await this.CreateIdentityCertAsync(
                             deviceId, token);
 
                         EdgeDevice device = await EdgeDevice.GetOrCreateIdentityAsync(
@@ -55,8 +56,8 @@ namespace Microsoft.Azure.Devices.Edge.Test.Helpers
                                 config.SetDeviceManualX509(
                                     device.HubHostname,
                                     device.Id,
-                                    certs.CertificatePath,
-                                    certs.KeyPath);
+                                    certPath,
+                                    keyPath);
                                 config.Update();
                                 return Task.FromResult((
                                     "with x509 certificate for device '{Identity}'",
@@ -70,7 +71,7 @@ namespace Microsoft.Azure.Devices.Edge.Test.Helpers
                 "Completed edge manual provisioning with self-signed certificate");
         }
 
-        async Task<(X509Thumbprint, IdCertificates)> CreateIdentityCertAsync(string deviceId, CancellationToken token)
+        async Task<(X509Thumbprint, string, string)> CreateIdentityCertAsync(string deviceId, CancellationToken token)
         {
             (string, string, string) rootCa =
             Context.Current.RootCaKeys.Expect(() => new InvalidOperationException("Missing root CA keys"));
@@ -87,13 +88,26 @@ namespace Microsoft.Azure.Devices.Edge.Test.Helpers
 
             var identityCerts = await ca.GenerateIdentityCertificatesAsync(deviceId, token);
 
+            // Generated credentials need to be copied out of the script path because future runs
+            // of the script will overwrite them.
+            string path = $"/etc/aziot/e2e_tests/{deviceId}";
+            string certPath = $"{path}/device_id_cert.pem";
+            string keyPath = $"{path}/device_id_cert_key.pem";
+
+            Directory.CreateDirectory(path);
+            File.Copy(identityCerts.CertificatePath, certPath);
+            OsPlatform.Current.SetFileOwner(certPath, "aziotcs", "644");
+            File.Copy(identityCerts.KeyPath, keyPath);
+            OsPlatform.Current.SetFileOwner(keyPath, "aziotks", "600");
+
             X509Certificate2 deviceCert = new X509Certificate2(identityCerts.CertificatePath);
 
             return (new X509Thumbprint()
             {
                 PrimaryThumbprint = deviceCert.Thumbprint
             },
-            identityCerts);
+            certPath,
+            keyPath);
         }
     }
 }
