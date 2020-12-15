0943e5ed05b4d63d5ca3dd0af133d548196261e5
Hugues Bouvier <33438817+huguesBouvier@users.noreply.github.com>
2020-12-14 20:22:53 -0800

    test

--- test/Microsoft.Azure.Devices.Edge.Test.Common/EdgeRuntime.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/EdgeRuntime.cs
@@ -41,7 +41,7 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
         {
             var builder = new EdgeConfigBuilder(this.DeviceId);
             builder.AddRegistries(this.registries);
-            builder.AddEdgeAgent("nestededgetest02.eastus2.cloudapp.azure.com:443/microsoft/azureiotedge-agent:20201214.4-linux-amd64")
+            builder.AddEdgeAgent(this.agentImage.OrDefault())bug
                 .WithEnvironment(new[] { ("RuntimeLogLevel", "debug") })
                 .WithProxy(this.proxy);
             builder.AddEdgeHub(this.hubImage.OrDefault(), this.optimizeForPerformance)
