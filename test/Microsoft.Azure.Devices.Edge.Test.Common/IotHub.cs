fb3803031b1210ee6be7521edd221c29bdc3c669
dylanbronson <dybronso@microsoft.com>
2020-12-15 12:34:36 -0800

    fix null

--- test/Microsoft.Azure.Devices.Edge.Test.Common/IotHub.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/IotHub.cs
@@ -92,7 +92,8 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
             async p =>
             {
                 Device parentDevice = await this.GetDeviceIdentityAsync(p, token);
-                Log.Verbose($"Got parent device from id. Parent scope: {parentDevice.Scope}");
+                string parentDeviceScope = parentDevice == null ? string.Empty : parentDevice.Scope;
+                Log.Verbose($"Got parent device from id. Parent scope: {parentDeviceScope}");
                 return new Device(deviceId)
                 {
                     Authentication = new AuthenticationMechanism()
