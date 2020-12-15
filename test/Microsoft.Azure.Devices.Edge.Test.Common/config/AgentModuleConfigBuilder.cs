a664befd4674ed3a50bedee1419b3f69022ab78b
Hugues Bouvier <33438817+huguesBouvier@users.noreply.github.com>
2020-12-15 11:31:33 -0800

    Adding images in azure portal

--- test/Microsoft.Azure.Devices.Edge.Test.Common/config/AgentModuleConfigBuilder.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/config/AgentModuleConfigBuilder.cs
@@ -5,10 +5,10 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common.Config
 
     public class AgentModuleConfigBuilder : BaseModuleConfigBuilder
     {
-        const string DefaultImage = "$upstream:443/microsoft/azureiotedge-agent:20201214.4-linux-amd64";
+        const string DefaultImage = "mcr.microsoft.com/azureiotedge-agent:1.0";
 
         public AgentModuleConfigBuilder(Option<string> image)
-            : base(ModuleName.EdgeAgent, image.GetOrElse(DefaultImage))
+            : base(ModuleName.EdgeAgent, "$upstream:443/microsoft/azureiotedge-agent:20201214.4-linux-amd64")
         {
         }
     }
