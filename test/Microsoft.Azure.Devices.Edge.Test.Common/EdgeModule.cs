c4b0ccd9c130f1f2dde3bd52dab891da7db4499f
Hugues Bouvier <33438817+huguesBouvier@users.noreply.github.com>
2020-12-16 10:44:46 -0800

    tesT

--- test/Microsoft.Azure.Devices.Edge.Test.Common/EdgeModule.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/EdgeModule.cs
@@ -209,8 +209,16 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
                     .Where(k => k.EndsWith("settings.image"));
                 foreach (var imageKeys in imagesKeys)
                 {
+                    Log.Information(imageKeys.ToString());
+                    Log.Information((string)result[imageKeys].Value);
+                    Log.Information(Regex.Replace((string)result[imageKeys].Value, ".*?/(.*)", m => m.Groups[1].Value));
+
                     result[imageKeys].Value = JObject
-                        .Parse(Regex.Replace((string)result[imageKeys].Value, ".*?/(.*)", m => m.Groups[1].Value))
+                        .Parse((string)result[imageKeys].Value)
+                        .ToString(Formatting.None);
+
+                    result[imageKeys].Value = JObject
+                        .Parse("microsoft/azureiotedge-agent:20201216.2-linux-amd64")
                         .ToString(Formatting.None);
                 }
 
