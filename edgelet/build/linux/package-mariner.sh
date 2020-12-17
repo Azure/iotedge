af52162dba964e6657900f7624cbba3114e44a66
Manuel Huber <mahuber@microsoft.com>
2020-12-17 15:37:55 -0800

    Moving mariner-specific files to mariner build subdirectory

--- edgelet/build/linux/package-mariner.sh
+++ edgelet/build/linux/package-mariner.sh
@@ -6,31 +6,32 @@ set -e
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 BUILD_REPOSITORY_LOCALPATH="$(realpath "${BUILD_REPOSITORY_LOCALPATH:-$DIR/../../..}")"
-PROJECT_ROOT="${BUILD_REPOSITORY_LOCALPATH}/edgelet"
+EDGELET_ROOT="${BUILD_REPOSITORY_LOCALPATH}/edgelet"
+MARINER_BUILD_ROOT="${BUILD_REPOSITORY_LOCALPATH}/builds/mariner"
 
 REVISION="${REVISION:-1}"
-DEFAULT_VERSION="$(cat "$PROJECT_ROOT/version.txt")"
+DEFAULT_VERSION="$(cat "$EDGELET_ROOT/version.txt")"
 VERSION="${VERSION:-$DEFAULT_VERSION}"
 
 # Create source tarball
-pushd "${PROJECT_ROOT}/.."
-tar -czf azure-iotedge-${VERSION}.tar.gz --transform='s,^iotedge/,azure-iotedge-${VERSION}/,' "${PROJECT_ROOT}"
+pushd "${BUILD_REPOSITORY_LOCALPATH}"
+tar -czf azure-iotedge-${VERSION}.tar.gz --transform='s,^iotedge/,azure-iotedge-${VERSION}/,' "${EDGELET_ROOT}"
 popd
 
 # Update expected tarball hash
-TARBALL_HASH=$(sha256sum "${PROJECT_ROOT}/../azure-iotedge-${VERSION}.tar.gz")
-sed -i 's/\(azure-iotedge-[0-9.]+.tar.gz": "\)[a-fA-F0-9]+/\1${TARBALL_HASH}/g' "${PROJECT_ROOT}/SPECS/azure-iotedge/azure-iotedge.signatures.json"
-sed -i 's/\(azure-iotedge-[0-9.]+.tar.gz": "\)[a-fA-F0-9]+/\1${TARBALL_HASH}/g' "${PROJECT_ROOT}/SPECS/libiothsm-std/libiothsm-std.signatures.json"
+TARBALL_HASH=$(sha256sum "${BUILD_REPOSITORY_LOCALPATH}/azure-iotedge-${VERSION}.tar.gz")
+sed -i 's/\(azure-iotedge-[0-9.]+.tar.gz": "\)[a-fA-F0-9]+/\1${TARBALL_HASH}/g' "${MARINER_BUILD_ROOT}/SPECS/azure-iotedge/azure-iotedge.signatures.json"
+sed -i 's/\(azure-iotedge-[0-9.]+.tar.gz": "\)[a-fA-F0-9]+/\1${TARBALL_HASH}/g' "${MARINER_BUILD_ROOT}/SPECS/libiothsm-std/libiothsm-std.signatures.json"
 
 # Copy source tarball to expected locations
-mkdir -p "${PROJECT_ROOT}/SPECS/azure-iotedge/SOURCES/"
-cp "${PROJECT_ROOT}/../azure-iotedge-${VERSION}.tar.gz" "${PROJECT_ROOT}/SPECS/azure-iotedge/SOURCES/"
-mkdir -p "${PROJECT_ROOT}/SPECS/libiothsm-std/SOURCES/"
-cp "${PROJECT_ROOT}/../azure-iotedge-${VERSION}.tar.gz" "${PROJECT_ROOT}/SPECS/libiothsm-std/SOURCES/"
+mkdir -p "${MARINER_BUILD_ROOT}/SPECS/azure-iotedge/SOURCES/"
+cp "${BUILD_REPOSITORY_LOCALPATH}/azure-iotedge-${VERSION}.tar.gz" "${MARINER_BUILD_ROOT}/SPECS/azure-iotedge/SOURCES/"
+mkdir -p "${MARINER_BUILD_ROOT}/SPECS/libiothsm-std/SOURCES/"
+cp "${BUILD_REPOSITORY_LOCALPATH}/azure-iotedge-${VERSION}.tar.gz" "${MARINER_BUILD_ROOT}/SPECS/libiothsm-std/SOURCES/"
 
 # Mariner package builds may not touch the internet, so provide Cargo dependencies
 curl "https://marineriotedge.file.core.windows.net/mariner-build-env/azure-iotedge-1.0.10-cargo.tar.gz?sv=2019-12-12&ss=bfqt&srt=o&sp=rlpx&se=2020-11-13T06:50:00Z&st=2020-11-12T22:50:00Z&spr=https&sig=islDvt5euCCf%2FyhKyAsY18TWepRgPq3pLkV9OQ%2FQbfs%3D" --output azure-iotedge-1.0.10-cargo.tar.gz
-mv azure-iotedge-1.0.10-cargo.tar.gz "${PROJECT_ROOT}/SPECS/azure-iotedge/SOURCES/"
+mv azure-iotedge-1.0.10-cargo.tar.gz "${MARINER_BUILD_ROOT}/SPECS/azure-iotedge/SOURCES/"
 
 # Download Mariner toolkit
 curl "https://marineriotedge.file.core.windows.net/mariner-build-env/toolkit-1.0.20201029-x86_64.tar.gz?sv=2019-12-12&ss=bfqt&srt=o&sp=rlpx&se=2020-11-13T06:50:00Z&st=2020-11-12T22:50:00Z&spr=https&sig=islDvt5euCCf%2FyhKyAsY18TWepRgPq3pLkV9OQ%2FQbfs%3D" --output toolkit-1.0.20201018.tar.gz
