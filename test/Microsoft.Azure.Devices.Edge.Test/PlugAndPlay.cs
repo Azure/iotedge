6e902d54f5c8e76c6adcd61911f6c200c82a5684
dylanbronson <dybronso@microsoft.com>
2020-12-22 09:31:15 -0800

    change CA

--- test/Microsoft.Azure.Devices.Edge.Test/PlugAndPlay.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/PlugAndPlay.cs
@@ -61,7 +61,7 @@ namespace Microsoft.Azure.Devices.Edge.Test
                 AuthenticationType.Sas,
                 Option.Some(this.runtime.DeviceId),
                 false,
-                CertificateAuthority.GetQuickstart(),
+                this.ca,
                 this.iotHub,
                 Context.Current.Hostname.GetOrElse(Dns.GetHostName().ToLower()),
                 token,
