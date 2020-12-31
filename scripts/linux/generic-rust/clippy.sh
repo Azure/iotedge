cde4bf0c685c25bbf25e3c994a21bdcf7c047470
Andrew Smith <als5ev@virginia.edu>
2020-12-31 04:16:10 +0000

    fix checkin

--- scripts/linux/generic-rust/clippy.sh
+++ scripts/linux/generic-rust/clippy.sh
@@ -13,7 +13,7 @@ set -e
 DIR=$(cd "$(dirname "$0")" && pwd)
 
 BUILD_REPOSITORY_LOCALPATH=${BUILD_REPOSITORY_LOCALPATH:-$DIR/../../..}
-PROJECT_ROOT=${BUILD_REPOSITORY_LOCALPATH}/edge-hub/watchdog
+PROJECT_ROOT=${BUILD_REPOSITORY_LOCALPATH}
 SCRIPT_NAME=$(basename "$0")
 RUSTUP="${CARGO_HOME:-"$HOME/.cargo"}/bin/rustup"
 CARGO="${CARGO_HOME:-"$HOME/.cargo"}/bin/cargo"
