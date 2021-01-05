19c87a8944ccb96a3799ffc31a27b6fe4ad3da05
richma-ms <54086435+richma-ms@users.noreply.github.com>
2021-01-04 20:09:54 -0800

    Re-factoring EdgeHub config parsing (#4081)

--- edge-hub/core/test/Microsoft.Azure.Devices.Edge.Hub.Core.Test/config/EdgeHubConfigParserTest.cs
+++ edge-hub/core/test/Microsoft.Azure.Devices.Edge.Hub.Core.Test/config/EdgeHubConfigParserTest.cs
@@ -5,14 +5,185 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test.Config
     using System.Collections.Generic;
     using Microsoft.Azure.Devices.Edge.Hub.Core.Config;
     using Microsoft.Azure.Devices.Edge.Hub.Core.Routing;
+    using Microsoft.Azure.Devices.Edge.Util;
     using Microsoft.Azure.Devices.Edge.Util.Test.Common;
     using Microsoft.Azure.Devices.Routing.Core;
+    using Microsoft.Azure.Devices.Routing.Core.Endpoints;
     using Moq;
+    using Newtonsoft.Json;
     using Xunit;
 
     [Unit]
     public class EdgeHubConfigParserTest
     {
+        [Theory]
+        [MemberData(nameof(GetSchemaVersionData))]
+        public void SchemaVersionCheckTest(string manifest, Type expectedException)
+        {
+            var validator = new Mock<BrokerPropertiesValidator>();
+            validator
+                .Setup(v => v.ValidateAuthorizationConfig(It.IsAny<AuthorizationProperties>()))
+                .Returns(new List<string>());
+            validator
+                .Setup(v => v.ValidateBridgeConfig(It.IsAny<BridgeConfig>()))
+                .Returns(new List<string>());
+
+            var endpointFactory = new Mock<IEndpointFactory>();
+            endpointFactory
+                .Setup(v => v.CreateSystemEndpoint(It.IsAny<string>()))
+                .Returns(new NullEndpoint("$upstream"));
+
+            var routeFactory = new EdgeRouteFactory(endpointFactory.Object);
+            var configParser = new EdgeHubConfigParser(routeFactory, validator.Object);
+
+            if (expectedException != null)
+            {
+                Assert.Throws(expectedException, () => configParser.GetEdgeHubConfig(manifest));
+            }
+            else
+            {
+                configParser.GetEdgeHubConfig(manifest);
+            }
+        }
+
+        public static IEnumerable<object[]> GetSchemaVersionData()
+        {
+            string noVersion =
+                @"{
+                  'routes': {
+                    'route1': 'FROM /* INTO $upstream'
+                  },
+                  'storeAndForwardConfiguration': {
+                    'timeToLiveSecs': 20
+                  },
+                  '$version': 2
+                }";
+
+            string version_0_1 =
+                    @"{
+                      'schemaVersion': '0.1',
+                      'routes': {
+                        'route1': 'FROM /* INTO $upstream'
+                      },
+                      'storeAndForwardConfiguration': {
+                        'timeToLiveSecs': 20
+                      },
+                      '$version': 2
+                    }";
+
+            string version_1 =
+                    @"{
+                      'schemaVersion': '1',
+                      'routes': {
+                        'route1': 'FROM /* INTO $upstream'
+                      },
+                      'storeAndForwardConfiguration': {
+                        'timeToLiveSecs': 20
+                      },
+                      '$version': 2
+                    }";
+
+            string version_1_0 =
+                    @"{
+                      'schemaVersion': '1.0',
+                      'routes': {
+                        'route1': 'FROM /* INTO $upstream'
+                      },
+                      'storeAndForwardConfiguration': {
+                        'timeToLiveSecs': 20
+                      },
+                      '$version': 2
+                    }";
+
+            string version_1_1 =
+                    @"{
+                      'schemaVersion': '1.1',
+                      'routes': {
+                        'route1': 'FROM /* INTO $upstream'
+                      },
+                      'storeAndForwardConfiguration': {
+                        'timeToLiveSecs': 20
+                      },
+                      '$version': 2
+                    }";
+
+            string version_1_1_0 =
+                    @"{
+                      'schemaVersion': '1.1.0',
+                      'routes': {
+                        'route1': 'FROM /* INTO $upstream'
+                      },
+                      'storeAndForwardConfiguration': {
+                        'timeToLiveSecs': 20
+                      },
+                      '$version': 2
+                    }";
+
+            string version_1_2 =
+                    @"{
+                      'schemaVersion': '1.2',
+                      'routes': {
+                        'route1': 'FROM /* INTO $upstream'
+                      },
+                      'storeAndForwardConfiguration': {
+                        'timeToLiveSecs': 20
+                      },
+                      'authorizations': [ ],
+                      '$version': 2
+                    }";
+
+            string version_2_0 =
+                    @"{
+                      'schemaVersion': '2.0',
+                      'routes': {
+                        'route1': 'FROM /* INTO $upstream'
+                      },
+                      'storeAndForwardConfiguration': {
+                        'timeToLiveSecs': 20
+                      },
+                      '$version': 2
+                    }";
+
+            string version_2_0_1 =
+                    @"{
+                      'schemaVersion': '2.0.1',
+                      'routes': {
+                        'route1': 'FROM /* INTO $upstream'
+                      },
+                      'storeAndForwardConfiguration': {
+                        'timeToLiveSecs': 20
+                      },
+                      '$version': 2
+                    }";
+
+            string versionMismatch =
+                @"{
+                  'schemaVersion': '1.0',
+                  'routes': {
+                    'route1': {
+                      'route': 'FROM /* INTO $upstream',
+                      'priority': 1,
+                      'timeToLiveSecs': 7200
+                    }
+                  },
+                  'storeAndForwardConfiguration': {
+                    'timeToLiveSecs': 20
+                  },
+                  '$version': 2
+                }";
+
+            yield return new object[] { noVersion, typeof(InvalidSchemaVersionException) };
+            yield return new object[] { version_0_1, typeof(InvalidSchemaVersionException) };
+            yield return new object[] { version_1, typeof(InvalidSchemaVersionException) };
+            yield return new object[] { version_1_0, null };
+            yield return new object[] { version_1_1, null };
+            yield return new object[] { version_1_1_0, null };
+            yield return new object[] { version_1_2, null };
+            yield return new object[] { version_2_0, typeof(InvalidSchemaVersionException) };
+            yield return new object[] { version_2_0_1, typeof(InvalidSchemaVersionException) };
+            yield return new object[] { versionMismatch, typeof(JsonReaderException) };
+        }
+
         [Fact]
         public void GetEdgeHubConfig_ValidInput_MappingIsCorrect()
         {
@@ -27,7 +198,7 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test.Config
             var routeFactory = new EdgeRouteFactory(new Mock<IEndpointFactory>().Object);
             var configParser = new EdgeHubConfigParser(routeFactory, validator.Object);
 
-            EdgeHubDesiredProperties properties = ConfigTestData.GetTestData();
+            EdgeHubDesiredProperties_1_2 properties = ConfigTestData.GetTestData();
 
             // act
             EdgeHubConfig result = configParser.GetEdgeHubConfig(properties);
@@ -103,9 +274,9 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test.Config
             };
 
             var brokerProperties = new BrokerProperties(new BridgeConfig(), authzProperties);
-            var properties = new EdgeHubDesiredProperties(
+            var properties = new EdgeHubDesiredProperties_1_2(
                 "1.2.0",
-                new Dictionary<string, RouteConfiguration>(),
+                new Dictionary<string, RouteSpec>(),
                 new StoreAndForwardConfiguration(100),
                 brokerProperties);
 
@@ -133,9 +304,9 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core.Test.Config
             };
 
             var brokerProperties = new BrokerProperties(bridgeConfig, new AuthorizationProperties());
-            var properties = new EdgeHubDesiredProperties(
+            var properties = new EdgeHubDesiredProperties_1_2(
                 "1.2.0",
-                new Dictionary<string, RouteConfiguration>(),
+                new Dictionary<string, RouteSpec>(),
                 new StoreAndForwardConfiguration(100),
                 brokerProperties);
 
