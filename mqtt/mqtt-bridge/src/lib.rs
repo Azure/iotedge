ab1bbdce0f1d91807d4d11d054040bafcb24cfb7
Andrew Smith <als5ev@virginia.edu>
2021-01-05 00:36:59 +0000

    ignore await holding refcell ref until they fix false positive

--- mqtt/mqtt-bridge/src/lib.rs
+++ mqtt/mqtt-bridge/src/lib.rs
@@ -8,7 +8,8 @@
     clippy::use_self,
     clippy::match_same_arms,
     clippy::must_use_candidate,
-    clippy::missing_errors_doc
+    clippy::missing_errors_doc,
+    clippy::clippy::await_holding_refcell_ref // https://github.com/rust-lang/rust-clippy/issues/6353
 )]
 
 mod bridge;
