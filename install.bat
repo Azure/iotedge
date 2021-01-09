810e7d29764e2936df7a13943132701b891aafe0
Hugues Bouvier <33438817+huguesBouvier@users.noreply.github.com>
2021-01-08 17:23:24 -0800

    test

--- install.bat
+++
@@ -1,5 +0,0 @@
-ECHO OFF
-dotnet build
-dotnet publish
-docker build --no-cache -t huguesb.azurecr.io/agent_test:16 --file ./edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Service/bin/Debug/netcoreapp3.1/publish/docker/linux/amd64/Dockerfile --build-arg EXE_DIR=. ./edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Service/bin/Debug/netcoreapp3.1/publish
-docker push huguesb.azurecr.io/agent_test:16
