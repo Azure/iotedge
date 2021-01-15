b822d233ab214d13d26be549fcd7800c09d549a5
Andrew Smith <als5ev@virginia.edu>
2021-01-15 22:10:53 +0000

    put errors in error message

--- test/modules/GenericMqttTester/src/lib.rs
+++ test/modules/GenericMqttTester/src/lib.rs
@@ -38,16 +38,16 @@ impl ShutdownHandle {
 
 #[derive(Debug, thiserror::Error)]
 pub enum MessageTesterError {
-    #[error("could not parse expected env vars")]
+    #[error("could not parse expected env vars: {0:?}")]
     ParseEnvironment(#[from] VarError),
 
-    #[error("could not get client publish handle")]
+    #[error("could not get client publish handle: {0:?}")]
     PublishHandle(#[source] PublishError),
 
-    #[error("failed to publish")]
+    #[error("failed to publish: {0:?}")]
     Publish(#[source] PublishError),
 
-    #[error("could not send shutdown signal")]
+    #[error("could not send shutdown signal: {0:?}")]
     SendShutdownSignal(#[from] SendError<()>),
 
     #[error("failure listening for shutdown")]
@@ -56,28 +56,28 @@ pub enum MessageTesterError {
     #[error("failure listening for incoming publications")]
     ListenForIncomingPublications,
 
-    #[error("thread panicked while waiting for shutdown")]
+    #[error("thread panicked while waiting for shutdown: {0:?}")]
     WaitForShutdown(#[from] JoinError),
 
-    #[error("could not send publication to message handler")]
+    #[error("could not send publication to message handler: {0:?}")]
     SendPublicationInChannel(#[from] SendError<ReceivedPublication>),
 
-    #[error("failure getting client subscription handle")]
+    #[error("failure getting client subscription handle: {0:?}")]
     UpdateSubscriptionHandle(#[source] UpdateSubscriptionError),
 
-    #[error("failure making client subscriptions")]
+    #[error("failure making client subscriptions: {0:?}")]
     UpdateSubscription(#[source] UpdateSubscriptionError),
 
-    #[error("failure creating stream to listen for unix signal")]
+    #[error("failure creating stream to listen for unix signal: {0:?}")]
     CreateUnixSignalListener(#[from] io::Error),
 
     #[error("received unexpected value from unix signal listener")]
     ListenForUnixSignal,
 
-    #[error("failed to parse sequence number from publication")]
+    #[error("failed to parse sequence number from publication: {0:?}")]
     DeserializeMessage(#[from] serde_json::Error),
 
-    #[error("failed to report test result")]
+    #[error("failed to report test result: {0:?}")]
     ReportResult(#[from] ReportResultError),
 
     #[error("received rejected subscription: {0}")]
