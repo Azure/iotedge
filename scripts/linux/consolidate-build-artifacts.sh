adca81441d3e071348ccb726aa72a141f332bdc3
Andrew Smith <als5ev@virginia.edu>
2021-01-22 05:32:51 +0000

    format shell script'

--- scripts/linux/consolidate-build-artifacts.sh
+++ scripts/linux/consolidate-build-artifacts.sh
@@ -22,19 +22,17 @@ SCRIPT_NAME=$(basename "$0")
 ###############################################################################
 # Print usage information pertaining to this script and exit
 ###############################################################################
-function usage()
-{
+function usage() {
     echo "$SCRIPT_NAME [options]"
     echo ""
     echo "options"
     echo " --artifact-name           The name of the artifact to consolidate for which to consolidate the artifacts."
     echo " --build-binaries-dir      The name of the artifact to consolidate for which to consolidate the artifacts."
     echo " -h, --help                Print this help and exit."
-    exit 1;
+    exit 1
 }
 
-function print_help_and_exit()
-{
+function print_help_and_exit() {
     echo "Run $SCRIPT_NAME --help for more information."
     exit 1
 }
@@ -42,23 +40,21 @@ function print_help_and_exit()
 ###############################################################################
 # Obtain and validate the options supported by this script
 ###############################################################################
-function process_args()
-{
+function process_args() {
     save_next_arg=0
-    for arg in "$@"
-    do
+    for arg in "$@"; do
         if [ ${save_next_arg} -eq 1 ]; then
             ARTIFACT_NAME=$arg
             save_next_arg=0
-        if [ ${save_next_arg} -eq 2 ]; then
+        elif [ ${save_next_arg} -eq 2 ]; then
             BUILD_BINARIESDIRECTORY=$arg
             save_next_arg=0
         else
             case "$arg" in
-                "-h" | "--help" ) usage;;
-                "--artifact-name" ) save_next_arg=1;;
-                "--build-binaries-dir" ) save_next_arg=2;;
-                * ) usage;;
+            "-h" | "--help") usage ;;
+            "--artifact-name") save_next_arg=1 ;;
+            "--build-binaries-dir") save_next_arg=2 ;;
+            *) usage ;;
             esac
         fi
     done
@@ -73,56 +69,56 @@ TARGET_ARM64V8="aarch64-unknown-linux-gnu"
 TARGET_AMD64_MUSL="x86_64-unknown-linux-musl"
 
 case "$ARTIFACT_NAME" in
-    generic-mqtt-tester)
-        ARTIFACTS_SOURCE="test/modules/$ARTIFACT_NAME"
-        ARTIFACTS_DEST="${BUILD_BINARIESDIRECTORY}/publish/$ARTIFACT_NAME"
+generic-mqtt-tester)
+    ARTIFACTS_SOURCE="test/modules/$ARTIFACT_NAME"
+    ARTIFACTS_DEST="${BUILD_BINARIESDIRECTORY}/publish/$ARTIFACT_NAME"
 
-        # make build context structure
-        mkdir "$ARTIFACTS_DEST"
-        mkdir -p "$ARTIFACTS_DEST/release" #non-musl amd64 does not have arch specific folder
-        mkdir -p "$ARTIFACTS_DEST/$TARGET_ARM32V7/release"
-        mkdir -p "$ARTIFACTS_DEST/$TARGET_ARM64V8/release"
+    # make build context structure
+    mkdir "$ARTIFACTS_DEST"
+    mkdir -p "$ARTIFACTS_DEST/release" #non-musl amd64 does not have arch specific folder
+    mkdir -p "$ARTIFACTS_DEST/$TARGET_ARM32V7/release"
+    mkdir -p "$ARTIFACTS_DEST/$TARGET_ARM64V8/release"
 
-        # copy artifacts
-        cp "$ARTIFACTS_SOURCE/target/release/$ARTIFACT_NAME" "$ARTIFACTS_DEST/release"
-        cp "$ARTIFACTS_SOURCE/target/$TARGET_ARM32V7/release/$ARTIFACT_NAME" "$ARTIFACTS_DEST/$TARGET_ARM32V7/release"
-        cp "$ARTIFACTS_SOURCE/target/$TARGET_ARM64V8/release/$ARTIFACT_NAME" "$ARTIFACTS_DEST/$TARGET_ARM64V8/release"
-        ;;
+    # copy artifacts
+    cp "$ARTIFACTS_SOURCE/target/release/$ARTIFACT_NAME" "$ARTIFACTS_DEST/release"
+    cp "$ARTIFACTS_SOURCE/target/$TARGET_ARM32V7/release/$ARTIFACT_NAME" "$ARTIFACTS_DEST/$TARGET_ARM32V7/release"
+    cp "$ARTIFACTS_SOURCE/target/$TARGET_ARM64V8/release/$ARTIFACT_NAME" "$ARTIFACTS_DEST/$TARGET_ARM64V8/release"
+    ;;
 
-    edge-hub)
-        EDGEHUB_ARTIFACTS_SOURCE="$BUILD_BINARIESDIRECTORY/publish/Microsoft.Azure.Devices.Edge.Hub.Service"
-        MQTT_ARTIFACTS_SOURCE="mqtt"
-        WATCHDOG_ARTIFACTS_SOURCE="edge-hub/watchdog"
-        ARTIFACTS_DEST="${BUILD_BINARIESDIRECTORY}/publish/$ARTIFACT_NAME"
+edge-hub)
+    EDGEHUB_ARTIFACTS_SOURCE="$BUILD_BINARIESDIRECTORY/publish/Microsoft.Azure.Devices.Edge.Hub.Service"
+    MQTT_ARTIFACTS_SOURCE="mqtt"
+    WATCHDOG_ARTIFACTS_SOURCE="edge-hub/watchdog"
+    ARTIFACTS_DEST="${BUILD_BINARIESDIRECTORY}/publish/$ARTIFACT_NAME"
 
-        # make build context structure
-        mkdir "$ARTIFACTS_DEST"
-        mkdir "$ARTIFACTS_DEST/mqtt"
-        mkdir -p "$ARTIFACTS_DEST/mqtt/$TARGET_AMD64_MUSL/release"
-        mkdir -p "$ARTIFACTS_DEST/mqtt/$TARGET_ARM32V7/release"
-        mkdir -p "$ARTIFACTS_DEST/mqtt/$TARGET_ARM64V8/release"
-        mkdir "$ARTIFACTS_DEST/watchdog"
-        mkdir -p "$ARTIFACTS_DEST/watchdog/$TARGET_AMD64_MUSL/release"
-        mkdir -p "$ARTIFACTS_DEST/watchdog/$TARGET_ARM32V7/release"
-        mkdir -p "$ARTIFACTS_DEST/watchdog/$TARGET_ARM64V8/release"
+    # make build context structure
+    mkdir "$ARTIFACTS_DEST"
+    mkdir "$ARTIFACTS_DEST/mqtt"
+    mkdir -p "$ARTIFACTS_DEST/mqtt/$TARGET_AMD64_MUSL/release"
+    mkdir -p "$ARTIFACTS_DEST/mqtt/$TARGET_ARM32V7/release"
+    mkdir -p "$ARTIFACTS_DEST/mqtt/$TARGET_ARM64V8/release"
+    mkdir "$ARTIFACTS_DEST/watchdog"
+    mkdir -p "$ARTIFACTS_DEST/watchdog/$TARGET_AMD64_MUSL/release"
+    mkdir -p "$ARTIFACTS_DEST/watchdog/$TARGET_ARM32V7/release"
+    mkdir -p "$ARTIFACTS_DEST/watchdog/$TARGET_ARM64V8/release"
 
-        # copy edgehub core artifacts
-        cp -r "$EDGEHUB_ARTIFACTS_SOURCE" "$ARTIFACTS_DEST"
+    # copy edgehub core artifacts
+    cp -r "$EDGEHUB_ARTIFACTS_SOURCE" "$ARTIFACTS_DEST"
 
-        # copy mqtt artifacts
-        cp "$MQTT_ARTIFACTS_SOURCE/target/$TARGET_AMD64_MUSL/release/mqttd" "$ARTIFACTS_DEST/mqtt/$TARGET_AMD64_MUSL/release"
-        cp "$MQTT_ARTIFACTS_SOURCE/target/$TARGET_ARM32V7/release/mqttd" "$ARTIFACTS_DEST/mqtt/$TARGET_ARM32V7/release"
-        cp "$MQTT_ARTIFACTS_SOURCE/target/$TARGET_ARM64V8/release/mqttd" "$ARTIFACTS_DEST/mqtt/$TARGET_ARM64V8/release"
-        cp "$MQTT_ARTIFACTS_SOURCE/contrib/edgehub/broker.json" "$ARTIFACTS_DEST/mqtt"
+    # copy mqtt artifacts
+    cp "$MQTT_ARTIFACTS_SOURCE/target/$TARGET_AMD64_MUSL/release/mqttd" "$ARTIFACTS_DEST/mqtt/$TARGET_AMD64_MUSL/release"
+    cp "$MQTT_ARTIFACTS_SOURCE/target/$TARGET_ARM32V7/release/mqttd" "$ARTIFACTS_DEST/mqtt/$TARGET_ARM32V7/release"
+    cp "$MQTT_ARTIFACTS_SOURCE/target/$TARGET_ARM64V8/release/mqttd" "$ARTIFACTS_DEST/mqtt/$TARGET_ARM64V8/release"
+    cp "$MQTT_ARTIFACTS_SOURCE/contrib/edgehub/broker.json" "$ARTIFACTS_DEST/mqtt"
 
-        # copy watchdog artifacts
-        cp "$WATCHDOG_ARTIFACTS_SOURCE/target/$TARGET_AMD64_MUSL/release/watchdog" "$ARTIFACTS_DEST/watchdog/$TARGET_AMD64_MUSL/release"
-        cp "$WATCHDOG_ARTIFACTS_SOURCE/target/$TARGET_ARM32V7/release/watchdog" "$ARTIFACTS_DEST/watchdog/$TARGET_ARM32V7/release"
-        cp "$WATCHDOG_ARTIFACTS_SOURCE/target/$TARGET_ARM64V8/release/watchdog" "$ARTIFACTS_DEST/watchdog/$TARGET_ARM64V8/release"
-        ;;
+    # copy watchdog artifacts
+    cp "$WATCHDOG_ARTIFACTS_SOURCE/target/$TARGET_AMD64_MUSL/release/watchdog" "$ARTIFACTS_DEST/watchdog/$TARGET_AMD64_MUSL/release"
+    cp "$WATCHDOG_ARTIFACTS_SOURCE/target/$TARGET_ARM32V7/release/watchdog" "$ARTIFACTS_DEST/watchdog/$TARGET_ARM32V7/release"
+    cp "$WATCHDOG_ARTIFACTS_SOURCE/target/$TARGET_ARM64V8/release/watchdog" "$ARTIFACTS_DEST/watchdog/$TARGET_ARM64V8/release"
+    ;;
 
-    *)
-        print_error "Invalid artifact name"
-        exit 1
-        ;;
+*)
+    print_error "Invalid artifact name"
+    exit 1
+    ;;
 esac
