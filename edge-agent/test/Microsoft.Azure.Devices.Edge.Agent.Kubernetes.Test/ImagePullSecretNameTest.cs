8d45a484d912028ecc539bc0f2e21cacf39b004a
David Robson <darobs@microsoft.com>
2021-01-28 19:48:50 -0800

    [k8s] Allow non standard ports for container registry. (#4208)
    * The conversion from the AuthConfig username and password
    to ImagePullName was allowing invalid characters to be set as the name.
    Specifically, valid server addresses can include port numbers and http/s
    prefixes.
    This change gets the only the server name and port (if port is set) and
    incorparates that into the image pull secret name. This will allow
    two registries on the same server name with different ports.
    A side-effect of this is that it will throw the exception in the command
    execution, which should be slightly easier to debug.
    
    * Implement for Rust as well.

--- edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Kubernetes.Test/ImagePullSecretNameTest.cs
+++ edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Kubernetes.Test/ImagePullSecretNameTest.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 namespace Microsoft.Azure.Devices.Edge.Agent.Kubernetes.Test
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using global::Docker.DotNet.Models;
     using Microsoft.Azure.Devices.Edge.Agent.Kubernetes.EdgeDeployment;
     using Microsoft.Azure.Devices.Edge.Util.Test.Common;
@@ -29,6 +31,88 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Kubernetes.Test
 
             Assert.False(name.Equals(null));
             Assert.False(name.Equals(new ImagePullSecretName("not a secret")));
+        }
+
+        public static IEnumerable<object[]> ValidServerAddresses()
+        {
+            (string ipsName,
+             AuthConfig auth
+            )[] data =
+            {
+                (
+                    "dockeruser-index.docker.io",
+                    new AuthConfig { Username = "dockeruser", Password = "password", ServerAddress = "https://index.docker.io/v1/" }
+                ),
+                (
+                    "dockeruser-a-registry.com-8000",
+                    new AuthConfig { Username = "dockeruser", Password = "password", ServerAddress = "http://a-registry.com:8000" }
+                ),
+                (
+                    "dockeruser-a-registry.com-8000",
+                    new AuthConfig { Username = "dockeruser", Password = "password", ServerAddress = "https://a-registry.com:8000" }
+                ),
+                (
+                    "dockeruser-a-registry.com-8000",
+                    new AuthConfig { Username = "dockeruser", Password = "password", ServerAddress = "http://a-registry.com:8000/v2/" }
+                ),
+                (
+                    "dockeruser-a-registry.com-8000",
+                    new AuthConfig { Username = "dockeruser", Password = "password", ServerAddress = "HTTP://a-registry.com:8000/v2" }
+                ),
+                (
+                    "dockeruser-docker.io",
+                    new AuthConfig { Username = "dockeruser", Password = "password", ServerAddress = "docker.io" }
+                ),
+                (
+                    "dockeruser-my-acr.io-8080",
+                    new AuthConfig { Username = "dockeruser", Password = "password", ServerAddress = "my-acr.io:8080" }
+                ),
+                (
+                    "dockeruser-any-acr-server.azurecr.io",
+                    new AuthConfig { Username = "dockeruser", Password = "password", ServerAddress = "any-acr-server.azurecr.io" }
+                ),
+                (
+                    "dockeruser-any-acr-server.azurecr.io-3030",
+                    new AuthConfig { Username = "dockeruser", Password = "password", ServerAddress = "any-acr-server.azurecr.io:3030" }
+                ),
+            };
+            return data.Select(
+                d => new object[]
+                {
+                    d.ipsName, d.auth
+                });
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidServerAddresses))]
+        [Unit]
+        public void CreatesValidImagePullNames(string ipsName, AuthConfig auth)
+        {
+            var name = ImagePullSecretName.Create(auth);
+
+            Assert.Equal(ipsName, name);
+        }
+
+        public static IEnumerable<object[]> InvalidServerAddresses()
+        {
+            AuthConfig[] data =
+            {
+                new AuthConfig { Username = "--", Password = "password", ServerAddress = "--" },
+                new AuthConfig { Username = "1111", Password = "password", ServerAddress = "2222" },
+            };
+            return data.Select(
+                d => new object[]
+                {
+                    d
+                });
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidServerAddresses))]
+        [Unit]
+        public void ThrowsOnInvalidImagePullNames(AuthConfig auth)
+        {
+            Assert.Throws<InvalidKubernetesNameException>(() => ImagePullSecretName.Create(auth));
         }
     }
 }
