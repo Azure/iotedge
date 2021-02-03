9a08617872b8e6b5546205f7b48999fb7e0cf422
Andrew Smith <als5ev@virginia.edu>
2021-02-03 21:08:52 +0000

    switch to repo path

--- scripts/linux/trcE2ETest.sh
+++ scripts/linux/trcE2ETest.sh
@@ -53,7 +53,7 @@ function usage() {
     echo ' -restartIntervalInMins                   Value for long haul specifying how often a random module will restart. If specified, then "desiredModulesToRestartCSV" must be specified as well.'
     echo ' -sendReportFrequency                     Value for long haul specifying how often TRC will send reports to LogAnalytics.'
     echo " -testMode                                Test mode for TestResultCoordinator to start up with correct settings. Value is either 'LongHaul' or 'Connectivity'."
-    echo " -checkoutPath                            Path of the checked-out iotedge repository for getting the deployment file."
+    echo " -repoPath                                Path of the checked-out iotedge repository for getting the deployment file."
     echo ' -cleanAll                                Do docker prune for containers, logs and volumes.'
     exit 1;
 }
@@ -162,7 +162,7 @@ function prepare_test_from_artifacts() {
     tar -C "$quickstart_working_folder" -xzf "$(get_artifact_file "$E2E_TEST_DIR" quickstart)"
 
     echo "Copy deployment artifact to $deployment_working_file"
-    cp "$CHECKOUT_PATH/e2e_deployment_files/$DEPLOYMENT_FILE_NAME" "$deployment_working_file"
+    cp "$REPO_PATH/e2e_deployment_files/$DEPLOYMENT_FILE_NAME" "$deployment_working_file"
 
     sed -i -e "s@<Architecture>@$image_architecture_label@g" "$deployment_working_file"
     sed -i -e "s/<Build.BuildNumber>/$ARTIFACT_IMAGE_BUILD_NUMBER/g" "$deployment_working_file"
@@ -473,7 +473,7 @@ function process_args() {
             TEST_MODE="$arg"
             saveNextArg=0;
         elif [ $saveNextArg -eq 48 ]; then
-            CHECKOUT_PATH="$arg"
+            REPO_PATH="$arg"
             saveNextArg=0;
         else
             case "$arg" in
@@ -525,7 +525,7 @@ function process_args() {
                 '-restartIntervalInMins' ) saveNextArg=45;;
                 '-sendReportFrequency' ) saveNextArg=46;;
                 '-testMode' ) saveNextArg=47;;
-                '-checkoutPath' ) saveNextArg=48;;
+                '-repoPath' ) saveNextArg=48;;
                 '-waitForTestComplete' ) WAIT_FOR_TEST_COMPLETE=1;;
                 '-cleanAll' ) CLEAN_ALL=1;;
 
@@ -552,7 +552,7 @@ function process_args() {
     [[ -z "$METRICS_UPLOAD_TARGET" ]] && { print_error 'Metrics upload target is required'; exit 1; }
     [[ -z "$STORAGE_ACCOUNT_CONNECTION_STRING" ]] && { print_error 'Storage account connection string is required'; exit 1; }
     [[ -z "$TEST_INFO" ]] && { print_error 'Test info is required'; exit 1; }
-    [[ -z "$CHECKOUT_PATH" ]] && { print_error 'Checkout path is required'; exit 1; }
+    [[ -z "$REPO_PATH" ]] && { print_error 'Checkout path is required'; exit 1; }
     [[ (-z "${TEST_NAME,,}") || ("${TEST_NAME,,}" != "${LONGHAUL_TEST_NAME,,}" && "${TEST_NAME,,}" != "${CONNECTIVITY_TEST_NAME,,}") ]] && { print_error 'Invalid test name'; exit 1; }
 
     echo 'Required parameters are provided'
