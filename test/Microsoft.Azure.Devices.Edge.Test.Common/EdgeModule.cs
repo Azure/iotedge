5a633bce646f4df45e12919aa9c462c258e3d877
dylanbronson <dybronso@microsoft.com>
2021-02-02 18:04:04 -0800

    adding logs

--- test/Microsoft.Azure.Devices.Edge.Test.Common/EdgeModule.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/EdgeModule.cs
@@ -141,6 +141,7 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common
                     Log.Information($"DRB - got twin: {twin.ToString()}");
                     Log.Information($"DRB - RP: {twin.Properties.Reported}");
                     Log.Information($"DRB - expected {expected.ToString()}");
+                    Log.Information($"DRB - expected {JsonConvert.ToString(expected)}");
                     return twin.Properties.Reported;
                 },
                 reported => JsonEquals((expected, "properties.reported"), (reported, string.Empty)),
