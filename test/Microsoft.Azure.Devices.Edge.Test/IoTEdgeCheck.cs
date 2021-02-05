89f6db2c705591ba0e98398fdad443f6e81ed8ec
dylanbronson <dybronso@microsoft.com>
2021-02-05 09:01:27 -0800

    Fixing iotedge check by adding auth before check call

--- test/Microsoft.Azure.Devices.Edge.Test/IoTEdgeCheck.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/IoTEdgeCheck.cs
@@ -3,6 +3,7 @@ namespace Microsoft.Azure.Devices.Edge.Test
 {
     using System;
     using System.Diagnostics;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Edge.Test.Helpers;
@@ -18,13 +19,41 @@ namespace Microsoft.Azure.Devices.Edge.Test
         public async Task IoTEdge_check()
         {
             CancellationToken token = this.TestToken;
-            // Need to deploy edgeHu or one check will fail
+            // Need to deploy edgeHub or one check will fail
             await this.runtime.DeployConfigurationAsync(token, Context.Current.NestedEdge);
 
             string diagnosticImageName = Context.Current
                 .DiagnosticsImage.Expect(() => new ArgumentException("Missing diagnostic image"));
 
-            var process = new System.Diagnostics.Process
+            // If we are in a nested configuration, we don't need to login to docker because the bottom layer
+            // will ask the upper layer for the image through ApiProxy, which will continue up the chain
+            // until the top layer, which has permissions, can get it, and send it back down.
+            // Non-nested configuration needs it because we are calling out to a new process, and this process
+            // needs auth
+            if (!Context.Current.NestedEdge)
+            {
+                var dockerLoginProcess = new System.Diagnostics.Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "sudo",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        ArgumentList = { "docker", "login", "--username", Context.Current.Registries.First().Username, "--password", Context.Current.Registries.First().Password, Context.Current.Registries.First().Address }
+                    }
+                };
+                dockerLoginProcess.Start();
+                await Task.Run(() =>
+                {
+                    while (!dockerLoginProcess.StandardOutput.EndOfStream)
+                    {
+                        string line = dockerLoginProcess.StandardOutput.ReadLine();
+                        Log.Information(line);
+                    }
+                });
+            }
+
+            var iotedgeCheckProcess = new System.Diagnostics.Process
             {
                 StartInfo = new ProcessStartInfo
                 {
@@ -35,12 +64,12 @@ namespace Microsoft.Azure.Devices.Edge.Test
                 }
             };
             string errors_number = string.Empty;
-            process.Start();
+            iotedgeCheckProcess.Start();
             await Task.Run(() =>
             {
-                while (!process.StandardOutput.EndOfStream)
+                while (!iotedgeCheckProcess.StandardOutput.EndOfStream)
                 {
-                    string line = process.StandardOutput.ReadLine();
+                    string line = iotedgeCheckProcess.StandardOutput.ReadLine();
                     Log.Information(line);
                     if (line.Contains("check(s) raised errors"))
                     {
