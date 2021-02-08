768a49e8467ea6b20343aa7fab1e481e9223731e
Robert Jang <robbaldwin95@gmail.com>
2021-02-07 19:54:58 -0800

    Make flush use duration

--- mqtt/mqtt-bridge/src/persist/waking_state/ring_buffer/flush.rs
+++ mqtt/mqtt-bridge/src/persist/waking_state/ring_buffer/flush.rs
@@ -1,27 +1,58 @@
-#[allow(dead_code)]
-#[derive(Clone, Copy, Debug)]
+use std::time::Duration;
+
+use serde::Deserialize;
+
+/// `RingBuffer` can be configured to flush at certain intervals:
+///
+/// `AfterEachWrite` - Every write to buffer will cause a flush. This gives the
+///                    most durability but will cause decrease in performance.
+///
+/// `AfterXWrites` - Similar to `AfterEachWrite` but  will instead only flush
+///                  after 'X' writes to the `RingBuffer`. This allows
+///                  sacrificing durability for some performance. It is also
+///                  important to note that if 'X' is 0 or 1, it is same as
+///                  `AfterEachWrite`.
+///
+/// `AfterXBytes` - If 'X' bytes are written then a flush will happen.
+///
+/// `AfterXTime` - This allows for configuring around intervals of time
+///                for flushing. This does not mean that every 'X' a
+///                flush is called regardless of data incoming. This is
+///                for the difference between data is incoming. So, if
+///                5 inserts occur and after 3 the 'X' elapse a
+///                flush will happen. If only 2 inserts occur and 'X'
+///                ms is not reached then no flush occurs.
+///
+/// `Off` - No explicit flush will be called. This does not guarantee that no
+///         flush will occur or that data will not be pushed to 'disk'. This is
+///         the most performant option, but also the least durable.
+///
+/// It is important to note that after the threshold is reached for `AfterX`...
+/// a new window for tracking begins.
+#[derive(Clone, Copy, Debug, PartialEq, Deserialize)]
+#[serde(rename_all = "lowercase")]
 pub enum FlushOptions {
     AfterEachWrite,
     AfterXWrites(usize),
     AfterXBytes(usize),
-    AfterXMilliseconds(usize),
+    AfterXTime(Duration),
     Off,
 }
 
-#[allow(dead_code)]
+/// A stateful object for tracking progress of any of the `AfterX`... options
+/// from `FlushOptions`.
 pub struct FlushState {
     pub writes: usize,
     pub bytes_written: usize,
-    pub millis_elapsed: usize,
+    pub millis_elapsed: Duration,
 }
 
-#[allow(dead_code)]
 impl FlushState {
     pub(crate) fn new() -> Self {
         Self {
             writes: usize::default(),
             bytes_written: usize::default(),
-            millis_elapsed: usize::default(),
+            millis_elapsed: Duration::default(),
         }
     }
 
@@ -29,19 +60,19 @@ impl FlushState {
         match flush_option {
             FlushOptions::AfterEachWrite => {}
             FlushOptions::AfterXWrites(_) => {
-                self.writes = 0;
+                self.writes = usize::default();
             }
             FlushOptions::AfterXBytes(_) => {
-                self.bytes_written = 0;
+                self.bytes_written = usize::default();
             }
-            FlushOptions::AfterXMilliseconds(_) => {
-                self.millis_elapsed = 0;
+            FlushOptions::AfterXTime(_) => {
+                self.millis_elapsed = Duration::default();
             }
             FlushOptions::Off => {}
         }
     }
 
-    pub(crate) fn update(&mut self, writes: usize, bytes_written: usize, millis_elapsed: usize) {
+    pub(crate) fn update(&mut self, writes: usize, bytes_written: usize, millis_elapsed: Duration) {
         self.bytes_written += bytes_written;
         self.millis_elapsed += millis_elapsed;
         self.writes += writes;
@@ -55,50 +86,50 @@ mod tests {
     #[test]
     fn it_updates_flush_state_on_update() {
         let mut fs = FlushState::new();
-        fs.update(1, 0, 0);
+        fs.update(1, 0, Duration::default());
         assert_eq!(fs.writes, 1);
         assert_eq!(fs.bytes_written, 0);
-        assert_eq!(fs.millis_elapsed, 0);
-        fs.update(0, 1, 0);
+        assert_eq!(fs.millis_elapsed, Duration::default());
+        fs.update(0, 1, Duration::default());
         assert_eq!(fs.writes, 1);
         assert_eq!(fs.bytes_written, 1);
-        assert_eq!(fs.millis_elapsed, 0);
-        fs.update(0, 0, 1);
+        assert_eq!(fs.millis_elapsed, Duration::default());
+        fs.update(0, 0, Duration::from_millis(1));
         assert_eq!(fs.writes, 1);
         assert_eq!(fs.bytes_written, 1);
-        assert_eq!(fs.millis_elapsed, 1);
-        fs.update(1, 1, 1);
+        assert_eq!(fs.millis_elapsed, Duration::from_millis(1));
+        fs.update(1, 1, Duration::from_millis(1));
         assert_eq!(fs.writes, 2);
         assert_eq!(fs.bytes_written, 2);
-        assert_eq!(fs.millis_elapsed, 2);
+        assert_eq!(fs.millis_elapsed, Duration::from_millis(2));
     }
 
     #[test]
     fn it_resets_flush_state_on_reset() {
         let mut fs = FlushState::new();
-        fs.update(1, 1, 1);
+        fs.update(1, 1, Duration::from_millis(1));
         assert_eq!(fs.writes, 1);
         assert_eq!(fs.bytes_written, 1);
-        assert_eq!(fs.millis_elapsed, 1);
+        assert_eq!(fs.millis_elapsed, Duration::from_millis(1));
         fs.reset(&FlushOptions::AfterEachWrite);
         assert_eq!(fs.writes, 1);
         assert_eq!(fs.bytes_written, 1);
-        assert_eq!(fs.millis_elapsed, 1);
+        assert_eq!(fs.millis_elapsed, Duration::from_millis(1));
         fs.reset(&FlushOptions::Off);
         assert_eq!(fs.writes, 1);
         assert_eq!(fs.bytes_written, 1);
-        assert_eq!(fs.millis_elapsed, 1);
+        assert_eq!(fs.millis_elapsed, Duration::from_millis(1));
         fs.reset(&FlushOptions::AfterXWrites(0));
         assert_eq!(fs.writes, 0);
         assert_eq!(fs.bytes_written, 1);
-        assert_eq!(fs.millis_elapsed, 1);
+        assert_eq!(fs.millis_elapsed, Duration::from_millis(1));
         fs.reset(&FlushOptions::AfterXBytes(0));
         assert_eq!(fs.writes, 0);
         assert_eq!(fs.bytes_written, 0);
-        assert_eq!(fs.millis_elapsed, 1);
-        fs.reset(&FlushOptions::AfterXMilliseconds(0));
+        assert_eq!(fs.millis_elapsed, Duration::from_millis(1));
+        fs.reset(&FlushOptions::AfterXTime(Duration::default()));
         assert_eq!(fs.writes, 0);
         assert_eq!(fs.bytes_written, 0);
-        assert_eq!(fs.millis_elapsed, 0);
+        assert_eq!(fs.millis_elapsed, Duration::default());
     }
 }
