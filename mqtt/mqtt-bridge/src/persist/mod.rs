7c55f39111c262d29a0abd9e5fc0adee502016c6
Robert Jang <robbaldwin95@gmail.com>
2021-02-07 19:54:23 -0800

    Fix usings style

--- mqtt/mqtt-bridge/src/persist/mod.rs
+++ mqtt/mqtt-bridge/src/persist/mod.rs
@@ -3,12 +3,12 @@ mod publication_store;
 mod waking_state;
 
 use bincode::Error as BincodeError;
+use serde::{Deserialize, Serialize};
+
 pub use loader::MessageLoader;
 pub use publication_store::PublicationStore;
-use serde::{Deserialize, Serialize};
+use waking_state::ring_buffer::error::RingBufferError;
 pub use waking_state::{memory::WakingMemoryStore, StreamWakeableState};
-
-use self::waking_state::ring_buffer::error::RingBufferError;
 
 /// Keys used in persistence.
 /// Ordered by offset
