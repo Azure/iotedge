8adffc6b3237b6b34e4db3ed01d700d71f725873
Arnav Singh <arsing@microsoft.com>
2021-02-09 14:31:56 -0800

    Make Edge Agent's Prometheus parser more in line with upstream's spec.
    The previous regex did not allow whitespace between the tokens or
    trailing commas after the last label value. It was also more permissive
    with regard to allowed characters in metric names, label names and label values.
    
    The regex also disallowed metrics with their own timestamps. With this change,
    the timestamp is used if present, otherwise it defaults to the current time
    as before.
    
    The one visible change for metrics that already parsed successfully is that
    escapes in the tag value are now evaluated by the parser, so that the published
    metrics do not retain the unnecessary double-escaping.
    
    Fixes #4361

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Diagnostics/util/PrometheusMetricsParser.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Diagnostics/util/PrometheusMetricsParser.cs
@@ -13,15 +13,54 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Diagnostics.Util
     public static class PrometheusMetricsParser
     {
         // Extracts data from prometheus format. Read about prometheus format here:
-        // https://prometheus.io/docs/concepts/data_model/
+        //
+        // - https://prometheus.io/docs/concepts/data_model/
+        // - https://prometheus.io/docs/instrumenting/exposition_formats/
+        // - https://github.com/prometheus/common/blob/master/expfmt/text_parse.go
+        //
         // Example:
-        // edgeagent_total_disk_read_bytes{iothub="lefitche-hub-3.azure-devices.net",edge_device="device4",instance_number="1",module="edgeHub"} 32908854
+        //
+        //     edgeagent_total_disk_read_bytes{iothub="lefitche-hub-3.azure-devices.net",edge_device="device4",instance_number="1",module="edgeHub"} 32908854
+
+        private const String MetricName = @"(?<metricname>[a-zA-Z_:][a-zA-Z_0-9:]*)";
+        private const String MetricValue = @"(?<metricvalue>[\d\.eE+-]+|NaN)";
+        private const String MetricTimestamp = @"(?<metrictimestamp>[\d]+)";
+        private const String LabelName = @"(?<labelname>[a-zA-Z_][a-zA-Z_0-9]*)";
+        private const String LabelValue = @"(?<labelvalue>(?:[^\\""]|\\""|\\\\|\\n)*)";
+        private const String Whitespace = @"[ \t]";
+        private static readonly String Label = $"{LabelName}{Whitespace}*={Whitespace}*\"{LabelValue}\"{Whitespace}*";
         private static readonly Regex PrometheusSchemaRegex = new Regex(
-            @"^(?<metricname>[^#\{\}]+)" + // metric name. The prometheus spec says metric names should match the regex [a-zA-Z_:][a-zA-Z0-9_:]*, but it doesn't hurt to be more permissive here
-            @"(\{(?:(?:(?<tagname>[^="",]+)=(?:\""(?<tagvalue>(?:\\.|[^\\""])*)\"")" + // first tag (name="value" pair)
-            @"(?:,(?<tagname>[^="",]+)=(?:\""(?<tagvalue>(?:\\.|[^\\""])*)\""))*))?\})?\s" + // all following tags
-            @"(?<metricvalue>[\d\.eE+-]+|NaN)$", // metric value (digits and .eE+-)
-            RegexOptions.Compiled);
+            @"^" +
+                MetricName +
+                $"{Whitespace}*" +
+                @"(?:" +
+                    @"\{" +
+                    $"{Whitespace}*" +
+                    @"(?:" +
+                        Label +
+                        @"(?:" +
+                            @"," +
+                            $"{Whitespace}*" +
+                            Label +
+                        @")*" +
+                        @"(?:" +
+                            @"," +
+                            $"{Whitespace}*" +
+                        @")?" +
+                    @")?" +
+                    @"\}" +
+                    $"{Whitespace}*" +
+                @")?" +
+                MetricValue +
+                @"(?:" +
+                    $"{Whitespace}+" +
+                    MetricTimestamp +
+                @")?" +
+            @"$",
+            RegexOptions.Compiled
+        );
+
+        private static readonly DateTime MetricTimestampEpoch = DateTime.UnixEpoch;
 
         public static IEnumerable<Metric> ParseMetrics(DateTime timeGeneratedUtc, string prometheusMessage)
         {
@@ -29,7 +68,7 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Diagnostics.Util
             using (StringReader sr = new StringReader(prometheusMessage))
             {
                 string line;
-                while ((line = sr.ReadLine()?.Trim()) != null)
+                while ((line = sr.ReadLine()) != null)
                 {
                     if (line.Length == 0 || line[0] == '#')
                     {
@@ -37,27 +76,71 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Diagnostics.Util
                     }
 
                     Match match = PrometheusSchemaRegex.Match(line);
-                    if (match.Success)
+                    if (!match.Success)
                     {
-                        double metricValue;
-                        if (!double.TryParse(match.Groups["metricvalue"]?.Value, out metricValue))
+                        continue;
+                    }
+
+                    double metricValue;
+                    if (!double.TryParse(match.Groups["metricvalue"]?.Value, out metricValue))
+                    {
+                        continue;
+                    }
+
+                    string metricName = match.Groups["metricname"]?.Value ?? string.Empty;
+                    var tagNames = (match.Groups["labelname"].Captures as IEnumerable<Capture>).Select(c => c.Value);
+                    var tagValues =
+                        (match.Groups["labelvalue"].Captures as IEnumerable<Capture>)
+                        .Select(value =>
                         {
+                            // Evaluate escaped characters
+                            string result = "";
+                            bool escaped = false;
+                            foreach (char c in value.Value)
+                            {
+                                if (escaped)
+                                {
+                                    if (c == 'n')
+                                    {
+                                        result += "\n";
+                                    }
+                                    else
+                                    {
+                                        result += c;
+                                    }
+
+                                    escaped = false;
+                                }
+                                else if (c == '\\')
+                                {
+                                    escaped = true;
+                                }
+                                else
+                                {
+                                    result += c;
+                                }
+                            }
+                            return result;
+                        });
+
+                    Dictionary<string, string> tags = tagNames.Zip(tagValues, (k, v) => new { k, v })
+                        .ToDictionary(x => x.k, x => x.v);
+
+                    string metricTimestampString = match.Groups["metrictimestamp"]?.Value;
+                    if (metricTimestampString.Length != 0) {
+                        long metricTimestamp;
+                        if (!long.TryParse(metricTimestampString, out metricTimestamp)) {
                             continue;
                         }
 
-                        string metricName = match.Groups["metricname"]?.Value ?? string.Empty;
-                        var tagNames = (match.Groups["tagname"].Captures as IEnumerable<Capture>).Select(c => c.Value);
-                        var tagValues = (match.Groups["tagvalue"].Captures as IEnumerable<Capture>).Select(c => c.Value);
-
-                        Dictionary<string, string> tags = tagNames.Zip(tagValues, (k, v) => new { k, v })
-                            .ToDictionary(x => x.k, x => x.v);
-
-                        yield return new Metric(
-                            timeGeneratedUtc,
-                            metricName,
-                            metricValue,
-                            tags);
+                        timeGeneratedUtc = MetricTimestampEpoch + TimeSpan.FromMilliseconds(metricTimestamp);
                     }
+
+                    yield return new Metric(
+                        timeGeneratedUtc,
+                        metricName,
+                        metricValue,
+                        tags);
                 }
             }
         }
