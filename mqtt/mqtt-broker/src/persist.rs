91655a3508729c05ee977674db5d67bf059468da
Vadim Kovalyov <vakavali@microsoft.com>
2021-02-12 12:43:04 -0700

    Rust clippy fix (#4163) (#4409)
    Fixing clippy
    
    This is something that I want to backport to 1.2 to ease the burden of merging later...

--- mqtt/mqtt-broker/src/persist.rs
+++ mqtt/mqtt-broker/src/persist.rs
@@ -8,7 +8,6 @@ use std::{
     error::Error as StdError,
     fs::{self, OpenOptions},
     io::{Read, Write},
-    iter::FromIterator,
     path::PathBuf,
 };
 
@@ -375,8 +374,8 @@ where
 {
     let payloads = HashMap::<u64, Vec<u8>>::deserialize(deserializer)?
         .into_iter()
-        .map(|(k, v)| (k, Bytes::from(v)));
-    let payloads = HashMap::from_iter(payloads);
+        .map(|(k, v)| (k, Bytes::from(v)))
+        .collect();
     Ok(payloads)
 }
 
