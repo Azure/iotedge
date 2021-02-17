2058aec9d495823bb7bbcca7609f25a8f2def8f7
Damon Barry <damonb@microsoft.com>
2021-02-17 12:17:50 -0800

    Update xenial reference to bionic

--- scripts/linux/generic-rust/install.sh
+++ scripts/linux/generic-rust/install.sh
@@ -90,7 +90,7 @@ sudo apt-get install -y \
     dh-systemd \
     valgrind
 sudo apt-get remove --yes libssl-dev
-sudo apt-get install --yes --target-release xenial-updates libssl-dev
+sudo apt-get install --yes --target-release bionic-updates libssl-dev
 
 if [[ -n "$ARM_PACKAGE" ]]; then
     # armhf cross tools for packaging
