19b26a153fbf853e3ad109059645cad6e4706317
David Lu <davilu@microsoft.com>
2021-02-19 11:07:28 -0800

    adding env variable to enabled new code path

--- edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.E2E.Test/DependencyManager.cs
+++ edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.E2E.Test/DependencyManager.cs
@@ -201,7 +201,7 @@ namespace Microsoft.Azure.Devices.Edge.Hub.E2E.Test
                     checkEntireQueueOnCleanup,
                     experimentalFeatures,
                     true,
-                    false));
+                    true));
 
             builder.RegisterModule(new HttpModule());
             builder.RegisterModule(new MqttModule(mqttSettingsConfiguration.Object, topics, this.serverCertificate, false, false, false, this.sslProtocols));
