4fdf33f7e53824925f7a46ea687f57bcaf6977b3
Andrew Smith <als5ev@virginia.edu>
2021-02-20 18:07:31 +0000

    add comments for filter

--- test/modules/TestResultCoordinator/Reports/TestResultFilter.cs
+++ test/modules/TestResultCoordinator/Reports/TestResultFilter.cs
@@ -35,14 +35,16 @@ namespace TestResultCoordinator
 
             List<TestOperationResult> expectedResults = await expectedTestResultsEnumerable.ToListAsync();
             List<TestOperationResult> actualResults = await actualTestResultsEnumerable.ToListAsync();
-            DateTime startIgnoringAt = DateTime.UtcNow.Subtract(unmatchedResultTolerance);
-
             List<TestOperationResult> expectedResultsOutput = new List<TestOperationResult>();
             List<TestOperationResult> actualResultsOutput = new List<TestOperationResult>();
+
+            DateTime startIgnoringAt = DateTime.UtcNow.Subtract(unmatchedResultTolerance);
+
             int expectedResultCounter = 0;
             int actualResultCounter = 0;
             while (expectedResultCounter < expectedResults.Count && expectedResults[expectedResultCounter].CreatedAt < startIgnoringAt)
             {
+                // add the expected result to output since it is within the ignore threshold
                 TestOperationResult expectedResult = expectedResults[expectedResultCounter];
                 expectedResultsOutput.Add(expectedResult);
                 expectedResultCounter += 1;
@@ -53,10 +55,11 @@ namespace TestResultCoordinator
                     bool doResultsMatch = this.comparer.Matches(expectedResult, actualResult);
                     if (doResultsMatch)
                     {
+                        // add the matching actual result to output regardless if it is within the ignore threshold
                         actualResultsOutput.Add(actualResult);
                         actualResultCounter += 1;
 
-                        // get all duplicate actual results
+                        // get all duplicate actual results regardless if it is within the ignore threshold
                         while (actualResultCounter < actualResults.Count && this.comparer.Matches(actualResult, actualResults[actualResultCounter]))
                         {
                             actualResultsOutput.Add(actualResults[actualResultCounter]);
@@ -66,6 +69,7 @@ namespace TestResultCoordinator
                 }
             }
 
+            // get all other actual results within the ignore threshold that have no matches 
             while (actualResultCounter < actualResults.Count && actualResults[actualResultCounter].CreatedAt < startIgnoringAt)
             {
                 actualResultsOutput.Add(actualResults[actualResultCounter]);
