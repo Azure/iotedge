30ca550a402579048a437dd18dd26ba3c3366a11
Daniel Prilik <daprilik@microsoft.com>
2021-02-22 16:48:10 -0500

    undo local lenient lints

--- edgelet/iotedge/src/lib.rs
+++ edgelet/iotedge/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-// #![deny(rust_2018_idioms, warnings)]
-// #![deny(clippy::all, clippy::pedantic)]
+#![deny(rust_2018_idioms, warnings)]
+#![deny(clippy::all, clippy::pedantic)]
 #![allow(
     clippy::default_trait_access,
     clippy::let_and_return,
