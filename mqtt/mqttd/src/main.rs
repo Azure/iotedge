b3891296114fa9b2fe3a59bb1284ae97ec8321e5
Noel Campbell <noelcampbell@microsoft.com>
2021-02-24 22:17:25 -0500

    Added opentelemetry instrumentation to MQTT broker for a single metric: mqtt.broker.client.messages.received

--- mqtt/mqttd/src/main.rs
+++ mqtt/mqttd/src/main.rs
@@ -6,9 +6,36 @@ use clap::{crate_description, crate_name, crate_version, App, Arg};
 
 use mqttd::{app, tracing};
 
+// TODO: Move to a separate module or file
+use futures::stream::Stream;
+use futures::StreamExt;
+use opentelemetry::metrics;
+use opentelemetry::sdk::metrics::{selectors, PushController};
+use opentelemetry_otlp::ExporterConfig;
+use std::time::Duration;
+
+fn delayed_interval(duration: Duration) -> impl Stream<Item = tokio::time::Instant> {
+    // opentelemetry::util::tokio_interval_stream(duration).skip(1)
+    tokio::time::interval(duration).skip(1)
+}
+
+fn init_otlp_metrics_pipeline() -> metrics::Result<PushController> {
+    let export_config = ExporterConfig {
+        endpoint: "http://localhost:4317".to_string(),
+        ..ExporterConfig::default()
+    };
+    opentelemetry_otlp::new_metrics_pipeline(tokio::spawn, delayed_interval)
+        .with_export_config(export_config)
+        .with_aggregator_selector(selectors::simple::Selector::Exact)
+        .build()
+}
+// END TODO
+
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing::init();
+
+    init_otlp_metrics_pipeline()?;
 
     let config_path = create_app()
         .get_matches()
