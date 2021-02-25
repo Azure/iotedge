3b7af90370bcc1bf36d23b0ce750321808dde6eb
dylanbronson <55515325+dylanbronson@users.noreply.github.com>
2021-02-25 13:30:59 -0800

    Improve DirectMethod Reporting for long haul (#4482)
    Previously, the DirectMethod report only reports 200's, 0's, and unknowns.
    The 0's are a known problem having to do with edgeHub restarts.
    Unknowns were its own bucket.
    This change buckets status codes together so we know exactly which (and how many) codes we got

--- test/modules/TestResultCoordinator/Reports/DirectMethod/LongHaul/DirectMethodLongHaulReport.cs
+++ test/modules/TestResultCoordinator/Reports/DirectMethod/LongHaul/DirectMethodLongHaulReport.cs
@@ -2,6 +2,9 @@
 namespace TestResultCoordinator.Reports.DirectMethod.LongHaul
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
     using Microsoft.Azure.Devices.Edge.Util;
 
     class DirectMethodLongHaulReport : TestResultReportBase
@@ -12,10 +15,10 @@ namespace TestResultCoordinator.Reports.DirectMethod.LongHaul
             string senderSource,
             Option<string> receiverSource,
             string resultType,
-            ulong senderSuccesses,
-            Option<ulong> receiverSuccesses,
-            ulong statusCodeZero,
-            ulong unknown)
+            long senderSuccesses,
+            Option<long> receiverSuccesses,
+            long statusCodeZero,
+            Dictionary<HttpStatusCode, long> other)
             : base(testDescription, trackingId, resultType)
         {
             if (receiverSource.HasValue ^ receiverSuccesses.HasValue)
@@ -28,15 +31,15 @@ namespace TestResultCoordinator.Reports.DirectMethod.LongHaul
             this.SenderSuccesses = senderSuccesses;
             this.ReceiverSuccesses = receiverSuccesses;
             this.StatusCodeZero = statusCodeZero;
-            this.Unknown = unknown;
+            this.Other = other;
         }
 
         public string SenderSource { get; }
         public Option<string> ReceiverSource { get; }
-        public ulong SenderSuccesses { get; }
-        public Option<ulong> ReceiverSuccesses { get; }
-        public ulong StatusCodeZero { get; }
-        public ulong Unknown { get; }
+        public long SenderSuccesses { get; }
+        public Option<long> ReceiverSuccesses { get; }
+        public long StatusCodeZero { get; }
+        public Dictionary<HttpStatusCode, long> Other { get; }
 
         public override string Title => this.ReceiverSource.HasValue ?
             $"DirectMethod LongHaul Report for [{this.SenderSource}] and [{this.ReceiverSource.OrDefault()}] ({this.ResultType})" : $"DirectMethod Report for [{this.SenderSource}] ({this.ResultType})";
@@ -45,9 +48,9 @@ namespace TestResultCoordinator.Reports.DirectMethod.LongHaul
 
         public bool IsPassedHelper()
         {
-            if (this.Unknown > 0)
+            if (this.Other.Sum(x => x.Value) > 0)
             {
-                // fail if we find any unknowns (anything that's not a 200 or a 0 - most notably, 500's)
+                // fail if we find anything that is not a 200 or 0 (most notably, 500's)
                 return false;
             }
 
@@ -56,7 +59,7 @@ namespace TestResultCoordinator.Reports.DirectMethod.LongHaul
             // Github issue: https://github.com/Azure/iotedge/issues/681
             // We expect to get this status sometimes because of edgehub restarts, but if we receive too many we should fail the tests.
             // TODO: When the SDK allows edgehub to de-register from subscriptions and we make the fix in edgehub, then we can fail tests for any status code 0.
-            ulong allStatusCount = this.SenderSuccesses + this.StatusCodeZero + this.Unknown;
+            long allStatusCount = this.SenderSuccesses + this.StatusCodeZero + this.Other.Sum(x => x.Value);
             bool statusCodeZeroBelowThreshold = (this.StatusCodeZero == 0) || (this.StatusCodeZero < ((double)allStatusCount / 100));
 
             // Pass if status code zero is below the threshold, and sender and receiver got same amount of successess (or receiver has no results)
