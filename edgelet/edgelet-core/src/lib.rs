16cc6184ec2e816ca5e8543fdb7825d3fcb81c18
Rajeev Massand <massand@gmail.com>
2021-03-04 16:56:16 -0800

    Ignore validity in cert API requests (#4532)
    Our current logic within `iotedge config import` discards the `auto_generated_edge_ca_expiry_days`. We need to respect the old value from the config.yaml during import. This change also ignores the module certificate validity information, since this is already determined by CS policy, (which is also set during `iotedge config` as well).
    
    Changes:
    - Ignore validity in cert API requests
    - Add support for old quickstart default expiration
    - Update test and template TOMLs

--- edgelet/edgelet-core/src/lib.rs
+++ edgelet/edgelet-core/src/lib.rs
@@ -53,9 +53,6 @@ pub use settings::{
 pub use virtualization::is_virtualized_env;
 pub use workload::WorkloadConfig;
 
-/// This is the default auto generated certificate life
-pub const DEFAULT_AUTO_GENERATED_CA_LIFETIME_DAYS: u16 = 90;
-
 lazy_static! {
     static ref VERSION: &'static str =
         option_env!("VERSION").unwrap_or_else(|| include_str!("../../version.txt").trim());
