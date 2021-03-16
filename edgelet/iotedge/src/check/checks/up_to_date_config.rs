fb50281f142e3db50606324dfe9261b3521aec62
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2021-03-16 14:05:04 -0700

    Use shared aziotctl_common check function

--- edgelet/iotedge/src/check/checks/up_to_date_config.rs
+++ edgelet/iotedge/src/check/checks/up_to_date_config.rs
@@ -1,7 +1,7 @@
-use std::fs;
-use std::path::Path;
+use failure::{self, Context};
 
-use failure::{self, Context, Fail};
+use aziotctl_common::check_last_modified::check_last_modified;
+use aziotctl_common::check_last_modified::CheckResult as InternalCheckResult;
 
 use crate::check::{checker::Checker, Check, CheckResult};
 
@@ -25,72 +25,15 @@ impl Checker for UpToDateConfig {
 
 impl UpToDateConfig {
     fn inner_execute(_check: &mut Check) -> Result<CheckResult, failure::Error> {
-        let config = Path::new("/etc/aziot/config.toml");
-
-        if !config.exists() {
-            return Ok(CheckResult::Ignored);
-        }
-
-        let config_metadata = match fs::metadata(config) {
-            Ok(m) => m,
-            Err(err) => {
-                return Ok(CheckResult::Failed(
-                    err.context(format!("Failed to query metadata of {}", config.display()))
-                        .into(),
-                ))
+        let check_result = match check_last_modified(&["edged"]) {
+            InternalCheckResult::Ok => CheckResult::Ok,
+            InternalCheckResult::Ignored => CheckResult::Ignored,
+            InternalCheckResult::Warning(message) => {
+                CheckResult::Warning(Context::new(message).into())
             }
+            InternalCheckResult::Failed(error) => CheckResult::Failed(error.into()),
         };
 
-        let config_last_modified = config_metadata
-            .modified()
-            .expect("file metadata should contain valid last_modified");
-
-        for service in &["keyd", "certd", "identityd", "tpmd", "edged"] {
-            let service_config = format!("/etc/aziot/{}/config.d/00-super.toml", service);
-            let service_config = Path::new(&service_config);
-
-            if !service_config.exists() {
-                return Ok(CheckResult::Warning(
-                    Context::new(format!(
-                        "{} does not exist.\n\
-                        Did you run 'iotedge config apply'?",
-                        service_config.display()
-                    ))
-                    .into(),
-                ));
-            }
-
-            let service_config_metadata = match fs::metadata(service_config) {
-                Ok(m) => m,
-                Err(err) => {
-                    return Ok(CheckResult::Failed(
-                        err.context(format!(
-                            "Failed to query metadata of {}",
-                            service_config.display()
-                        ))
-                        .into(),
-                    ))
-                }
-            };
-
-            let service_config_last_modified = service_config_metadata
-                .modified()
-                .expect("file metadata should contain valid last_modified");
-
-            if config_last_modified > service_config_last_modified {
-                return Ok(CheckResult::Warning(
-                    Context::new(format!(
-                        "{} was modified after {}'s config\n\
-                        You must run 'iotedge config apply' to update {}'s config with the latest config.toml",
-                        config.display(),
-                        service,
-                        service
-                    ))
-                    .into(),
-                ));
-            }
-        }
-
-        Ok(CheckResult::Ok)
+        Ok(check_result)
     }
 }
