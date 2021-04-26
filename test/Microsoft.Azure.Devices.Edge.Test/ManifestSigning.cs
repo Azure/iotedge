055b059d09c6ba86830af7845c9cfbd1380ca78b
ggjjj <gayathrijeganmohan@gmail.com>
2021-04-26 08:12:10 -0700

    e2e setup changes

---
+++ test/Microsoft.Azure.Devices.Edge.Test/ManifestSigning.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft. All rights reserved.
+namespace Microsoft.Azure.Devices.Edge.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Edge.Test.Common;
+    using Microsoft.Azure.Devices.Edge.Test.Common.Certs;
+    using Microsoft.Azure.Devices.Edge.Test.Common.Config;
+    using Microsoft.Azure.Devices.Edge.Test.Helpers;
+    using Microsoft.Azure.Devices.Edge.Util;
+    using Microsoft.Azure.Devices.Edge.Util.Test.Common.NUnit;
+    using Microsoft.Azure.Devices.Shared;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+    using Newtonsoft.Json.Linq;
+    using NUnit.Framework;
+    using Serilog;
+
+    [EndToEnd]
+    public class ManifestSigning : SasManualProvisioningFixture
+    {
+    }
+}
