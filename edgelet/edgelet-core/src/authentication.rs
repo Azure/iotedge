6f905e776c06843198d13cee335517f9a85dc0b3
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2021-05-04 10:34:34 -0700

    Update edgelet-core to async

--- edgelet/edgelet-core/src/authentication.rs
+++ edgelet/edgelet-core/src/authentication.rs
@@ -1,13 +1,11 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-use futures::Future;
-
 use crate::AuthId;
 
+#[async_trait::async_trait]
 pub trait Authenticator {
     type Error;
     type Request;
-    type AuthenticateFuture: Future<Item = AuthId, Error = Self::Error> + Send;
 
-    fn authenticate(&self, req: &Self::Request) -> Self::AuthenticateFuture;
+    async fn authenticate(&self, req: &Self::Request) -> Result<AuthId, Self::Error>;
 }
--- edgelet/edgelet-core/src/lib.rs
+++ edgelet/edgelet-core/src/lib.rs
@@ -39,7 +39,7 @@ pub use crypto::{
 };
 pub use error::{Error, ErrorKind};
 pub use identity::{AuthType, Identity, IdentityManager, IdentityOperation, IdentitySpec};
-pub use logs::{Chunked, LogChunk, LogDecode};
+//pub use logs::{Chunked, LogChunk, LogDecode};
 pub use module::{
     DiskInfo, ImagePullPolicy, LogOptions, LogTail, MakeModuleRuntime, Module, ModuleOperation,
     ModuleRegistry, ModuleRuntime, ModuleRuntimeErrorReason, ModuleRuntimeState, ModuleSpec,
