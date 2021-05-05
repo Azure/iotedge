848be2e21dc32e0fa4c96007f16648d5e7ec81d4
Robert Jang <robbaldwin95@gmail.com>
2021-05-04 17:56:57 -0700

    Extra spaces removed

--- edge-util/src/Microsoft.Azure.Devices.Edge.Util/StringValidationHelper.cs
+++ edge-util/src/Microsoft.Azure.Devices.Edge.Util/StringValidationHelper.cs
@@ -11,7 +11,7 @@ namespace Microsoft.Azure.Devices.Edge.Util
 
         private static readonly HashSet<char> base64Table =
             new HashSet<char>
-            {  
+            {
                 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
                 'P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d',
                 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
