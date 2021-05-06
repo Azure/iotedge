5421f9e7b88657b6e2d933380d82da0d815cc0cf
hugues bouvier <33438817+huguesBouvier@users.noreply.github.com>
2021-05-06 16:15:33 -0700

    Change config on initial twin (#4951)
    Co-authored-by: root <root@MININT-Q2QVRQH.redmond.corp.microsoft.com>

--- edge-modules/api-proxy-module/src/monitors/config_monitor.rs
+++ edge-modules/api-proxy-module/src/monitors/config_monitor.rs
@@ -1,15 +1,19 @@
-use std::{sync::Arc, time::Duration};
+use std::{collections::HashMap, sync::Arc, time::Duration};
 
-use anyhow::{Context, Error, Result};
+use anyhow::{anyhow, Context, Error, Result};
 use futures_util::{future::Either, pin_mut, StreamExt};
 use log::{error, info, warn};
+use serde_json::Value;
 use tokio::{sync::Notify, task::JoinHandle};
 
 use crate::monitors::config_parser;
 use crate::utils::file;
 use crate::utils::shutdown_handle;
 
-use azure_iot_mqtt::{module::Client, Transport::Tcp, TwinProperties};
+use azure_iot_mqtt::{
+    module::{Client, Message},
+    Transport::Tcp,
+};
 use config_parser::ConfigParser;
 use shutdown_handle::ShutdownHandle;
 
@@ -60,19 +64,26 @@ pub fn start(
                         warn!("Shutting down config monitor!");
                         return Ok(());
                     }
-                    Either::Right((Some(Ok(message)), _)) => {
-                        if let azure_iot_mqtt::module::Message::TwinPatch(twin) = message {
-                            if let Err(err) = save_raw_config(&twin) {
+                    Either::Right((Some(Ok(message)), _)) => match message {
+                        Message::TwinPatch(twin) => {
+                            if let Err(err) = save_raw_config(&twin.properties) {
                                 error!("received message {}", err);
                                 false
                             } else {
                                 info!("New config received from twin, reloading the config");
                                 true
                             }
-                        } else {
-                            false
                         }
-                    }
+                        Message::TwinInitial(twin) => {
+                            if save_raw_config(&twin.desired.properties).is_err() {
+                                false
+                            } else {
+                                info!("Received initial config from twin, reloading the config");
+                                true
+                            }
+                        }
+                        _ => false,
+                    },
                     Either::Right((Some(Err(err)), _)) => {
                         error!("Error receiving a message! {}", err);
                         false
@@ -96,20 +107,25 @@ pub fn start(
     Ok((monitor_loop, shutdown_handle))
 }
 
-fn save_raw_config(twin: &TwinProperties) -> Result<()> {
-    let json = twin.properties.get_key_value(PROXY_CONFIG_TAG);
+fn save_raw_config(twin: &HashMap<String, Value>) -> Result<()> {
+    let config = twin
+        .get(PROXY_CONFIG_TAG)
+        .ok_or_else(|| anyhow!("Key {} not found in twin", PROXY_CONFIG_TAG))?;
 
-    //Get value associated with the key and extract is as a string.
-    let str = (*(json
-        .context(format!("Key {} not found in twin", PROXY_CONFIG_TAG))?
-        .1))
+    let config = config
         .as_str()
         .context("Cannot extract json as base64 string")?;
 
-    let bytes = get_raw_config(str)?;
+    let bytes =
+        base64::decode(config).map_err(|err| anyhow!("Cannot decode base64. Caused by {}", err))?;
 
-    file::write_binary_to_file(&bytes, PROXY_CONFIG_PATH_RAW)?;
-
+    file::write_binary_to_file(&bytes, PROXY_CONFIG_PATH_RAW).map_err(|err| {
+        anyhow!(
+            "Cannot write config file to path: {}. Caused by {}",
+            PROXY_CONFIG_PATH_RAW,
+            err
+        )
+    })?;
     Ok(())
 }
 
@@ -123,13 +139,4 @@ fn parse_config(parse_config: &ConfigParser) -> Result<()> {
     file::write_binary_to_file(&str.as_bytes(), PROXY_CONFIG_PATH_PARSED)?;
 
     Ok(())
-}
-
-fn get_raw_config(encoded_file: &str) -> Result<Vec<u8>, anyhow::Error> {
-    let bytes = match base64::decode(encoded_file) {
-        Ok(bytes) => bytes,
-        Err(err) => return Err(anyhow::anyhow!(format!("Cannot decode base64 {}", err))),
-    };
-
-    Ok(bytes)
 }
