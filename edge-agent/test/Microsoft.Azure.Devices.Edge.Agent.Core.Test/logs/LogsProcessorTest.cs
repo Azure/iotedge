8de4c9c978b66226506b56a69938982e4d6ac56d
Bear Philavastvanid <yophilav@microsoft.com>
2021-05-09 17:03:55 -0700

    Fixed typo

--- edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Core.Test/logs/LogsProcessorTest.cs
+++ edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Core.Test/logs/LogsProcessorTest.cs
@@ -28,12 +28,12 @@ namespace Microsoft.Azure.Devices.Edge.Agent.Core.Test.Logs
 
         static readonly List<(string rawText, string parsedText, string timestamp)> TestLogLines = new List<(string, string, string)>
         {
-            ("[2019-02-08 02:23:22 : Starting Edge Agent", "[2019-02-08 02:23:22 : Starting Edge Agent", string.Empty),
-            ("[02/08/2019 02:23:22.907 AM] Edge Agent Main()", "[02/08/2019 02:23:22.907 AM] Edge Agent Main()", string.Empty),
-            ("2019-02-08 02:23:23.137 +00:00 [INF] - Starting module management agent.", "[INF] - Starting module management agent.", "2019-02-08 02:23:23.137 +00:00"),
-            ("2019-02-08 02:23:23.320 +00:00 [INF] - Version - 1.0.7-dev.20055162 (9c164f0fa1b6accb47db2874bd1df90f00c5bf38)", "[INF] - Version - 1.0.7-dev.20055162 (9c164f0fa1b6accb47db2874bd1df90f00c5bf38)", "2019-02-08 02:23:23.320 +00:00"),
-            ("2019-02-08 02:23:23.321 +00:00 [INF] - ", "[INF] - ", "2019-02-08 02:23:23.321 +00:00"),
-            ("        █████╗ ███████╗██╗   ██╗██████╗ ███████╗", "█████╗ ███████╗██╗   ██╗██████╗ ███████╗", string.Empty)
+            ("[2019-02-08 02:23:22 : Starting Edge Agent\n", "[2019-02-08 02:23:22 : Starting Edge Agent", string.Empty),
+            ("[02/08/2019 02:23:22.907 AM] Edge Agent Main()\n", "[02/08/2019 02:23:22.907 AM] Edge Agent Main()", string.Empty),
+            ("2019-02-08 02:23:23.137 +00:00 [INF] - Starting module management agent.\n", "[INF] - Starting module management agent.", "2019-02-08 02:23:23.137 +00:00"),
+            ("2019-02-08 02:23:23.320 +00:00 [INF] - Version - 1.0.7-dev.20055162 (9c164f0fa1b6accb47db2874bd1df90f00c5bf38)\n", "[INF] - Version - 1.0.7-dev.20055162 (9c164f0fa1b6accb47db2874bd1df90f00c5bf38)", "2019-02-08 02:23:23.320 +00:00"),
+            ("2019-02-08 02:23:23.321 +00:00 [INF] - \n", "[INF] - ", "2019-02-08 02:23:23.321 +00:00"),
+            ("        █████╗ ███████╗██╗   ██╗██████╗ ███████╗\n", "█████╗ ███████╗██╗   ██╗██████╗ ███████╗", string.Empty)
         };
 
         static readonly string[] TestLogTexts =
