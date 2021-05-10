ac10371421e1be13423bdc90e2389f7254f0f4e5
Noel Campbell <noelcampbell@microsoft.com>
2021-05-09 22:30:28 -0400

    More changes based on PR code review
    1. Removed unwraps and replaced blocking lock() with try_lock()
    2. Removed enable flags for ValueRecorder and ValueObserver because
       VO is not expensive and VR unbounded memory usage was fixed by
       specifying histogram aggregator, so no need to disable anymore.
    3. Added docstring to main()
    4. Made meter name a const

--- test/modules/obsagent-client/src/main.rs
+++ test/modules/obsagent-client/src/main.rs
@@ -13,13 +13,15 @@ use clap::{value_t, App, Arg};
 use futures::{Stream, StreamExt};
 use opentelemetry::{
     global,
-    metrics::{self, ObserverResult, ValueRecorder},
+    metrics::{self, ObserverResult},
     sdk::metrics::{selectors, PushController},
 };
 use opentelemetry_otlp::ExporterConfig;
 use rand::Rng;
-use tracing::{info, subscriber, Level};
+use tracing::{error, info, subscriber, Level};
 use tracing_subscriber::fmt::Subscriber;
+
+const METER_NAME: &str = "microsoft.com/azureiot-edge";
 
 // Skip first immediate tick from tokio, not needed for async_std.
 fn delayed_interval(duration: Duration) -> impl Stream<Item = tokio::time::Instant> {
@@ -33,7 +35,9 @@ fn init_meter(period: f64, otlp_endpoint: String) -> metrics::Result<PushControl
     };
     opentelemetry_otlp::new_metrics_pipeline(tokio::spawn, delayed_interval)
         .with_export_config(export_config)
-        .with_aggregator_selector(selectors::simple::Selector::Exact)
+        .with_aggregator_selector(selectors::simple::Selector::Histogram(vec![
+            0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+        ]))
         .with_period(Duration::from_secs_f64(period))
         .build()
 }
@@ -42,10 +46,7 @@ fn init_meter(period: f64, otlp_endpoint: String) -> metrics::Result<PushControl
 struct Config {
     update_rate: f64,
     push_rate: f64,
-    batching_enabled: bool,
     otlp_endpoint: String,
-    enable_value_recorder: bool,
-    enable_value_observer: bool,
 }
 
 fn init_config() -> Result<Config, Box<dyn Error + Send + Sync + 'static>> {
@@ -65,32 +66,11 @@ fn init_config() -> Result<Config, Box<dyn Error + Send + Sync + 'static>> {
                 .help("Rate at which measurements are pushed out of the client (pushes/sec)")
         )
         .arg(
-            Arg::with_name("batching-enabled")
-                .short("b")
-                .long("batching-enabled")
-                .takes_value(true)
-                .help("Enables or disables batch recording of measurements.")
-        )
-        .arg(
             Arg::with_name("otlp-endpoint")
                 .short("e")
                 .long("otlp-endpoint")
                 .takes_value(true)
                 .help("Endpoint to which OTLP messages will be sent.")
-        )
-        .arg(
-            Arg::with_name("enable-value-recorder")
-                .short("r")
-                .long("enable-value-recorder")
-                .takes_value(true)
-                .help("Enables or disables value recorder instrument.")
-        )
-        .arg(
-            Arg::with_name("enable-value-observer")
-                .short("o")
-                .long("enable-value-observer")
-                .takes_value(true)
-                .help("Enables or disables value observer instrument.")
         )
         .get_matches();
 
@@ -103,10 +83,6 @@ fn init_config() -> Result<Config, Box<dyn Error + Send + Sync + 'static>> {
             |_e| Ok(value_t!(matches.value_of("push-rate"), f64).unwrap_or(0.2)),
             |v| v.parse(),
         )?,
-        batching_enabled: std::env::var("BATCHING_ENABLED").map_or_else(
-            |_e| Ok(value_t!(matches.value_of("batching-enabled"), bool).unwrap_or(false)),
-            |v| v.parse(),
-        )?,
         otlp_endpoint: std::env::var("OTLP_ENDPOINT").map_or_else(
             |_e| {
                 Ok(matches
@@ -114,14 +90,6 @@ fn init_config() -> Result<Config, Box<dyn Error + Send + Sync + 'static>> {
                     .unwrap_or("http://localhost:4317")
                     .to_string())
             },
-            |v| v.parse(),
-        )?,
-        enable_value_recorder: std::env::var("ENABLE_VALUE_RECORDER").map_or_else(
-            |_e| Ok(value_t!(matches.value_of("enable-value-recorder"), bool).unwrap_or(false)),
-            |v| v.parse(),
-        )?,
-        enable_value_observer: std::env::var("ENABLE_VALUE_OBSERVER").map_or_else(
-            |_e| Ok(value_t!(matches.value_of("enable-value-observer"), bool).unwrap_or(false)),
             |v| v.parse(),
         )?,
     };
@@ -133,6 +101,21 @@ fn init_logging() {
     let _ = subscriber::set_global_default(subscriber);
 }
 
+/// This program sends example metrics to a configurable OTLP endpoint. The program
+/// loops, updating and exporting metrics for one example of each OTel instrument type (i.e,
+/// counter, up-down-counter, value-recorder, sum-observer, up-down-sum-observer,
+/// value-observer).  The rate at which the metrics are updated is configurable. The
+/// rate at which metrics are exported (or pushed) to the OTLP endpoint is also configurable.
+//
+/// The program is configurable via command line arguments as well as environment variables,
+/// with the environment variables taking precendence. The following is a list of the environment
+/// variables (and cmd line args in parenthesis):
+///     1. UPDATE_RATE (--update-rate/-u) - Rate at which each instrument is
+///            updated with a new metric measurement (updates/sec)
+///     2. PUSH_RATE (--push-rate/-p) - Rate at which measurements are pushed
+///            out of the client (pushes/sec)
+///     3. OTLP_ENDPOINT (--otlp-endpoint/-e) - Endpoint to which OTLP messages
+///             will be sent.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     init_logging();
@@ -142,28 +125,26 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
         config
     );
     let _started = init_meter(1.0 / config.push_rate, config.otlp_endpoint)?;
-    let meter = global::meter("microsoft.com/azureiot-edge");
+    let meter = global::meter(METER_NAME);
 
     // Init synchronous instruments
     let counter = meter.u64_counter("u64_counter_example").init();
     let ud_counter = meter
         .i64_up_down_counter("i64_up_down_counter_example")
         .init();
-    let mut value_recorder: Option<ValueRecorder<f64>> = None;
-    if config.enable_value_recorder {
-        value_recorder = Some(
-            meter
-                .f64_value_recorder("f64_value_recorder_example")
-                .init(),
-        );
-    }
+    let value_recorder = meter
+        .f64_value_recorder("f64_value_recorder_example")
+        .init();
 
     // Init asynchronous instruments
     let sum = Arc::new(Mutex::new(0));
     let sum_clone = Arc::clone(&sum);
     let sum_observer_cb = move |res: ObserverResult<u64>| {
-        let sum_clone = sum_clone.lock().unwrap();
-        res.observe(*sum_clone, &[]);
+        if let Ok(sum_clone) = sum_clone.try_lock() {
+            res.observe(*sum_clone, &[]);
+        } else {
+            error!("try_lock failed")
+        }
     };
     let _sum_observer = meter
         .u64_sum_observer("u64_sum_observer_example", sum_observer_cb)
@@ -171,26 +152,27 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     let ud_sum = Arc::new(Mutex::new(0));
     let ud_sum_clone = Arc::clone(&ud_sum);
     let ud_sum_observer_cb = move |res: ObserverResult<i64>| {
-        let ud_sum_clone = ud_sum_clone.lock().unwrap();
-        res.observe(*ud_sum_clone, &[]);
+        if let Ok(ud_sum_clone) = ud_sum_clone.try_lock() {
+            res.observe(*ud_sum_clone, &[]);
+        } else {
+            error!("try_lock failed")
+        }
     };
     let _ud_sum_observer = meter
         .i64_up_down_sum_observer("i64_up_down_sum_observer_example", ud_sum_observer_cb)
         .init();
-    let mut value: Option<Arc<Mutex<f64>>> = None;
-    if config.enable_value_observer {
-        value = Some(Arc::new(Mutex::new(0.0)));
-        if let Some(ref v) = value {
-            let value_clone = Arc::clone(v);
-            let value_observer_cb = move |res: ObserverResult<f64>| {
-                let value_clone = value_clone.lock().unwrap();
-                res.observe(*value_clone, &[]);
-            };
-            let _value_observer = meter
-                .f64_value_observer("f64_value_observer_example", value_observer_cb)
-                .init();
+    let value: Arc<Mutex<f64>> = Arc::new(Mutex::new(0.0));
+    let value_clone = Arc::clone(&value);
+    let value_observer_cb = move |res: ObserverResult<f64>| {
+        if let Ok(value_clone) = value_clone.try_lock() {
+            res.observe(*value_clone, &[]);
+        } else {
+            error!("try_lock failed")
         }
-    }
+    };
+    let _value_observer = meter
+        .f64_value_observer("f64_value_observer_example", value_observer_cb)
+        .init();
 
     // Loop, updating each metric value at the configured update rate
     let mut rng = rand::thread_rng();
@@ -199,20 +181,27 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     while !term.load(Ordering::Relaxed) {
         counter.add(1, &[]);
         ud_counter.add(1, &[]);
-        if let Some(ref v) = value_recorder {
-            v.record(rng.gen::<f64>(), &[]);
+        value_recorder.record(rng.gen::<f64>(), &[]);
+        {
+            if let Ok(mut sum) = sum.try_lock() {
+                *sum += 1;
+            } else {
+                error!("try_lock failed");
+            }
         }
         {
-            let mut sum = sum.lock().unwrap();
-            *sum += 1;
+            if let Ok(mut ud_sum) = ud_sum.try_lock() {
+                *ud_sum += 1;
+            } else {
+                error!("try_lock failed");
+            }
         }
         {
-            let mut ud_sum = ud_sum.lock().unwrap();
-            *ud_sum += 1;
-        }
-        if let Some(ref v) = value {
-            let mut v = v.lock().unwrap();
-            *v = rng.gen::<f64>();
+            if let Ok(mut value) = value.try_lock() {
+                *value = rng.gen::<f64>();
+            } else {
+                error!("try_lock failed");
+            }
         }
         tokio::time::sleep(Duration::from_secs_f64(1.0 / config.update_rate)).await;
     }
