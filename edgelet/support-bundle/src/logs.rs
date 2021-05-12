7b8cfbc8a3998f9e2dcd31d9c0cb7f241cc0cbdb
Lee <lefitche@microsoft.com>
2021-05-12 01:03:51 -0700

    update logs function

--- edgelet/support-bundle/src/logs.rs
+++ edgelet/support-bundle/src/logs.rs
@@ -1,42 +1,38 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-use std::io::{self, Write};
+use std::io::Write;
 
 use failure::Fail;
-use futures::prelude::*;
+use futures::StreamExt;
 
-use edgelet_core::{Chunked, LogChunk, LogDecode, LogOptions, ModuleRuntime};
+use edgelet_core::{LogOptions, ModuleRuntime};
 
 use crate::error::{Error, ErrorKind};
 
-pub fn pull_logs<M, W>(
+pub async fn pull_logs<M, W>(
     runtime: &M,
     id: &str,
     options: &LogOptions,
-    writer: W,
-) -> impl Future<Item = W, Error = Error> + Send
+    writer: &mut W,
+) -> Result<(), Error>
 where
-    M: 'static + ModuleRuntime,
+    M: ModuleRuntime,
     W: Write + Send,
 {
-    runtime
+    // Collect Logs
+    let logs = runtime
         .logs(id, options)
-        .map_err(|err| Error::from(err.context(ErrorKind::ModuleRuntime)))
-        .and_then(move |logs| {
-            let chunked =
-                Chunked::new(logs.map_err(|_| io::Error::new(io::ErrorKind::Other, "unknown")));
-            LogDecode::new(chunked)
-                .map_err(|err| Error::from(err.context(ErrorKind::ModuleRuntime)))
-                .fold(writer, |mut w, chunk| -> Result<W, Error> {
-                    match chunk {
-                        LogChunk::Stdin(b)
-                        | LogChunk::Stdout(b)
-                        | LogChunk::Stderr(b)
-                        | LogChunk::Unknown(b) => w
-                            .write(&b)
-                            .map_err(|err| Error::from(err.context(ErrorKind::Write)))?,
-                    };
-                    Ok(w)
-                })
-        })
+        .await
+        .map_err(|err| Error::from(err.context(ErrorKind::ModuleRuntime)))?;
+
+    // Write all logs
+    let write = logs.map(|part| writer.write_all(part.as_ref()));
+
+    // Extract errors
+    write
+        .collect::<Vec<_>>()
+        .await
+        .into_iter()
+        .collect::<Result<(), std::io::Error>>()
+        .map_err(|err| Error::from(err.context(ErrorKind::Write)))
 }
