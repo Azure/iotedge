11c064ecb5fa697bae8fad7959a2f6ca7a717303
Andrew Smith <als5ev@virginia.edu>
2021-06-11 16:44:25 -0700

    Builds: Eliminate insecure docker login (#5098)
    Replacing manual docker login with the service connection approach. There is one occurrence of manual docker login I could not replace as it happens through an ssh connection.
    https://github.com/Azure/iotedge/blob/master/builds/misc/templates/mqtt-perf-setup.yaml#L44

--- scripts/linux/buildManifest.sh
+++ scripts/linux/buildManifest.sh
@@ -65,37 +65,29 @@ process_args()
             DOCKER_REGISTRY="$arg"
             save_next_arg=0
         elif [ $save_next_arg -eq 2 ]; then
-            DOCKER_USERNAME="$arg"
-            save_next_arg=0
-        elif [ $save_next_arg -eq 3 ]; then
-            DOCKER_PASSWORD="$arg"
-            save_next_arg=0
-        elif [ $save_next_arg -eq 4 ]; then
             DOCKER_IMAGEVERSION="$arg"
             save_next_arg=0
-        elif [ $save_next_arg -eq 5 ]; then
+        elif [ $save_next_arg -eq 3 ]; then
             YAML_TEMPLATE="$arg"
             save_next_arg=0
-        elif [ $save_next_arg -eq 6 ]; then
+        elif [ $save_next_arg -eq 4 ]; then
             DOCKER_TAGS="$arg"
             save_next_arg=0
-        elif [ $save_next_arg -eq 7 ]; then
+        elif [ $save_next_arg -eq 5 ]; then
             DOCKER_NAMESPACE="$arg"
             save_next_arg=0
-        elif [ $save_next_arg -eq 8 ]; then
+        elif [ $save_next_arg -eq 6 ]; then
             DOCKER_IMAGE_NAME="$arg"
             save_next_arg=0
         else
             case "$arg" in
                 "-h" | "--help" ) usage;;
                 "-r" | "--registry" ) save_next_arg=1;;
-                "-u" | "--username" ) save_next_arg=2;;
-                "-p" | "--password" ) save_next_arg=3;;
-                "-v" | "--image-version" ) save_next_arg=4;;
-                "-t" | "--template" ) save_next_arg=5;;
-                       "--tags" ) save_next_arg=6;;
-                "-n" | "--namespace" ) save_next_arg=7;;
-                "-i" | "--image-name" ) save_next_arg=8;;
+                "-v" | "--image-version" ) save_next_arg=2;;
+                "-t" | "--template" ) save_next_arg=3;;
+                       "--tags" ) save_next_arg=4;;
+                "-n" | "--namespace" ) save_next_arg=5;;
+                "-i" | "--image-name" ) save_next_arg=6;;
                        "--ignore-missing" ) IGNORE_MISSING="--ignore-missing";;
                 * ) usage;;
             esac
@@ -104,16 +96,6 @@ process_args()
 
     if [[ -z ${DOCKER_REGISTRY} ]]; then
         echo "Registry Parameter Invalid"
-        print_help_and_exit
-    fi
-
-    if [[ -z ${DOCKER_USERNAME} ]]; then
-        echo "Docker Username Parameter Invalid"
-        print_help_and_exit
-    fi
-
-    if [[ -z ${DOCKER_PASSWORD} ]]; then
-        echo "Docker Password Parameter Invalid"
         print_help_and_exit
     fi
 
@@ -136,13 +118,6 @@ process_args()
 # Main Script Execution
 ###############################################################################
 process_args $@
-
-echo Logging in to Docker registry
-docker login $DOCKER_REGISTRY -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-if [ $? -ne 0 ]; then
-    echo "Docker Login Failed!"
-    exit 1
-fi
 
 # Create temp file to store modified yaml file
 manifest=$(mktemp /tmp/manifest.yaml.XXXXXX)
