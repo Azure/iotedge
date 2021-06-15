0ce36944845d3a63f48dc3ec7583d90819e73afe
Andrew Smith <als5ev@virginia.edu>
2021-06-15 20:24:34 +0000

    forgot to save file

--- scripts/linux/trcE2ETest.sh
+++ scripts/linux/trcE2ETest.sh
@@ -560,10 +560,7 @@ function process_args() {
                 '-sendReportFrequency' ) saveNextArg=43;;
                 '-testMode' ) saveNextArg=44;;
                 '-repoPath' ) saveNextArg=45;;
-<<<<<<< HEAD
                 '-clientModuleTransportType' ) saveNextArg=46;;
-=======
->>>>>>> fc0ba35db57444d1dfdfb01cffab5ae1ac1bee8f
                 '-waitForTestComplete' ) WAIT_FOR_TEST_COMPLETE=1;;
                 '-cleanAll' ) CLEAN_ALL=1;;
 
