d12d38456d998358a5caa09beff172c5fb6de2f8
Lee Fitchett <lefitche@microsoft.com>
2021-06-22 13:32:07 -0700

    Device config has allow_privileged flag (#5126)

--- edgelet/edgelet-core/src/settings.rs
+++ edgelet/edgelet-core/src/settings.rs
@@ -346,6 +346,9 @@ pub struct Settings<T> {
 
     pub homedir: PathBuf,
 
+    #[serde(default = "true_func")]
+    pub allow_elevated_docker_permissions: bool,
+
     pub agent: ModuleSpec<T>,
 
     pub connect: Connect,
@@ -360,6 +363,11 @@ pub struct Settings<T> {
     #[serde(default, skip_serializing)]
     #[cfg_attr(not(debug_assertions), serde(skip_deserializing))]
     pub endpoints: Endpoints,
+}
+
+// Serde default requires a function: https://github.com/serde-rs/serde/issues/1030
+fn true_func() -> bool {
+    true
 }
 
 impl<T> RuntimeSettings for Settings<T>
