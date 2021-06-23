42df11fa683ade4ef05ffeb00b9070528698998d
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2021-06-23 12:24:38 -0700

    Update edged with logging

--- edgelet/aziot-edged/src/main.rs
+++ edgelet/aziot-edged/src/main.rs
@@ -4,4 +4,18 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let version = edgelet_core::version_with_source_version();
+
+    clap::App::new(clap::crate_name!())
+        .version(version.as_str())
+        .author(clap::crate_authors!("\n"))
+        .about(clap::crate_description!())
+        .get_matches();
+
+    logger::try_init()
+        .expect("cannot fail to initialize global logger from the process entrypoint");
+
+    log::info!("Starting Azure IoT Edge Module Runtime");
+    log::info!("Version - {}", edgelet_core::version_with_source_version());
+}
