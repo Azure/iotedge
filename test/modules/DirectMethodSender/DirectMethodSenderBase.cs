8e2cda00028d3a066333e2ddbbe62c5bc66a1c39
Andrew Smith <als5ev@virginia.edu>
2021-06-25 13:49:12 -0700

    Testing: Sync state of tests on release/1.2 with master branch (#5170)
    We stopped cherry picking changes from 1.2 to master as there were missing ports, then the changes started backing up. I have cherry picked all my testing commits from release/1.2 into master. 
    
    For a few of them I had to resolve small conflicts. These conflicts were created because the following changes hit master and then weren't ported to 1.2:
    https://github.com/Azure/iotedge/pull/4520
    https://github.com/Azure/iotedge/pull/4869
    
    We should cherry pick the above PRs to 1.2 branch.

--- test/modules/DirectMethodSender/DirectMethodSenderBase.cs
+++ test/modules/DirectMethodSender/DirectMethodSenderBase.cs
@@ -3,6 +3,7 @@ namespace DirectMethodSender
 {
     using System;
     using System.Net;
+    using System.Net.Sockets;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Common.Exceptions;
@@ -56,6 +57,21 @@ namespace DirectMethodSender
             {
                 logger.LogInformation(e, $"DeviceNotFound exception caught with count {this.directMethodCount}");
                 return new Tuple<HttpStatusCode, ulong>(HttpStatusCode.NotFound, this.directMethodCount);
+            }
+            catch (SocketException e)
+            {
+                logger.LogInformation(e, $"Resource exception caught with count {this.directMethodCount}");
+                return new Tuple<HttpStatusCode, ulong>(HttpStatusCode.ServiceUnavailable, this.directMethodCount);
+            }
+            catch (UnauthorizedException e)
+            {
+                logger.LogInformation(e, $"Unauthorized exception caught with count {this.directMethodCount}");
+                return new Tuple<HttpStatusCode, ulong>(HttpStatusCode.Unauthorized, this.directMethodCount);
+            }
+            catch (Exception e) when (e is System.Net.Http.HttpRequestException || e is IotHubException || e is IotHubCommunicationException)
+            {
+                logger.LogInformation(e, $"Transient exception caught with count {this.directMethodCount}");
+                return new Tuple<HttpStatusCode, ulong>(HttpStatusCode.FailedDependency, this.directMethodCount);
             }
             catch (Exception e)
             {
