bee79d98c2c7d9e0a733a96b1c56774ae4205753
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2021-07-08 12:16:46 -0700

    Add edgelet-http-workload

---
+++ edgelet/edgelet-http-workload/src/lib.rs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+mod module;
+
+#[derive(Clone)]
+pub struct Service {
+
+}
+
+http_common::make_service! {
+    service: Service,
+    api_version: edgelet_http::ApiVersion,
+    routes: [
+        module::list::Route,
+    ],
+}
