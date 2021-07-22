c31cb65d2928c48d998ab81e64420eb4a681dd7b
Lee <lefitche@microsoft.com>
2021-07-22 10:41:29 -0700

    re-enable clippy

--- edgelet/edgelet-docker/src/lib.rs
+++ edgelet/edgelet-docker/src/lib.rs
@@ -1,15 +1,15 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-// #![deny(rust_2018_idioms, warnings)]
-// #![deny(clippy::all, clippy::pedantic)]
-// #![allow(
-//     clippy::default_trait_access,
-//     clippy::missing_errors_doc,
-//     clippy::module_name_repetitions,
-//     clippy::must_use_candidate,
-//     clippy::too_many_lines,
-//     clippy::use_self
-// )]
+#![deny(rust_2018_idioms, warnings)]
+#![deny(clippy::all, clippy::pedantic)]
+#![allow(
+    clippy::default_trait_access,
+    clippy::missing_errors_doc,
+    clippy::module_name_repetitions,
+    clippy::must_use_candidate,
+    clippy::too_many_lines,
+    clippy::use_self
+)]
 
 mod client;
 mod error;
