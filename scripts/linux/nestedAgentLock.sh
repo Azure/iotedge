61d913427f3d0eb50a861744d0d460ce5e510f65
Andrew Smith <als5ev@virginia.edu>
2021-07-28 03:22:20 +0000

    simplify acquiring locked agents in pipeline yamls

--- scripts/linux/nestedAgentLock.sh
+++ scripts/linux/nestedAgentLock.sh
@@ -3,7 +3,8 @@
 ###############################################################################
 # This script is used to "lock" agents for nested tests. When we lock an agent,
 # we alter the agent capability named "status" by appending the build id of
-# the current run.
+# the current run. We also add contextual information about the topology level
+# (i.e. L5, L4, L3) so that the test framework can book the agents.
 
 # Locking agents is not an atomic operation so this script has to account for
 # race conditions.
@@ -28,11 +29,11 @@ SCRIPT_NAME=$(basename $0)
 
 POOL_ID=123 # Devops agent pool id corresponding to "Azure-IoT-Edge-Core"
 API_VER=6.0
-AGENTS_NEEDED=3
 TIMEOUT_SECONDS=300
 
 AGENT_GROUP=
 BUILD_ID=
+RUNNER_AGENTS_NEEDED=
 
 ###############################################################################
 # Print usage information pertaining to this script and exit
@@ -46,6 +47,7 @@ usage()
     echo "options"
     echo " -a                 Agent Group from which we want to book agents. This agent group is a capability named 'agent-group'."
     echo " -b                 Devops build id used to tag locked agents."
+    echo " -n                 Number of test runner agents to book."
     exit 1;
 }
 
@@ -69,18 +71,22 @@ process_args()
         elif [ $save_next_arg -eq 2 ]; then
             BUILD_ID="$arg"
             save_next_arg=0
+        elif [ $save_next_arg -eq 3 ]; then
+            RUNNER_AGENTS_NEEDED="$arg"
+            save_next_arg=0
         else
             case "$arg" in
                 "-h" ) usage;;
                 "-a" ) save_next_arg=1;;
                 "-b" ) save_next_arg=2;;
+                "-n" ) save_next_arg=3;;
                 * ) usage;;
             esac
         fi
     done
 
     if [[ -z ${PAT} ]]; then
-        echo "Personal Access Token is a required parameter"
+        echo "Personal Access Token must be set in the environment"
         print_help_and_exit
     fi
 
@@ -93,28 +99,16 @@ process_args()
         echo "Build id is a required parameter."
         print_help_and_exit
     fi
+
+    if [[ -z ${RUNNER_AGENTS_NEEDED} ]]; then
+        echo "Number of runner agents is a required parameter."
+        print_help_and_exit
+    fi
 }
 
 ###############################################################################
 # Main Script Execution
 ###############################################################################
-
-function set_devops_output_vars() {
-    agents=("$@")
-    outputAgentNames=(l5AgentName l4AgentName l3AgentName)
-
-    echo "Setting devops vars for agent names. Needed for future unlock."
-
-    for i in "${!agents[@]}"; do
-        agentId="${agents[$i]}"
-
-        agentCapabilities=$(curl -s -u :$PAT --request GET "https://dev.azure.com/msazure/_apis/distributedtask/pools/$POOL_ID/agents/$agentId?includeCapabilities=true&api-version=$API_VER")
-        agentName=$(echo $agentCapabilities | jq '.systemCapabilities."Agent.Name"' | tr -d '[], "')
-
-        echo "Setting devops var for agent name: $agentName"
-        echo "##vso[task.setvariable variable=${outputAgentNames[$i]};isOutput=true]$agentName"
-    done
-}
 
 function update_capabilities() {
     agentId=$1
@@ -141,10 +135,20 @@ EOF
 function attempt_agent_lock() {
     agents=("$@")
 
-    # Lock the agents.
-    for agentId in "${agents[@]}"; do
+    # Lock the agents by appending to the capability 'status'.
+    for i in "${!agents[@]}"; do
+        agentCapabilityTag=
+        if [ "$i" -eq 0 ]; then
+            agentCapabilityTag="_${BUILD_ID}_L5"
+        elif [ "$i" -eq 1 ]; then
+            agentCapabilityTag="_${BUILD_ID}_L4"
+        else
+            agentCapabilityTag="_${BUILD_ID}_L3"
+        fi
+
+        agentId="${agents[$i]}"
         agentCapabilities=$(curl -s -u :$PAT --request GET "https://dev.azure.com/msazure/_apis/distributedtask/pools/$POOL_ID/agents/$agentId?includeCapabilities=true&api-version=$API_VER")
-        newAgentUserCapabilities=$(echo $agentCapabilities | jq '.userCapabilities | (.["status"]) |= sub("$"; '\"_$BUILD_ID\"')')
+        newAgentUserCapabilities=$(echo $agentCapabilities | jq '.userCapabilities | (.["status"]) |= sub("$"; '\"$agentCapabilityTag\"')')
 
         update_capabilities "$agentId" "$newAgentUserCapabilities"
     done
@@ -156,7 +160,9 @@ function attempt_agent_lock() {
         agentCapabilities=$(curl -s -u :$PAT --request GET "https://dev.azure.com/msazure/_apis/distributedtask/pools/$POOL_ID/agents/$agentId?includeCapabilities=true&api-version=$API_VER")
         lockStatus=$(echo $agentCapabilities | jq '.userCapabilities | .status' | tr -d '[], "')
 
-        if [ $lockStatus != "unlocked_$BUILD_ID" ]; then
+        if [ $lockStatus != "unlocked_${BUILD_ID}_L5" ] &&
+           [ $lockStatus != "unlocked_${BUILD_ID}_L4"] &&
+           [ $lockStatus != "unlocked_${BUILD_ID}_L3"]; then
             agentsAllLockedCorrectly=false
             break
         fi
@@ -172,7 +178,9 @@ function unlock_agents() {
         agentCapabilities=$(curl -s -u :$PAT --request GET "https://dev.azure.com/msazure/_apis/distributedtask/pools/$POOL_ID/agents/$agentId?includeCapabilities=true&api-version=$API_VER")
         lockStatus=$(echo $agentCapabilities | jq '.userCapabilities | .status')
 
-        if [ $lockStatus = '"unlocked_$(Build.BuildId)"' ]; then
+        if [ $lockStatus != "unlocked_${BUILD_ID}_L5" ] &&
+           [ $lockStatus != "unlocked_${BUILD_ID}_L4"] &&
+           [ $lockStatus != "unlocked_${BUILD_ID}_L3"]; then
             echo "Unlocking agent $agentId"
 
             newAgentUserCapabilities=$(echo $agentCapabilities | jq '.userCapabilities | (.["status"]) |= "unlocked"')
@@ -197,23 +205,24 @@ echo "Done validating jq"
 startSeconds=$((SECONDS))
 endSeconds=$((SECONDS + $TIMEOUT_SECONDS))
 backoffSeconds=1
+agentsNeeded=$(($RUNNER_AGENTS_NEEDED + 2))
 while true && [ $((SECONDS)) -lt $endSeconds ]; do
     # Wait 1-10 seconds to retry locking agents.
     # Random delay to avoid multiple instances of the script thrashing.
     sleep $[ ( $RANDOM % 10 )  + $backoffSeconds ]s
     backoffSeconds=$(($backoffSeconds * 2))
 
-    echo "Attempting to lock $AGENTS_NEEDED agents from the agent group $AGENT_GROUP..."
+    echo "Attempting to lock $agentsNeeded agents from the agent group $AGENT_GROUP..."
     agentsInfo=$(curl -s -u :$PAT --request GET "https://dev.azure.com/msazure/_apis/distributedtask/pools/$POOL_ID/agents?includeCapabilities=true&api-version=$API_VER")
     unlockedAgents=($(echo $agentsInfo | jq '.value | .[] | select(.userCapabilities.status=="unlocked" and .userCapabilities."agent-group"=='\"$AGENT_GROUP\"') | .id' | tr -d '[], "'))
 
     echo "Found these unlocked agents:"
     echo ${unlockedAgents[@]}
 
-    if [ ${#unlockedAgents[*]} -ge $AGENTS_NEEDED ]; then
+    if [ ${#unlockedAgents[*]} -ge $agentsNeeded ]; then
         # If we have enough agents, get random agents and book them all.
         shuffledUnlockedAgents=($(shuf -e "${unlockedAgents[@]}"))
-        filteredAgents=(${shuffledUnlockedAgents[@]:0:$AGENTS_NEEDED})
+        filteredAgents=(${shuffledUnlockedAgents[@]:0:$agentsNeeded})
 
         echo "Locking these agents:"
         echo ${filteredAgents[@]}
@@ -228,12 +237,11 @@ while true && [ $((SECONDS)) -lt $endSeconds ]; do
 
         else
             echo "Successfully locked agents"
-            set_devops_output_vars "${filteredAgents[@]}"
             exit 0
         fi
     fi
 
-    echo "Failed to acquire $AGENTS_NEEDED agents from pool. Will retry soon."
+    echo "Failed to acquire $agentsNeeded agents from pool. Will retry soon."
 done
 
 exit 1
