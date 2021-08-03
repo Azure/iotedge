6b4879a6248117b869751bde5ab7080a95c4ba6b
Andrew Smith <als5ev@virginia.edu>
2021-08-03 16:30:51 -0700

    cherry pick 1211f679fff557c0cd91919f8a93039d3b35877d with conflicts

--- test/Microsoft.Azure.Devices.Edge.Test/helpers/TestResultCoordinatorUtil.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/helpers/TestResultCoordinatorUtil.cs
@@ -18,6 +18,12 @@ namespace Microsoft.Azure.Devices.Edge.Test.Helpers
 
         public static Action<EdgeConfigBuilder> BuildAddNetworkControllerConfig(string trackingId, string networkControllerImage)
         {
+            string transportType = "Amqp";
+            if (Context.Current.TestRunnerProxy.HasValue)
+            {
+                transportType = "Amqp_WebSocket_Only";
+            }
+
             return new Action<EdgeConfigBuilder>(
                 builder =>
                 {
@@ -30,9 +36,11 @@ namespace Microsoft.Azure.Devices.Edge.Test.Helpers
                             ("RunFrequencies__0__OnlineFrequency", "00:00:00"),
                             ("RunFrequencies__0__RunsCount", "0"),
                             ("NetworkControllerRunProfile", "Online"),
-                            ("StartAfter", "00:00:00")
+                            ("StartAfter", "00:00:00"),
+                            ("TransportType", transportType)
                         })
-                        .WithSettings(new[] { ("createOptions", "{\"HostConfig\":{\"Binds\":[\"/var/run/docker.sock:/var/run/docker.sock\"], \"NetworkMode\":\"host\", \"Privileged\":true},\"NetworkingConfig\":{\"EndpointsConfig\":{\"host\":{}}}}") });
+                        .WithSettings(new[] { ("createOptions", "{\"HostConfig\":{\"Binds\":[\"/var/run/docker.sock:/var/run/docker.sock\"], \"NetworkMode\":\"host\", \"Privileged\":true},\"NetworkingConfig\":{\"EndpointsConfig\":{\"host\":{}}}}") })
+                        .WithProxy(Context.Current.TestRunnerProxy);
                 });
         }
 
