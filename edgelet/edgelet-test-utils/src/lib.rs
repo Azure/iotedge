7bb520ab5e1bd98d8668359cbefbbdd4a0639a98
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2021-08-09 12:41:09 -0700

    Clear edgelet-test-utils

--- edgelet/edgelet-test-utils/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-
-#![deny(rust_2018_idioms, warnings)]
-#![deny(clippy::all, clippy::pedantic)]
-
-pub mod cert;
-pub mod identity;
-pub mod module;
-pub mod server;
