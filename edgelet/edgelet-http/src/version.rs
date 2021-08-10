8dba80a311f1dff735005d51da60dd3271e6c277
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2021-08-10 09:34:24 -0700

    Add parse_api_version test

--- edgelet/edgelet-http/src/version.rs
+++ edgelet/edgelet-http/src/version.rs
@@ -9,7 +9,6 @@ pub enum ApiVersion {
     V2019_11_05,
     V2020_07_07,
     V2020_10_10,
-    Latest,
 }
 
 impl std::fmt::Display for ApiVersion {
@@ -20,7 +19,7 @@ impl std::fmt::Display for ApiVersion {
             ApiVersion::V2019_10_22 => "2019-10-22",
             ApiVersion::V2019_11_05 => "2019-11-05",
             ApiVersion::V2020_07_07 => "2020-07-07",
-            ApiVersion::V2020_10_10 | ApiVersion::Latest => "2020-10-10",
+            ApiVersion::V2020_10_10 => "2020-10-10",
         })
     }
 }
@@ -38,5 +37,24 @@ impl std::str::FromStr for ApiVersion {
             "2020-10-10" => Ok(ApiVersion::V2020_10_10),
             _ => Err(()),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::ApiVersion;
+
+    #[test]
+    fn parse_api_version() {
+        assert_eq!(ApiVersion::V2018_06_28, ApiVersion::from_str("2018-06-28").unwrap());
+        assert_eq!(ApiVersion::V2019_01_30, ApiVersion::from_str("2019-01-30").unwrap());
+        assert_eq!(ApiVersion::V2019_10_22, ApiVersion::from_str("2019-10-22").unwrap());
+        assert_eq!(ApiVersion::V2019_11_05, ApiVersion::from_str("2019-11-05").unwrap());
+        assert_eq!(ApiVersion::V2020_07_07, ApiVersion::from_str("2020-07-07").unwrap());
+        assert_eq!(ApiVersion::V2020_10_10, ApiVersion::from_str("2020-10-10").unwrap());
+
+        assert!(ApiVersion::from_str("1900-01-01").is_err());
     }
 }
