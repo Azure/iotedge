4c07839fbc20d4876aa4183c0e4c9171f63fc0a8
Lee <lefitche@microsoft.com>
2021-09-20 21:13:21 -0700

    4

--- test/Microsoft.Azure.Devices.Edge.Test/X509Device.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/X509Device.cs
@@ -16,7 +16,7 @@ namespace Microsoft.Azure.Devices.Edge.Test
     class X509Device : X509ManualProvisioningFixture
     {
         [Test]
-        [Category("Flaky")]
+        // [Category("Flaky")]
         public async Task X509ManualProvision()
         {
             CancellationToken token = this.TestToken;
