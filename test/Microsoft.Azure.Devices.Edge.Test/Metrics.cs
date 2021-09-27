57cb156823385615418ce9b6a87eac5ea44f714d
Bear Philavastvanid <yophilav@microsoft.com>
2021-09-27 14:35:04 -0700

    Disabling the ValidateMetrics test due to bug flakyness

--- test/Microsoft.Azure.Devices.Edge.Test/Metrics.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/Metrics.cs
@@ -22,6 +22,8 @@ namespace Microsoft.Azure.Devices.Edge.Test
         public const string ModuleName = "metricsValidator";
 
         [Test]
+        // [9/27/2021] Disabling the test because of the bug - https://github.com/Azure/iotedge/issues/5438
+        [Fact(Skip = "Flaky")]
         [Category("FlakyOnArm")]
         public async Task ValidateMetrics()
         {
