c0db49bb4dce6c854db65353cd7a7089e0593598
vipeller <51135538+vipeller@users.noreply.github.com>
2021-10-11 18:11:10 -0700

    [DONT MERGE] Added logging for http communication between edgeAgent and edged

--- edge-util/src/Microsoft.Azure.Devices.Edge.Util/HttpClientHelper.cs
+++ edge-util/src/Microsoft.Azure.Devices.Edge.Util/HttpClientHelper.cs
@@ -4,7 +4,10 @@ namespace Microsoft.Azure.Devices.Edge.Util
     using System;
     using System.Linq;
     using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Edge.Util.Uds;
+    using Microsoft.Extensions.Logging;
 
     public class HttpClientHelper
     {
@@ -18,7 +21,7 @@ namespace Microsoft.Azure.Devices.Edge.Util
 
             if (serverUri.Scheme.Equals(HttpScheme, StringComparison.OrdinalIgnoreCase) || serverUri.Scheme.Equals(HttpsScheme, StringComparison.OrdinalIgnoreCase))
             {
-                client = new HttpClient();
+                client = new HttpClient(new LoggingHandler(new HttpClientHandler()));
                 return client;
             }
 
@@ -39,6 +42,39 @@ namespace Microsoft.Azure.Devices.Edge.Util
             }
 
             return serverUri.OriginalString;
+        }
+    }
+
+    class LoggingHandler : DelegatingHandler
+    {
+        public static readonly ILogger Log = Logger.Factory.CreateLogger<LoggingHandler>();
+
+        public LoggingHandler(HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var requestContent = string.Empty;
+            if (request.Content != null)
+            {
+                requestContent = await request.Content.ReadAsStringAsync();
+            }
+
+            Log.LogInformation($"Request:\n\t{request}\n\t{requestContent}\n");
+
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+            var responseContent = string.Empty;
+            if (response.Content != null)
+            {
+                responseContent = await response.Content.ReadAsStringAsync();
+            }
+
+            Log.LogInformation($"Response:\n\t{response}\n\t{responseContent}");
+
+            return response;
         }
     }
 }
