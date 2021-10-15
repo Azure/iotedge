bed1d5863baa29f81e464e7bff18d8ffbd56e496
Gaurav Kulkarni <gakulkar@microsft.com>
2021-10-14 18:33:11 -0700

    "Added Regex tests and documenttaion explaining what's being tested"

--- edge-modules/azure-monitor/tests/Microsoft.Azure.Devices.Edge.Azure.Monitor.Test/MiscellaneousTests.cs
+++ edge-modules/azure-monitor/tests/Microsoft.Azure.Devices.Edge.Azure.Monitor.Test/MiscellaneousTests.cs
@@ -85,28 +85,67 @@ namespace Microsoft.Azure.Devices.Edge.Azure.Monitor.Test
             MetricFilter filter = new MetricFilter("total_network_out_bytes{edge_device=\"Ubuntu-20\"}[http://VeryNoisyModule:9001/metrics]");
             bigList = bigList.Where(x => filter.Matches(x));
             Assert.True(bigList.Count() == 2);
+
+            // next test -- just metrics, no labels
+            string prometheusMessage4 = "total_network_out_bytes{iothub=\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-20\",instance_number=\"b9e51990-f3f9-4d90-8fc7-ef62d01929b2\",module_name=\"edgeHub\",ms_telemetry=\"False\"} 1683388"
+               + System.Environment.NewLine
+               + "edgehub_gettwin_total{iothub =\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-20\",instance_number=\"b9e51990-f3f9-4d90-8fc7-ef62d01929b2\",module_name=\"edgeAgent\",ms_telemetry=\"False\"} 5134109"
+               + System.Environment.NewLine;
+
+            string prometheusMessage5 = "total_network_out_bytes{iothub =\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-20\",instance_number=\"023163b5-1db5-41bc-b57e-4c4d1047d9c9\",id=\"DeviceId: Ubuntu-20; ModuleId: IoTEdgeMetricsCollector [IotHubHostName: IoTHub.azure-devices.net]\"} 2"
+               + System.Environment.NewLine
+               + "edgehub_gettwin_total{iothub =\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-21\",instance_number=\"023163b5-1db5-41bc-b57e-4c4d1047d9c9\",id=\"DeviceId: Ubuntu-20; ModuleId: $edgeHub [IotHubHostName: IoTHub.azure-devices.net]\"} 1";
+
+            IEnumerable<Metric> metrics4 = PrometheusMetricsParser.ParseMetrics(testTime, prometheusMessage4, "http://VeryNoisyModule:9001/metrics");
+            IEnumerable<Metric> metrics5 = PrometheusMetricsParser.ParseMetrics(testTime, prometheusMessage5, "http://Google:9001/metrics");
+
+            bigList = metrics4.Concat(metrics5);
+
+            filter = new MetricFilter("total_network_out_bytes edgehub_gettwin_total");
+            bigList = bigList.Where(x => filter.Matches(x));
+            Assert.True(bigList.Count() == 4);
+
+            // add condition on one endpoint, but not the other
+            filter = new MetricFilter("total_network_out_bytes edgehub_gettwin_total{edge_device=\"Ubuntu-21\"}");
+            bigList = bigList.Where(x => filter.Matches(x));
+            Assert.True(bigList.Count() == 3);
         }
 
         // Label Equality & regex tests -- added for completeness and as a safeguard against future changes (and therefore, potential bugs)
 
         [Fact]
-        public void TestSimpleWildcardStar()
+        public void TestWildcard()
         {
             string prometheusMessage = "edgehub_gettwin_total{iothub=\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-20\",instance_number=\"b9e51990-f3f9-4d90-8fc7-ef62d01929b2\",module_name=\"edgeHub\",ms_telemetry=\"False\"} 1683388"
                 + System.Environment.NewLine
-                + "edgehub_gettwin_total{iothub =\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-20\",instance_number=\"b9e51990-f3f9-4d90-8fc7-ef62d01929b2\",module_name=\"edgeAgent\",ms_telemetry=\"False\"} 5134109"
+                + "total_network_out_bytes{iothub =\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-20\",instance_number=\"b9e51990-f3f9-4d90-8fc7-ef62d01929b2\",module_name=\"edgeAgent\",ms_telemetry=\"False\"} 5134109"
                 + System.Environment.NewLine
                 + "edgehub_gettwin_total{iothub =\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-21\",instance_number=\"023163b5-1db5-41bc-b57e-4c4d1047d9c9\",id=\"DeviceId: Ubuntu-20; ModuleId: IoTEdgeMetricsCollector [IotHubHostName: IoTHub.azure-devices.net]\"} 2"
                 + System.Environment.NewLine
-                + "edgehub_gettwin_total{iothub =\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-20\",instance_number=\"023163b5-1db5-41bc-b57e-4c4d1047d9c9\",id=\"DeviceId: Ubuntu-20; ModuleId: $edgeHub [IotHubHostName: IoTHub.azure-devices.net]\"} 1";
+                + "max_network_out_bytes{iothub =\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-20\",instance_number=\"023163b5-1db5-41bc-b57e-4c4d1047d9c9\",id=\"DeviceId: Ubuntu-20; ModuleId: $edgeHub [IotHubHostName: IoTHub.azure-devices.net]\"} 1";
 
             MetricFilter filter = new MetricFilter("*");
 
             IEnumerable<Metric> metrics = PrometheusMetricsParser.ParseMetrics(testTime, prometheusMessage, "http://VeryNoisyModule:9001/metrics");
 
-            // verify this fails (despite metric & label matching) because endpoint isn't specified
             IEnumerable<Metric> result = metrics.Where(x => filter.Matches(x));
             Assert.True(result.Count() == 4);
+
+            // Wildcards * (any characters) and ? (any single character) can be used in metric names.
+            // For example, *CPU would match maxCPU and minCPU but not CPUMaximum.
+
+            filter = new MetricFilter("*_out_bytes");
+            result = metrics.Where(x => filter.Matches(x));
+            Assert.True(result.Count() == 2);
+
+            // ???CPU would match maxCPU and minCPU but not maximumCPU.
+            filter = new MetricFilter("???_network_out_bytes");
+            result = metrics.Where(x => filter.Matches(x));
+            Assert.True(result.Count() == 1);
+
+            filter = new MetricFilter("?????_network_out_bytes");
+            result = metrics.Where(x => filter.Matches(x));
+            Assert.True(result.Count() == 1);
         }
 
         [Fact]
@@ -120,6 +159,8 @@ namespace Microsoft.Azure.Devices.Edge.Azure.Monitor.Test
                 + System.Environment.NewLine
                 + "edgehub_gettwin_total{iothub =\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-20\",instance_number=\"023163b5-1db5-41bc-b57e-4c4d1047d9c9\",id=\"DeviceId: Ubuntu-20; ModuleId: $edgeHub [IotHubHostName: IoTHub.azure-devices.net]\"} 1";
 
+            // Like PromQL, the following matching operators are allowed.
+            // != Match labels not exactly equal to the provided string.
             MetricFilter filter = new MetricFilter("edgehub_gettwin_total{edge_device!=\"Ubuntu-20\"}");
 
             IEnumerable<Metric> metrics = PrometheusMetricsParser.ParseMetrics(testTime, prometheusMessage, "http://VeryNoisyModule:9001/metrics");
@@ -128,11 +169,41 @@ namespace Microsoft.Azure.Devices.Edge.Azure.Monitor.Test
             Assert.True(result.Count() == 1);
             Assert.Contains("Ubuntu-21", result.ElementAt(0).Tags.Values);
 
+            // = Match labels exactly equal to the provided string(case sensitive).
             filter = new MetricFilter("edgehub_gettwin_total{instance_number=\"b9e51990-f3f9-4d90-8fc7-ef62d01929b2\"}");
             result = metrics.Where(x => filter.Matches(x));
             Assert.True(result.Count() == 2);
 
+            // Multiple metric values can be included in the curly brackets. The values should be comma-separated.
+            // A metric will be matched if at least all labels in the selector are present and also match.
             filter = new MetricFilter("edgehub_gettwin_total{instance_number=\"023163b5-1db5-41bc-b57e-4c4d1047d9c9\", edge_device!=\"Ubuntu-20\"}");
+            result = metrics.Where(x => filter.Matches(x));
+            Assert.True(result.Count() == 1);
+        }
+
+        [Fact]
+        public void RegexTest()
+        {
+            string prometheusMessage = "edgehub_gettwin_total{iothub=\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-20\",instance_number=\"b9e51990-f3f9-4d90-8fc7-ef62d01929b2\",module_name=\"edgeHub\",ms_telemetry=\"False\"} 1683388"
+                + System.Environment.NewLine
+                + "edgehub_gettwin_total{iothub =\"IoTHub.azure-devices.net\",edge_device=\"2012Wrong\",KingKong_SSN=\"!@!\",instance_number=\"b9e51990-f3f9-4d90-8fc7-ef62d01929b2\",module_name=\"edgeAgent\",ms_telemetry=\"False\"} 5134109"
+                + System.Environment.NewLine
+                + "edgehub_gettwin_total{iothub =\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-21\",instance_number=\"023163b5-1db5-41bc-b57e-4c4d1047d9c9\",id=\"DeviceId: Ubuntu-20; ModuleId: IoTEdgeMetricsCollector [IotHubHostName: IoTHub.azure-devices.net]\"} 2"
+                + System.Environment.NewLine
+                + "edgehub_gettwin_total{iothub =\"IoTHub.azure-devices.net\",edge_device=\"Ubuntu-20\",KingKong_SSN=\"63b5-1db5-41\",id=\"DeviceId: Ubuntu-20; ModuleId: $edgeHub [IotHubHostName: IoTHub.azure-devices.net]\"} 1";
+
+            // Like PromQL, the following matching operators are allowed
+            // = ~Match labels to a provided regex
+            MetricFilter filter = new MetricFilter("edgehub_gettwin_total{KingKong_SSN=~\"[-a-zA-Z0-9_]*\"}");
+
+            IEnumerable <Metric> metrics = PrometheusMetricsParser.ParseMetrics(testTime, prometheusMessage, "http://VeryNoisyModule:9001/metrics");
+
+            IEnumerable<Metric> result = metrics.Where(x => filter.Matches(x));
+            Assert.True(result.Count() == 1);
+
+            // != Match labels that don't fit a provided regex.
+            filter = new MetricFilter("edgehub_gettwin_total{KingKong_SSN!~\"[-a-zA-Z0-9_]*\"}");
+
             result = metrics.Where(x => filter.Matches(x));
             Assert.True(result.Count() == 1);
         }
