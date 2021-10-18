53157f28c55eae5058527d87e6555eab863927e4
hugues bouvier <33438817+huguesBouvier@users.noreply.github.com>
2021-10-18 11:24:55 -0700

    Fix permission denied on workload socket (#5705)
    Co-authored-by: Ubuntu <azureuser@debug.wciqoipclkaejnvszvrhsyebkf.bx.internal.cloudapp.net>

--- edgelet/iotedged/src/workload_manager.rs
+++ edgelet/iotedged/src/workload_manager.rs
@@ -133,13 +133,32 @@ where
     ) -> Result<(), Error> {
         let label = "work".to_string();
 
-        let (shutdown_sender, shutdown_receiver) = oneshot::channel();
+        // If a listener has already been created, remove previous listener.
+        // This avoid the launch of 2 listeners.
+        // We chose to remove instead and create a new one instead of
+        // just return and say, one listener has already been created:
+        // We chose to remove because a listener could crash without getting removed correctly.
+        // That could make the module crash. Then that module would be restarted without ever going to
+        // "stop"
+        // There is still a chance that 2 concurrent servers are launch with concurrence,
+        // But it is extremely unlikely and anyway doesn't have any side effect expect memory footprint.
+        if let Some(shutdown_sender) = self.shutdown_senders.remove(module_id) {
+            info!(
+                "Listener  {} already started, removing old listener",
+                module_id
+            );
+            shutdown_sender
+                .send(())
+                .map_err(|()| Error::from(ErrorKind::WorkloadService))?;
+        }
 
-        let cert_manager = self.cert_manager.clone();
-        let min_protocol_version = self.min_protocol_version;
+        let (shutdown_sender, shutdown_receiver) = oneshot::channel();
 
         self.shutdown_senders
             .insert(module_id.to_string(), shutdown_sender);
+
+        let cert_manager = self.cert_manager.clone();
+        let min_protocol_version = self.min_protocol_version;
 
         let future = WorkloadService::new(
             &self.key_store,
