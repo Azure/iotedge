17498b1b1e1f37d9a6e1412b56fe23cbef871418
Damon Barry <damonbarry@users.noreply.github.com>
2021-10-22 15:20:26 -0700

    Remove internal test dashboard code (#5738)
    This change removes tools/IoTEdgeDevOps from the repo. It's a test dashboard that is only used internally.

--- tools/IoTEdgeDevOps/IoTEdgeDashboard/AppConfig.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-namespace IoTEdgeDashboard
-{
-    public class AppConfig
-    {
-        public string PersonalAccessToken { get; set; }
-    }
-}
--- tools/IoTEdgeDevOps/VstsPipelineSync/db/dbo.QueryVstsBugsForMatchingBuild.sql
+++
@@ -1,6 +0,0 @@
-CREATE PROCEDURE [dbo].[QueryVstsBugsForMatchingBuild]
-    @BuildId varchar(20)
-AS
-    SELECT 1 FROM dbo.VstsBug WHERE BuildId = @BuildId
-GO
-
