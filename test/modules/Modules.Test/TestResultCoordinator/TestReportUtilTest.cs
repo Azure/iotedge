c42ff5b977572e52cb32857b11032500cf3ea287
brotherneeru <bilalsellak@microsoft.com>
2021-11-09 15:41:14 -0800

    fixed checkin issues

--- test/modules/Modules.Test/TestResultCoordinator/TestReportUtilTest.cs
+++ test/modules/Modules.Test/TestResultCoordinator/TestReportUtilTest.cs
@@ -79,7 +79,8 @@ namespace Modules.Test.TestResultCoordinator
 
             string trackingId = "fakeTrackingId";
             var countingReportMetadata = new CountingReportMetadata(TestDescription, "CountingExpectedSource", "CountingAcutalSource", TestOperationResultType.Messages, TestReportType.CountingReport, false);
-var twinCountingReportMetadata = new TwinCountingReportMetadata(TestDescription, Topology.SingleNode, "TwinExpectedSource", "TwinActualSource", TestReportType.TwinCountingReport, TwinTestPropertyType.Desired);            var deploymentReportMetadata = new DeploymentTestReportMetadata(TestDescription, "DeploymentExpectedSource", "DeploymentActualSource");
+            var twinCountingReportMetadata = new TwinCountingReportMetadata(TestDescription, Topology.SingleNode, "TwinExpectedSource", "TwinActualSource", TestReportType.TwinCountingReport, TwinTestPropertyType.Desired);
+            var deploymentReportMetadata = new DeploymentTestReportMetadata(TestDescription, "DeploymentExpectedSource", "DeploymentActualSource");
             var directMethodConnectivityReportMetadata = new DirectMethodConnectivityReportMetadata(TestDescription, "DirectMethodSenderSource", new TimeSpan(0, 0, 0, 0, 5), "DirectMethodReceiverSource");
             var directMethodConnectivityReportMetadataWithoutReceiverSource = new DirectMethodConnectivityReportMetadata(TestDescription, "DirectMethodSenderSource", new TimeSpan(0, 0, 0, 0, 5), "DirectMethodReceiverSource");
             var directMethodLongHaulReportMetadata = new DirectMethodLongHaulReportMetadata(TestDescription, "DirectMethodSenderSource", "DirectMethodReceiverSource");
