6a5c7369d6505a6bf5f7270727a07f6f8717e7fe
Andrew Smith <als5ev@virginia.edu>
2021-12-08 19:46:59 +0000

    squash dev commits

--- test/modules/TestResultCoordinator/Reports/LegacyTwin/LegacyTwinReportGenerator.cs
+++ test/modules/TestResultCoordinator/Reports/LegacyTwin/LegacyTwinReportGenerator.cs
@@ -11,6 +11,8 @@ namespace TestResultCoordinator.Reports.LegacyTwin
 
     sealed class LegacyTwinReportGenerator : ITestResultReportGenerator
     {
+        const double BigToleranceProportion = .005;
+        const double LittleToleranceProportion = .001;
         static readonly ILogger Logger = ModuleUtil.CreateLogger(nameof(LegacyTwinReportGenerator));
         readonly string trackingId;
 
@@ -47,15 +49,9 @@ namespace TestResultCoordinator.Reports.LegacyTwin
         {
             Logger.LogInformation($"Start to generate report by {nameof(LegacyTwinReportGenerator)} for Sources [{this.SenderSource}] ");
             IDictionary<int, int> results = new Dictionary<int, int>();
-            bool isPassed = true;
             while (await this.SenderTestResults.MoveNextAsync())
             {
                 int status = int.Parse(this.SenderTestResults.Current.Result.Substring(0, 3));
-                if (status > 299)
-                {
-                    isPassed = false;
-                }
-
                 if (results.ContainsKey(status))
                 {
                     results[status] = results[status] + 1;
@@ -65,6 +61,8 @@ namespace TestResultCoordinator.Reports.LegacyTwin
                     results[status] = 1;
                 }
             }
+
+            bool isPassed = this.IsPassed(results);
 
             var report = new LegacyTwinReport(
                 this.TestDescription,
@@ -76,6 +74,77 @@ namespace TestResultCoordinator.Reports.LegacyTwin
 
             Logger.LogInformation($"Successfully finished creating LegacyTwinReport for Source [{this.SenderSource}]");
             return report;
+        }
+
+        // See TwinTester/StatusCode.cs for reference.
+        bool IsPassed(IDictionary<int, int> statusCodesToCount)
+        {
+            bool isPassed = true;
+            int totalResults = statusCodesToCount.Sum(x => x.Value);
+
+            if (totalResults == 0)
+            {
+                return false;
+            }
+
+            // Product issue where:
+            // 1) We don't receive some desired properties in module-registered twin desired property callback
+            // 1) Module cannot make reported property update
+            if (this.Topology == Topology.Nested && this.MqttBrokerEnabled)
+            {
+                int[] bigToleranceStatusCodes = { };
+                int[] littleToleranceStatusCodes = { 501, 504 };
+                isPassed = this.GeneratePassResult(statusCodesToCount, bigToleranceStatusCodes, littleToleranceStatusCodes);
+            }
+            else
+            {
+                List<int> statusCodes = statusCodesToCount.Keys.ToList();
+                IEnumerable<int> failingStatusCodes = statusCodes.Where(s =>
+                {
+                    string statusCode = s.ToString();
+                    return !statusCode.StartsWith("2");
+                });
+
+                isPassed = failingStatusCodes.Count() == 0;
+            }
+
+            return isPassed;
+        }
+
+        bool GeneratePassResult(IDictionary<int, int> statusCodesToCount, int[] bigToleranceStatusCodes, int[] littleToleranceStatusCodes)
+        {
+            int totalResults = statusCodesToCount.Sum(x => x.Value);
+            foreach (KeyValuePair<int, int> statusCodeToCount in statusCodesToCount)
+            {
+                int statusCode = statusCodeToCount.Key;
+                int statusCodeCount = statusCodeToCount.Value;
+
+                // ignore the status codes indicating some success
+                if (statusCode.ToString().StartsWith("2"))
+                {
+                    continue;
+                }
+                else if (bigToleranceStatusCodes.Contains(statusCode))
+                {
+                    if ((double)statusCodeCount / totalResults > BigToleranceProportion)
+                    {
+                        return false;
+                    }
+                }
+                else if (littleToleranceStatusCodes.Contains(statusCode))
+                {
+                    if ((double)statusCodeCount / totalResults > LittleToleranceProportion)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
