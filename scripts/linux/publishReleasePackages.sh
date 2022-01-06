227b4aee50b53d862701d6b40c9522301fed549a
Nishant Manchanda <68707510+nimanch@users.noreply.github.com>
2022-01-06 23:24:31 +0000

    MCR Check

--- scripts/linux/publishReleasePackages.sh
+++ scripts/linux/publishReleasePackages.sh
@@ -82,7 +82,8 @@ sudo rm -rf $OS_NAME-$OS_VERSION-multi-aad.json || true
 # sudo apt-key add tux-devrepo.asc
 # echo "deb [arch=amd64] http://tux-devrepo.corp.microsoft.com/repos/tux-dev/ xenial main" | sudo tee /etc/apt/sources.list.d/tuxdev.list
 # sudo apt-get install -y --no-install-recommends azure-repoapi-client
-echo $(python --version)
+$value=$(az keyvault secret show -n registry-key --vault-name msint-community --query value)
+echo $value
 
 #Download Secrets - Requires az login and proper subscription to be selected
 az keyvault secret download --vault-name iotedge-packages -n private-key-pem -f private-key.pem
