7d478593a2f882b5ca137d359473209e61f5ba81
Nishant Manchanda <68707510+nimanch@users.noreply.github.com>
2022-01-07 19:38:20 +0000

    Cleanup and Commit

--- scripts/linux/publishReleasePackages.sh
+++ scripts/linux/publishReleasePackages.sh
@@ -103,7 +103,6 @@ echo "Work Dir is $WDIR"
 echo "Package OS DIR is $DIR"
 
 #Debug View of Package Dir Path
-#find $DIR | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"
 ls -al $DIR 
 
 #Cleanup
@@ -119,28 +118,25 @@ echo $(cat $WDIR/$OS_NAME-$OS_VERSION-multi-aad.json | jq '.AADClientCertificate
 
 REPOTOOLCMD="docker run -v $WDIR:$CONFIG_DIR -v $DIR:$PACKAGE_DIR --rm msint.azurecr.io/linuxrepos/repoclient:latest repoclient"
 
-#TODO: Enable After Testing
-# #Upload packages
-# output=$($REPOTOOLCMD -c $CONFIG_DIR/$OS_NAME-$OS_VERSION-multi-aad.json -v v3 package add $PACKAGE_DIR/)
-# echo $output
-# status=$(echo $output | jq '.status_code')
-# submission_id=$(echo $output | jq '.message.submissionId')
-# echo "StatusCode: $status"
+#Upload packages
+output=$($REPOTOOLCMD -c $CONFIG_DIR/$OS_NAME-$OS_VERSION-multi-aad.json -v v3 package add $PACKAGE_DIR/)
+echo $output
+status=$(echo $output | jq '.status_code')
+submission_id=$(echo $output | jq '.message.submissionId')
+echo "StatusCode: $status"
 
-# submission_id=$(echo $submission_id | tr -d '"')
-# echo "Submission ID: $submission_id"
+submission_id=$(echo $submission_id | tr -d '"')
+echo "Submission ID: $submission_id"
 
-# if [[ $status != "202" ]]; then
-#     echo "Received Incorrect Upload Status: $status"
-#     exit 1
-# fi
+if [[ $status != "202" ]]; then
+    echo "Received Incorrect Upload Status: $status"
+    exit 1
+fi
 
 #Wait upto 10 Minutes to see if package uploaded
 end_time=$((SECONDS + 600))
 uploaded=false
 
-#TODO: Remove this hardcoded value after test. Just to make sure YAML Build can reach service
-submission_id=61d7d691ea3a771e261fd598
 while [[ $SECONDS -lt $end_time ]]; do
     #Check for Successful Upload of Each of the Packages
     output=($($REPOTOOLCMD -c $CONFIG_DIR/$OS_NAME-$OS_VERSION-multi-aad.json -v v3 request check $submission_id | jq '.message.packages[].status'))
