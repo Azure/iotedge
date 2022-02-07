5069c431edca6d472c8262009e64e3553ae1548b
Anca Antochi <ancan@microsoft.com>
2022-02-07 11:31:16 -0800

    Fix edgeHub shutdown for renew certificate (#6037)
    * fix shutdown and E2E test

--- edge-hub/core/src/Microsoft.Azure.Devices.Edge.Hub.Mqtt/MqttProtocolHead.cs
+++ edge-hub/core/src/Microsoft.Azure.Devices.Edge.Hub.Mqtt/MqttProtocolHead.cs
@@ -50,6 +50,8 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Mqtt
 
         IChannel serverChannel;
         IEventLoopGroup eventLoopGroup;
+        IEventLoopGroup wsEventLoopGroup;
+        IEventLoopGroup parentEventLoopGroup;
 
         public MqttProtocolHead(
             ISettingsProvider settingsProvider,
@@ -106,13 +108,15 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Mqtt
         {
             try
             {
-                this.logger.LogInformation("Stopping");
+                this.logger.LogInformation("Stopping MQTT protocol head");
 
                 await (this.serverChannel?.CloseAsync() ?? TaskEx.Done);
                 await (this.eventLoopGroup?.ShutdownGracefullyAsync() ?? TaskEx.Done);
+                await (this.parentEventLoopGroup?.ShutdownGracefullyAsync() ?? TaskEx.Done);
+                await (this.wsEventLoopGroup?.ShutdownGracefullyAsync() ?? TaskEx.Done);
                 // TODO: gracefully shutdown the MultithreadEventLoopGroup in MqttWebSocketListener?
                 // TODO: this.webSocketListenerRegistry.TryUnregister("mqtts")?
-                this.logger.LogInformation("Stopped");
+                this.logger.LogInformation("Stopped MQTT protocol head");
             }
             catch (Exception ex)
             {
@@ -123,7 +127,6 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Mqtt
         public void Dispose()
         {
             this.mqttConnectionProvider.Dispose();
-            this.CloseAsync(CancellationToken.None).Wait();
         }
 
         ServerBootstrap SetupServerBootstrap()
@@ -138,11 +141,10 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Mqtt
 
             var bootstrap = new ServerBootstrap();
             // multithreaded event loop that handles the incoming connection
-            IEventLoopGroup parentEventLoopGroup = new MultithreadEventLoopGroup(parentEventLoopCount);
+            this.parentEventLoopGroup = new MultithreadEventLoopGroup(parentEventLoopCount);
             // multithreaded event loop (worker) that handles the traffic of the accepted connections
             this.eventLoopGroup = new MultithreadEventLoopGroup(threadCount);
-
-            bootstrap.Group(parentEventLoopGroup, this.eventLoopGroup)
+            bootstrap.Group(this.parentEventLoopGroup, this.eventLoopGroup)
                 .Option(ChannelOption.SoBacklog, listenBacklogSize)
                 // Allow listening socket to force bind to port if previous socket is still in TIME_WAIT
                 // Fixes "address is already in use" errors
@@ -185,6 +187,7 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Mqtt
                                     bridgeFactory));
                         }));
 
+            this.wsEventLoopGroup = new MultithreadEventLoopGroup(Environment.ProcessorCount);
             var mqttWebSocketListener = new MqttWebSocketListener(
                 settings,
                 bridgeFactory,
@@ -192,7 +195,7 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Mqtt
                 this.usernameParser,
                 this.clientCredentialsFactory,
                 () => this.sessionProvider,
-                new MultithreadEventLoopGroup(Environment.ProcessorCount),
+                this.wsEventLoopGroup,
                 this.byteBufferAllocator,
                 AutoRead,
                 maxInboundMessageSize,
