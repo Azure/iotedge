ab86bb87efde3a6913259337fde3de2ea0415038
vipeller <51135538+vipeller@users.noreply.github.com>
2022-02-07 20:33:51 -0800

    added workaround for windows-certificate import problem (#6087)

--- edge-util/src/Microsoft.Azure.Devices.Edge.Util/CertificateHelper.cs
+++ edge-util/src/Microsoft.Azure.Devices.Edge.Util/CertificateHelper.cs
@@ -485,6 +485,13 @@ namespace Microsoft.Azure.Devices.Edge.Util
                 throw new InvalidOperationException(errorMessage);
             }
 
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // On Windows the certificate in 'result' gives an error when used with kestrel: "No credentials are available in the security"
+                // This is a suggested workaround that seems working (https://github.com/dotnet/runtime/issues/45680)
+                result = new X509Certificate2(result.Export(X509ContentType.Pkcs12));
+            }
+
             return result;
         }
 
