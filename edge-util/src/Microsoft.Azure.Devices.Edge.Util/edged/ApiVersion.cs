7666cba081492792b7e31dbb1e00d5a0e7c6c6e0
onalante-msft <89409054+onalante-msft@users.noreply.github.com>
2022-03-21 12:04:31 -0700

    Revert VersionUnknown value change

--- edge-util/src/Microsoft.Azure.Devices.Edge.Util/edged/ApiVersion.cs
+++ edge-util/src/Microsoft.Azure.Devices.Edge.Util/edged/ApiVersion.cs
@@ -6,13 +6,13 @@ namespace Microsoft.Azure.Devices.Edge.Util.Edged
 
     public sealed class ApiVersion : IComparable<ApiVersion>
     {
-        public static readonly ApiVersion VersionUnknown = new ApiVersion(0, "Unknown");
         public static readonly ApiVersion Version20180628 = new ApiVersion(1, "2018-06-28");
         public static readonly ApiVersion Version20190130 = new ApiVersion(2, "2019-01-30");
         public static readonly ApiVersion Version20191022 = new ApiVersion(3, "2019-10-22");
         public static readonly ApiVersion Version20191105 = new ApiVersion(4, "2019-11-05");
         public static readonly ApiVersion Version20200707 = new ApiVersion(5, "2020-07-07");
         public static readonly ApiVersion Version20211207 = new ApiVersion(6, "2021-12-07");
+        public static readonly ApiVersion VersionUnknown = new ApiVersion(100, "Unknown");
 
         static readonly Dictionary<string, ApiVersion> Instance = new Dictionary<string, ApiVersion>
         {
