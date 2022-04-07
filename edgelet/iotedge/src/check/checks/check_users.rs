11220b6bad61bcc948da6b071cfabe8c6badb452
Nishant <68707510+nimanch@users.noreply.github.com>
2022-04-07 08:57:10 -0700

    Add Check to Verify Users (#6256)

---
+++ edgelet/iotedge/src/check/checks/check_users.rs
@@ -0,0 +1,39 @@
+use failure::Context;
+
+use crate::{
+    check::{Check, CheckResult, Checker, CheckerMeta},
+    internal::common::get_system_user,
+};
+
+#[derive(Default, serde_derive::Serialize)]
+pub(crate) struct CheckUsers {}
+
+#[async_trait::async_trait]
+impl Checker for CheckUsers {
+    fn meta(&self) -> CheckerMeta {
+        CheckerMeta {
+            id: "check-users",
+            description: "IoT Edge Users are valid and can be queried from the system",
+        }
+    }
+
+    async fn execute(&mut self, check: &mut Check) -> CheckResult {
+        self.inner_execute(check)
+            .await
+            .unwrap_or_else(CheckResult::Failed)
+    }
+}
+
+impl CheckUsers {
+    #[allow(clippy::unused_self)]
+    #[allow(unused_variables)]
+    async fn inner_execute(&mut self, check: &mut Check) -> Result<CheckResult, failure::Error> {
+        // Todo : Add Check for TPM User.
+        for user in ["aziotcs", "aziotks", "aziotid", "iotedge"] {
+            if let Err(e) = get_system_user(user) {
+                return Ok(CheckResult::Failed(Context::new(format!("{}", e)).into()));
+            }
+        }
+        Ok(CheckResult::Ok)
+    }
+}
