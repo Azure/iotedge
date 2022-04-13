d95602be146cb1fba5aeac587950b2e6f7780fbe
Nishant Manchanda <68707510+nimanch@users.noreply.github.com>
2022-04-13 22:51:25 +0000

    Incremental Changes

--- edgelet/iotedge/src/check/checks/check_sockets.rs
+++ edgelet/iotedge/src/check/checks/check_sockets.rs
@@ -1,9 +1,14 @@
+use std::os::unix::prelude::PermissionsExt;
+
 use edgelet_core::UrlExt;
 use edgelet_settings::RuntimeSettings;
 use failure::{Context, ResultExt};
 use url::Url;
 
 use crate::check::{Check, CheckResult, Checker, CheckerMeta};
+
+const MANAGEMENT_SOCKET_DEFAULT_PERMISSION: u32 = 0o660;
+const WORKLOAD_SOCKET_DEFAULT_PERMISSION: u32 = 0o666;
 
 #[derive(Default, serde_derive::Serialize)]
 pub(crate) struct CheckSockets {}
@@ -37,15 +42,11 @@ impl CheckSockets {
                 todo!()
             }
             None => {
-                // Only check for Management Socket because if settings have not been initialized, no workload socket exists
+                // Check for Existence and Permissions of Both Management and Legacy Workload Connect Sockets
                 let connect_management_uri: Url = std::env::var("IOTEDGE_CONNECT_MANAGEMENT_URI")
                     .unwrap_or_else(|_| "unix:///var/run/iotedge/mgmt.sock".to_string())
                     .parse()
                     .expect("failed to parse connnect management uri");
-                let listen_management_uri: Url = std::env::var("IOTEDGE_LISTEN_MANAGEMENT_URI")
-                    .unwrap_or_else(|_| "fd://aziot-edged.mgmt.socket".to_string())
-                    .parse()
-                    .expect("failed to parse listen management uri");
 
                 let connect_management_socket_path = connect_management_uri
                     .to_uds_file_path()
@@ -53,22 +54,59 @@ impl CheckSockets {
                     "Could not parse connect.management_uri: does not represent a valid file path",
                 )?;
 
-                if connect_management_socket_path.exists() {
-                    return Ok(CheckResult::Ok);
-                } else {
+                if !connect_management_socket_path.exists() {
                     return Ok(CheckResult::Failed(
                         Context::new(format!("Did not find connect management socket")).into(),
                     ));
                 }
-                // let connect_workload_socker_path = connect_workload_uri.to_uds_file_path().context("Could not parse connect.workload_url: does not represent a valid file path")?;
 
-                // if connect_workload_socker_path.exists(){
-                //     return Ok(CheckResult::Ok)
-                // }
-                // else
-                // {
-                //     return Ok(CheckResult::Failed(Context::new(format!("Did not find connect workload socket")).into()))
-                // }
+                let mgmt_socket_permission = connect_management_socket_path
+                    .metadata()?
+                    .permissions()
+                    .mode();
+                if mgmt_socket_permission != MANAGEMENT_SOCKET_DEFAULT_PERMISSION {
+                    return Ok(CheckResult::Failed(
+                        Context::new(format!(
+                            "Incorrect Permission for Connect Management Socket {:o}",
+                            mgmt_socket_permission
+                        ))
+                        .into(),
+                    ));
+                }
+
+                let legacy_connect_workload_uri: Url =
+                    std::env::var("IOTEDGE_CONNECT_WORKLOAD_URI")
+                        .unwrap_or_else(|_| "unix:///var/run/iotedge/workload.sock".to_string())
+                        .parse()
+                        .expect("failed to parse legacy connnect workload uri");
+
+                let workload_management_socket_path = legacy_connect_workload_uri
+                    .to_uds_file_path()
+                    .context(
+                    "Could not parse connect.workload_uri: does not represent a valid file path",
+                )?;
+
+                if !workload_management_socket_path.exists() {
+                    return Ok(CheckResult::Failed(
+                        Context::new(format!("Did not find connect workload socket")).into(),
+                    ));
+                }
+
+                let workload_socket_permission = workload_management_socket_path
+                    .metadata()?
+                    .permissions()
+                    .mode();
+                if workload_socket_permission != WORKLOAD_SOCKET_DEFAULT_PERMISSION {
+                    return Ok(CheckResult::Failed(
+                        Context::new(format!(
+                            "Incorrect Permission for Connect Workload Socket: {:o}",
+                            workload_socket_permission
+                        ))
+                        .into(),
+                    ));
+                }
+
+                Ok(CheckResult::Ok)
             }
         }
     }
