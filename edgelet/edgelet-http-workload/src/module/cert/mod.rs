eaaac7efb518cbae427eacf8ece2df703e1899d9
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2022-04-15 17:50:25 +0000

    Remove old cert renewal behavior

--- edgelet/edgelet-http-workload/src/module/cert/mod.rs
+++ edgelet/edgelet-http-workload/src/module/cert/mod.rs
@@ -7,15 +7,11 @@ pub(crate) mod server;
 use aziot_cert_client_async::Client as CertClient;
 #[cfg(not(test))]
 use aziot_key_client_async::Client as KeyClient;
-#[cfg(not(test))]
-use aziot_key_openssl_engine as KeyEngine;
 
 #[cfg(test)]
 use test_common::client::CertClient;
 #[cfg(test)]
 use test_common::client::KeyClient;
-#[cfg(test)]
-use test_common::client::KeyEngine;
 
 #[derive(Debug, serde::Serialize)]
 #[cfg_attr(test, derive(serde::Deserialize))]
@@ -41,11 +37,9 @@ pub(crate) enum SubjectAltName {
 }
 
 struct CertApi {
-    key_connector: http_common::Connector,
     key_client: std::sync::Arc<futures_util::lock::Mutex<KeyClient>>,
     cert_client: std::sync::Arc<futures_util::lock::Mutex<CertClient>>,
 
-    device_id: String,
     edge_ca_cert: String,
     edge_ca_key: String,
 }
@@ -58,10 +52,8 @@ impl CertApi {
         config: &crate::WorkloadConfig,
     ) -> Self {
         CertApi {
-            key_connector,
             key_client,
             cert_client,
-            device_id: config.device_id.clone(),
             edge_ca_cert: config.edge_ca_cert.clone(),
             edge_ca_key: config.edge_ca_key.clone(),
         }
@@ -81,17 +73,14 @@ impl CertApi {
         let csr = new_csr(common_name, keys, subject_alt_names, extensions)
             .map_err(|_| edgelet_http::error::server_error("failed to generate csr"))?;
 
-        let edge_ca_key_handle =
-            edge_ca_key_handle(self.key_client.clone(), &self.edge_ca_key).await?;
+        let edge_ca_key_handle = {
+            let key_client = self.key_client.lock().await;
 
-        check_edge_ca(
-            self.cert_client.clone(),
-            &self.edge_ca_cert,
-            &self.device_id,
-            &edge_ca_key_handle,
-            self.key_connector.clone(),
-        )
-        .await?;
+            key_client
+                .load_key_pair(&self.edge_ca_key)
+                .await
+                .map_err(|_| edgelet_http::error::server_error("failed to get edge CA key"))?
+        };
 
         let cert = self
             .create_cert(&cert_id, &csr, &edge_ca_key_handle)
@@ -221,131 +210,6 @@ fn key_to_pem(key: &openssl::pkey::PKey<openssl::pkey::Private>) -> String {
     key_pem
 }
 
-pub(crate) async fn edge_ca_key_handle(
-    key_client: std::sync::Arc<futures_util::lock::Mutex<KeyClient>>,
-    edge_ca_key: &str,
-) -> Result<aziot_key_common::KeyHandle, http_common::server::Error> {
-    let key_client = key_client.lock().await;
-
-    key_client
-        .create_key_pair_if_not_exists(edge_ca_key, Some("rsa-2048:*"))
-        .await
-        .map_err(|_| edgelet_http::error::server_error("failed to retrieve edge ca key"))
-}
-
-pub(crate) async fn check_edge_ca(
-    cert_client: std::sync::Arc<futures_util::lock::Mutex<CertClient>>,
-    edge_ca_cert: &str,
-    device_id: &str,
-    key_handle: &aziot_key_common::KeyHandle,
-    key_connector: http_common::Connector,
-) -> Result<(), http_common::server::Error> {
-    let cert_client = cert_client.lock().await;
-
-    log::info!("Checking Edge CA certificate...");
-
-    if should_renew(&cert_client, edge_ca_cert).await? {
-        log::info!("Requesting new Edge CA certificate...");
-
-        let common_name = format!("aziot-edge CA {}", device_id);
-        let keys = edge_ca_keys(key_connector, key_handle)?;
-
-        let extensions = edge_ca_extensions().map_err(|_| {
-            edgelet_http::error::server_error("failed to set edge ca csr extensions")
-        })?;
-
-        let csr = new_csr(common_name, keys, Vec::new(), extensions)
-            .map_err(|_| edgelet_http::error::server_error("failed to generate edge ca csr"))?;
-
-        cert_client
-            .create_cert(edge_ca_cert, &csr, None)
-            .await
-            .map_err(|_| edgelet_http::error::server_error("failed to create edge ca cert"))?;
-
-        log::info!("Created new Edge CA certificate");
-    } else {
-        log::info!("Current Edge CA certificate does not need to be renewed");
-    }
-
-    Ok(())
-}
-
-async fn should_renew(
-    cert_client: &CertClient,
-    cert_id: &str,
-) -> Result<bool, http_common::server::Error> {
-    match cert_client.get_cert(cert_id).await {
-        Ok(cert) => {
-            let cert = openssl::x509::X509::from_pem(&cert)
-                .map_err(|_| edgelet_http::error::server_error("failed to parse edge ca cert"))?;
-
-            let current_time =
-                openssl::asn1::Asn1Time::days_from_now(0).expect("current time must be valid");
-
-            let diff = current_time.diff(cert.not_after()).map_err(|_| {
-                edgelet_http::error::server_error("failed to determine edge ca expiration time")
-            })?;
-            let diff = i64::from(diff.secs) + i64::from(diff.days) * 86400;
-
-            // Renew certificate if it expires in the next 5 minutes.
-            Ok(diff < 300)
-        }
-        Err(_) => Ok(true),
-    }
-}
-
-fn edge_ca_keys(
-    key_connector: http_common::Connector,
-    key_handle: &aziot_key_common::KeyHandle,
-) -> Result<
-    (
-        openssl::pkey::PKey<openssl::pkey::Private>,
-        openssl::pkey::PKey<openssl::pkey::Public>,
-    ),
-    http_common::server::Error,
-> {
-    // The openssl engine must use a sync client. Elsewhere, the async client is used.
-    let key_client = aziot_key_client::Client::new(
-        aziot_key_common_http::ApiVersion::V2020_09_01,
-        key_connector,
-    );
-    let key_client = std::sync::Arc::new(key_client);
-    let key_handle =
-        std::ffi::CString::new(key_handle.0.clone()).expect("key handle contained null");
-
-    let mut engine = KeyEngine::load(key_client)
-        .map_err(|_| edgelet_http::error::server_error("failed to load openssl key engine"))?;
-
-    let private_key = engine
-        .load_private_key(&key_handle)
-        .map_err(|_| edgelet_http::error::server_error("failed to load edge ca private key"))?;
-
-    let public_key = engine
-        .load_public_key(&key_handle)
-        .map_err(|_| edgelet_http::error::server_error("failed to load edge ca public key"))?;
-
-    Ok((private_key, public_key))
-}
-
-fn edge_ca_extensions(
-) -> Result<openssl::stack::Stack<openssl::x509::X509Extension>, openssl::error::ErrorStack> {
-    let mut csr_extensions = openssl::stack::Stack::new()?;
-
-    let mut key_usage = openssl::x509::extension::KeyUsage::new();
-    key_usage.critical().digital_signature().key_cert_sign();
-
-    let mut basic_constraints = openssl::x509::extension::BasicConstraints::new();
-    basic_constraints.ca().critical().pathlen(0);
-
-    let key_usage = key_usage.build()?;
-    let basic_constraints = basic_constraints.build()?;
-
-    csr_extensions.push(key_usage)?;
-    csr_extensions.push(basic_constraints)?;
-
-    Ok(csr_extensions)
-}
-
 #[cfg(test)]
 mod tests {
     fn test_api() -> super::CertApi {
@@ -360,90 +224,11 @@ mod tests {
         let cert_client = std::sync::Arc::new(futures_util::lock::Mutex::new(cert_client));
 
         super::CertApi {
-            key_connector,
             key_client,
             cert_client,
 
-            device_id: "test-device".to_string(),
             edge_ca_cert: "test-device-cert".to_string(),
             edge_ca_key: "test-device-key".to_string(),
-        }
-    }
-
-    #[tokio::test]
-    async fn edge_ca_renew() {
-        // Test data. Tuples contain a test certificate and whether the test should expect
-        // the cert to be renewed.
-        let test_certs = vec![
-            // Expired certificate.
-            (
-                test_common::credential::custom_test_certificate("testCertificate", |cert| {
-                    let expired = openssl::asn1::Asn1Time::from_unix(1).unwrap();
-                    cert.set_not_after(&expired).unwrap();
-                }),
-                true,
-            ),
-            // Certificate that is near expiry.
-            (
-                test_common::credential::custom_test_certificate("testCertificate", |cert| {
-                    let now = std::time::SystemTime::now()
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .unwrap();
-
-                    // Certs within 5 mins of expiration should renew, so set an expiration
-                    // time 2 mins from now.
-                    let expiry_time = now.as_secs() + 120;
-                    let expiry_time: libc::time_t =
-                        std::convert::TryInto::try_into(expiry_time).unwrap();
-
-                    let expiry_time = openssl::asn1::Asn1Time::from_unix(expiry_time).unwrap();
-                    cert.set_not_after(&expiry_time).unwrap();
-                }),
-                true,
-            ),
-            // Certificate that is not near expiry.
-            (
-                test_common::credential::test_certificate("testCertificate"),
-                false,
-            ),
-        ];
-
-        for ((cert, _), should_renew) in test_certs {
-            let api = test_api();
-            let cert = cert.to_pem().unwrap();
-
-            // Place test certificate in cert client.
-            {
-                let cert_client = api.cert_client.lock().await;
-                cert_client
-                    .create_cert(&api.edge_ca_cert, &cert, None)
-                    .await
-                    .unwrap();
-            }
-
-            let key_handle = aziot_key_common::KeyHandle(api.edge_ca_key.clone());
-            super::check_edge_ca(
-                api.cert_client.clone(),
-                &api.edge_ca_cert,
-                &api.device_id,
-                &key_handle,
-                api.key_connector.clone(),
-            )
-            .await
-            .unwrap();
-
-            let new_cert = {
-                let cert_client = api.cert_client.lock().await;
-                cert_client.get_cert(&api.edge_ca_cert).await.unwrap()
-            };
-
-            // If the cert should have been renewed, check that the new cert is different.
-            // If the cert shouldn't have been renewed, check that it is unchanged.
-            if should_renew {
-                assert_ne!(new_cert, cert);
-            } else {
-                assert_eq!(new_cert, cert);
-            }
         }
     }
 
@@ -456,8 +241,8 @@ mod tests {
             (cert_client.issuer.clone(), cert_client.issuer_key.clone())
         };
 
-        // It doesn't matter what extensions we use for this test, so just use the edge CA extensions.
-        let extensions = super::edge_ca_extensions().unwrap();
+        // It doesn't matter what extensions we use for this test, so just use an empty stack.
+        let extensions = openssl::stack::Stack::new().unwrap();
 
         let response = api
             .issue_cert(
