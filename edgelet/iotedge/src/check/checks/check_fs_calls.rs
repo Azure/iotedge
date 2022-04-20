908ccbf004cdd0afdfadfa5c434a15baff38c8ce
gaya <33068020+ggjjj@users.noreply.github.com>
2022-04-20 17:16:33 +0000

    change to home directory

--- edgelet/iotedge/src/check/checks/check_fs_calls.rs
+++ edgelet/iotedge/src/check/checks/check_fs_calls.rs
@@ -1,6 +1,9 @@
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
+
+use edgelet_settings::RuntimeSettings;
 use failure::ResultExt;
 
 use crate::{
@@ -30,32 +33,39 @@ impl CheckFsCalls {
     #[allow(clippy::unused_self)]
     #[allow(unused_variables)]
     async fn inner_execute(&mut self, check: &mut Check) -> Result<CheckResult, failure::Error> {
+        let home_dir_str = match edgelet_settings::Settings::new() {
+            Ok(settings) => settings.homedir().to_str().unwrap().to_owned(),
+            _ => "/var/lib/aziot/edged".to_string(),
+        };
+
+        let home_dir = Path::new(&home_dir_str);
+
         // Test Read file and Remove file
-        File::create("createfile_test.txt").with_context(|_| format!("Unable to create test file for file read test"))?;
-        fs::read("createfile_test.txt").with_context(|_| format!("Unable to read the test file"))?;
-        fs::remove_file("createfile_test.txt").with_context(|_| format!("Unable to cleanup file create test file"))?;
+        File::create(home_dir.join("createfile_test.txt")).with_context(|_| format!("Unable to create test file for file read test"))?;
+        fs::read(home_dir.join("createfile_test.txt")).with_context(|_| format!("Unable to read the test file"))?;
+        fs::remove_file(home_dir.join("createfile_test.txt")).with_context(|_| format!("Unable to cleanup file create test file"))?;
 
         // Test write operation
-        fs::write("writefile_test.txt", "Testing write operation").with_context(|_| format!("Unable to write test file"))?;
-        fs::remove_file("writefile_test.txt").with_context(|_| format!("Unable to cleanup file write test file"))?;
+        fs::write(home_dir.join("writefile_test.txt"), "Testing write operation").with_context(|_| format!("Unable to write test file"))?;
+        fs::remove_file(home_dir.join("writefile_test.txt")).with_context(|_| format!("Unable to cleanup file write test file"))?;
 
         // Test read_to_string function
-        let mut test_file = File::create("read_to_string_test.txt").with_context(|_| format!("Unable to create test file for read_to_string test"))?;
+        let mut test_file = File::create(home_dir.join("read_to_string_test.txt")).with_context(|_| format!("Unable to create test file for read_to_string test"))?;
         test_file.write_all(b"Testing Read to string fucntion").with_context(|_| format!("Unable to write contents to test file"))?;
         let mut file_contents = String::new();
-        let mut open_file = File::open("read_to_string_test.txt").with_context(|_| format!("Unable to open test file for read_to_string test"))?;
+        let mut open_file = File::open(home_dir.join("read_to_string_test.txt")).with_context(|_| format!("Unable to open test file for read_to_string test"))?;
         open_file.read_to_string(&mut file_contents).with_context(|_| format!("Unable to read the file contents as string"))?;
         assert_eq!(file_contents, "Testing Read to string fucntion");
-        fs::remove_file("read_to_string_test.txt").with_context(|_| format!("Unable to cleanup file read_to_string test file"))?;
+        fs::remove_file(home_dir.join("read_to_string_test.txt")).with_context(|_| format!("Unable to cleanup file read_to_string test file"))?;
 
         // Test create_dir and remove_dir 
-        fs::create_dir("createdirtest1").with_context(|_| format!("Unable to create test directory"))?;
-        fs::remove_dir("createdirtest1").with_context(|_| format!("Unable to remove test directory"))?;
+        fs::create_dir(home_dir.join("createdirtest1")).with_context(|_| format!("Unable to create test directory"))?;
+        fs::remove_dir(home_dir.join("createdirtest1")).with_context(|_| format!("Unable to remove test directory"))?;
 
         // Test create_dir_all, read_dir and remove_dir_alliot
-        fs::create_dir_all("createtestdir2/level1/level2").with_context(|_| format!("Unable to perform create_dir_all"))?;
-        fs::read_dir("createtestdir2/level1/level2").with_context(|_| format!("Unable to perform read_dir"))?;
-        fs::remove_dir_all("createtestdir2").with_context(|_| format!("Unable to perform create_dir_all"))?;
+        fs::create_dir_all(home_dir.join("createtestdir2/level1/level2")).with_context(|_| format!("Unable to perform create_dir_all"))?;
+        fs::read_dir(home_dir.join("createtestdir2/level1/level2")).with_context(|_| format!("Unable to perform read_dir"))?;
+        fs::remove_dir_all(home_dir.join("createtestdir2")).with_context(|_| format!("Unable to perform create_dir_all"))?;
 
         Ok(CheckResult::Ok)
     }
