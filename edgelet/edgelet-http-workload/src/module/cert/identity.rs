a325ae6793edfb96472e944cc84418f1abf8538c
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2022-05-10 18:53:32 +0000

    Remove unused static lifetimes

--- edgelet/edgelet-http-workload/src/module/cert/identity.rs
+++ edgelet/edgelet-http-workload/src/module/cert/identity.rs
@@ -14,7 +14,7 @@ where
 #[async_trait::async_trait]
 impl<M> http_common::server::Route for Route<M>
 where
-    M: edgelet_core::ModuleRuntime + Send + Sync + 'static,
+    M: edgelet_core::ModuleRuntime + Send + Sync,
 {
     type ApiVersion = edgelet_http::ApiVersion;
     fn api_version() -> &'static dyn http_common::DynRangeBounds<Self::ApiVersion> {
