a863b4c53382c350822cc11b8ec1854173d98588
Joseph Knierman <joknierm@microsoft.com>
2022-05-20 11:19:24 -0700

    Revert "Mariner ARM64 Build [Squashed] (#6356)"
    This reverts commit 591cdf98b2fb85012c766fa176aba7165bf9dead.

--- edgelet/build/linux/package-mariner.sh
+++ edgelet/build/linux/package-mariner.sh
@@ -8,6 +8,26 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 export DEBIAN_FRONTEND=noninteractive
 export TZ=UTC
 
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:longsleep/golang-backports
+apt-get update -y
+apt-get install -y \
+    cmake curl gcc g++ git jq make pkg-config \
+    libclang1 libssl-dev llvm-dev \
+    cpio genisoimage golang-1.13-go qemu-utils pigz python-pip python3-distutils rpm tar wget
+
+rm -f /usr/bin/go
+ln -vs /usr/lib/go-1.13/bin/go /usr/bin/go
+if [ -f /.dockerenv ]; then
+    mv /.dockerenv /.dockerenv.old
+fi
+
+echo 'Installing rustup'
+curl -sSLf https://sh.rustup.rs | sh -s -- -y
+. ~/.cargo/env
+
 
 # need to use preview repo for the next 2 weeks untill mariner 2.0 gets moved to prod
 case "${MARINER_RELEASE}" in
@@ -28,45 +48,12 @@ EDGELET_ROOT="${BUILD_REPOSITORY_LOCALPATH}/edgelet"
 MARINER_BUILD_ROOT="${BUILD_REPOSITORY_LOCALPATH}/builds/${MarinerIdentity}"
 REVISION="${REVISION:-1}"
 
-apt-get update -y
-apt-get upgrade -y
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:longsleep/golang-backports
-apt-get update -y
-apt-get install -y \
-    cmake curl gcc g++ git jq make pkg-config \
-    libclang1 libssl-dev llvm-dev \
-    cpio genisoimage golang-1.13-go qemu-utils pigz python-pip python3-distutils rpm tar wget
-
-rm -f /usr/bin/go
-ln -vs /usr/lib/go-1.13/bin/go /usr/bin/go
-if [ -f /.dockerenv ]; then
-    mv /.dockerenv /.dockerenv.old
-fi
-
-# Download Mariner repo and build toolkit
-mkdir -p ${MARINER_BUILD_ROOT}
-MarinerToolkitDir='/tmp/CBL-Mariner'
-if ! [ -f "$MarinerToolkitDir/toolkit.tar.gz" ]; then
-    rm -rf "$MarinerToolkitDir"
-    git clone 'https://github.com/microsoft/CBL-Mariner.git' --branch "$MARINER_RELEASE" --depth 1 "$MarinerToolkitDir"
-    pushd "$MarinerToolkitDir/toolkit/"
-    make package-toolkit REBUILD_TOOLS=y
-    popd
-    cp "$MarinerToolkitDir"/out/toolkit-*.tar.gz "${MARINER_BUILD_ROOT}/toolkit.tar.gz"
-    rm -rf MarinerToolkitDir
-fi
-
-echo 'Installing rustup'
-curl -sSLf https://sh.rustup.rs | sh -s -- -y
-. ~/.cargo/env
-
 pushd $EDGELET_ROOT
 case "${MARINER_ARCH}" in
     'x86_64')
         rustup target add x86_64-unknown-linux-gnu
         ;;
-    'aarch64')
+    'arm64')
         rustup target add aarch64-unknown-linux-gnu
         ;;
 esac
@@ -135,6 +122,19 @@ cp -r ~/.cargo "rust"
 cp -r ~/.rustup "rust"
 tar cf "${MARINER_BUILD_ROOT}/SPECS/aziot-edge/SOURCES/rust.tar.gz" "rust"
 popd
+
+# Download Mariner repo and build toolkit
+echo "Cloning the \"${MARINER_RELEASE}\" tag of the CBL-Mariner repo."
+git clone https://github.com/microsoft/CBL-Mariner.git
+pushd CBL-Mariner
+git checkout ${MARINER_RELEASE}
+pushd toolkit
+make package-toolkit REBUILD_TOOLS=y
+popd
+mv out/toolkit-*.tar.gz "${MARINER_BUILD_ROOT}/toolkit.tar.gz"
+popd
+
+rm -rf CBL-Mariner
 
 # copy over IIS RPM
 mkdir -p ${MARINER_BUILD_ROOT}/out/RPMS/${MARINER_ARCH}
