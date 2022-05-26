a01dff567ea43e0ee3cb5296b97c6299fbe9ff75
Joseph Knierman <joknierm@microsoft.com>
2022-05-26 12:22:03 -0700

    use iis main branch

--- edgelet/build/linux/package-mariner.sh
+++ edgelet/build/linux/package-mariner.sh
@@ -98,7 +98,7 @@ replace-with = "vendored-sources"
 
 [source."https://github.com/Azure/iot-identity-service"]
 git = "https://github.com/Azure/iot-identity-service"
-branch = "release/1.2"
+branch = "main"
 replace-with = "vendored-sources"
 
 [source.vendored-sources]
