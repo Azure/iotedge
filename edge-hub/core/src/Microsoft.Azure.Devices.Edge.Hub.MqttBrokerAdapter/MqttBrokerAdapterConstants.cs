85084e4f04aafbd7b68931e80d3c84f28eb47585
Damon Barry <damonbarry@users.noreply.github.com>
2022-06-15 21:58:38 -0700

    Remove experimental mqtt broker code (#6410)
    This change removes `edge-hub/watchdog`  and most of `mqtt/` from the codebase and removes the experimental feature flag in Edge Hub. It also removes the test module `generic-mqtt-tester`, which is no longer needed. Finally, it removes several unused build/test pipelines, most of which were related to the broker.
    
    To test, I ran the following pipelines:
    - Build Images
    - Edgelet Packages (pretty sure this one was unnecessary)
    - Single-node End-to-end
    - Single-node Connectivity
    - Nested End-to-end
    
    ## Azure IoT Edge PR checklist:

--- edge-hub/core/src/Microsoft.Azure.Devices.Edge.Hub.MqttBrokerAdapter/MqttBrokerAdapterConstants.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-namespace Microsoft.Azure.Devices.Edge.Hub.MqttBrokerAdapter
-{
-    public static class MqttBrokerAdapterConstants
-    {
-        public const string DirectTopicPrefix = "$edgehub";
-        public const string IndirectTopicPrefix = "$iothub";
-    }
-}
