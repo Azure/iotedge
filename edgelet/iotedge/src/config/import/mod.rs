8a9d135911d2b6a4b05dfd35b3c2288fe37ef378
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2022-06-25 02:24:32 +0000

    Update test files

--- edgelet/iotedge/src/config/import/mod.rs
+++ edgelet/iotedge/src/config/import/mod.rs
@@ -350,7 +350,7 @@ fn execute_inner(
                 (
                     Some(super_config::EdgeCa::Quickstart {
                         auto_generated_edge_ca_expiry_days: auto_generated_ca_lifetime_days.into(),
-                        auto_renew: None,
+                        auto_renew: cert_renewal::AutoRenewConfig::default(),
                         subject: None,
                     }),
                     None,
