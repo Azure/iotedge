283ed25dc76616ca336cc6412dc99b43b4920fd2
onalante-msft <89409054+onalante-msft@users.noreply.github.com>
2022-06-27 16:00:44 -0700

    rustfmt

--- edgelet/edgelet-http-mgmt/src/module/create_or_list.rs
+++ edgelet/edgelet-http-mgmt/src/module/create_or_list.rs
@@ -51,11 +51,10 @@ where
     async fn get(self) -> http_common::server::RouteResponse {
         let runtime = self.runtime.lock().await;
 
-        let modules =
-            runtime
-                .list_with_details()
-                .await
-                .map_err(|err| edgelet_http::error::runtime_error(&*runtime, &err))?;
+        let modules = runtime
+            .list_with_details()
+            .await
+            .map_err(|err| edgelet_http::error::runtime_error(&*runtime, &err))?;
 
         let res: edgelet_http::ListModulesResponse = modules.into();
         let res = http_common::server::response::json(hyper::StatusCode::OK, &res);
