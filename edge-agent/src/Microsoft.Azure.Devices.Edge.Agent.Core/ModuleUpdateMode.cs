1213b6bc307eded98e2fc10f9b68321de8420672
Andrew Smith <als5ev@virginia.edu>
2022-06-28 20:00:51 +0000

    remove comment

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/ModuleUpdateMode.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/ModuleUpdateMode.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 namespace Microsoft.Azure.Devices.Edge.Agent.Core
 {
-    // TODO: move somewhere better
     public enum ModuleUpdateMode
     {
         WaitForAll,
