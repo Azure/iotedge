3f7e5bbb850a6095d4c49bb5eff1fd1169987d95
Joseph Knierman <joknierm@microsoft.com>
2022-08-03 23:39:06 -0700

    compress e2e changes

--- test/Microsoft.Azure.Devices.Edge.Test.Common/linux/PackageManagement.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/linux/PackageManagement.cs
@@ -68,7 +68,16 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common.Linux
                         "sudo mv -f ~/override.conf ${pathToOverride}/overrides.conf",
                         "sudo systemctl daemon-reload"
                     },
-                    _ => throw new NotImplementedException($"RPM packaging is set up only for Centos and RHEL, current OS '.{this.os}'"),
+                    "mariner" => new[]
+                    {
+                        "set -e",
+                        $"rpm --nodeps -i {string.Join(' ', packages)}",
+                        "pathToSystemdConfig=$(systemctl cat aziot-edged | head -n 1)",
+                        "sed 's/=on-failure/=no/g' ${pathToSystemdConfig#?} > ~/override.conf",
+                        "sudo mv -f ~/override.conf ${pathToSystemdConfig#?}",
+                        "sudo systemctl daemon-reload"
+                    },
+                    _ => throw new NotImplementedException($"RPM packaging is set up only for Centos, Mariner, and RHEL, current OS '.{this.os}'"),
                 },
                 _ => throw new NotImplementedException($"Don't know how to install daemon on for '.{this.packageExtension}'"),
             };
