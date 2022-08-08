dd553ae585f6468a22b273ae9b5d02f39d53b92d
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2022-08-08 13:04:30 +0000

    Fix module startup scripts

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Service/agentStart.sh
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Service/agentStart.sh
@@ -2,7 +2,7 @@
 
 ###############################################################################
 # Set up EdgeAgent to run as a non-root user at runtime, if allowed.
-# 
+#
 # If this script is started as root:
 #  1. It reads the EDGEAGENTUSER_ID environment variable, default UID=13622.
 #  2. If the User ID does not exist as a user, create it.
@@ -16,7 +16,7 @@
 # then start Edge Agent.
 #
 # This preserves backwards compatibility with earlier versions of edgeAgent and
-# allows some flexibility in the assignment of the edgeagent user id. The default 
+# allows some flexibility in the assignment of the edgeagent user id. The default
 # is UID 13622.
 #
 # A user is created because at this time DotNet Core 2.x and 3.x can only install
@@ -30,18 +30,11 @@ cuid=$(id -u)
 
 if [ $cuid -eq 0 ]
 then
-
   # Create the agent user id if it does not exist
   if ! getent passwd "${TARGET_UID}" >/dev/null
   then
-    echo "$(date --utc +"%Y-%m-%d %H:%M:%S %:z") Creating UID ${TARGET_UID} as agent${TARGET_UID}"
-    # Use "useradd" if it is available.
-    if command -v useradd >/dev/null
-    then
-      useradd -ms /bin/bash -u "${TARGET_UID}" "agent${TARGET_UID}"
-    else
-      adduser -Ds /bin/sh -u "${TARGET_UID}" "agent${TARGET_UID}"
-    fi
+    echo "$(date --utc +"%Y-%m-%d %H:%M:%S %:z") Creating UID ${TARGET_UID} as edgeagentuser"
+    adduser -D -S -H -s /sbin/nologin -u "${TARGET_UID}" edgeagentuser
   fi
 
   username=$(getent passwd "${TARGET_UID}" | awk -F ':' '{ print $1; }')
@@ -74,12 +67,12 @@ then
     chown -f "${TARGET_UID}" "$mgmt"
   fi
 
-  # Ensure backup.json is writeable. 
+  # Ensure backup.json is writeable.
   # Need only to check if user has set this to a non-default location,
   # because default location is in storageFolder, which was fixed above.
   backupjson=$(env | grep -m 1 -i BackupConfigFilePath | awk -F '=' '{ print $2;}')
   if [ -e "$backupjson" ]
-  then 
+  then
     echo "$(date --utc +"%Y-%m-%d %H:%M:%S %:z") Changing ownership of configuration back : ${backupjson}"
     chown -f "${TARGET_UID}" "$backupjson"
     chmod 600 "$backupjson"
--- edge-hub/core/src/Microsoft.Azure.Devices.Edge.Hub.Service/hubStart.sh
+++ edge-hub/core/src/Microsoft.Azure.Devices.Edge.Hub.Service/hubStart.sh
@@ -2,7 +2,7 @@
 
 ###############################################################################
 # Set up EdgeHub to run as a non-root user at runtime, if allowed.
-# 
+#
 # If this script is started as root:
 #  1. It reads the EDGEHUBUSER_ID environment variable, default UID=13623.
 #  2. If the User ID does not exist as a user, create it.
@@ -14,7 +14,7 @@
 # then start Edge Hub.
 #
 # This preserves backwards compatibility with earlier versions of edgeHub and
-# allows some flexibility in the assignment of the edgehub user id. The default 
+# allows some flexibility in the assignment of the edgehub user id. The default
 # is UID 13623.
 #
 # A user is created because at this time DotNet Core 2.x and 3.x can only install
@@ -28,27 +28,21 @@ cuid=$(id -u)
 
 if [ $cuid -eq 0 ]
 then
-  # Create the agent user id if it does not exist
+  # Create the hub user id if it does not exist
   if ! getent passwd "${TARGET_UID}" >/dev/null
   then
-    echo "$(date --utc +"%Y-%m-%d %H:%M:%S %:z") Creating UID ${TARGET_UID} as agent${TARGET_UID}"
-    # Use "useradd" if it is available.
-    if command -v useradd >/dev/null
-    then
-      useradd -ms /bin/bash -u "${TARGET_UID}" "agent${TARGET_UID}"
-    else
-      adduser -Ds /bin/sh -u "${TARGET_UID}" "agent${TARGET_UID}"
-    fi
+    echo "$(date --utc +"%Y-%m-%d %H:%M:%S %:z") Creating UID ${TARGET_UID} as edgehubuser"
+    adduser -D -S -H -s /sbin/nologin -u "${TARGET_UID}" edgehubuser
   fi
-  
+
   username=$(getent passwd "${TARGET_UID}" | awk -F ':' '{ print $1; }')
 
   # If "StorageFolder" env variable exists, use as basepath, else use /tmp
   # same for BackupFolder
   hubstorage=$(env | grep -m 1 -i StorageFolder | awk -F '=' '{ print $2; }')
   hubbackup=$(env | grep -m 1 -i BackupFolder | awk -F '=' '{ print $2; }')
-  storagepath=${hubstorage:-/tmp}/edgehub
-  backuppath=${hubbackup:-/tmp}/edgehub_backup
+  storagepath=${hubstorage:-/tmp}/edgeHub
+  backuppath=${hubbackup:-/tmp}/edgeHub_backup
   # If basepath/edgehub exists, make sure all files are owned by TARGET_UID
   if [ -d $storagepath ]
   then
