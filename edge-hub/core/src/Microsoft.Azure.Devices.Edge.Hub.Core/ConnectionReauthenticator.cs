f0d68cf2314d04fa115caddc2423fa32a77e87f2
Lee <lefitche@microsoft.com>
2022-08-16 14:45:17 -0700

    fix

--- edge-hub/core/src/Microsoft.Azure.Devices.Edge.Hub.Core/ConnectionReauthenticator.cs
+++ edge-hub/core/src/Microsoft.Azure.Devices.Edge.Hub.Core/ConnectionReauthenticator.cs
@@ -77,9 +77,9 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core
                                 async c =>
                                 {
                                     Console.WriteLine("Returning false for all clients.");
-                                    bool authRes = false; // await this.authenticator.ReauthenticateAsync(c);
-                                    Events.ClientCredentialsResult(identity, authRes);
-                                    return authRes;
+                                    // bool authRes = await this.authenticator.ReauthenticateAsync(c);
+                                    // Events.ClientCredentialsResult(identity, authRes);
+                                    return Task.FromResult(false);
                                 })
                             .GetOrElse(
                                 () =>
