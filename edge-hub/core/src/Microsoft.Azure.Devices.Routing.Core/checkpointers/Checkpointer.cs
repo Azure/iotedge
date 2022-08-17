d9f6a584f780e3d380463c5357c5e18200c2f8a8
Bear <yophilav@microsoft.com>
2022-08-17 15:23:01 -0700

    Added Debugging Events

--- edge-hub/core/src/Microsoft.Azure.Devices.Routing.Core/checkpointers/Checkpointer.cs
+++ edge-hub/core/src/Microsoft.Azure.Devices.Routing.Core/checkpointers/Checkpointer.cs
@@ -20,6 +20,8 @@ namespace Microsoft.Azure.Devices.Routing.Core.Checkpointers
         public static readonly DateTime DateTimeMinValue = new DateTime(1601, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         public static readonly long InvalidOffset = -1L;
 
+        public static int CommitMetricIncCounter = 0;
+
         readonly AtomicBoolean closed;
         readonly ICheckpointStore store;
 
@@ -72,6 +74,8 @@ namespace Microsoft.Azure.Devices.Routing.Core.Checkpointers
         {
             this.Proposed = Math.Max(message.Offset, this.Proposed);
             Metrics.IncrementQueueLength(this.EndpointId, this.Priority);
+            ++CommitMetricIncCounter;
+            Events.CommitMetricInc(this, CommitMetricIncCounter);
         }
 
         public bool Admit(IMessage message)
@@ -113,10 +117,13 @@ namespace Microsoft.Azure.Devices.Routing.Core.Checkpointers
                 await this.store.SetCheckpointDataAsync(this.Id, new CheckpointData(offset, this.LastFailedRevivalTime, this.UnhealthySince), token);
             }
 
+            int metricCount = 0;
             foreach (var message in successful)
             {
                 Metrics.DecrementQueueLength(this.EndpointId, this.Priority);
+                ++metricCount;
             }
+            Events.CommitMetricDec(this, metricCount);
 
             Events.CommitFinished(this);
         }
@@ -167,6 +174,8 @@ namespace Microsoft.Azure.Devices.Routing.Core.Checkpointers
                 CommitStarted,
                 CommitFinished,
                 Close,
+                CommitMetricInc,
+                CommitMetricDec,
             }
 
             public static void CreateStart(string id)
@@ -192,6 +201,16 @@ namespace Microsoft.Azure.Devices.Routing.Core.Checkpointers
             public static void Close(Checkpointer checkpointer)
             {
                 Log.LogInformation((int)EventIds.Close, "[CheckpointerClose] {context}", GetContextString(checkpointer));
+            }
+
+            public static void CommitMetricInc(Checkpointer checkpointer, int commitMetricIncCounter)
+            {
+                Log.LogInformation((int)EventIds.CommitMetricInc, "[CommitMetricInc] CommitMetricIncCounter: {0}, {1}", commitMetricIncCounter, GetContextString(checkpointer));
+            }
+
+            public static void CommitMetricDec(Checkpointer checkpointer, int successfulCount)
+            {
+                Log.LogInformation((int)EventIds.CommitMetricDec, "[CommitMetricDec] SuccessfulCount: {0}, {1}", successfulCount, GetContextString(checkpointer));
             }
 
             static string GetContextString(Checkpointer checkpointer)
