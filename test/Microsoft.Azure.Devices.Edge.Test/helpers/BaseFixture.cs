0354f493b3ac6e1114912b8f0af032aeb18dd09a
Damon Barry <damonb@microsoft.com>
2022-10-13 15:35:22 -0700

    Remove cancellation token while gathering logs

--- test/Microsoft.Azure.Devices.Edge.Test/helpers/BaseFixture.cs
+++ test/Microsoft.Azure.Devices.Edge.Test/helpers/BaseFixture.cs
@@ -41,8 +41,8 @@ namespace Microsoft.Azure.Devices.Edge.Test.Helpers
                     this.cts.Dispose();
                     if ((!Context.Current.ISA95Tag) && (TestContext.CurrentContext.Result.Outcome != ResultState.Ignored))
                     {
-                        using var cts = new CancellationTokenSource(Context.Current.TeardownTimeout);
-                        await NUnitLogs.CollectAsync(this.testStartTime, cts.Token);
+                        // using var cts = new CancellationTokenSource(Context.Current.TeardownTimeout);
+                        await NUnitLogs.CollectAsync(this.testStartTime, /*cts.Token*/CancellationToken.None);
                         if (Context.Current.GetSupportBundle)
                         {
                             try
@@ -51,7 +51,7 @@ namespace Microsoft.Azure.Devices.Edge.Test.Helpers
                                 await Process.RunAsync(
                                     "iotedge",
                                     $"support-bundle -o {supportBundlePath}/supportbundle-{TestContext.CurrentContext.Test.Name} --since \"{this.testStartTime:yyyy-MM-ddTHH:mm:ssZ}\"",
-                                    cts.Token);
+                                    /*cts.Token*/CancellationToken.None);
                             }
                             catch (Exception ex)
                             {
