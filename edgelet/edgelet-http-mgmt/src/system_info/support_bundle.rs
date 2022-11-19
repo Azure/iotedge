ed1b6eb1156900b09940507a70b7235a0fd0d05f
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2022-11-18 16:44:37 -0800

    Replace futures_util Mutex with tokio Mutex (#6789)
    Using a fair mutex resolves an issue where multiple requests weren't completing in order and causing a timeout for earlier requests.

--- edgelet/edgelet-http-mgmt/src/system_info/support_bundle.rs
+++ edgelet/edgelet-http-mgmt/src/system_info/support_bundle.rs
@@ -6,7 +6,7 @@ pub(crate) struct Route<M>
 where
     M: edgelet_core::ModuleRuntime + Send + Sync,
 {
-    runtime: std::sync::Arc<futures_util::lock::Mutex<M>>,
+    runtime: std::sync::Arc<tokio::sync::Mutex<M>>,
 
     since: Option<String>,
     until: Option<String>,
