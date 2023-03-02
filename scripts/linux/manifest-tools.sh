98a916b90cae1b919a39eca759d0bd1ebc4bafe9
Damon Barry <damonb@microsoft.com>
2023-03-02 10:16:41 -0800

    Use echo instead of cat in script

--- scripts/linux/manifest-tools.sh
+++ scripts/linux/manifest-tools.sh
@@ -62,7 +62,7 @@ get_docker_credentials() {
     if [[ "$(echo "$docker_config" | jq --arg reg "$REGISTRY" '.auths | .[$reg] | has("auth")')" == true ]]; then
         # Get credentials directly from config.json
         cred=$(
-            cat "$docker_config" | jq --arg reg "$REGISTRY" -r '.auths | .[$reg].auth' | base64 --decode
+            echo "$docker_config" | jq --arg reg "$REGISTRY" -r '.auths | .[$reg].auth' | base64 --decode
         )
     elif [[ "$(echo "$docker_config" | jq 'has("credsStore")')" == true ]]; then
         # Get credentials from store
