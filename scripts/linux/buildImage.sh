2504f501e1daebf7e80ab981e3861fd1f4d47a52
Damon Barry <damonb@microsoft.com>
2023-03-02 09:39:36 -0800

    Change expected bin dir for api proxy image build

--- scripts/linux/buildImage.sh
+++ scripts/linux/buildImage.sh
@@ -234,7 +234,7 @@ if [[ "$APP" == 'api-proxy-module' ]]; then
         docker buildx build \
             --no-cache \
             --platform "$PLATFORM" \
-            --file "$APP_BINARIESDIRECTORY/docker/$PLATFORM/Dockerfile" \
+            --file "$APP_BINARIESDIRECTORY/docker/$(convert_platform $PLATFORM)/Dockerfile" \
             --output=type=registry,name=$PLAT_IMAGE \
             --build-arg EXE_DIR=. \
             $([ -z "$BUILD_CONTEXT" ] || echo $BUILD_CONTEXT) \
