0bd0cd160fd74c4f1bab33442667ce7bc09297d4
Damon Barry <damonb@microsoft.com>
2023-03-03 15:49:23 -0800

    Don't accept oci media types

--- scripts/linux/manifest-tools.sh
+++ scripts/linux/manifest-tools.sh
@@ -218,8 +218,6 @@ pull_manifest() {
     local accept_types=(
         'application/vnd.docker.distribution.manifest.list.v2+json'
         'application/vnd.docker.distribution.manifest.v2+json'
-        'application/vnd.oci.image.index.v1+json'
-        'application/vnd.oci.image.manifest.v1+json'
     )
     local accept=$(printf '%s,' "${accept_types[@]}")
     accept="${accept:0:-1}"
