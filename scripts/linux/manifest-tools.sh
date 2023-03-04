74265b4e72d76db9f57e6997133bda17140266f0
Damon Barry <damonb@microsoft.com>
2023-03-03 16:02:25 -0800

    Revert "Don't accept oci media types"
    This reverts commit 0bd0cd160fd74c4f1bab33442667ce7bc09297d4.

--- scripts/linux/manifest-tools.sh
+++ scripts/linux/manifest-tools.sh
@@ -218,6 +218,8 @@ pull_manifest() {
     local accept_types=(
         'application/vnd.docker.distribution.manifest.list.v2+json'
         'application/vnd.docker.distribution.manifest.v2+json'
+        'application/vnd.oci.image.index.v1+json'
+        'application/vnd.oci.image.manifest.v1+json'
     )
     local accept=$(printf '%s,' "${accept_types[@]}")
     accept="${accept:0:-1}"
