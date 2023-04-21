5ee2087a235a157d5a4566c599581ec895cea718
Damon Barry <damonb@microsoft.com>
2023-04-21 09:53:59 -0700

    Don't unregister device identity when tests end

--- test/Microsoft.Azure.Devices.Edge.Test.Common/linux/PackageManagement.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/linux/PackageManagement.cs
@@ -173,10 +173,10 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common.Linux
             },
             SupportedPackageExtension.Snap => new string[]
             {
-                // "snap remove --purge azure-iot-identity",
-                // "snap remove --purge azure-iot-edge",
-                // "rm -r -f /etc/aziot",
-                // "snap restart docker" // we can remove after this is fixed (https://github.com/moby/moby/issues/23302)
+                "snap remove --purge azure-iot-identity",
+                "snap remove --purge azure-iot-edge",
+                "rm -r -f /etc/aziot",
+                "snap restart docker" // we can remove after this is fixed (https://github.com/moby/moby/issues/23302)
             },
             _ => throw new NotImplementedException($"Don't know how to uninstall daemon on for '.{this.PackageExtension}'")
         };
