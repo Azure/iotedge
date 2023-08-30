1224eae797931580b8e91162e2cbd3aa5e63de27
Damon Barry <damonb@microsoft.com>
2023-08-30 11:20:01 -0700

    Try go 1.19 instead of 1.21

--- edgelet/build/linux/package-mariner.sh
+++ edgelet/build/linux/package-mariner.sh
@@ -36,10 +36,10 @@ apt-get update -y
 apt-get install -y \
     cmake curl gcc g++ git jq make pkg-config \
     libclang1 libssl-dev llvm-dev \
-    cpio genisoimage golang-1.21-go qemu-utils pigz python3-pip python3-distutils rpm tar wget
+    cpio genisoimage golang-1.19-go qemu-utils pigz python3-pip python3-distutils rpm tar wget
 
 rm -f /usr/bin/go
-ln -vs /usr/lib/go-1.21/bin/go /usr/bin/go
+ln -vs /usr/lib/go-1.19/bin/go /usr/bin/go
 if [ -f /.dockerenv ]; then
     mv /.dockerenv /.dockerenv.old
 fi
