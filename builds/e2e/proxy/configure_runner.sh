093b50e647c4729743985e09b381c2f13239fe9f
Damon Barry <damonbarry@users.noreply.github.com>
2023-11-22 12:10:30 -0800

    Remove Ubuntu 18.04 support (#7143)
    Ubuntu 18.04 went out of support in May 2023. This change updates our build/release pipelines so that they won't produce Ubuntu 18.04 packages. It also removes Ubuntu 18.04 references generally.
    
    _Note:_ We can merge this change to main at any point, but we shouldn't make this change in the release/1.4 branch until after November 30, 2023, which is when we documented that we'll stop producing Ubuntu 18.04 packages.
    
    To test, I ran the CI Build pipeline to confirm that 18.04 packages are not built. Then I ran the end-to-end, nested end-to-end, and ISA-95 smoke test pipelines, and confirmed they run and pass.

--- builds/e2e/proxy/configure_runner.sh
+++ builds/e2e/proxy/configure_runner.sh
@@ -7,26 +7,15 @@ proxy="http://${1}:3128"
 export http_proxy=$proxy
 export https_proxy=$proxy
 
-echo 'Installing PowerShell Core and .NET 6.0'
+echo 'Installing .NET 6.0, PowerShell Core, and Moby engine'
 
 apt-get update
-apt-get install -y curl git wget apt-transport-https
-wget -q 'https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb'
+apt-get install -y curl git wget apt-transport-https software-properties-common
+wget -q 'https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb'
 dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 apt-get update
-add-apt-repository universe
-apt-get install -y powershell dotnet-sdk-6.0
-
-echo 'Installing Moby engine'
-
-curl -x $proxy 'https://packages.microsoft.com/config/ubuntu/18.04/multiarch/prod.list' > microsoft-prod.list
-mv microsoft-prod.list /etc/apt/sources.list.d/
-
-curl -x $proxy 'https://packages.microsoft.com/keys/microsoft.asc' | gpg --dearmor > microsoft.gpg
-mv microsoft.gpg /etc/apt/trusted.gpg.d/
-
-apt-get update
-apt-get install -y moby-engine
+apt-get install -y powershell dotnet-sdk-6.0 moby-engine
 
 > ~/proxy-env.override.conf cat <<-EOF
 [Service]
