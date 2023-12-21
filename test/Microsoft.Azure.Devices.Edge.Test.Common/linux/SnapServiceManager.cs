a6fcc2a4790b2caadb33b06e8e1260c1ee4335e7
Damon Barry <damonb@microsoft.com>
2023-12-21 15:47:20 -0800

    Fix IoTEdge_check test

--- test/Microsoft.Azure.Devices.Edge.Test.Common/linux/SnapServiceManager.cs
+++ test/Microsoft.Azure.Devices.Edge.Test.Common/linux/SnapServiceManager.cs
@@ -29,28 +29,25 @@ namespace Microsoft.Azure.Devices.Edge.Test.Common.Linux
 
         public async Task ConfigureAsync(CancellationToken token)
         {
-            // 'iotedge config apply' expects this directory to exist, but it doesn't for snaps
-            Directory.CreateDirectory(
-                "/var/snap/azure-iot-identity/current/shared/config/aziot/edged/config.d");
-
             // For snaps, we don't currently add 'RestartPreventExitStatus=153' to the systemd service unit file like
             // we do for other packages. This means that if the daemon is running but isn't yet configured, it will
             // keep restarting until unit start rate limiting kicks in. To prevent this scenario, we'll call
             // 'systemctl reset-failed' to flush the restart rate counter and allow the service to start.
             await Process.RunAsync("systemctl", "reset-failed snap.azure-iot-edge.aziot-edged", token);
 
-            await Process.RunAsync("azure-iot-edge.iotedge", "config apply", token);
+            var config = await File.ReadAllTextAsync(this.ConfigurationPath(), token);
+            await Process.RunAsync("snap", new string[] { "set", "azure-iot-edge", $"raw-config={config}" }, token);
 
-            // `iotedge config apply` for snaps only restarts aziot-edged, not identityd, keyd, certd, or tpmd.
-            // The identity service components would eventually recognize that the config has been updated, but we
-            // can't wait that long, so we bring down aziot-edged while we force-restart the identity service.
+            // `snap set azure-iot-edge raw-config=...` calls `iotedge config apply`, which, for snaps, only restarts
+            // aziot-edged, not identityd, keyd, certd, or tpmd. The identity service components need to refresh their
+            // config, so we'll force-restart them.
             await Process.RunAsync("snap", "stop azure-iot-edge.aziot-edged", token);
             await Process.RunAsync("snap", "restart azure-iot-identity", token);
             await Process.RunAsync("snap", "start azure-iot-edge.aziot-edged", token);
         }
 
         public string ConfigurationPath() =>
-            "/var/snap/azure-iot-identity/current/shared/config/aziot/config.toml";
+            "/var/snap/azure-iot-identity/current/shared/config/aziot/config-e2e.toml";
         public string GetCliName() => "azure-iot-edge.iotedge";
 
         async Task WaitForStatusAsync(ServiceStatus desired, CancellationToken token)
