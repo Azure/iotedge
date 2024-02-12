9403d888d865831dc09d44b352f65a2a83465c19
Alex Lewontin <alex.lewontin@canonical.com>
2024-02-12 14:22:52 -0500

    snapping: on exit status 153, stop but do not disable
    Signed-off-by: Alex Lewontin <alex.lewontin@canonical.com>

--- edgelet/contrib/snap/command-chain/handle-exit-status-153.sh
+++ edgelet/contrib/snap/command-chain/handle-exit-status-153.sh
@@ -6,7 +6,7 @@ EXIT_STATUS=0
 "$@" || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -eq 153 ] ; then
-    snapctl stop $SNAP_INSTANCE_NAME.aziot-edged --disable
+    snapctl stop $SNAP_INSTANCE_NAME.aziot-edged
 fi
 
 exit $EXIT_STATUS
