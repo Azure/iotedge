d8e6b3d029cd23dc7b28bcd5fec92257354790a2
Damon Barry <damonb@microsoft.com>
2024-05-07 11:06:18 -0700

    Fix script variable

--- scripts/linux/github/updateLatestVersion.sh
+++ scripts/linux/github/updateLatestVersion.sh
@@ -289,8 +289,8 @@ update_product_versions_json()
     remote_url="$(git config --get "remote.$AZURE_IOTEDGE_REPO_REMOTE.url")"
     # TODO: vvv remove temporary logging
     echo ">>> REMOTE URL FROM GIT: $remote_url"
-    remote_url="${remote_url/#https:\/\//https:\/\/$GITHUB_TOKEN@}" # add token to URL
-    echo ">>> UPDATED REMOTE URL: ${remote_url/${GITHUB_TOKEN:6}/******}"
+    remote_url="${remote_url/#https:\/\//https:\/\/$GITHUB_PAT@}" # add token to URL
+    echo ">>> UPDATED REMOTE URL: ${remote_url/${GITHUB_PAT:6}/******}"
 
     # commit changes, tag, and push
     git commit -m "Prepare for release $proposedProductVersion"
