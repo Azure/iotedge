e315e90958a2cc20791ed3f71e38867331ccc870
Gaurav Kulkarni <gakulkar@microsft.com>
2024-06-19 15:15:33 -0700

    Initial commit

--- test/modules/TestResultCoordinator/Settings.cs
+++ test/modules/TestResultCoordinator/Settings.cs
@@ -46,7 +46,7 @@ namespace TestResultCoordinator
             TimeSpan verificationDelay,
             TimeSpan sendReportFrequency,
             bool logUploadEnabled,
-            string storageAccountConnectionString,
+            Uri BlobStorageAccountUriWithSasToken,
             string networkControllerRunProfileName,
             ushort unmatchedResultsMaxSize,
             string testInfo,
@@ -71,7 +71,6 @@ namespace TestResultCoordinator
             {
                 this.TestResultReportingServiceSettings = Option.Some(new TestResultReportingServiceSettings()
                 {
-                    StorageAccountConnectionString = Preconditions.CheckNonWhiteSpace(storageAccountConnectionString, nameof(storageAccountConnectionString)),
                     LogAnalyticsLogType = Preconditions.CheckNonWhiteSpace(logAnalyticsLogType, nameof(logAnalyticsLogType)),
                     LogAnalyticsSharedKey = Preconditions.CheckNonWhiteSpace(logAnalyticsSharedKey, nameof(logAnalyticsSharedKey)),
                     LogAnalyticsWorkspaceId = Preconditions.CheckNonWhiteSpace(logAnalyticsWorkspaceId, nameof(logAnalyticsWorkspaceId)),
@@ -164,7 +163,7 @@ namespace TestResultCoordinator
                 configuration.GetValue("verificationDelay", TimeSpan.FromMinutes(15)),
                 configuration.GetValue("sendReportFrequency", TimeSpan.FromHours(24)),
                 configuration.GetValue<bool>("logUploadEnabled", true),
-                configuration.GetValue<string>("STORAGE_ACCOUNT_CONNECTION_STRING"),
+                configuration.GetValue<Uri>("BLOB_STORE_SAS"),
                 configuration.GetValue<string>(TestConstants.NetworkController.RunProfilePropertyName),
                 configuration.GetValue<ushort>("UNMATCHED_RESULTS_MAX_SIZE", DefaultUnmatchedResultsMaxSize),
                 configuration.GetValue<string>("TEST_INFO"),
@@ -282,7 +281,7 @@ namespace TestResultCoordinator
 
     internal struct TestResultReportingServiceSettings
     {
-        public string StorageAccountConnectionString;
+        public Uri BlobStorageAccountUriWithSasToken;
         public string LogAnalyticsWorkspaceId;
         public string LogAnalyticsSharedKey;
         public string LogAnalyticsLogType;
