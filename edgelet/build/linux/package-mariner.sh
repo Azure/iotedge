03cd85d6c093f34c033d8f0e924aa89da459db28
Damon Barry <damonbarry@users.noreply.github.com>
2025-02-20 15:33:10 -0800

    Add support for Azure Linux 3.0 (#7418)
    This change adds support for building and testing IoT Edge for Azure Linux 3.0.
    
    To test, I confirmed that the CI build passes, and that the right artifacts are produced. I also ran the end-to-end tests pipeline and confirmed that the Mariner and Azure Linux jobs (x64 and arm64) ran and passed.
    
    ## Azure IoT Edge PR checklist:

--- edgelet/build/linux/package-mariner.sh
+++ edgelet/build/linux/package-mariner.sh
@@ -9,50 +9,53 @@ export DEBIAN_FRONTEND=noninteractive
 export TZ=UTC
 
 
-# need to use preview repo for the next 2 weeks untill mariner 2.0 gets moved to prod
-case "${MARINER_RELEASE}" in
-    '1.0'*)
-        UsePreview=n
-        MarinerIdentity=mariner1
-        PackageExtension="cm1"
-        ;;
+case "$AZURELINUX_RELEASE" in
     '2.0'*)
         UsePreview=n
-        MarinerIdentity=mariner2
-        PackageExtension="cm2"
+        AzureLinuxIdentity=mariner2
+        PackageExtension=cm2
+        ;;
+    '3.0'*)
+        UsePreview=n
+        AzureLinuxIdentity=azurelinux3
+        PackageExtension=azl3
         ;;
 esac
 
 BUILD_REPOSITORY_LOCALPATH="$(realpath "${BUILD_REPOSITORY_LOCALPATH:-$DIR/../../..}")"
-EDGELET_ROOT="${BUILD_REPOSITORY_LOCALPATH}/edgelet"
-MARINER_BUILD_ROOT="${BUILD_REPOSITORY_LOCALPATH}/builds/${MarinerIdentity}"
+EDGELET_ROOT="$BUILD_REPOSITORY_LOCALPATH/edgelet"
+AZURELINUX_BUILD_ROOT="$BUILD_REPOSITORY_LOCALPATH/builds/$AzureLinuxIdentity"
 REVISION="${REVISION:-1}"
 
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:longsleep/golang-backports
-apt-get update -y
+
+if [ "${AZURELINUX_RELEASE:0:3}" = '2.0' ]; then
+    apt-get install -y software-properties-common
+    add-apt-repository -y ppa:longsleep/golang-backports
+    apt-get update -y
+fi
+
 apt-get install -y \
-    cmake curl gcc g++ git jq make pkg-config \
-    libclang1 libssl-dev llvm-dev \
-    cpio genisoimage golang-1.21-go qemu-utils pigz python3-pip python3-distutils rpm tar wget
+    acl cmake cpio curl g++ gcc genisoimage git golang-1.21-go jq libclang1 libssl-dev \
+    llvm-dev make pigz pkg-config python3-distutils python3-pip qemu-utils rpm tar \
+    wget zstd
 
 rm -f /usr/bin/go
 ln -vs /usr/lib/go-1.21/bin/go /usr/bin/go
 touch /.mariner-toolkit-ignore-dockerenv
 
-# Download Mariner repo and build toolkit
-mkdir -p ${MARINER_BUILD_ROOT}
-MarinerToolkitDir='/tmp/CBL-Mariner'
-if ! [ -f "$MarinerToolkitDir/toolkit.tar.gz" ]; then
-    rm -rf "$MarinerToolkitDir"
-    git clone 'https://github.com/microsoft/CBL-Mariner.git' --branch "$MARINER_RELEASE" --depth 1 "$MarinerToolkitDir"
-    pushd "$MarinerToolkitDir/toolkit/"
+# Build Azure Linux toolkit
+mkdir -p "$AZURELINUX_BUILD_ROOT"
+AzureLinuxToolkitDir='/tmp/azurelinux'
+if ! [ -f "$AzureLinuxToolkitDir/toolkit.tar.gz" ]; then
+    rm -rf "$AzureLinuxToolkitDir"
+    git clone 'https://github.com/microsoft/azurelinux.git' --branch "$AZURELINUX_RELEASE" --depth 1 "$AzureLinuxToolkitDir"
+    pushd "$AzureLinuxToolkitDir/toolkit/"
     make package-toolkit REBUILD_TOOLS=y
     popd
-    cp "$MarinerToolkitDir"/out/toolkit-*.tar.gz "${MARINER_BUILD_ROOT}/toolkit.tar.gz"
-    rm -rf MarinerToolkitDir
+    cp "$AzureLinuxToolkitDir"/out/toolkit-*.tar.gz "$AZURELINUX_BUILD_ROOT/toolkit.tar.gz"
+    rm -rf $AzureLinuxToolkitDir
 fi
 
 echo 'Installing rustup'
@@ -60,7 +63,7 @@ curl -sSLf https://sh.rustup.rs | sh -s -- -y
 . ~/.cargo/env
 
 pushd $EDGELET_ROOT
-case "${MARINER_ARCH}" in
+case "$ARCH" in
     'x86_64')
         rustup target add x86_64-unknown-linux-gnu
         ;;
@@ -72,30 +75,30 @@ popd
 
 # get aziot-identity-service version
 IIS_VERSION=$(
-    rpm -qp --queryformat '%{Version}' $(ls /src/aziot-identity-service-*.$PackageExtension.${MARINER_ARCH}.rpm | head -1)
+    rpm -qp --queryformat '%{Version}' $(ls /src/aziot-identity-service-*.$PackageExtension.$ARCH.rpm | head -1)
 )
 
 # Update versions in specfiles
-pushd "${BUILD_REPOSITORY_LOCALPATH}"
-sed -i "s/@@VERSION@@/${VERSION}/g" ${BUILD_REPOSITORY_LOCALPATH}/builds/mariner/SPECS/aziot-edge/aziot-edge.signatures.json
-sed -i "s/@@VERSION@@/${VERSION}/g" ${BUILD_REPOSITORY_LOCALPATH}/builds/mariner/SPECS/aziot-edge/aziot-edge.spec
-sed -i "s/@@RELEASE@@/${REVISION}/g" ${BUILD_REPOSITORY_LOCALPATH}/builds/mariner/SPECS/aziot-edge/aziot-edge.spec
+pushd "$BUILD_REPOSITORY_LOCALPATH"
+sed -i "s/@@VERSION@@/$VERSION/g" $BUILD_REPOSITORY_LOCALPATH/builds/mariner/SPECS/aziot-edge/aziot-edge.signatures.json
+sed -i "s/@@VERSION@@/$VERSION/g" $BUILD_REPOSITORY_LOCALPATH/builds/mariner/SPECS/aziot-edge/aziot-edge.spec
+sed -i "s/@@RELEASE@@/$REVISION/g" $BUILD_REPOSITORY_LOCALPATH/builds/mariner/SPECS/aziot-edge/aziot-edge.spec
 
 # Update aziot-identity-service version dependency
 if [[ ! -z $IIS_VERSION ]]; then
-    sed -i "s/@@IIS_VERSION@@/${IIS_VERSION}/g" ${BUILD_REPOSITORY_LOCALPATH}/builds/mariner/SPECS/aziot-edge/aziot-edge.spec
+    sed -i "s/@@IIS_VERSION@@/$IIS_VERSION/g" $BUILD_REPOSITORY_LOCALPATH/builds/mariner/SPECS/aziot-edge/aziot-edge.spec
 else
     # if a version could not be parsed remove the version dependency
-    sed -i "s/aziot-identity-service = @@IIS_VERSION@@%{?dist}/aziot-identity-service/g" ${BUILD_REPOSITORY_LOCALPATH}/builds/mariner/SPECS/aziot-edge/aziot-edge.spec
+    sed -i "s/aziot-identity-service = @@IIS_VERSION@@%{?dist}/aziot-identity-service/g" $BUILD_REPOSITORY_LOCALPATH/builds/mariner/SPECS/aziot-edge/aziot-edge.spec
 fi
 popd
 
-pushd "${EDGELET_ROOT}"
+pushd "$EDGELET_ROOT"
 
-# Cargo vendored dependencies are being downloaded now to be cached for mariner iotedge build.
+# Cargo vendored dependencies are being downloaded now to be cached for Azure Linux iotedge build.
 echo "set cargo home location"
-mkdir ${BUILD_REPOSITORY_LOCALPATH}/cargo-home
-export CARGO_HOME=${BUILD_REPOSITORY_LOCALPATH}/cargo-home
+mkdir $BUILD_REPOSITORY_LOCALPATH/cargo-home
+export CARGO_HOME=$BUILD_REPOSITORY_LOCALPATH/cargo-home
 
 echo "Vendoring Rust dependencies"
 cargo vendor vendor
@@ -122,40 +125,40 @@ cp ../LICENSE ./LICENSE
 popd # EDGELET_ROOT
 
 # Create source tarball, including cargo dependencies and license
-tmp_dir=$(mktemp -d -t mariner-iotedge-build-XXXXXXXXXX)
-pushd $tmp_dir
-echo "Creating source tarball aziot-edge-${VERSION}.tar.gz"
-tar -czvf aziot-edge-${VERSION}.tar.gz --transform s/./aziot-edge-${VERSION}/ -C "${BUILD_REPOSITORY_LOCALPATH}" .
+tmp_dir="$(mktemp -d -t azurelinux-iotedge-build-XXXXXXXXXX)"
+pushd "$tmp_dir"
+echo "Creating source tarball aziot-edge-$VERSION.tar.gz"
+tar -czvf aziot-edge-$VERSION.tar.gz --transform s/./aziot-edge-$VERSION/ -C "$BUILD_REPOSITORY_LOCALPATH" .
 popd
 
 
 # Copy source tarball to expected locations
-mkdir -p "${MARINER_BUILD_ROOT}/SPECS/aziot-edge/SOURCES/"
-mv "${tmp_dir}/aziot-edge-${VERSION}.tar.gz" "${MARINER_BUILD_ROOT}/SPECS/aziot-edge/SOURCES/"
-rm -rf ${tmp_dir}
+mkdir -p "$AZURELINUX_BUILD_ROOT/SPECS/aziot-edge/SOURCES/"
+mv "$tmp_dir/aziot-edge-$VERSION.tar.gz" "$AZURELINUX_BUILD_ROOT/SPECS/aziot-edge/SOURCES/"
+rm -rf "$tmp_dir"
 
 # Copy spec files to expected locations
-cp "${BUILD_REPOSITORY_LOCALPATH}/builds/mariner/SPECS/aziot-edge/aziot-edge.signatures.json" "${MARINER_BUILD_ROOT}/SPECS/aziot-edge/"
-cp "${BUILD_REPOSITORY_LOCALPATH}/builds/mariner/SPECS/aziot-edge/aziot-edge.spec" "${MARINER_BUILD_ROOT}/SPECS/aziot-edge/"
+cp "$BUILD_REPOSITORY_LOCALPATH/builds/mariner/SPECS/aziot-edge/aziot-edge.signatures.json" "$AZURELINUX_BUILD_ROOT/SPECS/aziot-edge/"
+cp "$BUILD_REPOSITORY_LOCALPATH/builds/mariner/SPECS/aziot-edge/aziot-edge.spec" "$AZURELINUX_BUILD_ROOT/SPECS/aziot-edge/"
 
 tmp_dir=$(mktemp -d)
 pushd $tmp_dir
 mkdir "rust"
 cp -r ~/.cargo "rust"
 cp -r ~/.rustup "rust"
-tar cf "${MARINER_BUILD_ROOT}/SPECS/aziot-edge/SOURCES/rust.tar.gz" "rust"
+tar cf "$AZURELINUX_BUILD_ROOT/SPECS/aziot-edge/SOURCES/rust.tar.gz" "rust"
 popd
 
 # copy over IIS RPM
-mkdir -p ${MARINER_BUILD_ROOT}/out/RPMS/${MARINER_ARCH}
-mv /src/aziot-identity-service-*.$PackageExtension.${MARINER_ARCH}.rpm ${MARINER_BUILD_ROOT}/out/RPMS/${MARINER_ARCH}
+mkdir -p $AZURELINUX_BUILD_ROOT/out/RPMS/$ARCH
+mv /src/aziot-identity-service-*.$PackageExtension.$ARCH.rpm $AZURELINUX_BUILD_ROOT/out/RPMS/$ARCH
 
 # Prepare toolkit
-pushd ${MARINER_BUILD_ROOT}
+pushd $AZURELINUX_BUILD_ROOT
 tar xzf toolkit.tar.gz
 pushd toolkit
 
-# Build Mariner RPM packages
+# Build Azure Linux RPM packages
 make build-packages PACKAGE_BUILD_LIST="aziot-edge" SRPM_FILE_SIGNATURE_HANDLING=update USE_PREVIEW_REPO=$UsePreview CONFIG_FILE= -j$(nproc)
 popd
 popd
