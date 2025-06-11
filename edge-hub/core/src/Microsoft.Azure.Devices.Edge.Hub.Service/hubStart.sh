0b8696738c3cab78ce9e6479337999046ac9b1fe
Robert Jang <robbaldwin95@gmail.com>
2025-06-11 11:53:42 -0700

    Update edge-hub/core/src/Microsoft.Azure.Devices.Edge.Hub.Service/hubStart.sh
    Co-authored-by: Arnav Singh <arsing@microsoft.com>

--- edge-hub/core/src/Microsoft.Azure.Devices.Edge.Hub.Service/hubStart.sh
+++ edge-hub/core/src/Microsoft.Azure.Devices.Edge.Hub.Service/hubStart.sh
@@ -72,8 +72,8 @@ then
   fi
 
   # Override the nofile to make soft == hard, then launch edgehub.
-  exec su "$username" -c "bash -c 'ulimit -Sn $(ulimit -Hn) && /usr/bin/dotnet Microsoft.Azure.Devices.Edge.Hub.Service.dll'"
+  exec su "$username" -c "bash -c 'ulimit -Sn \"$(ulimit -Hn)\" && /usr/bin/dotnet Microsoft.Azure.Devices.Edge.Hub.Service.dll'"
 else
-  ulimit -Sn $(ulimit -Hn)
+  ulimit -Sn "$(ulimit -Hn)"
   exec /usr/bin/dotnet Microsoft.Azure.Devices.Edge.Hub.Service.dll
 fi
