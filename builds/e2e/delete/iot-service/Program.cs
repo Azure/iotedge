bdb3213d983c0bdc724e4ce1eb6062c94cf9db45
Damon Barry <damonb@microsoft.com>
2025-12-17 14:16:33 -0800

    Try an app that works from my desktop

---
+++ builds/e2e/delete/iot-service/Program.cs
@@ -0,0 +1,10 @@
+﻿using Azure.Identity;
+using Microsoft.Azure.Devices;
+
+var hostname = "EdgeConnectivityTestHub.azure-devices.net";
+var settings = new HttpTransportSettings();
+RegistryManager rm = RegistryManager.Create(hostname, new DefaultAzureCredential(), settings);
+
+var deviceId = "ct1-Linux-amd64-connect-L0Wzdev6-Amqp-leaf";
+var device = await rm.GetDeviceAsync(deviceId);
+Console.WriteLine($"Device ID: {device.Id}, Status: {device.Status}, ETag: {device.ETag}");
