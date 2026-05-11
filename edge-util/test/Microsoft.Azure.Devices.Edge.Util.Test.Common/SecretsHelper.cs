e6db4d8ca23c040d3b5ff156b691038a93d129e0
Damon Barry <damonb@microsoft.com>
2026-05-10 21:50:59 -0700

    Throw an exception when a secret key is missing

--- edge-util/test/Microsoft.Azure.Devices.Edge.Util.Test.Common/SecretsHelper.cs
+++ edge-util/test/Microsoft.Azure.Devices.Edge.Util.Test.Common/SecretsHelper.cs
@@ -2,14 +2,14 @@
 namespace Microsoft.Azure.Devices.Edge.Util.Test.Common
 {
     using System;
-    using System.Threading.Tasks;
+    using System.Collections.Generic;
 
     public static class SecretsHelper
     {
         public static string GetSecret(string secret)
         {
             Preconditions.CheckNonWhiteSpace(secret, nameof(secret));
-            return ConfigHelper.TestConfig[secret] ?? string.Empty;
+            return ConfigHelper.TestConfig[secret] ?? throw new KeyNotFoundException($"Secret '{secret}' not found in configuration.");
         }
 
         public static string GetSecretFromConfigKey(string configName)
